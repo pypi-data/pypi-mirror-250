b2f8ed32d1d057af09e1c3713ed502e0cf38064b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 05:02:58 +0000

    Add package ChunkZIP/0.4.1/ChunkZIP-0.4.1.tar.gz

---
+++ packages/ChunkZIP/ChunkZIP-0.4.1.tar.gz/ChunkZIP-0.4.1/README.md
@@ -0,0 +1,27 @@
+# Chunk ZIP
+
+## Installation
+
+```shell
+pip install ChunkZIP
+#or
+pip install git+http://github.com/DingWB/czip
+```
+
+## Implementation
+
+|                                  | allcools | ballcools | czip |
+| -------------------------------- | -------- | --------- | ----- |
+| Format                           | .tsv.gz  | .ballc    | .cz   |
+| Compression algorithm            | bgzip    | bgzip     | czip |
+| Support Random Access ?          | Yes      | Yes       | Yes   |
+| Need extra index file for query? | Yes      | yes       | No    |
+| Quickly Merge?                   | No       | No        | Yes   |
+
+![img.png](docs/images/tab1.png)
+<!---
+![docs/images/img.png](docs/images/design.png)
+-->
+## Usage
+
+[Documentation](https://dingwb.github.io/czip)
---
+++ packages/ChunkZIP/ChunkZIP-0.4.1.tar.gz/ChunkZIP-0.4.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+@author: DingWB
+"""
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+from pathlib import Path
+# from Cython.Build import cythonize
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+__version__ = "0.4.1"
+
+setup(
+    name="czip",
+    version=__version__,
+    description="czip: Chunk based ZIP",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Wubin Ding",
+    author_email="ding.wu.bin.gm@gmail.com",
+    url="https://github.com/DingWB/czip",
+    packages=find_packages(exclude=('docs',)),
+    install_requires=['pandas', 'fire', 'numpy', 'cython', 'fast-fisher'],
+    include_package_data=True,
+    package_data={
+        '': ['*.txt', '*.tsv', '*.csv', '*.fa', '*Snakefile', '*ipynb']
+    },
+    entry_points={
+        'console_scripts':
+            [
+                'czip=czip:main',
+            ],
+    },
+
+    # ext_modules=cythonize("czip/cz.pyx",language_level = "3"), #python setup.py build_ext --inplace
+)
