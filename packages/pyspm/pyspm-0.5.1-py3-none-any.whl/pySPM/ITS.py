7a05ebba6c2c7aba644e0e4818ac3a0a164d8867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:20 +0000

    Add package pyspm/0.5.1/pyspm-0.5.1-py3-none-any.whl

---
+++ packages/pyspm/pyspm-0.5.1-py3-none-any.whl/pySPM/ITS.py
@@ -0,0 +1,29 @@
+# -- coding: utf-8 --
+
+# Copyright 2018 Olivier Scholder <o.scholder@gmail.com>
+
+import os.path
+
+from pySPM import Block
+
+
+class InvalidRAWdataformat(Exception):
+    def __init__(self, block, msg):
+        self.block = block
+        self.msg = msg
+
+    def __str__(self):
+        return "Invalid RAW dataformat seen in block " + self.block.parent + '/' + self.block.name + ' : ' + self.msg
+
+
+class ITS:
+    def __init__(self, filename, debug=False):
+        """
+        ITS
+        """
+        self.filename = filename
+        assert os.path.exists(filename)
+        self.f = open(self.filename, 'rb')
+        self.Type = self.f.read(8)
+        assert self.Type == b'ITStrF01'
+        self.root = Block.Block(self.f)
