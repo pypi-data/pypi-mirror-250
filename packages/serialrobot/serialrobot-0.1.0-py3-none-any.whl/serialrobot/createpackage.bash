a31cccea1f5fb82457d21901a62eba1014079777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:06 +0000

    Add package serialrobot/0.1.0/serialrobot-0.1.0-py3-none-any.whl

---
+++ packages/serialrobot/serialrobot-0.1.0-py3-none-any.whl/serialrobot/createpackage.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+package_name="first"
+ws_name="my_ws"
+
+if [[ -n "$1" ]]; then
+    package_name=$1
+else
+    package_name="robot"
+    echo "no input, using default ws $ws_name"
+fi
+
+source /opt/ros/humble/setup.bash
+cd ~
+mkdir -p $ws_name/src
+cd $ws_name/src
+[ ! -d "$package_name" ] && ros2 pkg create $package_name --build-type ament_python --dependencies rclpy
+cd $package_name
+mkdir -p urdf
+mkdir -p rviz
+cd ~/$ws_name
