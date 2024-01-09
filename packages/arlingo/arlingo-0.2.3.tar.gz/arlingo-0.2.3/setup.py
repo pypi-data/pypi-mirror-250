ac6bd7768f6ce8c04eb777d14a8fd964b398d15e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 04:03:27 +0000

    Add package arlingo/0.2.3/arlingo-0.2.3.tar.gz

---
+++ packages/arlingo/arlingo-0.2.3.tar.gz/arlingo-0.2.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arlingo',
+    version='0.2.3',
+    packages=find_packages(),
+    install_requires=[
+        "beautifulsoup4"
+    ],
+)
