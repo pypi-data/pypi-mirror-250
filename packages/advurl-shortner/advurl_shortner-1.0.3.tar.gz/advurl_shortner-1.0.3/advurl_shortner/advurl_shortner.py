4363315f2c6324b4cbaafa585f7b3df66574135b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:29 +0000

    Add package advurl-shortner/1.0.3/advurl_shortner-1.0.3.tar.gz

---
+++ packages/advurl-shortner/advurl_shortner-1.0.3.tar.gz/advurl_shortner-1.0.3/advurl_shortner/advurl_shortner.py
@@ -0,0 +1,29 @@
+import requests
+
+def short(url, password=None, second_url=None, description=None, ttl=None, weights=None):
+    data = {"url": url}
+    if password:
+        data["pass"]=password
+    if description:
+        data["description"]=description
+    if second_url:
+        data["second_url"]=second_url
+    if ttl:
+        data["ttl"]=ttl
+    if weights:
+        data["weights"]=weights
+    response = requests.post('https://advurlshortner.top/api/newurl/', json=data)
+
+    if response.status_code == 200:
+        output = response.json()
+    else:
+        return response.status_code
+    return output
+
+def stat(url,password):
+    response = requests.post("https://advurlshortner.top/api/stat/", json= {"short_url":url,"pass":password})
+    if response.status_code == 200:
+        output = response.json()
+    else:
+        return response.status_code
+    return output
