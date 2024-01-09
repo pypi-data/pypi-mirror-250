396f360e0bd1d32659f28ded7c3fa74e6f6fa3d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 04:03:41 +0000

    Add package aestream/0.6.2/aestream-0.6.2-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/aestream/aestream-0.6.2-cp39-cp39-manylinux_2_28_x86_64.whl/aestream/__init__.py
@@ -0,0 +1,37 @@
+"""
+AEStream library for streaming address-event representations.
+
+Please refer to https://github.com/aestream/aestream for usage
+"""
+import logging
+
+# Import AEStream modules
+from aestream.aestream_ext import Backend, Event
+from aestream._input import FileInput, UDPInput
+
+modules = []
+try:
+    from aestream._input import USBInput
+
+    modules.append("USBInput")
+except ImportError:
+    logging.debug("Failed to import AEStream USB Input")
+
+try:
+    import aestream._genn as genn
+
+    modules.append("genn")
+except ImportError as ex:
+    logging.debug("Failed to import GeNN: AEStream cannot use GeNN device")
+
+try:
+    from aestream._input import SpeckInput
+
+    modules.append("SpeckInput")
+except ImportError:
+    logging.debug("Failed to import ZMQ: AEStream cannot use ZMQ input")
+
+del logging
+
+__all__ = ["Event", "FileInput", "UDPInput"] + modules
+del modules
