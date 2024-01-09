6ed9aa11694fd82d34ed1c97a0f9b3bd3b7b9c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:23 +0000

    Add package pydmt/0.3.4/pydmt-0.3.4-py3-none-any.whl

---
+++ packages/pydmt/pydmt-0.3.4-py3-none-any.whl/pydmt/builders/reqs.py
@@ -0,0 +1,44 @@
+"""
+Install requirements
+"""
+
+
+from pydmt.api.one_source_one_target import OneSourceOneTarget
+from pydmt.utils.subprocess import check_call
+from pydmt.utils.filesystem import mkdir_touch
+from pydmt.utils.python import collect_reqs, collect_bootstrap_reqs, get_install_args
+
+
+class BuilderReqs(OneSourceOneTarget):
+    """
+    This is a review of how to build a python virtual environment:
+    # create the virtualenv
+    virtualenv [folder]
+    # activate it
+    source [folder]/bin/activate
+    # install packages from file
+    pip install -r requirements.txt
+    # OR
+    python -m pip install -r requirements.txt
+    # OR if you want to install a list of packages:
+    pip install -r [list of packages]
+    # OR
+    python -m pip install [list of packages]
+    """
+    def build(self) -> None:
+        """
+        Why do we do this in two stages? What's wrong with doing this in one stage?
+        Because importing python.py may fail because of prereqs that python.py
+        needs. In this case the user specifies these prereqs in bootstrap.py
+        """
+        args = get_install_args()
+        packs = collect_bootstrap_reqs()
+        if packs:
+            args.extend(packs)
+            check_call(args)
+        args = get_install_args()
+        packs = collect_reqs()
+        if packs:
+            args.extend(packs)
+            check_call(args)
+        mkdir_touch(self.target)
