e2265c495a5fb096d6169f21752dad6de95f24f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 04:03:28 +0000

    Add package odoo14-addon-ssi-rma/14.0.1.7.0/odoo14_addon_ssi_rma-14.0.1.7.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-rma/odoo14_addon_ssi_rma-14.0.1.7.0-py3-none-any.whl/odoo/addons/ssi_rma/models/rma_supplier.py
@@ -0,0 +1,20 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class RMASupplier(models.Model):
+    _name = "rma_supplier"
+    _description = "RMA Supplier"
+    _inherit = ["rma_order_mixin"]
+
+    type = fields.Selection(
+        string="Type",
+        default="supplier",
+    )
+    line_ids = fields.One2many(
+        comodel_name="rma_supplier_line",
+        inverse_name="order_id",
+    )
