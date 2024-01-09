973b49c825629f0f783837ecce2f19babad89e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:03:54 +0000

    Add package inox/0.4.0/inox-0.4.0-py3-none-any.whl

---
+++ packages/inox/inox-0.4.0-py3-none-any.whl/inox/nn/__init__.py
@@ -0,0 +1,15 @@
+r"""Neural networks, layers and modules"""
+
+from .activation import *
+from .attention import *
+from .container import *
+from .dropout import *
+from .einops import *
+from .linear import *
+from .module import *
+from .normalization import *
+from .padding import *
+from .pooling import *
+from .recurrent import *
+from .ssm import *
+from .stateful import *
