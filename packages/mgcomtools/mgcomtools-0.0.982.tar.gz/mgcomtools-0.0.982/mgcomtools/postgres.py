e6bdc6e19e011cfd884bad875dfd68f2fc6990b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 04:03:52 +0000

    Add package mgcomtools/0.0.982/mgcomtools-0.0.982.tar.gz

---
+++ packages/mgcomtools/mgcomtools-0.0.982.tar.gz/mgcomtools-0.0.982/mgcomtools/postgres.py
@@ -0,0 +1,53 @@
+import sqlalchemy
+from google.cloud.sql.connector import Connector, IPTypes
+import os
+import pg8000
+
+os.environ["PRIVATE_IP"] = "10.77.125.3" 
+
+
+def connect_with_connector() -> sqlalchemy.engine.base.Engine:
+    """
+    Initializes a connection pool for a Cloud SQL instance of Postgres.
+
+    Uses the Cloud SQL Python Connector package.
+    """
+    # Note: Saving credentials in environment variables is convenient, but not
+    # secure - consider a more secure solution such as
+    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+    # keep secrets safe.
+
+    instance_connection_name = 'newageriver:europe-west3:config-tables-psql'
+    db_user = 'postgres'
+    db_pass = 's7<f?exPFix65SUD'
+    db_name = 'test_db'
+
+    ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
+
+    # initialize Cloud SQL Python Connector object
+    connector = Connector()
+
+    def getconn() -> pg8000.dbapi.Connection:
+        conn: pg8000.dbapi.Connection = connector.connect(
+            instance_connection_name,
+            "pg8000",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+            ip_type=ip_type,
+        )
+        return conn
+
+    # The Cloud SQL Python Connector can be used with SQLAlchemy
+    # using the 'creator' argument to 'create_engine'
+    pool = sqlalchemy.create_engine(
+        "postgresql+pg8000://",
+        creator=getconn,
+        pool_size=5,
+        max_overflow=2,
+        pool_timeout=30,  # 30 seconds
+        pool_recycle=1800,  # 30 minutes
+    )
+    return pool
+
+conn = connect_with_connector()
