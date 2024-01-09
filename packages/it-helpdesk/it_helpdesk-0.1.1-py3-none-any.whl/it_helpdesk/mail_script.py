e96ca641206833a0f4ddf3769acc38f1f756d59c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:36 +0000

    Add package it-helpdesk/0.1.1/it_helpdesk-0.1.1-py3-none-any.whl

---
+++ packages/it-helpdesk/it_helpdesk-0.1.1-py3-none-any.whl/it_helpdesk/mail_script.py
@@ -0,0 +1,18 @@
+from sys import stdin
+import os
+from email import policy, message_from_file
+from it_helpdesk import get_request_email as gre
+import yaml
+
+def main():
+    path = os.path.join(os.path.dirname(__file__), 'ansible/vars/it_helpdesk_config.yaml')
+    with open(path) as config_file:
+        config = yaml.safe_load(config_file)
+        if config['testing_mode'] == True:
+            message = message_from_file(open(os.path.join(os.path.dirname(__file__),'ansible/sample_email.txt'), 'r'), policy=policy.default)
+        else:
+            message = message_from_file(stdin, policy=policy.default)
+        gre.main(message, os.path.join(config["helpdesk_dir"], 'ansible'))
+
+if __name__ == '__main__':
+    main()
