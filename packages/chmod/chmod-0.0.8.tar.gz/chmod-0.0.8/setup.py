6d678c1ff9d10df5268e4d8d9610836c70d390ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:07 +0000

    Add package chmod/0.0.8/chmod-0.0.8.tar.gz

---
+++ packages/chmod/chmod-0.0.8.tar.gz/chmod-0.0.8/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="chmod",
+    version="0.0.8",
+    author="Harry Sharma",
+    author_email="harrysharma1066@gmail.com",
+    description="Basic chmod conversion tool",
+    packages=["chmod"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
