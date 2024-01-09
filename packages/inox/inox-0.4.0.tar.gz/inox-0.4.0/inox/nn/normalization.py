43ee830f851b0c60cd1107ed270ebbc7f5d5bfe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:03:54 +0000

    Add package inox/0.4.0/inox-0.4.0.tar.gz

---
+++ packages/inox/inox-0.4.0.tar.gz/inox-0.4.0/inox/nn/normalization.py
@@ -0,0 +1,135 @@
+r"""Normalization layers"""
+
+__all__ = [
+    'BatchNorm',
+    'LayerNorm',
+]
+
+import jax
+import jax.numpy as jnp
+
+from jax import Array
+from typing import *
+
+from .module import Module
+from .stateful import Stateful, StateEntry
+
+
+class Statistics(NamedTuple):
+    mean: Array
+    var: Array
+
+
+class BatchNorm(Stateful):
+    r"""Creates a batch-normalization layer.
+
+    .. math:: y = \frac{x - \mathbb{E}[x]}{\sqrt{\mathbb{V}[x] + \epsilon}}
+
+    The mean and variance are calculated over the batch and spatial axes. During
+    training, the layer keeps running estimates of the mean and variance, which are then
+    used for normalization during evaluation. The update rule for a running average
+    statistic :math:`\hat{s}` is
+
+    .. math:: \hat{s} \gets \alpha \hat{s} + (1 - \alpha) s
+
+    where :math:`s` is the statistic calculated for the current batch.
+
+    References:
+        | Accelerating Deep Network Training by Reducing Internal Covariate Shift (Ioffe et al., 2015)
+        | https://arxiv.org/abs/1502.03167
+
+    Arguments:
+        channels: The number of channels :math:`C`.
+        epsilon: A numerical stability term :math:`\epsilon`.
+        momentum: The momentum :math:`\alpha \in [0, 1]` for the running estimates.
+    """
+
+    training: bool = True
+
+    def __init__(
+        self,
+        channels: int,
+        epsilon: float = 1e-05,
+        momentum: float = 0.9,
+    ):
+        self.epsilon = epsilon
+        self.momentum = momentum
+
+        self.stats = StateEntry(
+            Statistics(
+                mean=jnp.zeros(channels),
+                var=jnp.ones(channels),
+            )
+        )
+
+    def __call__(self, x: Array, state: Dict) -> Tuple[Array, Dict]:
+        r"""
+        Arguments:
+            x: The input tensor :math:`x`, with shape :math:`(N, *, C)`.
+            state: The state dictionary.
+
+        Returns:
+            The output tensor :math:`y`, with shape :math:`(N, *, C)`, and the
+            (updated) state dictionary.
+        """
+
+        if self.training:
+            assert x.ndim > 1, "the input tensor is not batched."
+
+            y = x.reshape(-1, x.shape[-1])
+            mean = jnp.mean(y, axis=0)
+            var = jnp.var(y, axis=0)
+
+            stats = state[self.stats]
+            stats = Statistics(
+                mean=self.ema(stats.mean, jax.lax.stop_gradient(mean)),
+                var=self.ema(stats.var, jax.lax.stop_gradient(var)),
+            )
+
+            state = self.update(state, {self.stats: stats})
+        else:
+            mean, var = state[self.stats]
+
+        y = (x - mean) / jnp.sqrt(var + self.epsilon)
+
+        return y, state
+
+    def ema(self, x: Array, y: Array) -> Array:
+        return self.momentum * x + (1 - self.momentum) * y
+
+
+class LayerNorm(Module):
+    r"""Creates a layer-normalization layer.
+
+    .. math:: y = \frac{x - \mathbb{E}[x]}{\sqrt{\mathbb{V}[x] + \epsilon}}
+
+    References:
+        | Layer Normalization (Ba et al., 2016)
+        | https://arxiv.org/abs/1607.06450
+
+    Arguments:
+        axis: The axis(es) over which the mean and variance are calculated.
+        epsilon: A numerical stability term :math:`\epsilon`.
+    """
+
+    def __init__(
+        self,
+        axis: Union[int, Sequence[int]] = -1,
+        epsilon: float = 1e-05,
+    ):
+        self.axis = axis
+        self.epsilon = epsilon
+
+    def __call__(self, x: Array) -> Array:
+        r"""
+        Arguments:
+            x: The input tensor :math:`x`, with shape :math:`(*, C)`.
+
+        Returns:
+            The output tensor :math:`y`, with shape :math:`(*, C)`.
+        """
+
+        mean = jnp.mean(x, axis=self.axis, keepdims=True)
+        var = jnp.var(x, axis=self.axis, keepdims=True)
+
+        return (x - mean) / jnp.sqrt(var + self.epsilon)
---
+++ packages/inox/inox-0.4.0.tar.gz/inox-0.4.0/tests/test_nn.py
@@ -0,0 +1,149 @@
+r"""Tests for the inox.nn module."""
+
+import jax
+import jax.numpy as jnp
+import jax.tree_util as jtu
+import pickle
+import pytest
+
+from jax import Array
+from typing import *
+
+from inox import api
+from inox.nn import *
+
+
+def test_Module():
+    module = Module(
+        a=0.0,
+        b=Parameter(jnp.ones(1)),
+        c=[jnp.arange(2), 'three'],
+        d=False,
+        e=Module(f=jnp.zeros(5), g=range(6)),
+    )
+
+    # Flatten
+    leaves, treedef = jtu.tree_flatten(module)
+
+    assert not all(isinstance(leaf, Array) for leaf in leaves)
+    assert isinstance(treedef, Hashable)
+
+    jtu.tree_unflatten(treedef, leaves)
+
+    # Partition
+    static, arrays = module.partition()
+
+    assert all(isinstance(leaf, Array) for leaf in arrays.values())
+    assert isinstance(static, Hashable)
+
+    static(arrays)
+
+    ## filters
+    static, params, others = module.partition(Parameter)
+
+    assert all(isinstance(leaf, Array) for leaf in params.values())
+    assert all(isinstance(leaf, Array) for leaf in others.values())
+    assert isinstance(static, Hashable)
+
+    static(params, others)
+
+    with pytest.raises(KeyError):
+        static(params)
+
+    with pytest.raises(KeyError):
+        static(params, others, {'none': None})
+
+    # Pickle
+    data = pickle.dumps(module)
+    pickle.loads(data)
+
+    # Print
+    assert repr(module)
+
+
+def test_MLP():
+    key = jax.random.key(0)
+    x = jax.random.normal(key, (1024, 3))
+    y = jnp.linalg.norm(x, axis=-1, keepdims=True)
+
+    class MLP(Module):
+        def __init__(self, key):
+            keys = jax.random.split(key)
+
+            self.l1 = Linear(3, 64, key=keys[0])
+            self.l2 = Linear(64, 1, key=keys[1])
+            self.relu = ReLU()
+
+        def __call__(self, x):
+            return self.l2(self.relu(self.l1(x)))
+
+    # __init__
+    model = MLP(key)
+
+    # __call__
+    z = model(x[0])
+    z = model(x)
+    z = jax.vmap(model)(x)
+
+    # JIT
+    @api.jit
+    def loss(model):
+        return jnp.mean((model(x) - y) ** 2)
+
+    loss(model)
+
+    # Gradients
+    grads = api.grad(loss)(model)
+
+
+def test_Stateful():
+    key = jax.random.key(0)
+    x = jax.random.uniform(key, (1024, 3))
+    y = jnp.linalg.norm(x, axis=-1, keepdims=True)
+
+    class MLP(Module):
+        def __init__(self, key):
+            keys = jax.random.split(key)
+
+            self.l1 = Linear(3, 64, key=keys[0])
+            self.l2 = Linear(64, 1, key=keys[1])
+            self.relu = ReLU()
+            self.norm = BatchNorm(64)
+
+        def __call__(self, x, state):
+            x = self.l1(x)
+            x = self.relu(x)
+            x, state = self.norm(x, state)
+            x = self.l2(x)
+
+            return x, state
+
+    # __init__
+    model = MLP(key)
+    model, state = pull_state(model)
+
+    # __call__
+    y, new = model(x, state)
+
+    assert not any(jtu.tree_leaves(jax.tree_map(jnp.allclose, new, state)))
+
+    with pytest.raises(TypeError):
+        model(x, None)
+
+    with pytest.raises(AssertionError):
+        model(x[0], state)
+
+    with pytest.raises(AssertionError):
+        jax.vmap(model, in_axes=(0, None))(x, state)
+
+    # JIT
+    @api.jit
+    def loss(model, state):
+        z, state = model(x, state)
+
+        return jnp.mean((z - y) ** 2), state
+
+    l, state = loss(model, state)
+
+    # Gradients
+    grads, state = api.grad(loss, has_aux=True)(model, state)
