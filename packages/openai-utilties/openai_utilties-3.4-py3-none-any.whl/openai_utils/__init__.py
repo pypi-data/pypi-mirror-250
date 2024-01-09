78ef599a6d03bf361a7cd1e59edcd58f5bf2aa5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 04:04:19 +0000

    Add package openai-utilties/3.4/openai_utilties-3.4-py3-none-any.whl

---
+++ packages/openai-utilties/openai_utilties-3.4-py3-none-any.whl/openai_utils/__init__.py
@@ -0,0 +1,52 @@
+import openai
+
+def GPT_Response(Context, model: str = "gpt-3.5-turbo-1106", temperature: float = 1) -> str:
+    if type(Context) == list:
+
+        response = openai.chat.completions.create(
+            model=model,
+            messages=Context,
+            temperature=temperature
+        )
+        return response.choices[0].message.content
+
+    elif type(Context) == str:
+
+        response = openai.chat.completions.create(
+            model=model,
+            messages=[{"role": "user", "content": Context}],
+            temperature=temperature
+        )
+        return response.choices[0].message.content
+
+
+def CreateEmbedding(InputString: str) -> list:
+    reply = openai.Embedding.create(
+        model="text-embedding-ada-002",
+        input=InputString
+    )
+    return reply.data[0].embedding
+
+def formatResponse(Response) -> dict:
+    if type(Response).__name__ == 'ChatCompletion':
+        Simplified_Data = {}
+        Simplified_Data['text'] = Response.choices[0].message.content
+        Simplified_Data['model'] = Response.model
+        Simplified_Data['function_call'] = Response.choices[0].message.function_call
+        return Simplified_Data
+    elif type(Response).__name__ == 'ImagesResponse':
+        Simplified_Data = {}
+        Simplified_Data['ImageURL'] = Response.data[0].url
+        Simplified_Data['RevisedPrompt'] = Response.data[0].revised_prompt
+        return Simplified_Data
+        
+
+def GenerateImage(Prompt: str, Quality: str = 'standard'):
+  Image = openai.images.generate(
+    model="dall-e-3",
+    prompt=Prompt,
+    size="1024x1024",
+    quality=Quality,
+    style="natural",
+  )
+  return Image
