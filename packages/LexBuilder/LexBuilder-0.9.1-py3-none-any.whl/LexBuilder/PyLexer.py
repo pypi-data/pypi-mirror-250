b999d1bda116144b171eeecbf347b7c9acf3535e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 04:03:20 +0000

    Add package LexBuilder/0.9.1/LexBuilder-0.9.1-py3-none-any.whl

---
+++ packages/LexBuilder/LexBuilder-0.9.1-py3-none-any.whl/LexBuilder/PyLexer.py
@@ -0,0 +1,38 @@
+from BaseLexer import types, lexer
+
+
+class PyBuilder:
+    def __init__(self, tokens):
+        self.tokens = tokens
+
+    def generate_code_for_tokens(self):
+        result1 = ""
+        result2 = ""
+        result3 = ""
+
+        for token in self.tokens:
+            name, value = token.split("; ")
+            result1 += f'{name} = "{name}"\n'
+            if len(set(value)) == 1:
+                result1 += f'{"_".join([name, name])} = "{"_".join([name, name])}"\n'
+                result2 += f"             if self.current_char == '{list(set(value))[0]}':\n"
+                result2 += '                 self.advance()\n'
+                result2 += f"                 if self.current_char == '{list(set(value))[0]}':\n"
+                result2 += f'                     self.advance()\n'
+                result2 += f'                     return Token({"_".join([name, name])}, "{value*2}")\n'
+                result2 += f'                 else:\n'
+                result2 += f"                     return Token({name}, '{value}')\n\n"
+            else:
+                result3 += f'                     if token_value == "{value}":\n'
+                result3 += f'                         return Token({name}, "{value}")\n\n'
+
+        return result1, result2, result3
+
+
+    def generate_lexer(self):
+        code1, code2, code3 = self.generate_code_for_tokens()
+        with open("Lexer.py", "w", encoding="utf-8") as file:
+            file.write("import sys\n\n\n")
+            file.write(types.format(code1) + "\n\n\n")
+            file.write(lexer.substitute(first=code2, second=code3) + "\n\n")
+
