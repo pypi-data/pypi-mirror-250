d9d82852b8edbd4ba8f8726bc5581b2d6855d28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 04:03:20 +0000

    Add package preloop-sdk/0.2.19/preloop_sdk-0.2.19.tar.gz

---
+++ packages/preloop-sdk/preloop_sdk-0.2.19.tar.gz/preloop_sdk-0.2.19/preloop/sdk/__init__.py
@@ -0,0 +1,5 @@
+from preloop.sdk.inception.constructs import datasources, feature
+from preloop.sdk.inception.models import AuthParamsPostgres, ConnectionParamsPostgres, PostgresDatasource
+from preloop.sdk.public_api_stubs.exceptions import PreloopError
+from preloop.sdk.public_api_stubs.models import *
+from preloop.sdk.public_api_stubs.preloop_client import PreloopClient
