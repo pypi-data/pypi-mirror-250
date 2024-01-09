66d84412295bbacf9108c7df614d091acd245ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:38 +0000

    Add package DrDictaphone/0.8.1/DrDictaphone-0.8.1.tar.gz

---
+++ packages/DrDictaphone/DrDictaphone-0.8.1.tar.gz/DrDictaphone-0.8.1/drdictaphone/logger_config.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Ole Kliemann
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import logging
+from drdictaphone.config import config
+
+logger_config = {
+  'level': logging.INFO,
+  'filename': config['paths']['log'],
+  'format': '%(asctime)s %(levelname)s: %(name)s: %(message)s'
+}
+
+logging.basicConfig(**logger_config)
