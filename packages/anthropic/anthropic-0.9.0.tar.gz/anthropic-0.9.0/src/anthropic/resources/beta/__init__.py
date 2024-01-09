0cd3a9676d06a2184f6ceb831ff148ebbfba822d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 05:03:11 +0000

    Add package anthropic/0.9.0/anthropic-0.9.0.tar.gz

---
+++ packages/anthropic/anthropic-0.9.0.tar.gz/anthropic-0.9.0/src/anthropic/resources/beta/__init__.py
@@ -0,0 +1,15 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from .beta import Beta, AsyncBeta, BetaWithRawResponse, AsyncBetaWithRawResponse
+from .messages import Messages, AsyncMessages, MessagesWithRawResponse, AsyncMessagesWithRawResponse
+
+__all__ = [
+    "Messages",
+    "AsyncMessages",
+    "MessagesWithRawResponse",
+    "AsyncMessagesWithRawResponse",
+    "Beta",
+    "AsyncBeta",
+    "BetaWithRawResponse",
+    "AsyncBetaWithRawResponse",
+]
