94f2a7d7f52bdf8fa091fa295f46e47e56204f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 04:03:40 +0000

    Add package hrcl-jobs/1.4.0/hrcl_jobs-1.4.0.tar.gz

---
+++ packages/hrcl-jobs/hrcl_jobs-1.4.0.tar.gz/hrcl_jobs-1.4.0/src/hrcl_jobs/__init__.py
@@ -0,0 +1,18 @@
+import warnings
+from . import sqlt
+from . import serial
+from . import jobspec
+try:
+    from . import parallel
+    from . import setup
+    from . import examples
+    from . import utils
+except ImportError:
+    warnings.warn("Could not import all modules from hrcl_jobs.parallel (install mpi4py)")
+    pass
+
+try:
+    from . import dataset 
+except ImportError:
+    warnings.warn("Could not import all modules from hrcl_jobs.dataset (install mpi4py and psi4)")
+    pass
