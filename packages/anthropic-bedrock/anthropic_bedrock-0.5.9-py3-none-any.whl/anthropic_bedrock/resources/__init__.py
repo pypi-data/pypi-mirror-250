b3ced8eedc7dc1e6713851eea699088c65a3666b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 05:03:01 +0000

    Add package anthropic-bedrock/0.5.9/anthropic_bedrock-0.5.9-py3-none-any.whl

---
+++ packages/anthropic-bedrock/anthropic_bedrock-0.5.9-py3-none-any.whl/anthropic_bedrock/resources/__init__.py
@@ -0,0 +1,5 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from .completions import Completions, AsyncCompletions, CompletionsWithRawResponse, AsyncCompletionsWithRawResponse
+
+__all__ = ["Completions", "AsyncCompletions", "CompletionsWithRawResponse", "AsyncCompletionsWithRawResponse"]
