8dde013f1d717e070b8ea336acf1d36e19de486b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:33 +0000

    Add package textual-fastdatatable/0.6.2/textual_fastdatatable-0.6.2.tar.gz

---
+++ packages/textual-fastdatatable/textual_fastdatatable-0.6.2.tar.gz/textual_fastdatatable-0.6.2/src/textual_fastdatatable/__main__.py
@@ -0,0 +1,16 @@
+from textual.app import App, ComposeResult
+
+from textual_fastdatatable import ArrowBackend, DataTable
+
+
+class TableApp(App, inherit_bindings=False):
+    BINDINGS = [("ctrl+q", "quit", "Quit")]
+
+    def compose(self) -> ComposeResult:
+        backend = ArrowBackend.from_parquet("./tests/data/lap_times_538121.parquet")
+        yield DataTable(backend=backend, cursor_type="row", max_column_content_width=5)
+
+
+if __name__ == "__main__":
+    app = TableApp()
+    app.run()
