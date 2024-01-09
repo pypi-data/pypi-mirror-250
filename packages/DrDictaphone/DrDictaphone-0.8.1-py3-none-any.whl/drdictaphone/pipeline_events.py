84f5ba53f638f2c94c250c6427e5d1cfc8f6c955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:37 +0000

    Add package DrDictaphone/0.8.1/DrDictaphone-0.8.1-py3-none-any.whl

---
+++ packages/DrDictaphone/DrDictaphone-0.8.1-py3-none-any.whl/drdictaphone/pipeline_events.py
@@ -0,0 +1,30 @@
+# Copyright 2023 Ole Kliemann
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+PipelineEvents = [
+  'result',
+  'start_rec',
+  'stop_rec',
+  'toggle_rec',
+  'discard_rec',
+  'costs_incurred',
+  'pause_mic',
+  'unpause_mic',
+  'clear_buffer',
+  'active',
+  'idle',
+  'error',
+]
+
+PipelineSlots = [
+  'onResult',
+  'onStartRec',
+  'onStopRec',
+  'onToggleRec',
+  'onDiscardRec',
+  'onCostsIncurred',
+  'onPauseMic',
+  'onUnpauseMic',
+  'onClearBuffer',
+  'onError',
+]
---
+++ packages/DrDictaphone/DrDictaphone-0.8.1-py3-none-any.whl/drdictaphone/server_pipeline.py
@@ -0,0 +1,52 @@
+# Copyright 2023 Ole Kliemann
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from drdictaphone.config import makeOutputFilename
+from drdictaphone.audio_feedback import AudioFeedback
+from drdictaphone.microphone import Microphone
+from drdictaphone.transcriber import Transcriber
+from drdictaphone.chat_gpt import ChatGpt
+from drdictaphone.post_processor import PostProcessor
+from drdictaphone.output import Output
+from drdictaphone.cost_counter import CostCounter
+from drdictaphone.outlet import Outlet
+from drdictaphone.pipeline import Pipeline
+from drdictaphone import logger
+
+logger = logger.get(__name__)
+
+class ServerPipeline:
+  def __init__(self, profile):
+    self.profile = profile
+    self.audio_feedback = AudioFeedback()
+    self.microphone = Microphone()
+    self.vad = None
+    if self.profile.enable_vad:
+      from drdictaphone.static_vad import StaticVad
+      self.vad = StaticVad()
+    self.transcriber = Transcriber(self.profile.language)
+    self.chat_gpt = ChatGpt(self.profile.post_processor)
+    self.post_processor = PostProcessor(self.chat_gpt)
+    self.output = None
+    if self.profile.output:
+      self.output = Output(makeOutputFilename(self.profile.output))
+    self.cost_counter = CostCounter()
+    self.outlet = Outlet()
+    modules = [
+      self.audio_feedback,
+      self.microphone,
+      self.vad,
+      self.transcriber,
+      self.post_processor,
+      self.output,
+      self.cost_counter,
+      self.outlet,
+    ]
+    self.pipeline = Pipeline(modules)
+
+  async def __aenter__(self):
+    await self.pipeline.__aenter__()
+    return self
+
+  async def __aexit__(self, exc_type, exc_value, traceback):
+    return await self.pipeline.__aexit__(exc_type, exc_value, traceback)
