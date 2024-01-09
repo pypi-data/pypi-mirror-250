f2f495ba1a69fd3e888b44f5fd33242c5c9f657a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:36 +0000

    Add package magCalEM/1.3.3/magCalEM-1.3.3.tar.gz

---
+++ packages/magCalEM/magCalEM-1.3.3.tar.gz/magCalEM-1.3.3/src/magCalibration/scripts/makeOneUnwhitenedPS.py
@@ -0,0 +1,32 @@
+import sys
+import mrcfile
+import numpy as np
+import os
+#import aliasPS
+
+def runScript(path, filename, new_dir):
+    #load mrc
+    mrc = mrcfile.open(path + filename, permissive=True)
+    data=mrc.data
+    mrc.close()
+    #work out final FFT size
+    size = data.shape
+    powerOfTwo_y = np.ceil(np.log2(size[0]))
+    powerOfTwo_x = np.ceil(np.log2(size[1])) 
+    powerOfTwo = powerOfTwo_y
+    if powerOfTwo_x > powerOfTwo:
+        powerOfTwo = powerOfTwo_x
+    fft_width = int(2**powerOfTwo)
+    #make PS
+    power = np.absolute(np.fft.fftshift(np.fft.fft2(data, s=(fft_width, fft_width))))**2
+    #write file
+    with mrcfile.new(f"{new_dir}/{filename[:-4]}_PS.mrc", overwrite=True) as mrc:
+        mrc.set_data(np.float32(power))
+    return power
+    
+        
+
+
+
+
+        
