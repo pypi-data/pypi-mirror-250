c5c24500c17de4b2b1ab2031a06ab3e32d3ce962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 04:03:36 +0000

    Add package metaphor-connectors/0.13.101/metaphor_connectors-0.13.101.tar.gz

---
+++ packages/metaphor-connectors/metaphor_connectors-0.13.101.tar.gz/metaphor_connectors-0.13.101/metaphor/dbt/cloud/config.py
@@ -0,0 +1,32 @@
+from dataclasses import field as dataclass_field
+from typing import List
+
+from pydantic.dataclasses import dataclass
+
+from metaphor.common.base_config import BaseConfig
+from metaphor.common.dataclass import ConnectorConfig
+from metaphor.dbt.config import MetaOwnership, MetaTag
+
+
+@dataclass(config=ConnectorConfig)
+class DbtCloudConfig(BaseConfig):
+    # dbt cloud account ID
+    account_id: int
+
+    # Service token for dbt cloud
+    service_token: str
+
+    # dbt cloud job IDs
+    job_ids: List[int] = dataclass_field(default_factory=list)
+
+    # dbt cloud project IDs
+    project_ids: List[int] = dataclass_field(default_factory=list)
+
+    # map meta field to ownerships
+    meta_ownerships: List[MetaOwnership] = dataclass_field(default_factory=list)
+
+    # map meta field to tags
+    meta_tags: List[MetaTag] = dataclass_field(default_factory=list)
+
+    # Base URL for dbt instance
+    base_url: str = "https://cloud.getdbt.com"
