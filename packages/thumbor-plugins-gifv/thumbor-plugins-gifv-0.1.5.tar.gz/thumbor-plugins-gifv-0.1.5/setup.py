85ee283fec590167df61878b76a9eb6477bccef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 04:03:43 +0000

    Add package thumbor-plugins-gifv/0.1.5/thumbor-plugins-gifv-0.1.5.tar.gz

---
+++ packages/thumbor-plugins-gifv/thumbor-plugins-gifv-0.1.5.tar.gz/thumbor-plugins-gifv-0.1.5/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+
+setup(
+    name="thumbor-plugins-gifv",
+    version="0.1.5",
+    keywords="thumbor ffmpeg gifv mp4",
+    author="Guilherme Souza",
+    author_email="guilherme@souza.tech",
+    url="https://github.com/thumbor/thumbor-plugins",
+    license="MIT",
+    description="Thumbor optimizer to add support to Gifv(mp4)",
+    long_description="""
+        Available Configs:
+        - FFMPEG_PATH
+
+        This package is part of the thumbor-plugins repository.
+        For more information, visit: https://github.com/thumbor/thumbor-plugins
+    """,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Multimedia :: Graphics :: Presentation",
+    ],
+    packages=[
+        "thumbor_plugins.optimizers.gifv",
+    ],
+    package_dir={
+        "thumbor_plugins.optimizers.gifv": ".",
+    },
+    install_requires=[
+        "webcolors==1.*,>=1.13.0",
+    ],
+)
