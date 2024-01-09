ab5b283f0e1a6dd0166f966695cc2db38b116ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:40 +0000

    Add package drdictaphone-shared/0.8.0/drdictaphone_shared-0.8.0-py3-none-any.whl

---
+++ packages/drdictaphone-shared/drdictaphone_shared-0.8.0-py3-none-any.whl/drdictaphone_shared/status_line.py
@@ -0,0 +1,21 @@
+# Copyright 2023 Ole Kliemann
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+def makeStatusLine(status):
+  to_print = [
+    'MIC' if status['mic'] else '',
+    'TRANS' if status['processing'] else '',
+    'ERR' if status['error'] else '',
+  ]
+  left = '  ' + ''.join(f'{word:<6}' for word in to_print)
+  right = f'{status["profile_name"]}      costs: {(status["costs"] / 100):.2f}$  '
+  return { 'left': left, 'right': right  }
+
+def makeStatusLinePlugin(status):
+  to_print = [
+    'MIC' if status['mic'] else '',
+    'TRANS' if status['processing'] else '',
+    'ERR' if status['error'] else '',
+  ]
+  line = f' {status["profile_name"]} ' + ''.join(f'{word:<6}' for word in to_print)
+  return line
