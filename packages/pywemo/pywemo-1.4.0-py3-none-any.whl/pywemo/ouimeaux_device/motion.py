961fef8ddbe10544364c1d5248cc199420aa25e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 04:04:06 +0000

    Add package pywemo/1.4.0/pywemo-1.4.0-py3-none-any.whl

---
+++ packages/pywemo/pywemo-1.4.0-py3-none-any.whl/pywemo/ouimeaux_device/motion.py
@@ -0,0 +1,6 @@
+"""Representation of a WeMo Motion device."""
+from . import Device
+
+
+class Motion(Device):
+    """Representation of a WeMo Motion device."""
---
+++ packages/pywemo/pywemo-1.4.0-py3-none-any.whl/pywemo/ouimeaux_device/outdoor_plug.py
@@ -0,0 +1,6 @@
+"""Representation of a WeMo OutdoorPlug device."""
+from .switch import Switch
+
+
+class OutdoorPlug(Switch):
+    """Representation of a WeMo Motion device."""
