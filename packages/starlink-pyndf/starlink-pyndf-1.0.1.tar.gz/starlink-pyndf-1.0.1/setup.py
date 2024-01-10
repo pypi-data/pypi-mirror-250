a9b4cbe9ee1bdc898eaa242c43dcad0855b7628c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 07:05:19 +0000

    Add package starlink-pyndf/1.0.1/starlink-pyndf-1.0.1.tar.gz

---
+++ packages/starlink-pyndf/starlink-pyndf-1.0.1.tar.gz/starlink-pyndf-1.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='starlink-pyndf', #package name
+      version='1.0.1',
+      description='Python Package By jordin',
+      author='jordin',
+      license='MIT',
+      zip_safe=False
+      )
