58a17905eede9169b1d5b6ec6b4809adf940a01e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 06:04:18 +0000

    Add package fastapi-extend/0.0.7/fastapi_extend-0.0.7-py3-none-any.whl

---
+++ packages/fastapi-extend/fastapi_extend-0.0.7-py3-none-any.whl/fastapi_extend/__init__.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+"""
+Name : __init__.py.py
+Author : lvyunze
+Time : 2023/3/2 23:58
+Desc : fastapi_extend
+"""
+from fastapi_extend.fastapi_jwt import JwtAuthorizationCredentials, AuthHandler
+from fastapi_extend.pagenator import PageNumberPagination
+from fastapi_extend.serializer import dump, model2schema, Serializer
+from fastapi_extend.utils import value_dispatch, SingletonType, repeat_every
+
+__all__ = [
+    "JwtAuthorizationCredentials",
+    "AuthHandler",
+    "PageNumberPagination",
+    "Serializer",
+    "dump",
+    "model2schema",
+    "value_dispatch",
+    "SingletonType",
+    "repeat_every"
+]
