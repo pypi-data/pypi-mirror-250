194f36667733fed0a86a49cb1e0368504042811f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 04:04:03 +0000

    Add package financial-statement/0.1.2/financial_statement-0.1.2-py3-none-any.whl

---
+++ packages/financial-statement/financial_statement-0.1.2-py3-none-any.whl/financial_statement/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 29 13:18:20 2023
+
+@author: wenkangng
+"""
+
+from .financial_statement import income_statement
+from .financial_statement import balance_sheet
+from .financial_statement import cash_flow_statement
+from .financial_statement import get_dividend
+from .financial_statement import shareholder
+from .financial_statement import current_price
+
+
+# financial-statement from you python file name
+# import from you package name !
