a0def834d11012d5c55ecd8aab7287ab1e727cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 08:04:15 +0000

    Add package hack4U-Grx/0.1.1/hack4U_Grx-0.1.1.tar.gz

---
+++ packages/hack4U-Grx/hack4U_Grx-0.1.1.tar.gz/hack4U_Grx-0.1.1/hack4u/cursos.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+class Curso:
+    def __init__(self, name, duration, link):
+        self.name = name
+        self.link = link
+        self.duration = duration
+    
+    def __repr__(self): #similar a __str__
+        return f"{self.name} [{self.duration} horas] ({self.link})"
+
+
+
+cursos = [
+    Curso("Introducción a Linux", 15, "https://hack4u.io/cursos/introduccion-a-linux/11149" ),
+    Curso("Personalización de Linux", 3, "https://hack4u.io/cursos/personalizacion-de-entorno-en-linux/14290"),
+    Curso("Introducción al Hacking", 53, "https://hack4u.io/cursos/introduccion-al-hacking/14610"),
+    Curso("Python Ofensivo", 35, "https://hack4u.io/cursos/python-ofensivo/18870")
+]
+
+def list_curso():
+    for i in cursos:
+        print(i)
+
+def search_curso_by_name(name):
+    for i in cursos:
+        if i.name == name:
+            return i
+        
+    return None
