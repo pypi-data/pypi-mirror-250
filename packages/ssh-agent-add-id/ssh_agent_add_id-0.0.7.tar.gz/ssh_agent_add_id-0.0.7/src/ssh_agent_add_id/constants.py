2da52182334c6ba63138206b5d6c45a97668453c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 06:04:41 +0000

    Add package ssh-agent-add-id/0.0.7/ssh_agent_add_id-0.0.7.tar.gz

---
+++ packages/ssh-agent-add-id/ssh_agent_add_id-0.0.7.tar.gz/ssh_agent_add_id-0.0.7/src/ssh_agent_add_id/constants.py
@@ -0,0 +1,6 @@
+from typing import Final
+
+
+APP_DESCRIPTION: Final[str] = "Checks whether an identity file has already been stored by the SSH \
+agent. If not, it adds this identity by prompting for its passphrase."
+APP_NAME: Final[str] = "ssh-agent-add-id"
