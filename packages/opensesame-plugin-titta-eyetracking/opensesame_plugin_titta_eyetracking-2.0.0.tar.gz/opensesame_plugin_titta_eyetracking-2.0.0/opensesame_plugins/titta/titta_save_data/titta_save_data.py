c9a1b3a06cde0573ef951558bfa94d980d536ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 09:03:30 +0000

    Add package opensesame-plugin-titta-eyetracking/2.0.0/opensesame_plugin_titta_eyetracking-2.0.0.tar.gz

---
+++ packages/opensesame-plugin-titta-eyetracking/opensesame_plugin_titta_eyetracking-2.0.0.tar.gz/opensesame_plugin_titta_eyetracking-2.0.0/opensesame_plugins/titta/titta_save_data/titta_save_data.py
@@ -0,0 +1,50 @@
+"""
+No rights reserved. All files in this repository are released into the public
+domain.
+"""
+
+from libopensesame.py3compat import *
+from libopensesame.item import Item
+from libqtopensesame.items.qtautoplugin import QtAutoPlugin
+from libopensesame.exceptions import OSException
+from libopensesame.oslogging import oslogger
+
+
+class TittaSaveData(Item):
+
+    def prepare(self):
+        super().prepare()
+        self._check_init()
+
+    def run(self):
+        self._check_stop()
+        self.set_item_onset()
+        self.experiment.tracker.save_data()
+
+    def _check_init(self):
+        if hasattr(self.experiment, "titta_dummy_mode"):
+            self.dummy_mode = self.experiment.titta_dummy_mode
+            self.verbose = self.experiment.titta_verbose
+        else:
+            raise OSException('You should have one instance of `Titta Init` at the start of your experiment')
+
+    def _check_stop(self):
+        if not hasattr(self.experiment, "titta_stop_recording"):
+            raise OSException(
+                    '`Titta Stop Recording` item is missing')
+        elif self.experiment.titta_recording:
+                raise OSException(
+                        'Titta still recording, you first have to stop recording before saving data')
+
+    def _show_message(self, message):
+        oslogger.debug(message)
+        if self.verbose == 'yes':
+            print(message)
+
+
+class QtTittaSaveData(TittaSaveData, QtAutoPlugin):
+
+    def __init__(self, name, experiment, script=None):
+        TittaSaveData.__init__(self, name, experiment, script)
+        QtAutoPlugin.__init__(self, __file__)
+
