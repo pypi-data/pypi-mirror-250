a5a5ecdc04f6fe5897c0677753831f15ae214561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 15:03:36 +0000

    Add package vectara-skunk-client/0.4.6/vectara-skunk-client-0.4.6.tar.gz

---
+++ packages/vectara-skunk-client/vectara-skunk-client-0.4.6.tar.gz/vectara-skunk-client-0.4.6/test/test_documents.py
@@ -0,0 +1,44 @@
+import unittest
+from vectara.util import MarkdownFormatter, render_markdown_req
+from test.base import BaseClientTest
+from test.util import check_metadata
+import json
+
+class BasicQuery(BaseClientTest):
+
+    def setUp(self):
+        super().setUp()
+
+    def test_documents_info(self):
+        """
+        Test Document List Test.
+        :return:
+        """
+
+        dict1 = {"owner": "david"}
+        dict2 = {"owner": "justin"}
+
+        #resp1 = self.index_test_doc("./resources/filter_attributes/document_1.json", force=True)
+        #resp2 = self.index_test_doc("./resources/filter_attributes/document_2.json", force=True)
+        #resp3 = self.index_test_doc("./resources/filter_attributes/document_3.json", force=True)
+
+        doc_service = self.client.document_service
+
+        doc_list = doc_service.list_documents(self.corpus_id)
+        for doc in doc_list:
+            self.logger.info(f"We found [{doc}]")
+
+        # TODO Assert metadata returned.
+
+        doc_list = doc_service.list_documents(self.corpus_id, page_size=2)
+        self.assertEqual(3, len(doc_list))
+
+
+        # Test Metadata filter
+        doc_list = doc_service.list_documents(self.corpus_id, metadata_filter="doc.id = 'doc-id-1'")
+        self.assertEqual(1, len(doc_list))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
