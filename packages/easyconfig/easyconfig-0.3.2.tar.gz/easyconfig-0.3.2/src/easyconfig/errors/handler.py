f1134d6c48b7dc637741cbbb80df21a3baf40f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 15:04:06 +0000

    Add package easyconfig/0.3.2/easyconfig-0.3.2.tar.gz

---
+++ packages/easyconfig/easyconfig-0.3.2.tar.gz/easyconfig-0.3.2/src/easyconfig/errors/handler.py
@@ -0,0 +1,17 @@
+from typing import Any, Callable
+
+
+def default_exception_handler(e: Exception):
+    raise e
+
+
+HANDLER: Callable[[Exception], Any] = default_exception_handler
+
+
+def set_exception_handler(handler: Callable[[Exception], Any]):
+    global HANDLER  # noqa: PLW0603
+    HANDLER = handler
+
+
+def process_exception(e: Exception):
+    HANDLER(e)
---
+++ packages/easyconfig/easyconfig-0.3.2.tar.gz/easyconfig-0.3.2/src/easyconfig/models/app.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from easyconfig.models.config import ConfigMixin
+
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+class AppConfigMixin(ConfigMixin):
+    def set_file_path(self, path: Path | str):
+        """Set the path to the configuration file.
+        If no file extension is specified ``.yml`` will be automatically appended.
+
+        :param path: Path obj or str
+        """
+
+    def load_config_dict(self, cfg: dict, /, expansion: bool = True):
+        """Load the configuration from a dictionary
+
+        :param cfg: config dict which will be loaded
+        :param expansion: Expand ${...} in strings
+        """
+
+    def load_config_file(self, path: Path | str | None = None, expansion: bool = True):
+        """Load configuration from a yaml file. If the file does not exist a default file will be created
+
+        :param path: Path to file
+        :param expansion: Expand ${...} in strings
+        """
+
+    def generate_default_yaml(self) -> str:
+        """Generate the default YAML structure
+
+        :returns: YAML structure as a string
+        """
