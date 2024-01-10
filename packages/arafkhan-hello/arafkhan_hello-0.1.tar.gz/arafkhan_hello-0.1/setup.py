0eb59e0dacaf78d925a6e9b12aea65bfe2455347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 15:04:16 +0000

    Add package arafkhan-hello/0.1/arafkhan_hello-0.1.tar.gz

---
+++ packages/arafkhan-hello/arafkhan_hello-0.1.tar.gz/arafkhan_hello-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='arafkhan_hello',
+    version='0.1',
+    packages=['arafkhan_hello'],
+    url='',
+    license='',
+    author='Araf',
+    author_email='arafkhan565@gmail.com',
+    description='Simple Hello world package'
+)
