3f6ad42324de48c22fd3abf7bd8e730a1b3b013a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 15:03:59 +0000

    Add package qmiconfigs/0.1/qmiconfigs-0.1.tar.gz

---
+++ packages/qmiconfigs/qmiconfigs-0.1.tar.gz/qmiconfigs-0.1/README.md
@@ -0,0 +1,2 @@
+# pd-mktp-ops-qmi-common
+Repository for common modules used across qmi projects
---
+++ packages/qmiconfigs/qmiconfigs-0.1.tar.gz/qmiconfigs-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='qmiconfigs',
+    version='0.1',
+    packages=["qmiconfigs"],
+    install_requires=[
+        # List your dependencies here
+    ],
+)
