39ce9dc806bcfc1903e179d3a3b2031f476fde8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 06:04:17 +0000

    Add package versions/2.0.0/versions-2.0.0.tar.gz

---
+++ packages/versions/versions-2.0.0.tar.gz/versions-2.0.0/versions/converters.py
@@ -0,0 +1,25 @@
+try:
+    from versions.converters_modern import (
+        simplify,
+        specifier_from_version_set,
+        specifier_to_version_set,
+        version_set_from_specifier,
+        version_set_to_specifier,
+    )
+
+except SyntaxError:
+    from versions.converters_normal import (
+        simplify,
+        specifier_from_version_set,
+        specifier_to_version_set,
+        version_set_from_specifier,
+        version_set_to_specifier,
+    )
+
+__all__ = (
+    "simplify",
+    "specifier_from_version_set",
+    "specifier_to_version_set",
+    "version_set_from_specifier",
+    "version_set_to_specifier",
+)
