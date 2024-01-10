3597099e6b22c8c8fa79d919560d199d5cdd4efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:04:22 +0000

    Add package imgtests/2/imgtests-2.tar.gz

---
+++ packages/imgtests/imgtests-2.tar.gz/imgtests-2/imgtests/__init__.py
@@ -0,0 +1,6 @@
+from IPython.display import Image
+d = {'условие1':display(Image(filename='1.png')), 'условие2': display(Image(filename='2.png')), 'условие3': display(Image(filename='4.png'))
+def find(usl):
+    for key in d.keys():
+        if usl in key:
+            print(d[key])
