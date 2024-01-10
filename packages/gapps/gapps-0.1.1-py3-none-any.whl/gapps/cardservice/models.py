538eb8b68f025fc64e2889a294a499cbc87cb56a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 04:04:32 +0000

    Add package gapps/0.1.1/gapps-0.1.1-py3-none-any.whl

---
+++ packages/gapps/gapps-0.1.1-py3-none-any.whl/gapps/cardservice/models.py
@@ -0,0 +1,124 @@
+"""Module to manage fastapi models."""
+
+from typing import List
+from pydantic import BaseModel
+
+
+class TimeZone(BaseModel):
+    id: str
+    offset: int
+
+
+class CommonEvent(BaseModel):
+    userLocale: str = None
+    hostApp: str
+    platform: str
+    timeZone: TimeZone = None
+    parameters: dict = {}
+    formInputs: dict = {}
+
+
+class AuthorizationEvent(BaseModel):
+    userOAuthToken: str
+    systemIdToken: str
+    userIdToken: str
+
+
+class DriveItem(BaseModel):
+    id: str = None
+    iconUrl: str = None
+    mimeType: str = None
+    title: str = None
+    addonHasFileScopePermission: bool = False
+
+
+class DriveEvent(BaseModel):
+    selectedItems: List[DriveItem] = []
+    activeCursorItem: DriveItem = None
+
+
+class EditorEvent(BaseModel):
+    """Docs, Sheets, slides """
+    id: str = None
+    title: str = None
+    addonHasFileScopePermission: bool = False
+
+
+class GmailEvent(BaseModel):
+    messageId: str = None
+    threadId: str = None
+    accessToken: str = None
+    toRecipients: List[str] = []
+    ccRecipients: List[str] = []
+    bccRecipients: List[str] = []
+
+
+class Organizer(BaseModel):
+    email: str = None
+
+
+class Capabilities(BaseModel):
+    canSeeAttendees: bool = None
+    canAddAttendees: bool = None
+    canSeeConferenceData: bool = None
+    canSetConferenceData: bool = None
+
+
+class Attendee(BaseModel):
+    email: str = None
+    optional: bool = None
+    displayName: str = None
+    organizer: bool = None
+    # self: bool = None
+    resource: bool = None
+    responseStatus: str = None
+    comment: str = None
+    additionalGuests: int = None
+
+
+class ConferenceSolution(BaseModel):
+    iconUri: str = None
+    key: dict = None
+    name: str = None
+
+
+class EntryPoint(BaseModel):
+    accessCode: str = None
+    entryPointFeatures: List[str] = None
+    entryPointType: str = None
+    label: str = None
+    meetingCode: str = None
+    passcode: str = None
+    password: str = None
+    pin: str = None
+    regionCode: str = None
+    uri: str = None
+
+
+class ConferenceData(BaseModel):
+    conferenceId: str = None
+    conferenceSolution: ConferenceSolution = None
+    entryPoints: List[EntryPoint] = None
+    notes: str = None
+    parameters: dict = None
+
+
+class CalendarEvent(BaseModel):
+    id: str = None   # the event id
+    recurringEventId: str = None
+    calendarId: str = None
+    organizer: Organizer = None
+    attendees: List[Attendee] = []
+    conferenceData: ConferenceData = None
+    capabilities: Capabilities = None
+
+
+class GEvent(BaseModel):
+    commonEventObject: CommonEvent
+    authorizationEventObject: AuthorizationEvent
+    drive: DriveEvent = None
+    docs: EditorEvent = None
+    sheets: EditorEvent = None
+    slides: EditorEvent = None
+    gmail: GmailEvent = None
+    calendar: CalendarEvent = None
---
+++ packages/gapps/gapps-0.1.1-py3-none-any.whl/gapps/cardservice/utilities.py
@@ -0,0 +1,143 @@
+
+from google.auth.transport import requests
+from google.oauth2.id_token import verify_oauth2_token
+
+
+def delete_none(_dict):
+    """Delete None values recursively from all of the dictionaries.
+
+    Parameters
+    ----------
+    _dict : dict
+        dictionnaries containing None values
+
+    Returns
+    -------
+    _dict : dict
+        dictionnaries without any None values
+
+    """
+    for key, value in list(_dict.items()):
+        if isinstance(value, dict):
+            delete_none(value)
+        elif value is None:
+            del _dict[key]
+        elif isinstance(value, list):
+            for v_i in value:
+                if isinstance(v_i, dict):
+                    delete_none(v_i)
+
+    return _dict
+
+
+def update_actions(_dict):
+    """Update missing keys in a action dictionary.
+
+    Parameters
+    ----------
+    _dict : dict
+        dictionnaries containing None values
+
+    Returns
+    -------
+    _dict : dict
+        dictionnaries without any None values
+
+    """
+    for key, value in list(_dict.items()):
+        if key in ['onClick']:
+            _dict[key] = {'action': value}
+        if key in ['openLink', 'openDynamicLinkAction']:
+            d_temp = _dict.get('onClick', {})
+            d_temp.update({key: value})
+            _dict['onClick'] = d_temp
+            del _dict[key]
+        elif key == 'selection_item':
+            del _dict[key]
+            _dict["items"] = [{'text': text, 'value': val, 'selected': sel}
+                              for text, val, sel in value]
+        elif isinstance(value, dict):
+            update_actions(value)
+        elif isinstance(value, list):
+            for v_i in value:
+                if isinstance(v_i, dict):
+                    update_actions(v_i)
+
+    return _dict
+
+
+def decode_user(token: str):
+    """Decode Google User ID Token.
+
+    Parameters
+    ----------
+    token : str
+        An end user ID token. The user’s email address is encoded in
+        the end user ID token, formatted as a JSON web token
+
+    Returns
+    -------
+    dict
+        user id info
+
+    """
+    request = requests.Request()
+    id_info = verify_oauth2_token(token, request)
+    # TODO: Handle the valueerror below
+    # ValueError: Token expired, 1659048959 < 1659050005
+    return id_info
+
+
+def decode_email(token: str):
+    """Decode Google User ID Token.
+
+    Parameters
+    ----------
+    token : str
+        An end user ID token. The user’s email address is encoded in
+        the end user ID token, formatted as a JSON web token
+
+    Returns
+    -------
+    dict
+        user id info
+
+    """
+    user = decode_user(token)
+    return user.get("email", "")
+
+
+def hex2floats(h):
+    """Take a hex rgb string and returns an RGB tuple.
+
+    Parameters
+    ----------
+    h: str
+        hex rgb string (e.g. #ffffff)
+
+    Returns
+    -------
+    rgb: tuple(float, float, float)
+
+    """
+    if not h or not isinstance(h, str) or not h.startswith('#'):
+        return None
+    return tuple(int(h[i:i + 2], 16) / 255. for i in (1, 3, 5))  # skip '#'
+
+
+def floats2hex(rgb):
+    """Takes an RGB tuple or list and returns a hex RGB string."""
+    if not rgb or not isinstance(rgb, (tuple, list)):
+        return None
+    return f'#{int(rgb[0]*255):02x}{int(rgb[1]*255):02x}{int(rgb[2]*255):02x}'
+
+
+def get_form_value(form, key):
+    """Return Form value."""
+    if not isinstance(form, dict):
+        return None
+    if not isinstance(key, str):
+        return None
+
+    val = form.get(key, {}).get('stringInputs', {}).get('value', None)
+    return val
