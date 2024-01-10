f63043a0cf6837ae27e2d7a19d139b49f203066b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 09:03:21 +0000

    Add package rosecape-airflow/0.0.19/rosecape_airflow-0.0.19.tar.gz

---
+++ packages/rosecape-airflow/rosecape_airflow-0.0.19.tar.gz/rosecape_airflow-0.0.19/rosecape_airflow/slack/messages.py
@@ -0,0 +1,124 @@
+from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
+
+def task_failed_slack_alert(context, slack_conn_id='slack_default', message=None):
+    """
+    Sends message to Slack about task failure.
+
+    :param context: The execution context
+    :type context: dict
+    :param slack_conn_id: Slack connection ID
+    :type slack_conn_id: str
+    :return: None
+
+    slack_conn_id: When specifying a custom connection for a specific channel,
+    ensure the the connection is created as follow:
+        - host: https://hooks.slack.com/services/
+        - password: <webhook_token> (rest of the hook url)
+    """
+    
+    slack_msg = """
+            :red_circle: Task Failed. 
+            *Task*: {task}  
+            *Dag*: {dag} 
+            *Configuration*: {conf}
+            *Message*: {message}
+            *Execution Time*: {exec_date}  
+            *Log Url*: {log_url} 
+            """.format(
+            task=context.get('task_instance').task_id,
+            dag=context.get('task_instance').dag_id,
+            ti=context.get('task_instance'),
+            conf=context.get('dag_run').conf,
+            exec_date=context.get('execution_date'),
+            log_url=context.get('task_instance').log_url,
+            message=message
+        )
+    
+    failed_alert = SlackWebhookOperator(
+        task_id='task_failed_slack_alert',
+        message=slack_msg,
+        slack_webhook_conn_id=slack_conn_id
+    )
+    return failed_alert.execute(context=context)
+
+def task_success_slack_alert(context, slack_conn_id='slack_default', message=None):
+    """
+    Sends message to Slack about task success.
+
+    :param context: The execution context
+    :type context: dict
+    :param slack_conn_id: Slack connection ID
+    :type slack_conn_id: str
+    :return: None
+
+    slack_conn_id: When specifying a custom connection for a specific channel,
+    ensure the the connection is created as follow:
+        - host: https://hooks.slack.com/services/
+        - password: <webhook_token> (rest of the hook url)
+    """
+    
+    slack_msg = """
+            :white_check_mark: Task Success. 
+            *Task*: {task}  
+            *Dag*: {dag} 
+            *Configuration*: {conf}
+            *Message*: {message}
+            *Execution Time*: {exec_date}  
+            *Log Url*: {log_url} 
+            """.format(
+            task=context.get('task_instance').task_id,
+            dag=context.get('task_instance').dag_id,
+            ti=context.get('task_instance'),
+            conf=context.get('dag_run').conf,
+            exec_date=context.get('execution_date'),
+            log_url=context.get('task_instance').log_url,
+            message=message
+        )
+
+    success_alert = SlackWebhookOperator(
+        task_id='task_success_slack_alert',
+        message=slack_msg,
+        slack_webhook_conn_id=slack_conn_id,
+    )
+    return success_alert.execute(context=context)
+
+def task_warning_slack_alert(context, slack_conn_id='slack_default', message=None):
+    """
+    Sends message to Slack about task warning.
+
+    :param context: The execution context
+    :type context: dict
+    :param slack_conn_id: Slack connection ID
+    :type slack_conn_id: str
+    :return: None
+
+    slack_conn_id: When specifying a custom connection for a specific channel,
+    ensure the the connection is created as follow:
+        - host: https://hooks.slack.com/services/
+        - password: <webhook_token> (rest of the hook url)
+    """
+    
+    slack_msg = """
+            :warning: Task Warning. 
+            *Task*: {task}  
+            *Dag*: {dag} 
+            *Configuration*: {conf}
+            *Message*: {message}
+            *Execution Time*: {exec_date}  
+            *Log Url*: {log_url} 
+            """.format(
+            task=context.get('task_instance').task_id,
+            dag=context.get('task_instance').dag_id,
+            ti=context.get('task_instance'),
+            conf=context.get('dag_run').conf,
+            exec_date=context.get('execution_date'),
+            log_url=context.get('task_instance').log_url,
+            message=message
+        )
+    
+    warning_alert = SlackWebhookOperator(
+        task_id='task_warning_slack_alert',
+        message=slack_msg,
+        slack_webhook_conn_id=slack_conn_id
+    )
+    return warning_alert.execute(context=context)
---
+++ packages/rosecape-airflow/rosecape_airflow-0.0.19.tar.gz/rosecape_airflow-0.0.19/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.19'
+DESCRIPTION = 'Airflow utilities'
+LONG_DESCRIPTION = 'A package that makes it easy to interact with AWS services from Airflow using PythonOperator'
+
+setup(
+    name="rosecape_airflow",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Nick Joanis",
+    author_email="nicholas@rosecape.ca",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'boto3==1.26.142'
+    ],
+    keywords='Airflow',
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
