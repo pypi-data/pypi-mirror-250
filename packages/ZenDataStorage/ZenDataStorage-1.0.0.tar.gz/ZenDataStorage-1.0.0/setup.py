6aa027acbee4b4ae2d5566c6f434b7241e0590ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 14:03:54 +0000

    Add package ZenDataStorage/1.0.0/ZenDataStorage-1.0.0.tar.gz

---
+++ packages/ZenDataStorage/ZenDataStorage-1.0.0.tar.gz/ZenDataStorage-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION: str = "A Python package for processing data from .zenf files."
+LONG_DESCRIPTION: str = "A Python package providing useful and important features to process data from .zenf files and to parse them to local python types."
+
+setup(
+    name="ZenDataStorage",
+    author="SpiralCreationsPyPi",
+    author_email="SpiralCreationsPyPi@proton.me",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=["colorama"],
+    keywords=[
+        "Data Storage",
+        "Storage",
+        "Data"
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: OS Independent"
+    ]
+)
