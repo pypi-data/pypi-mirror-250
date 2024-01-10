247f636bf71b39f2f759b6ba5f75c1f11e963fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 15:03:53 +0000

    Add package evox/0.6.3/evox-0.6.3.tar.gz

---
+++ packages/evox/evox-0.6.3.tar.gz/evox-0.6.3/src/evox/__init__.py
@@ -0,0 +1,7 @@
+from .core.algorithm import Algorithm
+from .core.module import *
+from .core.operator import Operator
+from .core.problem import Problem
+from .core.state import State
+
+from . import algorithms, monitors, operators, workflows, problems, utils
