3c3bcad90a351938a8aead7ee496482ef07f9ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 08:04:10 +0000

    Add package pyminerva/0.0.7/pyminerva-0.0.7-py3-none-any.whl

---
+++ packages/pyminerva/pyminerva-0.0.7-py3-none-any.whl/pyminerva/__init__.py
@@ -0,0 +1,11 @@
+# Copyright 2023-2025 Jeongmin Kang, jarvisNim @ GitHub
+# See LICENSE for details.
+
+__author__ = "jarvisNim in GitHub"
+__version__ = "0.0.5"
+
+
+from .techs import (
+    analyse_techs,
+    analyse_DrawDown,
+)
