f47d13ca91971323dc1714e11204c6e554708a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 06:04:24 +0000

    Add package npyosmium/3.6.3/npyosmium-3.6.3-cp39-cp39-macosx_14_0_arm64.whl

---
+++ packages/npyosmium/npyosmium-3.6.3-cp39-cp39-macosx_14_0_arm64.whl/npyosmium/__init__.py
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# This file is part of pyosmium. (https://osmcode.org/pyosmium/)
+#
+# Copyright (C) 2023 Sarah Hoffmann <lonvia@denofr.de> and others.
+# For a full list of authors see the git log.
+
+from npyosmium._osmium import *
+from npyosmium.helper import *
+import npyosmium.io
+import npyosmium.osm
+import npyosmium.index
+import npyosmium.geom
