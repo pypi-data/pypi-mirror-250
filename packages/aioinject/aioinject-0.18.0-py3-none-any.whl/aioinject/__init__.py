8bc9dba53912f43e18ed9d03ce610980c1523455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 04:04:24 +0000

    Add package aioinject/0.18.0/aioinject-0.18.0-py3-none-any.whl

---
+++ packages/aioinject/aioinject-0.18.0-py3-none-any.whl/aioinject/__init__.py
@@ -0,0 +1,21 @@
+from aioinject.containers import Container
+from aioinject.context import InjectionContext, SyncInjectionContext
+from aioinject.decorators import inject
+from aioinject.markers import Inject
+from aioinject.providers import Callable, Factory, Object, Provider, Singleton
+
+
+__all__ = [
+    "Callable",
+    "Container",
+    "Factory",
+    "Inject",
+    "InjectionContext",
+    "Object",
+    "Provider",
+    "Singleton",
+    "SyncInjectionContext",
+    "inject",
+]
+
+__version__ = "0.18.0"
