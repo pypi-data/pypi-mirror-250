0992a808a7e601588496a9e866677531e0381f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 14:04:43 +0000

    Add package nbserv-client/1.5.0/nbserv_client-1.5.0-py3-none-any.whl

---
+++ packages/nbserv-client/nbserv_client-1.5.0-py3-none-any.whl/nbserv_client/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from nbserv_client.api.api_spec_api import ApiSpecApi
+from nbserv_client.api.config_api import ConfigApi
+from nbserv_client.api.contents_api import ContentsApi
+from nbserv_client.api.default_api import DefaultApi
+from nbserv_client.api.identity_api import IdentityApi
+from nbserv_client.api.kernels_api import KernelsApi
+from nbserv_client.api.kernelspecs_api import KernelspecsApi
+from nbserv_client.api.sessions_api import SessionsApi
+from nbserv_client.api.status_api import StatusApi
+from nbserv_client.api.terminals_api import TerminalsApi
+
