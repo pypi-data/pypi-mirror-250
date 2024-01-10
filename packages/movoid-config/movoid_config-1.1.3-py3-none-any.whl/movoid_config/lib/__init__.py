0f2308b96424cb24ee9795df97b63b209b85d74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 08:04:18 +0000

    Add package movoid-config/1.1.3/movoid_config-1.1.3-py3-none-any.whl

---
+++ packages/movoid-config/movoid_config-1.1.3-py3-none-any.whl/movoid_config/lib/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__.py
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/1/1 12:27
+# Description   : 
+"""
+from .config import Config
+if __name__ == '__main__':
+    pass
