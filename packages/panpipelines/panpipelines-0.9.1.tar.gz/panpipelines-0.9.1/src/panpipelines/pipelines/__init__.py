eece8ff5d528b324e0047790fdfd78b5179ab801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 14:04:49 +0000

    Add package panpipelines/0.9.1/panpipelines-0.9.1.tar.gz

---
+++ packages/panpipelines/panpipelines-0.9.1.tar.gz/panpipelines-0.9.1/src/panpipelines/pipelines/__init__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    "aslprep_panpipeline",
+    "basil_panpipeline",
+    "fmriprep_panpipeline",
+    "freesurfer_panpipeline",
+    "qsiprep_panpipeline",
+    "tensor_panpipeline",
+    "noddi_panpipeline",
+    "panpipeline",
+    "dummy_panpipeline",
+    "volmeasures_panpipeline",
+    "textmeasures_panpipeline",
+    "collatecsv_panpipeline",
+    "collatecsvgroup_panpipeline",
+    "lst_panpipeline"
+]
---
+++ packages/panpipelines/panpipelines-0.9.1.tar.gz/panpipelines-0.9.1/src/panpipelines/workflows/__init__.py
@@ -0,0 +1,15 @@
+__all__ = [
+    "qsiprep_workflow",
+    "tensor_workflow",
+    "noddi_workflow",
+    "freesurfer_workflow",
+    "fmriprep_workflow",
+    "basil_workflow",
+    "aslprep_workflow",
+    "dummy_workflow",
+    "volmeasures_workflow",
+    "textmeasures_workflow",
+    "collatecsv_workflow",
+    "collatecsvgroup_workflow",
+    "lst_workflow"
+]
