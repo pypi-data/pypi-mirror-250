2cffe452c031bf730fbd8d2a371c774e98681393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:04:46 +0000

    Add package session-repository/0.4.5/session-repository-0.4.5.tar.gz

---
+++ packages/session-repository/session-repository-0.4.5.tar.gz/session-repository-0.4.5/session_repository/__init__.py
@@ -0,0 +1,4 @@
+from session_repository.decorators import with_session
+from session_repository.models.repository import SessionRepository
+from session_repository.models.service import SessionService
+from session_repository.enum import Operators, LoadingTechnique
---
+++ packages/session-repository/session-repository-0.4.5.tar.gz/session-repository-0.4.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+version = "0.4.5"
+
+setup(
+    name="session-repository",
+    version=version,
+    packages=[
+        "session_repository",
+        "session_repository.models",
+    ],
+    install_requires=[
+        "sqlalchemy",
+    ],
+    license="MIT",
+    author="Maxime MARTIN",
+    author_email="maxime.martin02@hotmail.fr",
+    description="A project to have a base repository class to perform select/insert/update/delete with dynamic syntax",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Impro02/session-repository",
+    download_url="https://github.com/Impro02/session-repository/archive/refs/tags/%s.tar.gz"
+    % version,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
