e49a9cd307711c3ae317d92331b7b431481116ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 14:04:04 +0000

    Add package pyBrainGenixClient/2024.1.9.5/pyBrainGenixClient-2024.1.9.5.tar.gz

---
+++ packages/pyBrainGenixClient/pyBrainGenixClient-2024.1.9.5.tar.gz/pyBrainGenixClient-2024.1.9.5/BrainGenix/NES/Models/Connections/Staple/Staple.py
@@ -0,0 +1,26 @@
+# BrainGenix-NES
+# AGPLv3
+
+import json
+
+from . import Configuration
+
+from BrainGenix.NES.Client import RequestHandler
+
+import BrainGenix.LibUtils.GetID
+
+
+class Staple:
+
+    def __init__(self, _Configuration:Configuration, _RequestHandler:RequestHandler, _SimulationID:int):
+        # Create Attributes
+        self.Name =  _Configuration.Name
+        self.RequestHandler = _RequestHandler
+        
+
+        # Create Box On Server
+        SourceCompartmentID = BrainGenix.LibUtils.GetID.GetID(_Configuration.SourceCompartment.ID)
+        DestinationCompartmentID = BrainGenix.LibUtils.GetID.GetID(_Configuration.DestinationCompartment.ID)
+        Response = self.RequestHandler.MakeAuthenticatedQuery(f"/NES/Connection/Staple/Create?SimulationID={_SimulationID}&Name={_Configuration.Name}&SourceCompartmentID={SourceCompartmentID}&DestinationCompartmentID={DestinationCompartmentID}")
+        assert(Response != None)
+        self.ID = Response["CompartmentID"]
