052b09fde9f9f44b957510d6f77f3347c4082b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 06:04:10 +0000

    Add package pyobject/1.2.2/pyobject-1.2.2-py3-none-any.whl

---
+++ packages/pyobject/pyobject-1.2.2-py3-none-any.whl/pyobj_extension.py
@@ -0,0 +1,11 @@
+﻿print("""请安装Python 3.6/3.7/3.8/3.9/3.10或更高版本, 并安装pyobject库, 再导入此pyobj_extension模块。
+如果没有安装这些版本的Python, 也可以安装Visual Studio 2019并勾选C++开发, 再从pypi官网下载tar.gz格式的源文件并用pip安装。
+但是, pyobj_extension是可选组件, 此模块的缺失, 并不会影响pyobject库其他模块的正常工作。
+
+如有疑问, 请联系作者 CSDN: https://blog.csdn.net/qfcy_ 或QQ: 3076711200。
+
+Error:Please install Python 3.6/3.7/3.8/3.9/3.10 or higher versions, and then you're available to use the module pyobj_extension.
+If you don't have these versions of Python installed,install visual studio 2019 with C++ feature checked instead, then install the source file in tar.gz format downloaded from pypi.org with PIP.
+However, pyobj_extension is an optional component. The absence of this module will not affect the function of other modules in the pyobject library.
+
+If you have any questions, please contact the author's CSDN: https://blog.csdn.net/qfcy_ Or QQ: 3076711200.""")
