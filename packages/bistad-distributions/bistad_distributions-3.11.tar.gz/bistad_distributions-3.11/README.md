ac9acba3ead4612fd2224eded912f32902286957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 06:04:30 +0000

    Add package bistad-distributions/3.11/bistad_distributions-3.11.tar.gz

---
+++ packages/bistad-distributions/bistad_distributions-3.11.tar.gz/bistad_distributions-3.11/README.md
@@ -0,0 +1,9 @@
+# bistad-distributions package
+
+This is a simple project.
+
+# Files
+
+Explanation of files in the package
+
+# Installations
---
+++ packages/bistad-distributions/bistad_distributions-3.11.tar.gz/bistad_distributions-3.11/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bistad_distributions',
+      version='3.11',
+      description='Gaussian and Binomial Distributions',
+      author='Dinesh Bista',
+      author_email = 'bistad57@gmail.com',
+      packages=['bistad_distributions'],
+      zip_safe=False)
