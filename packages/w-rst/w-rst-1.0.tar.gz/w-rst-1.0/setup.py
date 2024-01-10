ee2dd2cf81ca8e227f561b7a96fe7071d1b48ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:05:04 +0000

    Add package w-rst/1.0/w-rst-1.0.tar.gz

---
+++ packages/w-rst/w-rst-1.0.tar.gz/w-rst-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="w-rst",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        "datetime",
+    ],
+    author="woi",
+    author_email="willooi@qq.com",
+    description="A useless output model",
+    license="MIT",
+    keywords="wpgs",
+    url="https://github.com/mkitpro/wpgs.git"
+)
