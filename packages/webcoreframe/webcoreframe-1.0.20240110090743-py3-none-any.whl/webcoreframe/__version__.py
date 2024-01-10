6c096b3602112189b4f156ab623abad810336783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 14:04:10 +0000

    Add package webcoreframe/1.0.20240110090743/webcoreframe-1.0.20240110090743-py3-none-any.whl

---
+++ packages/webcoreframe/webcoreframe-1.0.20240110090743-py3-none-any.whl/webcoreframe/__version__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# code build user: bean
+# code build date: 2024-01-10 09:08:11
+# code release date: 2024-01-10 09:07:43
+# code commit hash: 89cd1aa9df64131f1ff3e3102e82ae483e1b78a5
+
+
+VERSION = "1.0.20240110090743"
+
---
+++ packages/webcoreframe/webcoreframe-1.0.20240110090743-py3-none-any.whl/webcoreframe/contrib/admin/static/pageUtils-97ff7292.js
@@ -0,0 +1,1 @@
+const a=20;function o(e){return Math.floor(e/20)+(e%20!==0?1:0)}export{a as P,o as a};
