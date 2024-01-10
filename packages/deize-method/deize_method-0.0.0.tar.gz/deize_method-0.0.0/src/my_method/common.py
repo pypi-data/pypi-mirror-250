2660e044f93f2ac177f04ac1d95742e6bc98c11c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:03:57 +0000

    Add package deize-method/0.0.0/deize_method-0.0.0.tar.gz

---
+++ packages/deize-method/deize_method-0.0.0.tar.gz/deize_method-0.0.0/src/my_method/common.py
@@ -0,0 +1,8 @@
+import os
+import re
+import subprocess
+import time
+from datetime import datetime
+from typing import Final, Union
+
+__all__ = ["os", "re", "datetime", "Final", "Union", "subprocess", "time"]
