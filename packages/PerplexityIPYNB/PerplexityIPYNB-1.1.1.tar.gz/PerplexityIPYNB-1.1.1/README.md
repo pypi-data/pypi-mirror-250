5ebc1b170fca24814214f0ebd0268d84cb507298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:03:42 +0000

    Add package PerplexityIPYNB/1.1.1/PerplexityIPYNB-1.1.1.tar.gz

---
+++ packages/PerplexityIPYNB/PerplexityIPYNB-1.1.1.tar.gz/PerplexityIPYNB-1.1.1/README.md
@@ -0,0 +1,31 @@
+# PerplexityIPYNB
+
+PerplexityIPYNB is a Python module that allows you to interact with the Perplexity AI.
+
+## Installation
+
+You can install PerplexityIPYNB using pip. Run the following command in your terminal:
+
+```bash
+pip install PerplexityIPYNB
+```
+
+## Usage
+
+### Functional
+
+```python
+from PerplexityIPYNB import Perplexity
+
+ai = Perplexity()
+prompt = input()
+
+print(ai.RUN(prompt))
+```
+
+### Magic commands
+
+```python
+%%perplexity
+prompt
+```
---
+++ packages/PerplexityIPYNB/PerplexityIPYNB-1.1.1.tar.gz/PerplexityIPYNB-1.1.1/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    README = f.read()
+
+setup(
+    name="PerplexityIPYNB",
+    version="1.1.1",
+    description="Perplexity AI in iPython Notebooks",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vladimirkucin634/PerplexityIPYNB",
+    author="vladimirkucin634",
+    author_email="vladimirkucin634@mail.ru",
+    license="MIT",
+    keywords=[
+        "artificial-intelligence",
+        "perplexityai",
+        "perplexity",
+        "python",
+        "api",
+        "ai",
+    ],
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    packages=find_packages(exclude=[".github"]),
+    install_requires=[
+        "requests",
+        "websocket-client==1.6.2",
+    ],
+    project_urls={
+        "Source": "https://github.com/vladimirkucin634/PerplexityIPYNB",
+    },
+)
