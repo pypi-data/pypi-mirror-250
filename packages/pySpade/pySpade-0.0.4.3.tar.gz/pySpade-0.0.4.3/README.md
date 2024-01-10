384fb6d54ee8718b32cd98dd438c8e4c5cbddae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 14:03:59 +0000

    Add package pySpade/0.0.4.3/pySpade-0.0.4.3.tar.gz

---
+++ packages/pySpade/pySpade-0.0.4.3.tar.gz/pySpade-0.0.4.3/README.md
@@ -0,0 +1,88 @@
+# pySpade: Single cell Perturbations - Analysis of Differential gene Expression
+
+## Overview
+_________
+`pySpade` is a user friendly tool to perform the whole transcriptome analysis of single cell perturbation dataset. With the direct output of Cellranger, `pySpade` utilizes hypergeomtric test to analyze the whole transcriptome differential expression and generates hits table csv file. User can use the table to do downstream processing like generating Manhattan plots (tutorial includes). Currently we support human genome.     
+
+## Requirement
+_________
+* Python (3.7 +)
+* Numpy (1.21 +)
+* Pandas (1.3.5 +)
+* Scipy (1.6.2 +)
+
+## Installation
+________
+`pySpade` can be installed with `pip`
+
+```shell
+pip install pySpade
+```
+
+## Usage
+________
+```
+$pySpade
+usage: pySpade [-h]  ...
+
+pySpade 
+Version: 0.0.4.3
+
+optional arguments:
+  -h, --help  show this help message and exit
+
+functions:
+  
+    process   process mapping output and reformat for downstream analysis.
+    explevel  check the average expression level of query genes in single cell matrix
+    fc        check the fold change of sgrna
+    DEobs     perform differential expression analysis of observed cells
+    DErand    perform differential expression analysis of random selection background
+    local     perform local hit analysis with observation data and random background
+    global    perform global hit analysis with observation data and random background
+    
+```
+
+* `process` : Process transcriptome output and sgrna output to remove experimental doublets and sgrna outlier cells.
+    * Input 1: Transcriptome matrix is from Cellranger output (outs folder).
+    * Input 2: sgrna matrix column: cell barcodes consistent with transcriptome matrix, rows: sgrna sequence. The sgrna matrix is already filtered out potential noise sgrna. Acceptable format: pkl and csv.
+    * The final output format is h5 file. The final output can be compressed to save disk space, but it may take more time to write the final output file.
+
+
+* `explevel` : Check the average expression level of query genes in single cell matrix.
+    * Input 1: processed transcriptome matrix from the `process` output.
+    * Input 2: Query genes list has to be txt file, genes are seperated with new line.
+
+
+* `fc` : Check the fold change of perturbed region and individual sgRNA for query region and gene. Good for test if positive controls work. P-value are calculated with Student's t-test.
+    * Input 1: processed transcriptome and sgrna matrix from the `process` output
+    * Inout 2: sgrna dict file (perturbation region hg38 coordinates and the sgrna name targeting that region. Region and sgrnas separated by tab, and sgrnas separated by comma. The sgrna name must match the index of sgrna matrix.)
+    * Example:
+	* chr1:1234567-1235067    sg1;sg2;sg3;sg4;sg5
+	* chr2:1234567-1235067    sg6;sg7;sg8;sg9;sg10
+    * Input 3: Query file, the query region and query test, separate by tab.
+    * Example:
+	* chr1:1234567-1235067    GENE1
+	* chr2:1234567-1235067    GENE2
+
+
+* `DEobs` : Perfrom the genome wide differential expression analysis of all the perturbation regions.
+    * Input 1: processed transcriptome and sgrna matrix from the `process` output
+    * Input 2: sgrna dict file (perturbation region hg38 coordinates and the sgrna sequence targeting that region. Region and sgrnas separated by tab, and  sgrnas separated by comma. The sgrna name must match the index of sgrna matrix).
+    * Output files: up regulation p-value, downregulation p-value, fold change(compare with all the other cells) and average cpm.
+
+
+* `DErand` : Perfrom the genome wide differential expression analysis of 1000 random selection cells.
+    * There are two options for random selection: all cells with equal probability or probability based on sgrna number in the cells. User should specify the cell number to select randomly. It is recommended with either exact cell number or bins (with large amount of perturbation experiment in order to reduce computational overhead).
+
+
+* `local` : Using the observation p value and randomization bavckground p value to calculate the adjusted p value based on Gaussian distribution approximation. Local hits calculation includes the genes within plus and minus 2 Mb of the perturbation region. The output is a csv file with all hits information.
+
+
+* `global` : Using the observation p value and randomization background p value to calculate the adjusted p value based on Gaussian distribution approximation. The output is a csv file with all hits information. 
+
+
+## Contacts
+_______
+* Yihan Wang `Yihan.Wang@UTSouthwestern.edu`
+* Gary Hon `Gary.Hon@UTSouthwestern.edu`
---
+++ packages/pySpade/pySpade-0.0.4.3.tar.gz/pySpade-0.0.4.3/pySpade/local.py
@@ -0,0 +1,165 @@
+#local.py
+
+import os
+import re
+import sys
+import collections
+import argparse
+from threading import local
+import tables
+import itertools 
+import scipy
+import csv
+import glob
+
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import scipy.sparse as sp_sparse
+import scipy.io as sio
+
+from collections import defaultdict
+from scipy import sparse
+from scipy.sparse import csr_matrix
+
+from pySpade.utils import get_logger, read_annot_df, get_neighbor_genes, get_distance, read_sgrna_dict
+
+logger = get_logger(logger_name=__name__)
+
+def local_analysis(FILE_DIR,
+                    OBS_DIR,
+                    DISTRI_DIR,
+                    SGRNA_DICT,
+                    OUTPUT_DF):
+    
+    logger.info('Loading files.')
+
+    #read the gene sequence file 
+    gene_seq = np.load(FILE_DIR + 'Trans_genome_seq.npy', allow_pickle=True)
+    if len(gene_seq) != len(set(gene_seq)):
+        logger.critical('Duplication of mapping genes. Duplicates are removed in the analysis.')
+    unique_elements, counts = np.unique(gene_seq, return_counts=True)
+    duplicate_elements = unique_elements[counts > 1]
+ 
+        
+    #read the plotting annotation
+    annot_df_dup = read_annot_df()
+    #There are many non-coding genes duplication in the annot_df, only keep one.
+    annot_df = annot_df_dup.drop_duplicates(subset='gene_names', keep='first')
+    
+    #Load sgRNA dict: All regions 
+    sgrna_dict  = read_sgrna_dict(SGRNA_DICT)
+
+    #read all the perturbation files
+    pval_files = glob.glob(OBS_DIR + '*-down_log-pval')
+
+    df_column_list = [
+        'gene_names', 'chromosome', 'pos', 'strand', 
+        'color_idx', 'chr_idx', 'region', 'distance', 'num_cell', 'bin', 
+        'log(pval)-hypergeom', 'fc', 'log(padj)-Gaussian', 'fc_by_rand_dist_cpm', 'pval-empirical', 'cpm_perturb', 'cpm_bg']
+    local_gene_df = pd.DataFrame(columns=df_column_list)
+
+    #Read the background distribution file
+    Num = [int(i.split('/')[-1].split('.')[0].split('-')[-1]) for i in glob.glob(DISTRI_DIR+ '/Down_dist_mean-*')]
+    if len([int(i.split('/')[-1].split('.')[0].split('-')[-1]) for i in glob.glob(DISTRI_DIR+ '/Down_dist_mean-*')]) != \
+        len([int(i.split('/')[-1].split('.')[0].split('-')[-1]) for i in glob.glob(DISTRI_DIR+ '/Up_dist_mean-*')]):
+        logger.critical('Background distribution files error!')
+        sys.exit(0)
+
+    #start calculating local hits for each perturbation region
+    logger.info('Start analysis of ' + str(len(pval_files)) + ' regions.')
+    for region in list(sgrna_dict.keys()):
+        logger.info(f'  Processing region: {region}')
+        if region.startswith('chr') == False:   
+            logger.info('No chromosome coordination in this region, cannot compute local analysis.')
+            continue
+
+        cpm_file = OBS_DIR + region + '-cpm'
+        cpm = sio.loadmat(cpm_file)['matrix'][0]        
+        fc_files = glob.glob(OBS_DIR + region + '*-foldchange')
+        if len(fc_files) == 1:
+            fc_file = fc_files[0]
+            cell_num = int(fc_file.split('/')[-1].split('-')[-2])
+        else:    
+            numbers = np.array([int(float(i.split('/')[-1].split('-')[2])) for i in fc_files])
+            chosen_num = np.max(numbers)
+            cell_num = int(chosen_num)
+            fc_file = OBS_DIR + region + '-' + str(chosen_num) + '-foldchange'
+        
+        fc = sio.loadmat(fc_file)['matrix'][0]  #compare to all the other cells as background
+
+        #get the gene idx within local analysis window and filter with fold change 
+        local_gene = get_neighbor_genes(region, 2e6, annot_df)
+        fc_cutoff = 0.01
+        pval_cutoff = 0
+
+        #read the pval matrix and process
+        pval = sio.loadmat(OBS_DIR + region + '-down_log-pval')['matrix'][0] #raw hypergeom p value
+        pval[np.argwhere(pval == 0)] = 1
+        pval[np.isinf(pval)] = 0
+        pval[np.isnan(pval)] = 0
+
+        #Calculate the overlap genes with annot_df, and only save the information on those genes.
+        down_i = np.where(np.array(fc) < (1 - fc_cutoff))[0]
+        down_idx = np.array(list(set(down_i).intersection(set(np.where(pval < pval_cutoff)[0]))))
+        ##Remove the duplicate value of gene index, return a list with unique gene names. 
+        ##Unique_elements is sorted by the gene names, unique_indices return the original index from gene_seq.
+        unique_elements, unique_indices = np.unique(gene_seq, return_index=True)
+        down_keep_genes = list((set(annot_df['gene_names']).intersection(set(gene_seq[down_idx]))).intersection(set(local_gene)))
+        down_keep_genes_idx = sorted(list(unique_indices[np.where(np.isin(unique_elements, down_keep_genes))[0]]))
+        #Make this true: gene_seq[down_keep_genes_idx] == down_keep_genes
+
+        if len(down_keep_genes_idx) ==0:
+            logger.info(f'  No down-regulation genes within local analysis windown. ')
+            continue
+        
+        #Calculate the adjusted pval with closet cell number distribution
+        chosen_dist = Num[np.argmin([np.absolute(n - cell_num) for n in Num])]
+        
+        #Load the file
+        down_mean = np.load(DISTRI_DIR + 'Down_dist_mean-%s.npy'%(str(chosen_dist)))
+        down_std = np.load(DISTRI_DIR + 'Down_dist_std-%s.npy'%(str(chosen_dist)))
+        cpm_mean = np.load(DISTRI_DIR + 'Cpm_mean-%s.npy'%(str(chosen_dist)))
+        fc_rand = cpm/cpm_mean
+        
+        #Calculate adjusted p value of Gaussian padj
+        down_zscore_list = (pval[down_keep_genes_idx] - down_mean[down_keep_genes_idx]) / down_std[down_keep_genes_idx]
+        down_padj_list = scipy.stats.norm.logsf(abs(down_zscore_list))
+        hits_fc = fc[down_keep_genes_idx]
+        hits_fc_rand = fc_rand[down_keep_genes_idx]
+
+        #Load pvalue matrix and calculate empirical p-value
+        rand_down_file = sio.loadmat(DISTRI_DIR + '%s-down_log-pval'%(str(chosen_dist)))
+        rand_down_matrix = []
+        rand_down_matrix = sp_sparse.vstack(rand_down_file['matrix'])
+        iter_num, gene_num = rand_down_matrix.shape
+        emp_pval = np.sum(np.asarray(rand_down_matrix.tocsr()[:, down_keep_genes_idx].todense()) < pval[down_keep_genes_idx], axis=0) / iter_num
+
+        #Save to csv file 
+        local_gene_series = annot_df[annot_df['gene_names'].isin(down_keep_genes)].set_index('idx').sort_index()
+        local_gene_series['region'] = region
+        dist_list = []
+        for i in local_gene_series['pos']:
+            dist = get_distance(region, i)
+            dist_list.append(dist)
+        local_gene_series['distance'] = dist_list
+        local_gene_series['num_cell'] = cell_num
+        local_gene_series['bin'] = chosen_dist
+        local_gene_series['log(pval)-hypergeom'] = pval[down_keep_genes_idx] 
+        local_gene_series['fc'] = hits_fc
+        local_gene_series['log(padj)-Gaussian'] = down_padj_list
+        local_gene_series['fc_by_rand_dist_cpm'] = hits_fc_rand
+        local_gene_series['pval-empirical'] = emp_pval
+        local_gene_series['cpm_perturb'] = cpm[down_keep_genes_idx]
+        local_gene_series['cpm_bg'] = cpm_mean[down_keep_genes_idx]
+        #local_gene_df = local_gene_df.append(local_gene_series)
+        local_gene_df = pd.concat([local_gene_df, local_gene_series], ignore_index=True)
+        local_gene_df = local_gene_df.reindex(columns=df_column_list)
+
+    if OUTPUT_DF.endswith('.csv'):
+        local_gene_df[~local_gene_df['gene_names'].isin(duplicate_elements)].to_csv(OUTPUT_DF)
+    else:
+        local_gene_df[~local_gene_df['gene_names'].isin(duplicate_elements)].to_csv(OUTPUT_DF + '.csv')
+
+if __name__ == '__main__':
+    pass
