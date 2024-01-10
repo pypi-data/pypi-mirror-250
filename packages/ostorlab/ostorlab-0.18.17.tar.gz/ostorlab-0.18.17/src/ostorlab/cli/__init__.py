0593edab8e7c3384afd3f602c98e144604d079a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 15:04:00 +0000

    Add package ostorlab/0.18.17/ostorlab-0.18.17.tar.gz

---
+++ packages/ostorlab/ostorlab-0.18.17.tar.gz/ostorlab-0.18.17/src/ostorlab/cli/__init__.py
@@ -0,0 +1,8 @@
+"""Ostorlab cli package."""
+from ostorlab.cli import scan
+from ostorlab.cli import auth
+from ostorlab.cli import agent
+from ostorlab.cli import vulnz
+from ostorlab.cli import agentgroup
+from ostorlab.cli import ci_scan
+from ostorlab.cli import scanner
