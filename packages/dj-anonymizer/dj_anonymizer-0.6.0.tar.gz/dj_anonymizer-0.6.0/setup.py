c7986802ed0bfc6997516144bdd9c82f3e44c14d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 06:04:14 +0000

    Add package dj-anonymizer/0.6.0/dj_anonymizer-0.6.0.tar.gz

---
+++ packages/dj-anonymizer/dj_anonymizer-0.6.0.tar.gz/dj_anonymizer-0.6.0/setup.py
@@ -0,0 +1,48 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+setup(
+    name='dj_anonymizer',
+    packages=['dj_anonymizer'],
+    include_package_data=True,
+    version='0.6.0',
+    description='This project helps anonymize production database '
+                + 'with fake data of any kind.',
+    long_description=(read('README.md')),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author='Tim Pagurets',
+    url='https://github.com/preply/dj_anonymizer',
+    keywords=['django', 'data', 'database', 'anonymization', 'dj-anonymizer'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+        'Topic :: Database'
+    ],
+    install_requires=[
+        'Django>=3.2',
+    ]
+)
