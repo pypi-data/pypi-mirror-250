af384c18b242b444eb995ff904094ccbbd97d127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 14:04:46 +0000

    Add package thirdai/0.7.27/thirdai-0.7.27-cp38-cp38-win_amd64.whl

---
+++ packages/thirdai/thirdai-0.7.27-cp38-cp38-win_amd64.whl/thirdai/neural_db/__init__.py
@@ -0,0 +1,26 @@
+try:
+    from . import parsing_utils
+    from .constraint_matcher import AnyOf, EqualTo, GreaterThan, InRange, LessThan
+    from .documents import (
+        CSV,
+        DOCX,
+        PDF,
+        URL,
+        Document,
+        InMemoryText,
+        Reference,
+        SalesForce,
+        SentenceLevelDOCX,
+        SentenceLevelPDF,
+        SharePoint,
+        SQLDatabase,
+        Unstructured,
+    )
+    from .model_bazaar import Bazaar, ModelBazaar
+    from .neural_db import CancelState, NeuralDB, Strength, Sup
+
+except ImportError as error:
+    raise ImportError(
+        "To use thirdai.neural_db, please install the additional dependencies by"
+        " running 'pip install thirdai[neural_db]'"
+    )
