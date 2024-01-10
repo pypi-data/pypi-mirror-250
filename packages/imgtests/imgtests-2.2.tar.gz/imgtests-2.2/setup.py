331b232babd0f849b9820f0dfd656fcfa97c1d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:04:33 +0000

    Add package imgtests/2.2/imgtests-2.2.tar.gz

---
+++ packages/imgtests/imgtests-2.2.tar.gz/imgtests-2.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='imgtests',
+      version='2.2',
+      description='none',
+      packages=['imgtests'],
+      author_email='antoha123_3@mail.ru',
+      zip_safe=False)
