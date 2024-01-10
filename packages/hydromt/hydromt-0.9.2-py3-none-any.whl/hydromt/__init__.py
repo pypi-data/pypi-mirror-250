d068fc220070d72a8ab7afe49e6016e133cb8fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 08:03:58 +0000

    Add package hydromt/0.9.2/hydromt-0.9.2-py3-none-any.whl

---
+++ packages/hydromt/hydromt-0.9.2-py3-none-any.whl/hydromt/__init__.py
@@ -0,0 +1,22 @@
+"""HydroMT: Automated and reproducible model building and analysis."""
+
+# version number without 'v' at start
+__version__ = "0.9.2"
+
+# pkg_resource deprication warnings originate from dependencies
+# so silence them for now
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+# required for accessor style documentation
+from xarray import DataArray, Dataset
+
+# submodules
+from . import cli, flw, raster, stats, vector, workflows
+from .data_catalog import *
+from .io import *
+
+# high-level methods
+from .models import *
