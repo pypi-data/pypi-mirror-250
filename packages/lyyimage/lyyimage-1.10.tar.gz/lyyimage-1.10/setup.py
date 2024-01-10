123e3111e8d875fec126eb1c239ef99307ca8379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 14:04:38 +0000

    Add package lyyimage/1.10/lyyimage-1.10.tar.gz

---
+++ packages/lyyimage/lyyimage-1.10.tar.gz/lyyimage-1.10/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lyyimage',
+    version='1.10',
+    author='lyy',
+    author_email='',
+    description='lyyimagetools for lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+
+
+    ],
+)
