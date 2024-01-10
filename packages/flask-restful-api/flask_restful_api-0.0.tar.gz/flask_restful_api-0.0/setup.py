654ebf825f39113a2bf491dc9eafa1c59fcb4b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 06:04:11 +0000

    Add package flask-restful-api/0.0/flask_restful_api-0.0.tar.gz

---
+++ packages/flask-restful-api/flask_restful_api-0.0.tar.gz/flask_restful_api-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='flask_restful_api',
+      version='0.0',
+      description='Make restful api simple and fast',
+      packages=['flask_restful_api'],
+      author_email='glebisi@yandex.ru',
+      zip_safe=False)
