6198978c4e918fa03e26768e1438946884ff4382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:03:57 +0000

    Add package PermutiveAPI/3.4.5/PermutiveAPI-3.4.5.tar.gz

---
+++ packages/PermutiveAPI/PermutiveAPI-3.4.5.tar.gz/PermutiveAPI-3.4.5/PermutiveAPI/__init__.py
@@ -0,0 +1,9 @@
+from .Cohort import Cohort
+from .Import import Import
+from .Segment import Segment
+from .User import User
+from .Workspace import Workspace, WorkspaceList
+from .Query import Query, QueryList
+
+__all__ = ["Cohort", "Import", "Segment", "User",
+           "Workspace", "WorkspaceList", "Query", "QueryList"]
