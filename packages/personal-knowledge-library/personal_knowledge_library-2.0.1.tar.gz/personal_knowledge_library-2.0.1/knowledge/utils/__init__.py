51b1502f1549236dd6e676b7b78f22d7328dd008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 09:03:32 +0000

    Add package personal-knowledge-library/2.0.1/personal_knowledge_library-2.0.1.tar.gz

---
+++ packages/personal-knowledge-library/personal_knowledge_library-2.0.1.tar.gz/personal_knowledge_library-2.0.1/knowledge/utils/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2024 Wacom. All rights reserved.
+""""Utilities"""
+
+__all__ = ['wikipedia', 'graph']
+
+
+from knowledge.utils import wikipedia
+from knowledge.utils import graph
