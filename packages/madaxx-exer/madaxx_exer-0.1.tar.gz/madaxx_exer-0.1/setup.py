99dfdb22c6dc851d850731c5bb39b489de2af255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 07:05:28 +0000

    Add package madaxx-exer/0.1/madaxx_exer-0.1.tar.gz

---
+++ packages/madaxx-exer/madaxx_exer-0.1.tar.gz/madaxx_exer-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='madaxx_exer',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = "Madaki Fatsen",
+      author_email = "madakifatsen@gmail.com",
+      zip_safe=False)
