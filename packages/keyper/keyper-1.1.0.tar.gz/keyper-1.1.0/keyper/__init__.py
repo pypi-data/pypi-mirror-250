f600b97cb4a51f89a43717d4f07bd89c901a9526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 07:04:56 +0000

    Add package keyper/1.1.0/keyper-1.1.0.tar.gz

---
+++ packages/keyper/keyper-1.1.0.tar.gz/keyper-1.1.0/keyper/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""A utility for dealing with the macOS keychain."""
+
+import platform
+
+from .certificate import Certificate
+from .keychain import Keychain, get_password, set_password, delete_password
+from .temporary_keychain import TemporaryKeychain
+from .exceptions import KeyperException
+from .keychain_certificate import KeychainCertificate, load_all_certificates
+
+
+if platform.system() != "Darwin":
+    raise KeyperException("This tool is only supported on macOS")
+
+if tuple(map(int, platform.mac_ver()[0].split("."))) < (10, 13):
+    raise KeyperException("This tool is only supported on macOS 10.13.0 or higher")
