bc8b21d570c6ec4f7de1a37d99895eab79db6aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 04:04:13 +0000

    Add package preloop-sdk/0.2.21/preloop_sdk-0.2.21.tar.gz

---
+++ packages/preloop-sdk/preloop_sdk-0.2.21.tar.gz/preloop_sdk-0.2.21/preloop/sdk/__init__.py
@@ -0,0 +1,30 @@
+from preloop.sdk.inception.constructs import datasources, feature
+from preloop.sdk.inception.models import AuthParamsPostgres, ConnectionParamsPostgres, PostgresDatasource
+from preloop.sdk.public_api_stubs.exceptions import PreloopError
+from preloop.sdk.public_api_stubs.models import (
+    CreationMethod,
+    DatasourceIdentifierField,
+    DeleteDatasourceRequest,
+    DeleteDatasourceResult,
+    DeleteFeatureRequest,
+    DeleteFeatureResult,
+    FeatureIdentifierField,
+    GetFeatureRequest,
+    ListDatasourcesRequest,
+    ListDatasourcesResult,
+    ListFeatureExecutionsRequest,
+    ListFeatureExecutionsResult,
+    ListFeaturesRequest,
+    ListFeaturesResult,
+    ModifiableDatasourceFields,
+    ModifiableFeatureFields,
+    ModifyDatasourceRequest,
+    ModifyDatasourceResult,
+    ModifyFeatureRequest,
+    ModifyFeatureResult,
+    TriggerFeatureExecutionRequest,
+    TriggerFeatureExecutionResult,
+    UploadFeatureScriptRequest,
+    UploadFeatureScriptResult,
+)
+from preloop.sdk.public_api_stubs.preloop_client import PreloopClient
