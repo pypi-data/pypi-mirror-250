7d771be83b72978f29bf43fd2133e847e1a5d45b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:05:04 +0000

    Add package cobyqa/1.0.0/cobyqa-1.0.0.tar.gz

---
+++ packages/cobyqa/cobyqa-1.0.0.tar.gz/cobyqa-1.0.0/cobyqa/utils/__init__.py
@@ -0,0 +1,5 @@
+from .exceptions import MaxEvalError, TargetSuccess, FeasibleSuccess
+from .math import get_arrays_tol, exact_1d_array
+from .versions import show_versions
+
+__all__ = ['MaxEvalError', 'TargetSuccess', 'FeasibleSuccess', 'get_arrays_tol', 'exact_1d_array', 'show_versions']
