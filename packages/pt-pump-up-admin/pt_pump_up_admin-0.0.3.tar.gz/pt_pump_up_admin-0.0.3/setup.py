f3aba41c309e929ccbd31abc7d4a2f509fb1c833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 06:04:21 +0000

    Add package pt-pump-up-admin/0.0.3/pt_pump_up_admin-0.0.3.tar.gz

---
+++ packages/pt-pump-up-admin/pt_pump_up_admin-0.0.3.tar.gz/pt_pump_up_admin-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pt_pump_up_admin',
+    version='0.0.3',
+    description='Administrative tools to interact with PT-Pump-Up Platform',
+    install_requires=[
+        'requests',
+        'mechanize',
+        'pandas',
+        'scikit-learn',
+        'numpy',
+        'huggingface_hub',
+        'mechanize',
+        'nltk',
+        'tqdm',
+    ],
+    packages=find_packages(),
+    author='Rúben Almeida'
+)
