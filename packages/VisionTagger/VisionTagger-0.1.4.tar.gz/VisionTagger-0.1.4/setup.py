68712284b1efda5594b38df779659e122d7f8d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 14:04:10 +0000

    Add package VisionTagger/0.1.4/VisionTagger-0.1.4.tar.gz

---
+++ packages/VisionTagger/VisionTagger-0.1.4.tar.gz/VisionTagger-0.1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="VisionTagger",
+    version="0.1.4",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "visiontagger": ["*.png"],
+    },
+    install_requires=[
+        "pillow",
+        "opencv-python",
+        "tk"
+    ],
+    entry_points={
+        'console_scripts': [
+            'visiontagger=visiontagger.main:main',
+        ],
+    },
+)
