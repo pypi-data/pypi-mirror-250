7196152068c6fa849622bef85b9eb5ee034adbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 08:04:21 +0000

    Add package django-permissions-auditor/1.2.0/django_permissions_auditor-1.2.0-py3-none-any.whl

---
+++ packages/django-permissions-auditor/django_permissions_auditor-1.2.0-py3-none-any.whl/permissions_auditor/apps.py
@@ -0,0 +1,19 @@
+from django.apps import AppConfig
+from django.core.cache import cache
+from django.utils.translation import gettext_lazy as _
+
+from permissions_auditor.core import _get_setting
+
+
+class PermissionsAuditorConfig(AppConfig):
+    name = 'permissions_auditor'
+    verbose_name = _('Permissions Auditor')
+
+    default_auto_field = 'django.db.models.BigAutoField'
+
+    def ready(self):
+        # Delete the cached views list on application reload.
+        cache.delete_many([
+            _get_setting('PERMISSIONS_AUDITOR_CACHE_KEY'),
+            _get_setting('PERMISSIONS_AUDITOR_CACHE_KEY') + '_BASE_URL',
+        ])
