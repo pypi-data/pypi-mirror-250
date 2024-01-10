391dc1c896b07abe676c4c62bb85df1bc049464f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 04:04:10 +0000

    Add package tempowork/0.1.2/tempowork-0.1.2.tar.gz

---
+++ packages/tempowork/tempowork-0.1.2.tar.gz/tempowork-0.1.2/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['tempowork']
+setup_kwargs = {
+    'name': 'tempowork',
+    'version': '0.1.2',
+    'description': 'A library for mining temporal networks where time is represented as a continuous dimension!',
+    'long_description': None,
+    'author': 'alijazayeri',
+    'author_email': 'jazayeri.ali@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
