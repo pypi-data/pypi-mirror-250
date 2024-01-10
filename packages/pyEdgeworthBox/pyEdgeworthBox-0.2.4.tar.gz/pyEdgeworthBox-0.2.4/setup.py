11e46f3fadeddd5f06fce157faca833eb48fdd62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 15:03:47 +0000

    Add package pyEdgeworthBox/0.2.4/pyEdgeworthBox-0.2.4.tar.gz

---
+++ packages/pyEdgeworthBox/pyEdgeworthBox-0.2.4.tar.gz/pyEdgeworthBox-0.2.4/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import os
+
+long_description = """
+pyEdgeworthBox calculates concepts of the pure exchange economy
+(subject to microeconomics) and draws the Edgeworth box.
+"""
+
+setup(
+    name='pyEdgeworthBox',
+    version = os.environ.get('CI_COMMIT_TAG', '0.0.0-dev'),
+    description='Python Library to draw the Edgeworth box and calculate equilibrium, core, pareto efficient allocations etc',
+    long_description=long_description,  # Include the long description
+    long_description_content_type='text/plain',  # Specify the content type, can be 'text/markdown' for Markdown
+    author='Maxim Litvak',
+    author_email='maxim@litvak.eu',
+    url='https://gitlab.com/maxlit/pyEdgeworthBox',
+    test_suite='test',
+    packages=['pyEdgeworthBox'],
+    classifiers=[
+        # Your classifiers
+    ],
+)
