0b0f6278a1e6e179f8e73b7aa4116517049a151a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 03:04:05 +0000

    Add package qbraid/0.5.0.dev20240108230921/qbraid-0.5.0.dev20240108230921.tar.gz

---
+++ packages/qbraid/qbraid-0.5.0.dev20240108230921.tar.gz/qbraid-0.5.0.dev20240108230921/docs/api/qbraid.transpiler.rst
@@ -0,0 +1,21 @@
+qbraid.transpiler
+===================
+
+.. automodule:: qbraid.transpiler
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Submodules
+-----------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   openqasm3
+   cirq
+   qiskit
+   braket
+   pytket
+   pyquil
+   pennylane
---
+++ packages/qbraid/qbraid-0.5.0.dev20240108230921.tar.gz/qbraid-0.5.0.dev20240108230921/qbraid/__init__.py
@@ -0,0 +1,70 @@
+# Copyright (C) 2023 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+This top level module contains the main qBraid public functionality.
+
+.. currentmodule:: qbraid
+
+.. _data_types:
+
+Data Types
+-----------
+
+.. autodata:: QPROGRAM
+   :annotation: = Type alias defining all supported quantum circuit / program types
+
+.. autodata:: QDEVICE
+   :annotation: = Type alias defining all supported quantum device / backend types
+
+Functions
+----------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   get_devices
+   refresh_devices
+   circuit_wrapper
+   device_wrapper
+   job_wrapper
+   get_jobs
+   convert_to_package
+
+
+Exceptions
+-----------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   QbraidError
+   PackageValueError
+   ProgramTypeError
+   VisualizationError
+   QasmError
+
+"""
+from . import _warnings
+from ._qdevice import QDEVICE, QDEVICE_TYPES
+from ._qprogram import QPROGRAM, QPROGRAM_LIBS, QPROGRAM_TYPES, SUPPORTED_QPROGRAMS
+from ._version import __version__
+from .exceptions import (
+    PackageValueError,
+    ProgramTypeError,
+    QasmError,
+    QbraidError,
+    VisualizationError,
+)
+from .get_devices import get_devices, refresh_devices
+from .get_jobs import get_jobs
+from .interface import convert_to_package
+from .load_program import circuit_wrapper
+from .load_provider import device_wrapper, job_wrapper
