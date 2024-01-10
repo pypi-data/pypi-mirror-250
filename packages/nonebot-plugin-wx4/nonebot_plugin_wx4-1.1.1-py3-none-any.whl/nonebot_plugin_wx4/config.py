bce10dbfadd5dacf73f1de470dda007f40960546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 04:04:02 +0000

    Add package nonebot-plugin-wx4/1.1.1/nonebot_plugin_wx4-1.1.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-wx4/nonebot_plugin_wx4-1.1.1-py3-none-any.whl/nonebot_plugin_wx4/config.py
@@ -0,0 +1,18 @@
+
+class Config:
+    
+    #指定群聊，群号
+    #指定群所有人都可以调用文心
+    GROUP_LIST=[]
+    #超级ID，QQ号
+    #超级ID可以在任意群调用文心
+    SUPER_ID=[]
+
+    #数据库名字
+    DBNAME="wxbot.db"
+
+    API_KEY = ""  
+    SECRET_KEY = ""  
+
+    #最大对话记忆
+    MAX_MESSAGES = 5  
