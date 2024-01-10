81bc6c7fabe16eb2152f4e3c2de5487c911963fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 04:03:40 +0000

    Add package roleft/1.0.16/roleft-1.0.16.tar.gz

---
+++ packages/roleft/roleft-1.0.16.tar.gz/roleft-1.0.16/tests/test_8.py
@@ -0,0 +1,24 @@
+# from roleft.Entities.QueryObject4Mysql import QueryObject4Mysql
+
+
+# from roleft.Entities import QueryObject4Mysql
+
+
+
+# from roleft.Entities.RoleftMysql import QueryObject4Mysql
+
+
+from roleft.Entities.RoleftMysql import QueryObject4Mysql
+
+
+
+host = "sgt_190"
+port = 9001
+db = "sgt_gen"
+user = "other_root"
+pwd = "VpizyOz5eJon32Q2SzDg"
+
+# qo = QueryObject4Mysql(host, port, db, user, pwd)
+
+# hehe = qo.query_mpns('select * from aliyun_prodocts limit 10;')
+pass
