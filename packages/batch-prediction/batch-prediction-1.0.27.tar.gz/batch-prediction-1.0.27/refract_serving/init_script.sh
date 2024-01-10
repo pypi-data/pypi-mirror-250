800087b7f800c9dcbb48c5e93c4817ca3aa1ee51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:03:57 +0000

    Add package batch-prediction/1.0.27/batch-prediction-1.0.27.tar.gz

---
+++ packages/batch-prediction/batch-prediction-1.0.27.tar.gz/batch-prediction-1.0.27/refract_serving/init_script.sh
@@ -0,0 +1,6 @@
+echo "Installing packaeges"
+#pip install refractml
+#pip install refractio
+#pip install refractio[local]
+echo "Done with dependency installation!";
+#pip install -U xgboost==1.6.2
