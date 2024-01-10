72c4dcd88c9756a7533663682463fc3886324b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 04:04:49 +0000

    Add package ipih/1.42/ipih-1.42-py3-none-any.whl

---
+++ packages/ipih/ipih-1.42-py3-none-any.whl/ipih/__init__.py
@@ -0,0 +1,19 @@
+import sys
+import importlib.util
+import platform
+
+FACADE_NAME: str = "facade"
+WINDOWS_SHARE_DOMAIN_NAME: str = "pih"
+PIH_MODULE_NAME: str = "pih"
+MODULE_NAME: str = "ipih"
+MODULE_VERSION: str = "1.42"
+
+def import_pih() -> None:
+    module_is_exists = importlib.util.find_spec(PIH_MODULE_NAME) is not None
+    if not module_is_exists:
+        if platform.system() == "Linux":
+            sys.path.append(f"//mnt/{FACADE_NAME}")
+        else:
+            sys.path.append(f"//{WINDOWS_SHARE_DOMAIN_NAME}/{FACADE_NAME}")
+
+import_pih()
