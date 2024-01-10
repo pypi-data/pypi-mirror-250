4f9958a5dc1914c517ef96e308d97d6e35c73c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 08:04:14 +0000

    Add package leet-topic/0.0.11/leet_topic-0.0.11.tar.gz

---
+++ packages/leet-topic/leet_topic-0.0.11.tar.gz/leet_topic-0.0.11/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
+
+VERSION = '0.0.11'
+DESCRIPTION = 'A new transformer-based topic modeling library.'
+
+setup(
+    name="leet_topic",
+    author="WJB Mattingly",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=["pandas>=1.0.0,<2.0.0",
+                     "bokeh>=2.4.0, <2.4.3",
+                     "sentence_transformers>=2.0.0",
+                     "umap-learn>=0.5.0",
+                     "hdbscan>=0.8.0",
+                    #  "protobuf<=3.20.0",
+                     "wrapt==1.14.0",
+                     "tensorflow>=2.8.0",
+                     "spacy>=3.3.0",
+                     "gensim>=4.3.0",
+                     "annoy>=1.17.0"
+                     ],
+)
