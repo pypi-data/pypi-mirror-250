74579d71ad6e5613c1dc0dc227848aec5fd1ef4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 14:04:17 +0000

    Add package annoworkcli/3.3.0/annoworkcli-3.3.0.tar.gz

---
+++ packages/annoworkcli/annoworkcli-3.3.0.tar.gz/annoworkcli-3.3.0/annoworkcli/annofab/utils.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+import annofabapi
+from annofabapi import build as build_annofabapi
+from annofabapi.exceptions import MfaEnabledUserExecutionError as AnnofabApiMfaEnabledUserExecutionError
+
+
+def build_annofabapi_resource_and_login(*, mfa_code: Optional[str] = None) -> annofabapi.Resource:
+    """
+    annofabapi.Resourceインスタンスを生成したあと、ログインする。
+
+    Args:
+        args: コマンドライン引数の情報
+
+    Returns:
+        annofabapi.Resourceインスタンス
+
+    """
+
+    service = build_annofabapi()
+
+    try:
+        if mfa_code is not None:
+            service.api.login(mfa_code=mfa_code)
+        else:
+            service.api.login()
+        return service
+
+    except AnnofabApiMfaEnabledUserExecutionError:
+        # 標準入力からMFAコードを入力させる
+        inputted_mfa_code = ""
+        while inputted_mfa_code == "":
+            inputted_mfa_code = input("Enter MFA Code for Annofab: ")
+
+        service.api.login(mfa_code=inputted_mfa_code)
+        return service
