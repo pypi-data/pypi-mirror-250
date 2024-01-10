2de6ef9c40557a87384178d99c54fe12af13b640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 14:04:17 +0000

    Add package pyBrainGenixClient/2024.1.9.6/pyBrainGenixClient-2024.1.9.6.tar.gz

---
+++ packages/pyBrainGenixClient/pyBrainGenixClient-2024.1.9.6.tar.gz/pyBrainGenixClient-2024.1.9.6/BrainGenix/NES/Models/Connections/Receptor/Receptor.py
@@ -0,0 +1,27 @@
+# BrainGenix-NES
+# AGPLv3
+
+import json
+
+from . import Configuration
+
+from BrainGenix.NES.Client import RequestHandler
+
+import BrainGenix.LibUtils.GetID
+
+
+class Receptor:
+
+    def __init__(self, _Configuration:Configuration, _RequestHandler:RequestHandler, _SimulationID:int):
+        # Create Attributes
+        self.Name = _Configuration.Name
+        self.RequestHandler = _RequestHandler
+        
+
+        # Create Box On Server
+        SourceCompartmentID = BrainGenix.LibUtils.GetID.GetID(_Configuration.SourceCompartment)
+        DestinationCompartmentID = BrainGenix.LibUtils.GetID.GetID(_Configuration.DestinationCompartment)
+        ReceptorLocation = json.dumps(_Configuration.ReceptorLocation_um)
+        Response = self.RequestHandler.MakeAuthenticatedQuery(f"/NES/Connection/Receptor/Create?SimulationID={_SimulationID}&Name={_Configuration.Name}&SourceCompartmentID={SourceCompartmentID}&DestinationCompartmentID={DestinationCompartmentID}&Conductance_nS={_Configuration.Conductance_nS}&TimeConstant_ns={_Configuration.TimeConstant_ns}&ReceptorLocation_um={ReceptorLocation}")
+        assert(Response != None)
+        self.ID = Response["CompartmentID"]
