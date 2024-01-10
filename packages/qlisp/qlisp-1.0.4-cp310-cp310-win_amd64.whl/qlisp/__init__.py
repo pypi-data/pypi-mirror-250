7864bc81936d9ab071cb1b0071d21c94dc79eccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 04:04:24 +0000

    Add package qlisp/1.0.4/qlisp-1.0.4-cp310-cp310-win_amd64.whl

---
+++ packages/qlisp/qlisp-1.0.4-cp310-cp310-win_amd64.whl/qlisp/__init__.py
@@ -0,0 +1,5 @@
+from .matricies import (CR, CX, CZ, SWAP, H, S, Sdag, SQiSWAP, T, Tdag, U,
+                        fSim, iSWAP, make_immutable, rfUnitary, sigmaI, sigmaM,
+                        sigmaP, sigmaX, sigmaY, sigmaZ,
+                        synchronize_global_phase)
+from .simple import applySeq, regesterGateMatrix, seq2mat
