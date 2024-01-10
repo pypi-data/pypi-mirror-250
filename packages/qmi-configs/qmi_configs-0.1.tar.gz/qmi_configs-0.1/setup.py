cfe2c81c3b72b9bf4909517a2435a5deea3ab412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 07:05:25 +0000

    Add package qmi-configs/0.1/qmi_configs-0.1.tar.gz

---
+++ packages/qmi-configs/qmi_configs-0.1.tar.gz/qmi_configs-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='qmi_configs',
+    version='0.1',
+    packages=["qmi_configs"],
+    install_requires=[
+        # List your dependencies here
+    ],
+)
