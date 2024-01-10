cbf1e4e94e81f18ddd819f93651c5002e4cf2738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:03:47 +0000

    Add package chatllm/2024.1.9.16.8.16/chatllm-2024.1.9.16.8.16.tar.gz

---
+++ packages/chatllm/chatllm-2024.1.9.16.8.16.tar.gz/chatllm-2024.1.9.16.8.16/chatllm/llmchain/completions/backup_completions.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : barkup
+# @Time         : 2024/1/9 09:50
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+
+from meutils.pipe import *
+
+from openai import OpenAI
+from chatllm.schemas.openai_types import chat_completion_error, chat_completion_chunk_error
+
+
+def create(**data):
+    try:
+        data['model'] = f"backup-{data['model']}"  # todo: 模型加前缀, 更加通用
+        ########## 删除多余参数 todo：更优雅的方法
+        data.pop("additional_kwargs", None)
+        data.pop("rag", None)
+        data.pop("file_id", None)
+        data.pop("embedding_model", None)
+
+        ########## 删除多余参数
+
+        _ = (
+            OpenAI(
+                api_key=os.getenv("BACKUP_API_KEY"),
+                base_url=os.getenv("OPENAI_BASE_URL", "https://api.chatllm.vip/v1")
+            )
+            .chat.completions.create(**data)
+        )
+        # logger.debug(_)
+        return _
+    except Exception as e:
+        if data.get('stream'):
+            chat_completion_chunk_error.choices[0].delta.content = str(e)
+            return chat_completion_chunk_error
+        else:
+            chat_completion_error.choices[0].message.content = str(e)
+            return chat_completion_error
+
+
+if __name__ == '__main__':
+    data = {
+        'model': 'gpt-3.5-turbo',
+        'messages': [
+            {'role': 'system', 'content': "你是gpt4, Let's think things through one step at a time."},
+            {'role': 'user', 'content': '1+1'}
+        ],
+        'stream': False
+    }
+
+    print(create(**data))
