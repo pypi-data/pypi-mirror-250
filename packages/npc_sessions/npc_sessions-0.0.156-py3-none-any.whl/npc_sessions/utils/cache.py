edbfb726159fd15776605e8447c397d48aecffb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 14:04:09 +0000

    Add package npc_sessions/0.0.156/npc_sessions-0.0.156-py3-none-any.whl

---
+++ packages/npc_sessions/npc_sessions-0.0.156-py3-none-any.whl/npc_sessions/utils/cache.py
@@ -0,0 +1,222 @@
+"""
+
+- write units to cache file
+
+"""
+from __future__ import annotations
+
+import importlib.metadata
+import logging
+import typing
+from collections.abc import Mapping
+
+import npc_lims
+import npc_session
+import pandas as pd
+import pyarrow
+import pyarrow.dataset
+import pyarrow.parquet
+import pynwb
+
+if typing.TYPE_CHECKING:
+    import npc_sessions
+
+logger = logging.getLogger(__name__)
+
+
+def _get_nwb_component(
+    session: pynwb.NWBFile | npc_sessions.DynamicRoutingSession,
+    component_name: npc_lims.NWBComponentStr,
+) -> pynwb.core.NWBContainer | pd.DataFrame | None:
+    def _component_metadata_to_single_row_df(component):
+        # if value is a list, wrap it in another list so pandas doesn't interpret
+        # it as multiple rows
+        return pd.DataFrame(
+            {
+                k: (v if not isinstance(v, list) else [v])
+                for k, v in component.fields.items()
+                if not isinstance(v, (Mapping, pynwb.core.NWBContainer))
+            },
+            index=[0],
+        )
+
+    if component_name == "session":
+        if not isinstance(session, pynwb.NWBFile):
+            session = session.metadata
+        return _component_metadata_to_single_row_df(session).drop(
+            columns="file_create_date"
+        )
+    elif component_name == "subject":
+        return _component_metadata_to_single_row_df(session.subject)
+    elif component_name in ("vis_rf_mapping", "VisRFMapping"):
+        return session.intervals.get("VisRFMapping", None)
+    elif component_name in ("aud_rf_mapping", "AudRFMapping"):
+        return session.intervals.get("AudRFMapping", None)
+    elif component_name in ("optotagging", "OptoTagging"):
+        return session.intervals.get("OptoTagging", None)
+    elif component_name == "performance":
+        if session.analysis:
+            return session.analysis.get("performance", None)
+        else:
+            return None
+    else:
+        c = getattr(session, component_name, None)
+        if c is None:
+            raise ValueError(
+                f"Unknown NWB component {component_name!r} - available tables include {typing.get_args(npc_lims.NWBComponentStr)}"
+            )
+        return c
+
+
+def write_nwb_component_to_cache(
+    component: pynwb.core.NWBContainer | pd.DataFrame,
+    component_name: npc_lims.NWBComponentStr,
+    session_id: npc_session.SessionRecord,
+    skip_existing: bool = True,
+) -> None:
+    """Write units to cache file (e.g. .parquet) after processing columns.
+
+    - ND arrays are not supported, so waveform mean/sd are condensed to 1D arrays
+      with data for peak channel only, or dropped entirely
+    - links to NWBContainers cannot be stored, so extract necessary 'foreign key'
+      to enable joining tables later
+    """
+    if component_name == "units":
+        component = _flatten_units(component)
+    df = _remove_pynwb_containers(component)
+    df = add_session_metadata(df, session_id)
+    _write_to_cache(
+        session_id=session_id,
+        component_name=component_name,
+        df=df,
+        skip_existing=skip_existing,
+    )
+
+
+def write_all_components_to_cache(
+    session: pynwb.NWBFile | npc_sessions.DynamicRoutingSession,
+    skip_existing: bool = True,
+) -> None:
+    """Write all components to cache files (e.g. .parquet) after processing columns.
+
+    - ND arrays are not supported, so waveform mean/sd are condensed to 1D arrays
+      with data for peak channel only, or dropped entirely
+    - links to NWBContainers cannot be stored, so extract necessary 'foreign key'
+      to enable joining tables later
+    """
+    logger.info(f"Writing all components to cache for {session.id}")
+    for component_name in typing.get_args(npc_lims.NWBComponentStr):
+        # skip before we potentially do a lot of processing to get component
+        if (
+            skip_existing
+            and npc_lims.get_cache_path(
+                nwb_component=component_name, session_id=session.id
+            ).exists()
+        ):
+            logger.info(
+                f"Skipping {session.id} {component_name} - already in cache (set skip_existing=False if you want to overwrite)"
+            )
+            continue
+        try:
+            component = _get_nwb_component(session, component_name)
+            # component may be None
+        except AttributeError:
+            component = None
+        if component is None:
+            logger.debug(f"{component_name} not available for {session.id}")
+            return
+        write_nwb_component_to_cache(
+            component=component,
+            component_name=component_name,
+            session_id=session.id,
+            skip_existing=skip_existing,
+        )
+
+
+def add_session_metadata(
+    df: pd.DataFrame, session_id: str | npc_session.SessionRecord
+) -> pd.DataFrame:
+    session_id = npc_session.SessionRecord(session_id)
+    df = df.copy()
+    df["session_idx"] = session_id.idx
+    df["date"] = session_id.date.dt
+    df["subject_id"] = session_id.subject
+    return df
+
+
+def _write_to_cache(
+    session_id: str | npc_session.SessionRecord,
+    component_name: npc_lims.NWBComponentStr,
+    df: pd.DataFrame,
+    skip_existing: bool = True,
+) -> None:
+    """Write dataframe to cache file (e.g. .parquet)."""
+    version = importlib.metadata.version("npc-sessions")
+    cache_path = npc_lims.get_cache_path(
+        nwb_component=component_name, session_id=session_id, version=version
+    )
+    if cache_path.suffix != ".parquet":
+        raise NotImplementedError(f"{cache_path.suffix=}")
+    if skip_existing and cache_path.exists():
+        logger.debug(
+            f"Skipping write to {cache_path} - already exists and skip_existing=True"
+        )
+        return
+    table = pyarrow.Table.from_pandas(df, preserve_index=False)
+    if component_name == "units" and "location" in table.schema.names:
+        table = table.sort_by("location")
+        # most common access will be units from the same areas, so make sure
+        # these rows are stored together
+    pyarrow.parquet.write_table(
+        table=table,
+        where=cache_path.as_posix(),
+        row_group_size=20 if "component" == "units" else None,
+        # each list in the spike_times column is large - should not really be
+        # stored in this format. But we can at least optimize for it by creating
+        # smaller row groups, so querying a single unit returns a chunk of rows
+        # equal to row_group_size, instead of default 10,000 rows per session
+        compression="zstd",
+        compression_level=15,
+    )
+    logger.info(f"Wrote {cache_path}")
+
+
+def get_dataset(
+    nwb_component: npc_lims.NWBComponentStr,
+    session_id: str | npc_session.SessionRecord | None = None,
+    version: str | None = None,
+) -> pyarrow.dataset.Dataset:
+    """Get dataset for all sessions, for all components, for the latest version."""
+    return pyarrow.dataset.dataset(
+        paths=npc_lims.get_cache_path(
+            nwb_component=nwb_component,
+            session_id=session_id,
+            version=version,
+        ),
+        format=npc_lims.get_cache_file_suffix(nwb_component).lstrip("."),
+    )
+
+
+def _flatten_units(units: pynwb.misc.Units | pd.DataFrame) -> pd.DataFrame:
+    units = units[:].copy()
+    # deal with links to other NWBContainers
+    units["device_name"] = units["electrode_group"].apply(lambda eg: eg.device.name)
+    units["electrode_group_name"] = units["electrode_group"].apply(lambda eg: eg.name)
+    return _remove_pynwb_containers(units)
+
+
+def _remove_pynwb_containers(
+    table_or_df: pynwb.core.DynamicTable | pd.DataFrame,
+) -> pd.DataFrame:
+    df = table_or_df[:].copy()
+    return df.drop(
+        [col for col in df.columns if isinstance(df[col][0], pynwb.core.NWBContainer)]
+    )
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(
+        optionflags=(doctest.IGNORE_EXCEPTION_DETAIL | doctest.NORMALIZE_WHITESPACE)
+    )
---
+++ packages/npc_sessions/npc_sessions-0.0.156-py3-none-any.whl/npc_sessions/utils/spikeinterface.py
@@ -0,0 +1,278 @@
+"""
+Helper functions for getting paths from spikeinterface output (developed for use
+with the aind kilosort 2.5 "pipeline" spike-sorting capsule).
+"""
+
+from __future__ import annotations
+
+import dataclasses
+import functools
+import io
+import json
+import logging
+from typing import Union
+
+import npc_lims
+import npc_session
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+import upath
+from typing_extensions import TypeAlias
+
+import npc_sessions.utils as utils
+
+logger = logging.getLogger(__name__)
+
+SpikeInterfaceData: TypeAlias = Union[
+    str, npc_session.SessionRecord, utils.PathLike, "SpikeInterfaceKS25Data"
+]
+
+
+def get_spikeinterface_data(
+    session_or_root_path: SpikeInterfaceData,
+) -> SpikeInterfaceKS25Data:
+    """Return a SpikeInterfaceKS25Data object for a session.
+
+    >>> paths = get_spikeinterface_data('668759_20230711')
+    >>> paths.root == get_spikeinterface_data('s3://codeocean-s3datasetsbucket-1u41qdg42ur9/4797cab2-9ea2-4747-8d15-5ba064837c1c').root
+    True
+    """
+    if isinstance(session_or_root_path, SpikeInterfaceKS25Data):
+        return session_or_root_path
+    try:
+        session = npc_session.SessionRecord(str(session_or_root_path))
+        root = None
+    except ValueError:
+        session = None
+        root = utils.from_pathlike(session_or_root_path)
+    return SpikeInterfaceKS25Data(session=session, root=root)
+
+
+@dataclasses.dataclass(unsafe_hash=True, eq=True)
+class SpikeInterfaceKS25Data:
+    """The root directory of the result data asset produced by the 'pipeline'
+    KS2.5 sorting capsule contains `processing.json`, `postprocessed`,
+    `spikesorted`, etc. This class just simplifies access to the data in those
+    files and dirs.
+
+    Provide a session ID or a root path:
+    >>> paths = SpikeInterfaceKS25Data('668759_20230711')
+    >>> paths.root
+    S3Path('s3://codeocean-s3datasetsbucket-1u41qdg42ur9/4797cab2-9ea2-4747-8d15-5ba064837c1c')
+
+    >>> paths.template_metrics_dict('probeA')
+    {'metric_names': ['peak_to_valley', 'peak_trough_ratio', 'half_width', 'repolarization_slope', 'recovery_slope'], 'sparsity': None, 'peak_sign': 'neg', 'upsampling_factor': 10, 'window_slope_ms': 0.7}
+
+    >>> paths.quality_metrics_df('probeA').columns
+    Index(['num_spikes', 'firing_rate', 'presence_ratio', 'snr',
+           'isi_violations_ratio', 'isi_violations_count', 'rp_contamination',
+           'rp_violations', 'sliding_rp_violation', 'amplitude_cutoff',
+           'drift_ptp', 'drift_std', 'drift_mad', 'isolation_distance', 'l_ratio',
+           'd_prime'],
+          dtype='object')
+    """
+
+    session: str | npc_session.SessionRecord | None = None
+    root: upath.UPath | None = None
+
+    def __post_init__(self) -> None:
+        if self.root is None and self.session is None:
+            raise ValueError("Must provide either session or root")
+        if self.root is None:
+            self.root = npc_lims.get_sorted_data_paths_from_s3(self.session)[0].parent
+
+    @staticmethod
+    def format_path(*path_components: utils.PathLike) -> upath.UPath:
+        """SpikeInterface makes paths with '#' in them, which is not allowed in s3
+        paths in general - run paths through this function to fix them."""
+        return utils.from_pathlike("/".join(str(path) for path in path_components))
+
+    @staticmethod
+    def read_json(path: upath.UPath) -> dict:
+        return json.loads(path.read_text())
+
+    @staticmethod
+    def read_csv(path: upath.UPath) -> pd.DataFrame:
+        return pd.read_csv(path, index_col=0)
+
+    def get_json(self, filename: str) -> dict:
+        assert self.root is not None
+        return self.read_json(self.format_path(self.root, filename))
+
+    def get_path(self, dirname: str, probe: str | None = None) -> upath.UPath:
+        """Return a path to a single dir or file: either `self.root/dirname` or, if `probe` is specified,
+        the probe-specific sub-path within `self.root/dirname`."""
+        assert self.root is not None
+        if probe is None:
+            return self.format_path(self.root, dirname)
+        else:
+            return next(
+                path
+                for path in self.format_path(self.root, dirname).iterdir()
+                if npc_session.ProbeRecord(probe)
+                == npc_session.ProbeRecord(path.as_posix())
+            )
+
+    # json data
+    processing_json = functools.partialmethod(get_json, "processing.json")
+    subject_json = functools.partialmethod(get_json, "subject.json")
+    data_description_json = functools.partialmethod(get_json, "data_description.json")
+    procedures_json = functools.partialmethod(get_json, "procedures.json")
+    visualization_output_json = functools.partialmethod(
+        get_json, "visualization_output.json"
+    )
+
+    # dirs
+    drift_maps = functools.partialmethod(get_path, "drift_maps")
+    output = functools.partialmethod(get_path, "output")
+    postprocessed = functools.partialmethod(get_path, "postprocessed")
+    sorting_precurated = functools.partialmethod(get_path, "sorting_precurated")
+    spikesorted = functools.partialmethod(get_path, "spikesorted")
+
+    @functools.cache
+    def quality_metrics_dict(self, probe: str) -> dict:
+        return self.read_json(
+            self.format_path(
+                self.postprocessed(probe), "quality_metrics", "params.json"
+            )
+        )
+
+    @functools.cache
+    def postprocessed_params_dict(self, probe: str) -> dict:
+        return self.read_json(
+            self.format_path(self.postprocessed(probe), "params.json")
+        )
+
+    @functools.cache
+    def quality_metrics_df(self, probe: str) -> pd.DataFrame:
+        return self.read_csv(
+            self.format_path(
+                self.postprocessed(probe), "quality_metrics", "metrics.csv"
+            )
+        )
+
+    @functools.cache
+    def template_metrics_dict(self, probe: str) -> dict:
+        return self.read_json(
+            self.format_path(
+                self.postprocessed(probe), "template_metrics", "params.json"
+            )
+        )
+
+    @functools.cache
+    def template_metrics_df(self, probe: str) -> pd.DataFrame:
+        return self.read_csv(
+            self.format_path(
+                self.postprocessed(probe), "template_metrics", "metrics.csv"
+            )
+        )
+
+    def templates_average(self, probe: str) -> npt.NDArray[np.floating]:
+        logger.debug("Loading templates_average.npy for %s - typically ~200 MB", probe)
+        return np.load(
+            io.BytesIO(
+                self.format_path(
+                    self.postprocessed(probe), "templates_average.npy"
+                ).read_bytes()
+            )
+        )
+
+    def templates_std(self, probe: str) -> npt.NDArray[np.floating]:
+        logger.debug("Loading templates_std.npy for %s - typically ~200 MB", probe)
+        return np.load(
+            io.BytesIO(
+                self.format_path(
+                    self.postprocessed(probe), "templates_std.npy"
+                ).read_bytes()
+            )
+        )
+
+    @functools.cache
+    def sorting_cached(self, probe: str) -> dict[str, npt.NDArray]:
+        return np.load(
+            io.BytesIO(
+                self.format_path(
+                    self.spikesorted(probe), "sorting_cached.npz"
+                ).read_bytes()
+            ),
+            allow_pickle=True,
+        )
+
+    @functools.cache
+    def default_qc(self, probe: str) -> npt.NDArray[np.floating]:
+        return np.load(
+            io.BytesIO(
+                self.format_path(
+                    self.sorting_precurated(probe), "properties", "default_qc.npy"
+                ).read_bytes()
+            )
+        )
+
+    @functools.cache
+    def unit_locations(self, probe: str) -> npt.NDArray[np.floating]:
+        return np.load(
+            io.BytesIO(
+                self.format_path(
+                    self.postprocessed(probe), "unit_locations", "unit_locations.npy"
+                ).read_bytes()
+            )
+        )
+
+    @functools.cache
+    def sorting_json(self, probe: str) -> dict:
+        return self.read_json(
+            self.format_path(self.postprocessed(probe), "sorting.json")
+        )
+
+    @functools.cache
+    def recording_attributes_json(self, probe: str) -> dict:
+        return self.read_json(
+            self.format_path(
+                self.postprocessed(probe), "recording_info", "recording_attributes.json"
+            )
+        )
+
+    @functools.cache
+    def sparse_channel_indices(self, probe: str) -> tuple[int, ...]:
+        """SpikeInterface stores channels as 1-indexed integers: "AP1", ...,
+        "AP384". This method returns the 0-indexed *integers* for each probe
+        recorded, for use in indexing into the electrode table.
+        """
+
+        def int_ids(recording_attributes_json: dict) -> tuple[int, ...]:
+            """
+            >>> int_ids({'channel_ids': ['AP1', '2', 'CH3', ]})
+            (0, 1, 2)
+            """
+            values = tuple(
+                sorted(
+                    int("".join(i for i in str(id_) if i.isdigit())) - 1
+                    for id_ in recording_attributes_json["channel_ids"]
+                )
+            )
+            assert (
+                m := min(values)
+            ) >= 0, f"Expected all channel_ids from SpikeInterface to be 1-indexed: min = {m + 1}"
+            return values
+
+        return int_ids(self.recording_attributes_json(probe))
+
+    @functools.cache
+    def electrode_locations_xy(self, probe: str) -> npt.NDArray[np.floating]:
+        return np.array(
+            self.sorting_json(probe)["annotations"]["__sorting_info__"]["recording"][
+                "properties"
+            ]["location"]
+        )
+
+
+if __name__ == "__main__":
+    import doctest
+
+    import dotenv
+
+    dotenv.load_dotenv(dotenv.find_dotenv(usecwd=True))
+    doctest.testmod(
+        optionflags=(doctest.IGNORE_EXCEPTION_DETAIL | doctest.NORMALIZE_WHITESPACE)
+    )
