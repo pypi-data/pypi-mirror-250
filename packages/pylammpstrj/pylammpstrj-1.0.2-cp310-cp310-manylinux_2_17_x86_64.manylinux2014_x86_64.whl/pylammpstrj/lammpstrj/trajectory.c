9e8bf14df5e4ba1a80839e9ccf6ec160bde556d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 06:04:23 +0000

    Add package pylammpstrj/1.0.2/pylammpstrj-1.0.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pylammpstrj/pylammpstrj-1.0.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pylammpstrj/lammpstrj/trajectory.c
@@ -0,0 +1,56 @@
+#include "utils.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void trajectory_init(struct Trajectory *trajectory,
+                     struct AtomBuilder atom_builder,
+                     unsigned long N_configurations, unsigned int *steps,
+                     unsigned int *N_atoms, struct Box *box, struct Atom *atoms)
+{
+    trajectory->atom_builder = atom_builder;
+    trajectory->N_configurations = N_configurations;
+    trajectory->steps = steps;
+    trajectory->N_atoms = N_atoms;
+    trajectory->box = box;
+    trajectory->atoms = atoms;
+}
+
+void delete_atoms(struct Trajectory *trajectory)
+{
+    for (unsigned int c = 0, i = 0; c < trajectory->N_configurations; c++)
+        for (unsigned int a = 0; a < (trajectory->N_atoms)[c]; a++, i++)
+            atom_delete(trajectory->atoms + i);
+}
+
+void trajectory_delete(struct Trajectory *trajectory)
+{
+    atom_builder_delete(&(trajectory->atom_builder));
+
+    if (trajectory->atoms != NULL)
+    {
+        delete_atoms(trajectory);
+        free(trajectory->atoms);
+        trajectory->atoms = NULL;
+    }
+
+    if (trajectory->box != NULL)
+    {
+        free(trajectory->box);
+        trajectory->box = NULL;
+    }
+
+    if (trajectory->steps != NULL)
+    {
+        free(trajectory->steps);
+        trajectory->steps = NULL;
+    }
+
+    if (trajectory->N_atoms != NULL)
+    {
+        free(trajectory->N_atoms);
+        trajectory->N_atoms = NULL;
+    }
+}
