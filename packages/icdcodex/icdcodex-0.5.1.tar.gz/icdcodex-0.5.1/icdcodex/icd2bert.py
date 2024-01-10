4e45acd2631000c72dcbe7608f027c14f8350a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 04:04:35 +0000

    Add package icdcodex/0.5.1/icdcodex-0.5.1.tar.gz

---
+++ packages/icdcodex/icdcodex-0.5.1.tar.gz/icdcodex-0.5.1/icdcodex/icd2bert.py
@@ -0,0 +1,9 @@
+"""Build a vector embedding from a networkX representation of the ICD hierarchy
+using Transformers"""
+
+
+class Icd2BERT:
+    def __init__(self, 
+                 lm
+                 ):
+        
