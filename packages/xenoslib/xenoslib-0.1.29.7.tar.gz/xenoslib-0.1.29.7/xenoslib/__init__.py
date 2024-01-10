0ed9194f1dfbfdcf89b5d4f04569b0aed6c73883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 15:03:30 +0000

    Add package xenoslib/0.1.29.7/xenoslib-0.1.29.7.tar.gz

---
+++ packages/xenoslib/xenoslib-0.1.29.7.tar.gz/xenoslib-0.1.29.7/xenoslib/__init__.py
@@ -0,0 +1,10 @@
+import sys
+
+from .about import __version__  # noqa
+
+if sys.platform == "win32":
+    from .windows import *  # noqa
+    from .win_trayicon import *  # noqa
+elif sys.platform == "linux":
+    from .linux import *  # noqa
+from .base import *  # noqa
