24cd80de733dd566350edeacc6f9685114caa738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 07:04:11 +0000

    Add package opensr-utils/0.1.2/opensr-utils-0.1.2.tar.gz

---
+++ packages/opensr-utils/opensr-utils-0.1.2.tar.gz/opensr-utils-0.1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='opensr-utils',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[],
+    # Add more fields as needed
+)
