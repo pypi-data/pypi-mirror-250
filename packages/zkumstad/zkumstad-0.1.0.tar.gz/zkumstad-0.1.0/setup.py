b981cd3ae6af6f8578b024de9be293e6da50b119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 08:04:20 +0000

    Add package zkumstad/0.1.0/zkumstad-0.1.0.tar.gz

---
+++ packages/zkumstad/zkumstad-0.1.0.tar.gz/zkumstad-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="zkumstad",
+    version="0.1.0",
+    author="Berkin Gurcan, Dogukan Akar",
+    author_email="berkingurcan@gmail.com, dogukanakarc@gmail.com",
+    description="CLI AI Agent for zkApps Developers",
+    long_description=open("./README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/UmstadAI/zkappumstad",
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points="""
+        [console_scripts]
+        zkumstad-start=zkappumstad:main
+        zkumstad-create=create_zk_project:main
+    """,
+    classifiers=[
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.10', 
+    keywords='zkapps, zkappumstad, zkapps agent',
+    zip_safe=False
+)
