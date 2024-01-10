31a715750050f3ecc84830428966ecdf606c437c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 14:04:49 +0000

    Add package Akatosh/2.3.3/Akatosh-2.3.3-py3-none-any.whl

---
+++ packages/Akatosh/Akatosh-2.3.3-py3-none-any.whl/Akatosh/__init__.py
@@ -0,0 +1,6 @@
+from .entity import Entity, EntityList
+from .event import (ContinuousEvent, Event, InstantEvent, continuous_event,
+                    instant_event)
+from .resource import Resource
+from .states import State
+from .universe import Mundus
