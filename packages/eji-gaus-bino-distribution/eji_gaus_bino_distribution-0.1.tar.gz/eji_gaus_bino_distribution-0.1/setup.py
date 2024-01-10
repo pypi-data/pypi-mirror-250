6f4cd3bc61649bae3ae5e2df4f784682ab6d9ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 04:04:09 +0000

    Add package eji-gaus-bino-distribution/0.1/eji_gaus_bino_distribution-0.1.tar.gz

---
+++ packages/eji-gaus-bino-distribution/eji_gaus_bino_distribution-0.1.tar.gz/eji_gaus_bino_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='eji_gaus_bino_distribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['eji_gaus_bino_distribution'],
+      author='Elijah James Ikpeme',
+      author_email='ikpemeelijahjames@outlook.com',
+      zip_safe=False)
