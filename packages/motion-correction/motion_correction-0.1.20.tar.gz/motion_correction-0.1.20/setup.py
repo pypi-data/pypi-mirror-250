0cb3fe48cb3e9a9301ec53ff1a827fe35d703ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 04:04:10 +0000

    Add package motion-correction/0.1.20/motion_correction-0.1.20.tar.gz

---
+++ packages/motion-correction/motion_correction-0.1.20.tar.gz/motion_correction-0.1.20/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Pillow>=10.0.0',
+    'numba>=0.57',
+    'numba-progress>=1.1.0',
+    'opencv-python>=4.8',
+    "torch>=2.0.1",
+    "numpy==1.23.5",
+    "matplotlib==3.7.2",
+    "image-registration==0.2.6",
+    "pytest>=7.4",
+    "dipy>=1.7.0",
+    "tqdm>=4.66",
+    "scikit-image==0.21",
+    "sparse>=0.14.0",
+]
+
+extras = {
+    "gpu":["cupy-cuda11x"]
+}
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Timothy Kallady",
+    author_email='t.kallady@garvan.org.au',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Motion correction algorithms for FLIM data.",
+    install_requires=requirements,
+    extras_require=extras,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='motion_correction',
+    name='motion_correction',
+    packages=find_packages(include=['motion_correction', 'motion_correction.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/Garvan-Data-Science-Platformmotion_correction',
+    version='0.1.20',
+    zip_safe=False
+)
