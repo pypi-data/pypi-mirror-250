be6cd0157447f88e7b1b827032401be602826d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 15:03:29 +0000

    Add package w-dao/1.1/w-dao-1.1.tar.gz

---
+++ packages/w-dao/w-dao-1.1.tar.gz/w-dao-1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="w-dao",
+    version="1.1",
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    author="zzt",
+    author_email="zzt@qq.com",
+    description="A useless database access model",
+    license="MIT",
+    keywords="w_dao",
+    url="https://github.com/zzt/w-dao.git"
+)
