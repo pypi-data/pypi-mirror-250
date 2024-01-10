4932a8dce11395040f75c5edc85600a5855c9f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:04:03 +0000

    Add package finbourne-access-sdk/0.0.3586/finbourne_access_sdk-0.0.3586-py3-none-any.whl

---
+++ packages/finbourne-access-sdk/finbourne_access_sdk-0.0.3586-py3-none-any.whl/finbourne_access/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from finbourne_access.api.application_metadata_api import ApplicationMetadataApi
+from finbourne_access.api.policies_api import PoliciesApi
+from finbourne_access.api.roles_api import RolesApi
+from finbourne_access.api.user_roles_api import UserRolesApi
