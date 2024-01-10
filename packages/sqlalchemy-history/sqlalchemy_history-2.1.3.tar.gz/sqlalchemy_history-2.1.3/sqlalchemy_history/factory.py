9564724579636adf4e0d93e1421252f9cde612ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 07:05:51 +0000

    Add package sqlalchemy-history/2.1.3/sqlalchemy_history-2.1.3.tar.gz

---
+++ packages/sqlalchemy-history/sqlalchemy_history-2.1.3.tar.gz/sqlalchemy_history-2.1.3/sqlalchemy_history/factory.py
@@ -0,0 +1,18 @@
+"""
+Factory package manages and makes sure if a model class already exists indeclarative model registry or not
+"""
+
+
+class ModelFactory(object):
+    model_name = None
+
+    def __call__(self, manager):
+        """Create model class but only if it doesn't already exist
+        in declarative model registry.
+        """
+        Base = manager.declarative_base
+        registry = Base.registry._class_registry
+
+        if self.model_name not in registry:
+            return self.create_class(manager)
+        return registry[self.model_name]
