5a607eb9d2998afe7bf24880a0106b1e03cf3d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 09:03:21 +0000

    Add package ares-util/0.3.0/ares_util-0.3.0.tar.gz

---
+++ packages/ares-util/ares_util-0.3.0.tar.gz/ares_util-0.3.0/setup.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+
+import io
+
+from setuptools import setup
+
+setup(
+    name='ares_util',
+    version='0.3.0',
+    description='A tool for information system allowing a retrieval '
+                'of information on economic entities registered in '
+                'the Czech Republic (ARES - Access to Registers of Economic Subjects / Entities).',
+    long_description=io.open('README.rst', 'r', encoding='utf-8').read(),
+    url='https://github.com/illagrenan/ares_util',
+    license='MIT',
+    author='Vasek Dohnal',
+    author_email='vaclav.dohnal@gmail.com',
+    packages=['ares_util'],
+    install_requires=['requests'],
+    include_package_data=True,
+    zip_safe=True,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Framework :: Django',
+        'Framework :: Django :: 4',
+        'Framework :: Django :: 5.0',
+    ],
+    test_suite='tests',
+    tests_require=['responses']
+)
