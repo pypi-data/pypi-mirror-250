ffcb03959a20d3c34abd097c0cc100e5be54e830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 15:03:52 +0000

    Add package django-oscar-odin/0.1.0a1/django_oscar_odin-0.1.0a1-py3-none-any.whl

---
+++ packages/django-oscar-odin/django_oscar_odin-0.1.0a1-py3-none-any.whl/oscar_odin/django_resolver.py
@@ -0,0 +1,25 @@
+"""Resolver for resolving attributes on Django models."""
+from typing import Dict, Optional
+
+from odin import Field
+from odin.mapping import FieldResolverBase
+from odin.utils import getmeta
+
+
+class ModelFieldResolver(FieldResolverBase):
+    """Field resolver for Django models."""
+
+    # pylint: disable=protected-access
+    def get_field_dict(self) -> Dict[str, Optional[Field]]:
+        """Get a dictionary of fields from the source object."""
+        meta = getmeta(self.obj)
+
+        fields = meta._forward_fields_map
+
+        fields.update(
+            (r.related_name, r.field)
+            for r in meta.related_objects
+            if r.related_name != "+"
+        )
+
+        return fields
