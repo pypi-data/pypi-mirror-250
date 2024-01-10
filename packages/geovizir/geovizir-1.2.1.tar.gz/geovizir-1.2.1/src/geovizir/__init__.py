3fad87e03d4b3932662be93a37dda2b2843f75ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 07:05:51 +0000

    Add package geovizir/1.2.1/geovizir-1.2.1.tar.gz

---
+++ packages/geovizir/geovizir-1.2.1.tar.gz/geovizir-1.2.1/src/geovizir/__init__.py
@@ -0,0 +1,7 @@
+# read version from installed package
+from importlib.metadata import version
+__version__ = version("geovizir")
+
+from geovizir.dplyr import *
+from geovizir.features import *
+from geovizir.scales import *
