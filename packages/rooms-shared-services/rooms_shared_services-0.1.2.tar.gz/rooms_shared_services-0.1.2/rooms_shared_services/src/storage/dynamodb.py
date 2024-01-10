f6aa3314cc4ea0fe8756fc6fe6d48b9c7a25ca7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:04:55 +0000

    Add package rooms-shared-services/0.1.2/rooms_shared_services-0.1.2.tar.gz

---
+++ packages/rooms-shared-services/rooms_shared_services-0.1.2.tar.gz/rooms_shared_services-0.1.2/rooms_shared_services/src/storage/dynamodb.py
@@ -0,0 +1,20 @@
+import logging
+
+from rooms_shared_services.src.storage.abstract import AbstractStorageClient
+
+
+from pydantic_settings import BaseSettings
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+class DynamodbSettings(BaseSettings):
+    table: str
+
+
+class DynamodbStorageClient(AbstractStorageClient):
+    def __init__(self, settings: DynamodbSettings):
+        ...
+        
+    def __call__(self):
+        logger.info("Hello world")
