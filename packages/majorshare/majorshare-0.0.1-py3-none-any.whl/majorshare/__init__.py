fee892a4701a9081249ea74e4b85da67edb4e566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:04:25 +0000

    Add package majorshare/0.0.1/majorshare-0.0.1-py3-none-any.whl

---
+++ packages/majorshare/majorshare-0.0.1-py3-none-any.whl/majorshare/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*- 
+import os
+
+__version__ = '1.0.0'
+__author__ = 'majorshare'
+
+
+from majorshare.data_proxy import pro_api,pro_bar
+from majorshare.upass import (get_token, set_token)
