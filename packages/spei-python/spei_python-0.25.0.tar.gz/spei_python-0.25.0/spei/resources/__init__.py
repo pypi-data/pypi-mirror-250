5d75138c0baac2afbe471598c1b61f4b5cd287e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 07:05:33 +0000

    Add package spei-python/0.25.0/spei_python-0.25.0.tar.gz

---
+++ packages/spei-python/spei_python-0.25.0.tar.gz/spei_python-0.25.0/spei/resources/__init__.py
@@ -0,0 +1,4 @@
+from spei.resources.acuse import Acuse  # noqa: F401
+from spei.resources.cda import CDA  # noqa: F401
+from spei.resources.orden import Orden, TerceroATercero  # noqa: F401
+from spei.resources.respuesta import Respuesta  # noqa: F401
