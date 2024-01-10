75445d12bde549054981da011ea9a21c771419f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 07:04:33 +0000

    Add package vsc-dm/0.0.1.7463774376/vsc_dm-0.0.1.7463774376-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/vsc-dm/vsc_dm-0.0.1.7463774376-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/vsc_dm/include/vsc/dm/impl/ValRef.h
@@ -0,0 +1,292 @@
+/**
+ * ValRef.h
+ *
+ * Copyright 2023 Matthew Ballance and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may 
+ * not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ * Created on:
+ *     Author: 
+ */
+#pragma once
+#include <stdexcept>
+#include <stdint.h>
+#include "vsc/dm/IValAlloc.h"
+#include "vsc/dm/IValOps.h"
+#include "vsc/dm/IDataType.h"
+#include "vsc/dm/ITypeField.h"
+#include "vsc/dm/Val.h"
+
+#define VALREF_FLAGSET(e, flag) ((static_cast<uint32_t>(e) & static_cast<uint32_t>(flag)) != 0)
+#define VALREF_SETFLAG(e, flag) (static_cast<ValRef::Flags>((static_cast<uint32_t>(e) | static_cast<uint32_t>(flag))))
+#define VALREF_CLRFLAG(e, flag) (static_cast<ValRef::Flags>((static_cast<uint32_t>(e) & ~static_cast<uint32_t>(flag))))
+
+namespace vsc {
+namespace dm {
+
+class IDataType;
+class ITypeField;
+class IValOps;
+
+
+class ValRef {
+public:
+    enum class Flags {
+        None     = 0,
+        Void     = (1 << 0), // Does not hold a valid value
+        Root     = (1 << 1), // 
+        Owned    = (1 << 2), // Storage is owned by this handle
+        IsPtr    = (1 << 3),
+        HasField = (1 << 4),
+        Mutable  = (1 << 5)
+    };
+
+
+public:
+    ValRef() : m_vp(0), m_type_field(static_cast<IDataType *>(0)), m_flags(Flags::Void) { }
+    
+    ValRef(const ValRef &rhs) : 
+        m_vp(rhs.m_vp), 
+        m_type_field(rhs.m_type_field), 
+        m_flags(rhs.m_flags) {
+        fprintf(stdout, "ValRef(rhs): vp=0x%08llx\n", m_vp);
+        fflush(stdout);
+
+        if (VALREF_FLAGSET(m_flags, Flags::Owned)) {
+            // The producer is passing ownership. Accept
+            Val *v = Val::ValPtr2Val(m_vp);
+            v->owner = this;
+        }
+    }
+
+    void operator = (const ValRef &rhs) {
+        m_vp = rhs.m_vp;
+        m_type_field = rhs.m_type_field;
+        m_flags = rhs.m_flags;
+
+        fprintf(stdout, "ValRef == : vp=0x%08llx\n", m_vp);
+        fflush(stdout);
+
+        if (VALREF_FLAGSET(m_flags, Flags::Owned)) {
+            // The producer is passing ownership. Accept
+            Val *v = Val::ValPtr2Val(m_vp);
+            v->owner = this;
+        }
+    }
+
+    ValRef(
+        uintptr_t       vp,
+        IDataType       *type,
+        Flags           flags) : m_vp(vp), m_type_field(type), 
+        m_flags(VALREF_CLRFLAG(flags, Flags::HasField)) { 
+
+        fprintf(stdout, "ValRef(type): vp=0x%08llx\n", m_vp);
+        fflush(stdout);
+    }
+
+    ValRef(
+        uintptr_t       vp,
+        ITypeField      *field,
+        Flags           flags) : m_vp(vp), m_type_field(field), 
+            m_flags(VALREF_SETFLAG(flags, Flags::HasField)) { 
+        fprintf(stdout, "ValRef(field): vp=0x%08llx\n", m_vp);
+        fflush(stdout);
+    }
+
+    virtual ~ValRef() {
+        if (VALREF_FLAGSET(m_flags, Flags::Owned) && m_vp) {
+           fprintf(stdout, "~ValRef: vp=0x%08llx\n", m_vp);
+           fflush(stdout);
+            Val *vp = Val::ValPtr2Val(m_vp);
+            if (vp->owner == this) {
+                if (type()) {
+                    type()->finiVal(*this);
+                }
+//                vp->p.ap->freeVal(vp);
+                m_vp = 0;
+            }
+        }
+    }
+
+    bool valid() const { return m_type_field.m_type; }
+
+    Flags flags() const { return m_flags; }
+
+    void set(const ValRef &rhs) {
+        m_vp = rhs.m_vp;
+        m_flags = rhs.m_flags;
+        m_type_field = rhs.m_type_field;
+    }
+
+    void move(ValRef &rhs) {
+        m_vp = rhs.m_vp;
+        m_flags = rhs.m_flags;
+        m_type_field = rhs.m_type_field;
+        if (VALREF_FLAGSET(m_flags, Flags::Owned)) {
+            rhs.m_vp = 0;
+        }
+    }
+
+    bool isVoid() const { return VALREF_FLAGSET(m_flags, Flags::Void); }
+
+    bool isMutable() const { return VALREF_FLAGSET(m_flags, Flags::Mutable); }
+
+    void setWeakRef(const ValRef &rhs) {
+        reset(); // Clear out prior value first
+        m_vp = rhs.m_vp;
+        m_flags = rhs.m_flags; // TODO: don't want this to pull along ownership
+        m_type_field = rhs.m_type_field;
+    }
+
+    ValRef copyVal() const {
+//        return ValRef(type()->copyVal(*this));
+        return ValRef(type()->copyVal(*this));
+//        return cp;
+//        return type()->copyVal(*this);
+    }
+
+    ValRef toMutable() const {
+        // We want 
+        if (VALREF_FLAGSET(m_flags, Flags::Mutable)) {
+            // This value is already mutable
+            Flags flags = VALREF_CLRFLAG(m_flags, Flags::Owned);
+            // A mutable ref must change its source. In order for that
+            // to happen, we need to convert a non-pointer reference
+            // to a pointer reference
+#ifdef UNDEFINED
+            uintptr_t vp;
+            if (!VALREF_FLAGSET(m_flags, Flags::IsPtr)) {
+                fprintf(stdout, "Transform to pointer\n");
+                fflush(stdout);
+                vp = reinterpret_cast<uintptr_t>(&m_vp);
+                flags = VALREF_SETFLAG(flags, Flags::IsPtr);
+            } else {
+                vp = m_vp;
+            }
+#endif /* UNDEFINED */
+            if (VALREF_FLAGSET(m_flags, Flags::HasField)) {
+                return ValRef(m_vp, m_type_field.m_field, flags);
+            } else {
+                return ValRef(m_vp, m_type_field.m_type, flags);
+            }
+        } else {
+            // TODO: maybe this is just a failure?
+            fprintf(stdout, "Error: cannot make an immutable field mutable\n");
+            throw std::runtime_error("Error: cannot make an immutable field mutable");
+
+            // Create a copy and make it mutable
+            ValRef cp(copyVal());
+            cp.m_flags = VALREF_SETFLAG(cp.m_flags, Flags::Mutable);
+            return cp;
+        }
+    }
+
+    ValRef toImmutable() const {
+        if (VALREF_FLAGSET(m_flags, Flags::HasField)) {
+            return ValRef(m_vp, m_type_field.m_field, 
+                VALREF_CLRFLAG(m_flags, Flags::Mutable));
+        } else {
+            return ValRef(m_vp, m_type_field.m_type, 
+                VALREF_CLRFLAG(m_flags, Flags::Mutable));
+        }
+    }
+
+    ValRef toUnowned() const {
+        Flags flags = VALREF_CLRFLAG(m_flags, Flags::Owned);
+        return (VALREF_FLAGSET(m_flags, Flags::HasField))?
+            ValRef(m_vp, m_type_field.m_field, flags):
+            ValRef(m_vp, m_type_field.m_type, flags);
+    }
+
+    void reset() {
+        if (m_vp && (static_cast<uint32_t>(m_flags) & static_cast<uint32_t>(Flags::Owned))) {
+            if (type()) {
+                type()->finiVal(*this);
+            }
+            Val *vp = Val::ValPtr2Val(m_vp);
+            vp->p.ap->freeVal(vp);
+        }
+        m_flags = VALREF_SETFLAG(m_flags, Flags::Void);
+        m_type_field.m_type = 0;
+    }
+
+    std::string name() const {
+        if (VALREF_FLAGSET(m_flags, Flags::HasField)) {
+            return m_type_field.m_field->name();
+        } else {
+            return "";
+        }
+    }
+
+    ITypeField *field() const { 
+        if (VALREF_FLAGSET(m_flags, Flags::HasField)) {
+            return m_type_field.m_field; 
+        } else {
+            return 0;
+        }
+    }
+
+    IDataType *type() const {
+        if (VALREF_FLAGSET(m_flags, Flags::HasField)) {
+            return m_type_field.m_field->getDataType();
+        } else {
+            return m_type_field.m_type; 
+        }
+    }
+
+    template <class T> T *typeT() const {
+        return dynamic_cast<T *>(type());
+    }
+
+    uintptr_t vp() const { return m_vp; }
+
+protected:
+    uintptr_t           m_vp;
+    union TypeField {
+        TypeField() : m_type(0) { }
+        TypeField(IDataType *t) : m_type(t) { }
+        TypeField(ITypeField *t) : m_field(t) { }
+        IDataType       *m_type;
+        ITypeField      *m_field;
+    }                   m_type_field;
+    Flags               m_flags;
+};
+
+}
+}
+
+
+namespace vsc {
+namespace dm {
+
+static inline ValRef::Flags operator | (const ValRef::Flags lhs, const ValRef::Flags rhs) {
+	return static_cast<ValRef::Flags>(
+			static_cast<uint32_t>(lhs) | static_cast<uint32_t>(rhs));
+}
+
+static inline ValRef::Flags operator & (const ValRef::Flags lhs, const ValRef::Flags rhs) {
+	return static_cast<ValRef::Flags>(
+			static_cast<uint32_t>(lhs) & static_cast<uint32_t>(rhs));
+}
+
+static inline ValRef::Flags operator ~ (const ValRef::Flags lhs) {
+	return static_cast<ValRef::Flags>(~static_cast<uint32_t>(lhs));
+}
+
+} /* namespace dm */
+} /* namespace vsc */
+
+#undef VALREF_FLAGSET
+#undef VALREF_SETFLAG
+#undef VALREF_CLRFLAG
+
---
+++ packages/vsc-dm/vsc_dm-0.0.1.7463774376-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/vsc_dm/include/vsc/dm/impl/ValRefStruct.h
@@ -0,0 +1,84 @@
+/**
+ * ValRefStruct.h
+ *
+ * Copyright 2023 Matthew Ballance and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may 
+ * not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ * Created on:
+ *     Author: 
+ */
+#pragma once
+#include "vsc/dm/IDataTypeStruct.h"
+#include "vsc/dm/impl/TaskIsFieldValPtr.h"
+#include "vsc/dm/impl/ValRef.h"
+
+namespace vsc {
+namespace dm {
+
+
+
+class ValRefStruct : public ValRef {
+public:
+    friend class Context;
+
+    ValRefStruct(const ValRef &rhs) : ValRef(rhs) { }
+
+    virtual ~ValRefStruct() { }
+
+    int32_t getNumFields() const {
+        return dynamic_cast<IDataTypeStruct *>(type())->getFields().size();
+    }
+
+    ValRef getFieldRef(int32_t i) const {
+        char *ptr;
+        if ((m_flags & Flags::IsPtr) != Flags::None) {
+            ptr = reinterpret_cast<char *>(
+                *reinterpret_cast<uintptr_t *>(m_vp));
+        } else {
+            ptr = reinterpret_cast<char *>(m_vp);
+        }
+        IDataTypeStruct *dt_s = typeT<IDataTypeStruct>();
+        ITypeField *field = dt_s->getField(i);
+        ValRef::Flags flags = static_cast<ValRef::Flags>(
+            static_cast<uint32_t>(m_flags) & ~static_cast<uint32_t>(ValRef::Flags::Owned));
+
+        if (TaskIsFieldValPtr().check(field)) {
+            fprintf(stdout, "Field %s is a pointer\n", field->name().c_str());
+            flags = flags | ValRef::Flags::IsPtr;
+        } else {
+            fprintf(stdout, "Field %s is NOT a pointer\n", field->name().c_str());
+            flags = static_cast<ValRef::Flags>(
+                static_cast<uint32_t>(m_flags) & ~static_cast<uint32_t>(ValRef::Flags::IsPtr));
+        }
+        fflush(stdout);
+        return ValRef(
+            reinterpret_cast<uintptr_t>(&ptr[field->getOffset()]),
+            field,
+            flags
+        );
+    }
+
+protected:
+
+    ValRefStruct(
+        ValData             vp,
+        IDataTypeStruct     *type,
+        ValRef::Flags        flags) : ValRef(vp, type, flags) { }
+
+};
+
+} /* namespace dm */
+} /* namespace vsc */
+
+
