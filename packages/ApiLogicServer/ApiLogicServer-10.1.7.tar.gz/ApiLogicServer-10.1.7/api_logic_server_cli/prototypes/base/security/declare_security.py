4657bf0e9a2b90c10333abc194a923228e02e471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 04:03:59 +0000

    Add package ApiLogicServer/10.1.7/ApiLogicServer-10.1.7.tar.gz

---
+++ packages/ApiLogicServer/ApiLogicServer-10.1.7.tar.gz/ApiLogicServer-10.1.7/api_logic_server_cli/prototypes/base/security/declare_security.py
@@ -0,0 +1,21 @@
+from security.system.authorization import Grant, Security, Security, DefaultRolePermission, GlobalFilter
+import logging
+from database import models
+import safrs
+
+db = safrs.DB
+session = db.session
+
+app_logger = logging.getLogger(__name__)
+
+"""
+First, Activate Security: https://apilogicserver.github.io/Docs/Security-Activation/
+
+Then, Declare Security here: https://apilogicserver.github.io/Docs/Security-Authorization/
+
+See documentation: https://apilogicserver.github.io/Docs/Security-Overview/
+
+Security is invoked on server start (api_logic_server_run), per activation in `config.py`
+
+Your Code Goes Here
+"""
---
+++ packages/ApiLogicServer/ApiLogicServer-10.1.7.tar.gz/ApiLogicServer-10.1.7/api_logic_server_cli/prototypes/nw_plus/readme.md
@@ -0,0 +1,18 @@
+# Purpose
+
+Used internally to develop new features for nw.
+
+&nbsp;
+
+# Setup: Create Project
+
+To generate this app, *either:*
+
+1. Use Dev IDE, Run Config `Create servers/ApiLogicProject (new IDE) -- nw+` (it's near the top, under *2*), *or*
+2. Use Preview Build
+    * `python -m pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple ApiLogicServer==9.5.10`
+    * Then, create as usual
+
+```
+ApiLogicServer create --project_name=nw_plus --db_url=nw+
+```
