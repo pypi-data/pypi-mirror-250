ec3b92a5e3d2459664172d33fa82ce944f4a011f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 07:05:47 +0000

    Add package piper-whistle/1.6.73/piper_whistle-1.6.73.tar.gz

---
+++ packages/piper-whistle/piper_whistle-1.6.73.tar.gz/piper_whistle-1.6.73/src/piper_whistle/__init__.py
@@ -0,0 +1,11 @@
+"""CLI tool to manage piper voices.
+"""
+# 2023-∞ (c) blurryroots innovation qanat OÜ. All rights reserved.
+
+
+def version ():
+	return "1.6.73"
+
+
+if '__main__' == __name__:
+	print (version ())
