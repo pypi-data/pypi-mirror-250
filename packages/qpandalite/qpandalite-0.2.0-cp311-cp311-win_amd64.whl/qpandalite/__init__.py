06b41ced3e8d036b804b61b0ed7277adddba2dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:04:00 +0000

    Add package qpandalite/0.2.0/qpandalite-0.2.0-cp311-cp311-win_amd64.whl

---
+++ packages/qpandalite/qpandalite-0.2.0-cp311-cp311-win_amd64.whl/qpandalite/__init__.py
@@ -0,0 +1,17 @@
+from .circuit_builder import Circuit
+from .originir import OriginIR_Parser
+try:
+    from .simulator import OriginIR_Simulator
+except:
+    # warning has been omitted in the submodule.
+    pass
+import QPandaLitePy
+from .analyzer import convert_originq_result, calculate_expectation, shots2prob, kv2list
+from .qcloud_config.originq_online_config import (create_originq_online_config, 
+                                                  create_originq_dummy_config,
+                                                  create_originq_config)
+from .qcloud_config.quafu_online_config import create_quafu_online_config
+from .qcloud_config.ibm_online_config import create_ibm_online_config
+from .qcloud_config.originq_cloud_config import create_originq_cloud_config
+from .transpiler import plot_time_line
+from .task.task_utils import (get_last_taskid, load_circuit, load_circuit_group, load_all_online_info)
