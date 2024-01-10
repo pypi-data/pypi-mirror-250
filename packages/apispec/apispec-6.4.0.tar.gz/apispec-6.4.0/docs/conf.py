9755ec9843e21100b2995bde4a0417191aeb331f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 14:04:50 +0000

    Add package apispec/6.4.0/apispec-6.4.0.tar.gz

---
+++ packages/apispec/apispec-6.4.0.tar.gz/apispec-6.4.0/docs/conf.py
@@ -0,0 +1,49 @@
+import datetime as dt
+import os
+import sys
+import time
+
+import sphinx_rtd_theme
+
+sys.path.insert(0, os.path.abspath(os.path.join("..", "src")))
+import apispec  # noqa: E402
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.todo",
+    "sphinx_issues",
+]
+
+primary_domain = "py"
+default_role = "py:obj"
+
+intersphinx_mapping = {
+    "python": ("https://python.readthedocs.io/en/latest/", None),
+    "marshmallow": ("https://marshmallow.readthedocs.io/en/latest/", None),
+    "webargs": ("https://webargs.readthedocs.io/en/latest/", None),
+}
+
+issues_github_path = "marshmallow-code/apispec"
+
+
+# Use SOURCE_DATE_EPOCH for reproducible build output
+# https://reproducible-builds.org/docs/source-date-epoch/
+build_date = dt.datetime.utcfromtimestamp(
+    int(os.environ.get("SOURCE_DATE_EPOCH", time.time()))
+)
+
+source_suffix = ".rst"
+master_doc = "index"
+project = "apispec"
+copyright = f"2014-{build_date:%Y}, Steven Loria and contributors"
+
+version = release = apispec.__version__
+
+exclude_patterns = ["_build"]
+
+# THEME
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
