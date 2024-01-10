3878f34a6c39e32b19fd567d8329a0a3e158a212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 07:05:34 +0000

    Add package py-llm-core/2.8.1/py-llm-core-2.8.1.tar.gz

---
+++ packages/py-llm-core/py-llm-core-2.8.1.tar.gz/py-llm-core-2.8.1/src/llm_core/settings.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import os
+from decouple import config
+
+MODELS_CACHE_DIR = config(
+    "MODELS_CACHE_DIR",
+    default=os.path.expanduser("~/.cache/py-llm-core/models"),
+)
+
+
+if not os.path.exists(MODELS_CACHE_DIR):
+    os.makedirs(MODELS_CACHE_DIR)
+
+# Store models in the same directory to simplify purges
+os.environ["TIKTOKEN_CACHE_DIR"] = MODELS_CACHE_DIR
+
+
+USE_AZURE_OPENAI = config("USE_AZURE_OPENAI", cast=bool, default=False)
+AZURE_OPENAI_ENDPOINT = config("AZURE_OPENAI_ENDPOINT", default=None)
