53ecd21f7e6ff3dd9c9012f8171dd6758755d876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:05:03 +0000

    Add package futuresio/0.0.0/futuresio-0.0.0.tar.gz

---
+++ packages/futuresio/futuresio-0.0.0.tar.gz/futuresio-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='futuresio',
+  version='0.0.0',
+  packages=find_packages(),
+  install_requires=[],
+  license='MIT/Apache-2.0',
+  url="http://github.com/al8n/pyo3-io",
+  author='Al Liu',
+  author_email='scygliu1@gmail.com'
+)
