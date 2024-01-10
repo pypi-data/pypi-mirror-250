bce9caaf618ca93cda794d0fbadf4631590f6257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 05:05:07 +0000

    Add package zermelo-api-vogk/0.2.17/zermelo_api_vogk-0.2.17.tar.gz

---
+++ packages/zermelo-api-vogk/zermelo_api_vogk-0.2.17.tar.gz/zermelo_api_vogk-0.2.17/app/zermelo_api/src/vakken.py
@@ -0,0 +1,53 @@
+from .zermelo_api import ZermeloCollection, zermelo
+from .logger import makeLogger, DEBUG
+from .groepen import Groepen, Groep
+from dataclasses import dataclass, InitVar, field
+
+logger = makeLogger("VAKKEN")
+
+
+@dataclass
+class Vak:
+    id: int
+    subject: int
+    departmentOfBranch: int
+    studentCanEdit: bool
+    sectionOfBranch: int
+    courseType: str
+    lessonHoursInClassPeriods: list[dict]
+    excludedSegments: list[int]
+    referenceWeek: dict  # (year:int, weekNumber: int, schoolYear: int)
+    isExam: bool
+    scheduleCode: str
+    subjectType: str
+    subjectCode: str
+    departmentOfBranchCode: str
+    iltCode: int
+    qualifiedCode: str
+    subjectScheduleCode: str
+    subjectName: str
+    sectionOfBranchAbbreviation: str
+
+    def getName(self) -> str:
+        if "/" in self.subjectName:
+            logger.debug(f"old name: {self.subjectName}")
+            parts = self.subjectName.split("/")
+            frontpart = parts[0]
+            nameparts = frontpart.split(" ")
+            nameparts.pop(-1)
+            name = " ".join(nameparts)
+            logger.debug(f"new name: {name}")
+            return name.strip()
+        return self.subjectName.strip()
+
+
+@dataclass
+class Vakken(ZermeloCollection, list[Vak]):
+    schoolinschoolyear: InitVar
+
+    def __post_init__(self, schoolinschoolyear: int):
+        query = f"choosableindepartments?schoolInSchoolYear={schoolinschoolyear}"
+        self.load_collection(query, Vak)
+
+    def get_leerjaar_vakken(self, leerjaar_id: int) -> list[Vak]:
+        return [vak for vak in self if vak.departmentOfBranch == leerjaar_id]
