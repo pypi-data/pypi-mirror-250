257f409177d5a37129091babb96a196349df0119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 15:03:27 +0000

    Add package demommath/0.1.4/demommath-0.1.4.tar.gz

---
+++ packages/demommath/demommath-0.1.4.tar.gz/demommath-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='demommath',
+    version='0.1.4',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'your-command-name=your_package.module1:main_function',
+        ],
+    },
+    author='Gangesh',
+    author_email='gvelip@nio.org',
+    description='This package is for testing And How to create a package',
+
+)
+
