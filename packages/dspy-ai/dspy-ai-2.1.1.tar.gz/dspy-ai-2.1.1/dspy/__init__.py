661ff0df3e1e61c260db7ac0312610c597d8d2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 08:03:55 +0000

    Add package dspy-ai/2.1.1/dspy-ai-2.1.1.tar.gz

---
+++ packages/dspy-ai/dspy-ai-2.1.1.tar.gz/dspy-ai-2.1.1/dspy/__init__.py
@@ -0,0 +1,29 @@
+from dsp.modules.hf_client import ChatModuleClient
+from dsp.modules.hf_client import HFServerTGI, HFClientVLLM, HFClientSGLang
+from .signatures import *
+
+from .retrieve import *
+from .predict import *
+from .primitives import *
+# from .evaluation import *
+
+
+# FIXME:
+
+
+import dsp
+
+settings = dsp.settings
+
+OpenAI = dsp.GPT3
+ColBERTv2 = dsp.ColBERTv2
+Pyserini = dsp.PyseriniRetriever
+
+HFClientTGI = dsp.HFClientTGI
+HFClientVLLM = HFClientVLLM
+
+Anyscale = dsp.Anyscale
+HFModel = dsp.HFModel
+
+configure = settings.configure
+context = settings.context
