183ed9f535164dd693e823290a884060941b0070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-10 15:04:16 +0000

    Add package arafkhan-hello/0.3/arafkhan_hello-0.3.tar.gz

---
+++ packages/arafkhan-hello/arafkhan_hello-0.3.tar.gz/arafkhan_hello-0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arafkhan_hello',
+    version='0.3',
+    packages=['arafkhan_hello'],
+    url='',
+    license='',
+    author='Araf',
+    author_email='arafkhan565@gmail.com',
+    description='Simple Hello world package',
+    install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+    ],
+    entry_points={
+        "console_scripts": [
+            "arafkhan-hello = arafkhan_hello:hello",
+        ],
+    },
+)
