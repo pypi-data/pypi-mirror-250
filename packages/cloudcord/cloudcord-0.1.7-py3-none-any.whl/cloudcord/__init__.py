7f5e1a2a5f39d7ba8826c71de42aa36a2d6e6d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:05:48 +0000

    Add package cloudcord/0.1.7/cloudcord-0.1.7-py3-none-any.whl

---
+++ packages/cloudcord/cloudcord-0.1.7-py3-none-any.whl/cloudcord/__init__.py
@@ -0,0 +1,14 @@
+__title__ = "cloudcord"
+__author__ = "littxle"
+__license__ = "MIT"
+__version__ = "0.1.7"
+
+from . import blacklist, components, emb, logs, sql, times, utils
+from .bot import *
+from .components import *
+from .emb import *
+from .enums import *
+from .logs import *
+from .sql import *
+from .times import *
+from .utils import *
