897c6feb1827312c6419e749dffd16462681488e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:05:17 +0000

    Add package rolfdog/0.0.5/rolfdog-0.0.5.tar.gz

---
+++ packages/rolfdog/rolfdog-0.0.5.tar.gz/rolfdog-0.0.5/setup.py
@@ -0,0 +1,41 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    # Basic package information:
+    name='rolfdog',  
+    version='0.0.5',
+    packages=find_packages(),  # Automatically find packages in the directory
+
+    # Dependencies:
+    install_requires=[
+        'numpy>=1.1.1',  
+        'pandas>=0.1.1',  
+    ],
+
+    # Metadata for PyPI:
+    author          ='James A. Rolfsen',
+    author_email    ='james.rolfsen@think.dev', 
+	description     ='Rolfdog Rolfdog Rolfdog',
+	url             ='https://github.com/jrolf/rolfdog',    # Link to your repo
+    
+    #long_description=open('README.md').read(),
+    #long_description_content_type='text/markdown',  # If your README is in markdown
+
+    # More classifiers: https://pypi.org/classifiers/
+    classifiers=[
+        'Programming Language :: Python :: 3.7', 
+        'License :: OSI Approved :: MIT License',  # Ensure this matches your LICENSE file
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    
+
+)
+
+
+
+
+
