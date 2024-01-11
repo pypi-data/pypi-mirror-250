9da97b6cfd90a8a058385245b4e79cbdf8b6e76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 03:04:18 +0000

    Add package platform-users/0.5/platform_users-0.5.tar.gz

---
+++ packages/platform-users/platform_users-0.5.tar.gz/platform_users-0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='platform_users',
+    version='0.5',
+    author='Vishwajeet Kale',
+    author_email='vishwajeet.kale.v2stech@gmail.com',
+    description='This is an app for managing users in a SaaS product named Platform.',
+    packages=find_packages(),
+    install_requires=[
+        'Django>=3.1',
+        'djangorestframework>=3.11.1'
+        # Add other dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
