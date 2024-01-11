a20e089bfe85f6a3825a7e6ff6db8dba06f81ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 03:05:12 +0000

    Add package yuanfen/2024.1.10.2/yuanfen-2024.1.10.2.tar.gz

---
+++ packages/yuanfen/yuanfen-2024.1.10.2.tar.gz/yuanfen-2024.1.10.2/src/yuanfen/__init__.py
@@ -0,0 +1,22 @@
+from . import time
+from .config import Config
+from .email import Email
+from .env import APP_ENV
+from .group_robot import GroupRobot
+from .logger import Logger
+from .response import BaseResponse, ErrorResponse, SuccessResponse
+
+__version__ = "2024.1.10.2"
+
+__all__ = [
+    "APP_ENV",
+    "BaseResponse",
+    "Config",
+    "Email",
+    "ErrorResponse",
+    "GroupRobot",
+    "Logger",
+    "SuccessResponse",
+    "VERSION",
+    "time",
+]
