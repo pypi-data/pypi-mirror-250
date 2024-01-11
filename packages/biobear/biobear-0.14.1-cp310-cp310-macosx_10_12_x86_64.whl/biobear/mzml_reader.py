b09631b2e24f99ba823fd31e3694f07283e9fa91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 15:04:12 +0000

    Add package biobear/0.14.1/biobear-0.14.1-cp310-cp310-macosx_10_12_x86_64.whl

---
+++ packages/biobear/biobear-0.14.1-cp310-cp310-macosx_10_12_x86_64.whl/biobear/mzml_reader.py
@@ -0,0 +1,35 @@
+"""MzML File Reader."""
+
+import os
+
+from biobear.compression import Compression
+from biobear.reader import Reader
+from biobear.deprecated import deprecated
+
+from .biobear import _ExonReader
+
+
+@deprecated
+class MzMLReader(Reader):
+    """A MzML File Reader."""
+
+    def __init__(
+        self, path: os.PathLike, compression: Compression = Compression.INFERRED
+    ):
+        """Initialize the MzMLReader.
+
+        Args:
+            path: The path to the MzML file.
+        """
+
+        self.compression = compression.infer_or_use(path)
+
+        if self.compression == Compression.GZIP:
+            self._reader = _ExonReader(str(path), "MZML", "GZIP")
+        else:
+            self._reader = _ExonReader(str(path), "MZML", None)
+
+    @property
+    def inner(self):
+        """Return the inner reader."""
+        return self._reader
