4bd2984ea46962fbf02ee8edbf94d43e8b7a25c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 03:03:39 +0000

    Add package w-rst/1.3/w-rst-1.3.tar.gz

---
+++ packages/w-rst/w-rst-1.3.tar.gz/w-rst-1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="w-rst",
+    version="1.3",
+    packages=find_packages(),
+    install_requires=[
+        "datetime",
+    ],
+    author="zzt",
+    author_email="zzt@qq.com",
+    description="A useless output model",
+    license="MIT",
+    keywords="wrst",
+    url="https://github.com/zzt/w-rst.git"
+)
