ae95561471404e47e03f2a4598b1a4879f4237a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 03:04:08 +0000

    Add package my-programming-library/0.1.0/my_programming_library-0.1.0-py3-none-any.whl

---
+++ packages/my-programming-library/my_programming_library-0.1.0-py3-none-any.whl/my_programming_library/__init__.py
@@ -0,0 +1,22 @@
+def add(number1: float, number2: float) -> float:
+    """
+    Function adds two numbers and return sum of two numbers.
+    Args:
+        number1 (float): 1st float number
+        number2 (float): 2nd float number
+    Returns:
+        float: Sum of number1 and number2
+    """
+    return number1+number2
+
+
+def subtract(number1: float, number2: float) -> float:
+    """
+    Function subtracts two numbers and return difference of two numbers.
+    Args:
+        number1 (float): 1st float number
+        number2 (float): 2nd float number
+    Returns:
+        float: Difference of number1 and number2
+    """
+    return number1-number2
