da5e5a48e1042798fe016f2b8411ec917a91a215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 14:04:00 +0000

    Add package pyproject-fmt/1.6.0/pyproject_fmt-1.6.0-py3-none-any.whl

---
+++ packages/pyproject-fmt/pyproject_fmt-1.6.0-py3-none-any.whl/pyproject_fmt/__init__.py
@@ -0,0 +1,12 @@
+"""Format your pyproject.toml."""
+from __future__ import annotations
+
+from ._version import version as __version__
+from .formatter import format_pyproject
+from .formatter.config import Config
+
+__all__ = [
+    "__version__",
+    "Config",
+    "format_pyproject",
+]
