9afe5f65f558f629a303fa05f07534c59f526bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 06:04:13 +0000

    Add package lionagi/0.0.116/lionagi-0.0.116.tar.gz

---
+++ packages/lionagi/lionagi-0.0.116.tar.gz/lionagi-0.0.116/lionagi/core/instruction_set/instruction_sets.py
@@ -0,0 +1,7 @@
+from lionagi.structures import Relationship, Structure
+from ..messages import Message, Instruction
+
+# dynamically structured preconfigured instructions
+
+class InstructionSet(Structure):
+    ...
