20d7acb97464af56090f59bf8b2c44acc4317d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 15:04:05 +0000

    Add package vt4-client-test/0.1.10/vt4_client_test-0.1.10.tar.gz

---
+++ packages/vt4-client-test/vt4_client_test-0.1.10.tar.gz/vt4_client_test-0.1.10/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vt4_client_test',
+    version='0.1.10',
+    packages=find_packages(where="src"),
+    package_dir={'': 'src'},
+    install_requires=[],
+    author='Adam Drag',
+    description='Python client for the CalcTree API',
+    url='https://github.com/adam-drag/calctree-client',
+)
---
+++ packages/vt4-client-test/vt4_client_test-0.1.10.tar.gz/vt4_client_test-0.1.10/src/calctree_client/client.py
@@ -0,0 +1,33 @@
+import json
+import ssl
+from urllib import request
+
+
+class CalctreeClient:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def run_calculation(self, page_id, ct_cells, host="https://api.calctree.com"):
+        url = f"{host}/api/calctree-cell/run-calculation"
+        headers = {
+            "x-api-key": self.api_key,  # local
+            "content-type": "application/json"
+        }
+        body = {
+            "pageId": page_id,
+            "ctCells": ct_cells
+        }
+
+        data = json.dumps(body).encode("utf8")
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+
+        req = request.Request(url, data, headers)
+        res = request.urlopen(req, context=ctx)
+        output = res.read()
+
+        if 200 <= res.status <= 299:
+            return [{"param": j["title"], "value": j["value"]} for j in json.loads(output)]
+        else:
+            raise Exception(f"Error-: {res.status_code}, {res.text}")
