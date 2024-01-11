ca45a002eb9ce91ee30f946234c79de8498bb614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 06:03:26 +0000

    Add package upstash-vector/0.1.1/upstash_vector-0.1.1.tar.gz

---
+++ packages/upstash-vector/upstash_vector-0.1.1.tar.gz/upstash_vector-0.1.1/upstash_vector/core/index_operations.py
@@ -0,0 +1,32 @@
+# Define vector operations here:
+# Upsert and query functions and signatures
+
+from typing import Any
+
+ResponseType = Any
+
+
+class IndexOperations:
+    def execute_request(self, payload, path) -> ResponseType:
+        raise NotImplementedError("execute_request")
+
+    # TODO: define list size here, maybe?
+    # TODO: maybe, also define payload type
+    def upsert(self, id: str | int="", vector: list[float]=[], vectors:list[dict]=[]) -> ResponseType:
+        payload = {"id": id, "vector": vector}
+
+        if len(vectors) > 0:
+            payload = [{"id": vector[0], "vector": vector[1]} for vector in vectors]
+
+        path = "/upsert"
+        return self.execute_request(payload=payload, path=path)
+
+    def query(self, vector: list[float], top_k: int, include_vectors: bool):
+        payload = {"vector": vector, "topK": top_k, "includeVectors": include_vectors}
+        path = "/query"
+        return self.execute_request(payload=payload, path=path)
+
+    def delete(self, id_list: list[int|str]):
+        payload = id_list
+        path = "/delete"
+        return self.execute_request(payload=payload, path=path)
