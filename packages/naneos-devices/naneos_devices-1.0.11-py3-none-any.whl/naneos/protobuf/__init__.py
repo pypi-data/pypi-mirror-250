d2b4d9523d6d723791f5b4444cae1eb11dac3076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 05:03:42 +0000

    Add package naneos-devices/1.0.11/naneos_devices-1.0.11-py3-none-any.whl

---
+++ packages/naneos-devices/naneos_devices-1.0.11-py3-none-any.whl/naneos/protobuf/__init__.py
@@ -0,0 +1,11 @@
+from .protobuf import (
+    create_combined_entry,
+    create_partector1_entry,
+    create_partector_2_pro_garagenbox,
+)
+
+__all__ = [
+    "create_partector1_entry",
+    "create_combined_entry",
+    "create_partector_2_pro_garagenbox",
+]
