ca2fa2b8192a8125da9273c98d72bdfc290386a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 06:03:59 +0000

    Add package pyastgrep/1.3.2/pyastgrep-1.3.2-py3-none-any.whl

---
+++ packages/pyastgrep/pyastgrep-1.3.2-py3-none-any.whl/pyastgrep/files.py
@@ -0,0 +1,126 @@
+from __future__ import annotations
+
+import ast
+import os
+import re
+from dataclasses import dataclass
+from pathlib import Path
+from typing import BinaryIO, Iterable, Sequence
+
+from .ignores import DirWalker, WalkError
+
+
+@dataclass(frozen=True)
+class MissingPath:
+    path: Path
+
+
+def get_files_to_search(
+    paths: Sequence[Path | BinaryIO],
+    include_hidden: bool = False,
+    respect_global_ignores: bool = True,
+    respect_vcs_ignores: bool = True,
+) -> Iterable[Path | BinaryIO | MissingPath | WalkError]:
+    """
+    Entry-point function for finding files to search.
+
+    By default, POSIX hidden files (starting with `.`) will be skipped - pass
+    `include_hidden=True` to include them.
+
+    By default, global .gitignore file will be respected - pass
+    `respect_global_ignores=False` to ignore it
+    """
+    walker = DirWalker(
+        glob="*.py",
+        include_hidden=include_hidden,
+        respect_global_ignores=respect_global_ignores,
+        respect_vcs_ignores=respect_vcs_ignores,
+    )
+    working_dir = Path(os.getcwd())
+    for path in paths:
+        if isinstance(path, Path):
+            if not path.exists():
+                yield MissingPath(path)
+            elif path.is_file():
+                yield path
+            else:
+                yield from walker.for_dir(path, working_dir).walk()
+        else:
+            # BinaryIO
+            yield path
+
+
+# See https://peps.python.org/pep-0263/
+# I couldn't find a stdlib function for this
+_ENCODING_RE = re.compile(b"^[ \t\f]*#.*?coding[:=][ \t]*([-_.a-zA-Z0-9]+)")
+
+
+def get_encoding(python_file_bytes: bytes) -> str:
+    # Search in first two lines:
+    current_idx = 0
+    for line_num in (1, 2):
+        # what does a line break character look like
+        # if we don't know the encoding? Have to assume '\n' for now
+        linebreak_idx = python_file_bytes.find(b"\n", current_idx)
+        if linebreak_idx < 0:
+            line = python_file_bytes[current_idx:]
+        else:
+            line = python_file_bytes[current_idx:linebreak_idx]
+        coding_match = _ENCODING_RE.match(line)
+        if coding_match:
+            return coding_match.groups()[0].decode("ascii")
+        if linebreak_idx < 0:
+            break
+        else:
+            current_idx = linebreak_idx + 1
+    return "utf-8"
+
+
+def parse_python_file(contents: bytes, filename: str | Path, *, auto_dedent: bool) -> tuple[str, ast.AST]:
+    """
+    Parse Python file and return a tuple of (contents as string, parsed contents)
+    """
+    if auto_dedent:
+        contents = auto_dedent_code(contents)
+
+    parsed_ast: ast.AST = ast.parse(contents, str(filename))
+    # Need `parent` backlinks for StatementContext and for position_from_node:
+    for node in ast.walk(parsed_ast):
+        for child in ast.iter_child_nodes(node):
+            child.parent = node  # type: ignore
+
+    # ast.parse does it's own encoding detection, which we have to replicate
+    # here since we can't assume utf-8
+    encoding = get_encoding(contents)
+    str_contents = contents.decode(encoding)
+    return str_contents, parsed_ast
+
+
+def auto_dedent_code(python_code: bytes) -> bytes:
+    # Can't use textwrap.dedent, it only works on str.
+
+    # Plus we can have a simpler algo:
+    # - optimize for the case where there is no whitespace prefix
+    # - bail out if the first whitespace prefix is
+    #   longer than others, because this will be invalid Python
+    #   code anyway.
+
+    # Find whitespace prefix of first non-empty line
+    m = re.match(rb"(^( +)\S|^\s*\n( +)\S)", python_code, re.MULTILINE)
+    if not m:
+        return python_code
+
+    groups = m.groups()
+    # One of the groups will be `None`
+    whitespace_prefix = groups[1] or groups[2]
+    strip_amount = len(whitespace_prefix)
+    new_lines = []
+    for line in python_code.split(b"\n"):
+        to_strip = line[0:strip_amount]
+        # If not all whitespace, we've made a mistake
+        # and the first line indent is not a common indent.
+        # (this will be invalid Python code anyway)
+        if to_strip.strip() != b"":
+            return python_code
+        new_lines.append(line[strip_amount:])
+    return b"\n".join(new_lines)
---
+++ packages/pyastgrep/pyastgrep-1.3.2-py3-none-any.whl/pyastgrep/search.py
@@ -0,0 +1,160 @@
+"""Functions for searching the XML from file, file contents, or directory."""
+from __future__ import annotations
+
+import ast
+from dataclasses import dataclass
+from pathlib import Path
+from typing import BinaryIO, Callable, Iterable, Literal, Sequence, Union
+
+from lxml.etree import _Element
+
+from pyastgrep.ignores import WalkError
+
+from . import xml
+from .asts import ast_to_xml
+from .files import MissingPath, get_files_to_search, parse_python_file
+
+Pathlike = Union[Path, Literal["<stdin>"]]
+
+
+@dataclass(frozen=True)
+class Match:
+    path: Pathlike
+    file_lines: list[str]
+    xml_element: _Element
+    position: Position
+    ast_node: ast.AST
+
+    @property
+    def matching_line(self) -> str:
+        return self.file_lines[self.position.lineno - 1]
+
+
+@dataclass(frozen=True)
+class Position:
+    lineno: int  # 1-indexed, as per AST
+    col_offset: int  # 0-indexed, as per AST
+
+
+@dataclass(frozen=True)
+class ReadError:
+    path: str
+    exception: Exception
+
+
+class NonElementReturned(ValueError):
+    pass
+
+
+@dataclass(frozen=True)
+class FileFinished:
+    """
+    Sentinel used for flushing output
+    """
+
+    source: Path | BinaryIO
+
+
+def position_from_node(node: ast.AST) -> Position | None:
+    lineno = getattr(node, "lineno", None)
+    col_offset = getattr(node, "col_offset", None)
+    if lineno is not None and col_offset is not None:
+        return Position(int(lineno), int(col_offset))
+    if (parent := getattr(node, "parent", None)) is not None:
+        return position_from_node(parent)
+    return None
+
+
+def get_query_func(*, xpath2: bool) -> Callable[[_Element, str], Iterable[_Element]]:
+    if xpath2:
+        from .xpath2 import elementpath_query
+
+        return elementpath_query
+    else:
+        return xml.lxml_query
+
+
+def search_python_files(
+    paths: Sequence[Path | BinaryIO],
+    expression: str,
+    xpath2: bool = False,
+    include_hidden: bool = False,
+    respect_global_ignores: bool = True,
+    respect_vcs_ignores: bool = True,
+) -> Iterable[Match | MissingPath | ReadError | WalkError | NonElementReturned | FileFinished]:
+    """
+    Perform a recursive search through Python files.
+
+    Paths may include directories, e.g "." for the current directory.
+    .gitignore rules will be applied automatically.
+
+    """
+    query_func = get_query_func(xpath2=xpath2)
+
+    for path in get_files_to_search(
+        paths,
+        include_hidden=include_hidden,
+        respect_global_ignores=respect_global_ignores,
+        respect_vcs_ignores=respect_vcs_ignores,
+    ):
+        if isinstance(path, MissingPath):
+            yield path
+        elif isinstance(path, WalkError):
+            yield path
+        else:
+            yield from search_python_file(path, query_func, expression)
+            yield FileFinished(path)
+
+
+def search_python_file(
+    path: Path | BinaryIO,
+    query_func: Callable[[ast.AST, str], Iterable[_Element]],
+    expression: str,
+) -> Iterable[Match | ReadError | NonElementReturned]:
+    node_mappings: dict[_Element, ast.AST] = {}
+    source: Pathlike
+    auto_dedent = False
+    if isinstance(path, Path):
+        source = path
+        try:
+            contents = path.read_bytes()
+        except OSError as ex:
+            yield ReadError(str(path), ex)
+            return
+    else:
+        source = "<stdin>"
+        contents = path.read()
+        auto_dedent = True
+
+    try:
+        str_contents, parsed_ast = parse_python_file(contents, source, auto_dedent=auto_dedent)
+    except (SyntaxError, ValueError) as ex:
+        yield ReadError(str(source), ex)
+        return
+
+    file_lines = str_contents.splitlines()
+    xml_ast = ast_to_xml(
+        parsed_ast,
+        node_mappings,
+    )
+
+    matching_elements = query_func(xml_ast, expression)
+
+    try:
+        iterator = iter(matching_elements)
+    except TypeError:
+        yield NonElementReturned(matching_elements)
+        return
+
+    for element in iterator:
+        if not isinstance(element, _Element):
+            # Most likely an _ElementUnicodeResult, the result of a query that terminated in
+            # an attribute rather than a node. We have no way of getting from here to
+            # something representing an AST node.
+            yield NonElementReturned(element)
+
+        ast_node = node_mappings.get(element, None)
+        if ast_node is not None:
+            position = position_from_node(ast_node)
+            if position is not None:
+                yield Match(source, file_lines, element, position, ast_node)
