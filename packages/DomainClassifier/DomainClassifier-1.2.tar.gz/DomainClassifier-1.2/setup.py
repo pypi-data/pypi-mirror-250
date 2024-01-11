a44e82a303cf303bce5cf0ff64789ac16dd487b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 04:04:30 +0000

    Add package DomainClassifier/1.2/DomainClassifier-1.2.tar.gz

---
+++ packages/DomainClassifier/DomainClassifier-1.2.tar.gz/DomainClassifier-1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name="DomainClassifier",
+    version="1.2",
+    packages=find_packages(),
+    install_requires=['dnspython', 'IPy', 'pybgpranking'],
+    author="Alexandre Dulaunoy",
+    author_email="a@foo.be",
+    description="DomainClassifier is a Python library to extract and classify Internet domains/hostnames/IP addresses from raw unstructured text files following their existence, localization or attributes.",
+    long_description="DomainClassifier is a Python library to extract and classify Internet domains/hostnames/IP addresses from raw unstructured text files following their existence, localization or attributes.",
+    license="AGPL",
+    keywords="internet mining domain resolver geolocalisation",
+    url="http://github.com/adulau/DomainClassifier"
+)
