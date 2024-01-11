82f1e03ab4ac9c933295c5f468cb5230facb3161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:05:04 +0000

    Add package chatllm/2024.1.11.17.38.56/chatllm-2024.1.11.17.38.56-py3-none-any.whl

---
+++ packages/chatllm/chatllm-2024.1.11.17.38.56-py3-none-any.whl/chatllm/webui/chatfile_nesc_v2.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : st_chat
+# @Time         : 2023/8/11 14:45
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+
+import streamlit as st
+# st.set_page_config('🔥ChatLLM', layout='wide', initial_sidebar_state='collapsed')
+
+from chatllm.llmchain import init_cache
+from chatllm.llmchain.applications import ChatFiles
+
+from meutils.pipe import *
+from meutils.notice.feishu import send_message
+from meutils.serving.streamlit import hide_st_style, st_chat_message, ChatMessage
+
+send_message = partial(
+    send_message,
+    url="https://open.feishu.cn/open-apis/bot/v2/hook/f0475882-ad39-49d9-ad77-523c0e768e96"
+)
+
+hide_st_style()
+
+init_cache(1)
+
+
+@st.cache_resource(show_spinner=False)
+def fn(file_path):
+    chatfile = (
+        ChatFiles(
+            model="gpt-3.5-turbo",
+            embedding_model="text-embedding-ada-002",
+        )
+        .load_file(file_path=file_path, chunk_size=500)
+    )
+
+    return chatfile
+
+
+chatfile = fn("附件1：东北证券股份有限公司员工合规手册（2023年12月）.docx")
+
+if __name__ == '__main__':
+    col1, col2, col3, *_ = st.columns(3)
+    with col1:
+        st.image('规丞相.png', width=64)
+    with col2:
+        st.markdown('##### ')
+        st.markdown('##### ')
+        st.markdown('##### 规丞相驾到')
+
+    st.markdown('> ⚠️“规丞相”仅供东北证券内部测试使用，所做回答不得用于东北证券官方回复。')
+
+    st_chat_message(ChatMessage(avatar="规丞相.png", generator="欢迎来找**规丞相**，您有什么要咨询的吗❓"))
+
+    for message in st.session_state.messages:  # 设计his
+        st_chat_message(message)
+
+    print(st.session_state.messages)
+
+    prompt = st.chat_input("    🤔 你可以问我任何问题", key='xx')  # 最下面
+    if prompt:
+        send_message(prompt)
+        with st.spinner('AI 🤔'):
+            st_chat_message(ChatMessage(name="user", generator=prompt), is_history=True)
+
+            st_chat_message(
+                ChatMessage(
+                    avatar="!.png",
+                    generator="`大模型根据公司相关法规及提问语义做答，不作为合规管理官方回复。`"
+                )
+            )
+
+            generator = chatfile.create(prompt)
+
+            st_chat_message(ChatMessage(avatar="规丞相.png", generator=generator), is_history=True)
