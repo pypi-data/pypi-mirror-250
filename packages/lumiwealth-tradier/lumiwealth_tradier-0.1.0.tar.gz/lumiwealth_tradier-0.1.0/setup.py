9476586b81d8f749f8beaf4cb1be55312dc79a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 06:04:07 +0000

    Add package lumiwealth-tradier/0.1.0/lumiwealth_tradier-0.1.0.tar.gz

---
+++ packages/lumiwealth-tradier/lumiwealth_tradier-0.1.0.tar.gz/lumiwealth_tradier-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lumiwealth_tradier",
+    version="0.1.0",
+    author="David MacLeod and Robert Grzesik",
+    description="lumiwealth_tradier is a Python package that serves as a wrapper for the Tradier brokerage API. "
+    "This package simplifies the process of making API requests, handling responses, and performing various trading "
+    "and account management operations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lumiwealth/lumiwealth_tradier",
+    packages=find_packages(),
+    project_urls={"Bug Tracker": "https://github.com/Lumiwealth/lumiwealth_tradier/issues"},
+    keywords="tradier finance api",
+    install_requires=["pandas>=2.0.0", "numpy"],
+    python_requires=">=3.7",
+)
