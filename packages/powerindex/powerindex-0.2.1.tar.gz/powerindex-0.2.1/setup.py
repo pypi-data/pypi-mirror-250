bb8273c5aedf2e6b777f3d9281c2c4e36fdbf869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 04:05:17 +0000

    Add package powerindex/0.2.1/powerindex-0.2.1.tar.gz

---
+++ packages/powerindex/powerindex-0.2.1.tar.gz/powerindex-0.2.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import os
+
+long_description = """powerindex calculates some common power indices (related to game theory and political science)
+"""
+
+setup(name='powerindex',
+    version=os.environ['CI_COMMIT_TAG'],
+    description='python library to calculate power indices in weighted voting games',
+    author='Maxim Litvak',
+    author_email='maxim@litvak.eu',
+    url='http://gitlab.com/maxlit/powerindex',
+    test_suite='test',
+    packages=['powerindex'],
+    keywords=['power index','voting','Banzhaf','Shapley','Shubik', 'weighted voting game','game theory', 'political science'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Education',
+        'Topic :: Sociology'
+        ],
+    entry_points={
+        'console_scripts': [
+            'px = powerindex.powerindex:main'
+        ]
+        }
+                                         )
+
