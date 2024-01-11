20e759008dd0f1ea5170d554bbcd066590b4b9c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:05:28 +0000

    Add package Zazzle/0.1.1/Zazzle-0.1.1.tar.gz

---
+++ packages/Zazzle/Zazzle-0.1.1.tar.gz/Zazzle-0.1.1/zazzle/zz_globals.py
@@ -0,0 +1,11 @@
+class Variables():
+    gl_log_name = None
+    gl_log_directory = None
+    gl_log_format = None
+    gl_log_write_method = None
+    gl_log_level = None
+    gl_function_tracking = False
+    gl_function_tracking_style = "curved"
+    gl_current_function = None
+    gl_current_function_string = 0
+    gl_last_message = None
