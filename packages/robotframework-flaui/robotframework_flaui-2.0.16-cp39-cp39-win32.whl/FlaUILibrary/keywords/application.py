24cc6f6fcb1ee6c6ecf0fc1234f49e9cfbf09de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 15:04:34 +0000

    Add package robotframework-flaui/2.0.16/robotframework_flaui-2.0.16-cp39-cp39-win32.whl

---
+++ packages/robotframework-flaui/robotframework_flaui-2.0.16-cp39-cp39-win32.whl/FlaUILibrary/keywords/application.py
@@ -0,0 +1,154 @@
+from robotlibcore import keyword
+from FlaUILibrary.flaui.module.application import Application
+from FlaUILibrary.flaui.util.automationinterfacecontainer import AutomationInterfaceContainer
+
+
+class ApplicationKeywords:
+    """
+    Interface implementation from robotframework usage for application keywords.
+    """
+
+    def __init__(self, container: AutomationInterfaceContainer):
+        """
+        Constructor for application keywords.
+
+        ``container`` User automation container to handle element interaction.
+        """
+        self._container = container
+
+    @keyword
+    def attach_application_by_name(self, name, msg=None):
+        """
+        Attach to a running application by name.
+
+        If application with name not exists an error message will be thrown.
+
+        Arguments:
+        | Argument   | Type   | Description            |
+        | name       | string | Process name to attach |
+        | msg        | string | Custom error message   |
+
+        Examples:
+        | ${pid}  Attach Application By Name  <APPLICATION>                     |
+        | ${pid}  Attach Application By Name  <APPLICATION>  You shall not pass |
+
+        Returns:
+        | Process id from attached process if successfully |
+        """
+        return self._container.create_or_get_module().action(Application.Action.ATTACH_APPLICATION_BY_NAME,
+                                                             Application.create_value_container(name=name, msg=msg),
+                                                             msg)
+
+    @keyword
+    def attach_application_by_pid(self, pid, msg=None):
+        """
+        Attach to a running application by pid.
+
+        If application with pid not exists an error message will be thrown.
+
+        Arguments:
+        | Argument   | Type   | Description                  |
+        | pid        | number | Process identifier to attach |
+        | msg        | string | Custom error message         |
+
+        Examples:
+        | ${pid}  Attach Application By PID  <PID_NUMBER>                     |
+        | ${pid}  Attach Application By PID  <PID_NUMBER>  You shall not pass |
+
+        Returns:
+        | Process id from attached process if successfully |
+
+        """
+        return self._container.create_or_get_module().action(Application.Action.ATTACH_APPLICATION_BY_PID,
+                                                             Application.create_value_container(pid=pid, msg=msg),
+                                                             msg)
+
+    @keyword
+    def close_application(self, pid, msg=None):
+        """
+        Closes the attached application.
+
+        If no application is attached an error message will be thrown.
+
+        Arguments:
+        | Argument   | Type   | Description          |
+        | pid        | int    | Process id to close  |
+        | msg        | string | Custom error message |
+
+        Examples:
+        | ${pid}  Launch Application  <APPLICATION> |
+        | Close Application  ${pid}                 |
+
+        """
+        self._container.create_or_get_module().action(Application.Action.EXIT_APPLICATION,
+                                                      Application.create_value_container(pid=pid, msg=msg),
+                                                      msg=msg)
+
+    @keyword
+    def close_application_by_name(self, name, msg=None):
+        """
+        Closes the attached application by name.
+
+        If no application is attached an error message will be thrown.
+
+        Arguments:
+        | Argument   | Type   | Description          |
+        | name       | string | Process name to close|
+        | msg        | string | Custom error message |
+
+        Examples:
+        | Close Application By Name  $[name}         |
+
+        """
+        self._container.create_or_get_module().action(Application.Action.CLOSE_APPLICATION_BY_NAME,
+                                                      Application.create_value_container(name=name, msg=msg),
+                                                      msg=msg)
+
+    @keyword
+    def launch_application(self, application, msg=None):
+        """
+        Launches an application.
+
+        If application could not be found an error message will be thrown.
+
+        Arguments:
+        | Argument    | Type   | Description                                        |
+        | application | string | Relative or absolute path to executable to launch  |
+        | msg         | string | Custom error message                               |
+
+        Examples:
+        | ${pid}  Launch Application  <APPLICATION> |
+
+        Returns:
+        | Process id from started process if successfully |
+
+        """
+        return self._container.create_or_get_module().action(Application.Action.LAUNCH_APPLICATION,
+                                                             Application.create_value_container(name=application,
+                                                                                                msg=msg),
+                                                             msg)
+
+    @keyword
+    def launch_application_with_args(self, application, arguments, msg=None):
+        """
+        Launches an application with given arguments.
+
+        If application could not be found an error message will be thrown.
+
+        Arguments:
+        | Argument    | Type   | Description                                        |
+        | application | string | Relative or absolute path to executable to launch  |
+        | arguments   | string | Arguments for application to start                 |
+        | msg         | string | Custom error message                               |
+
+        Examples:
+        | ${pid}  Launch Application With Args  <APPLICATION>  <ARGUMENTS> |
+
+        Returns:
+        | Process id from started process if successfully |
+
+        """
+        module = self._container.create_or_get_module()
+        return module.action(Application.Action.LAUNCH_APPLICATION_WITH_ARGS,
+                             Application.create_value_container(name=application, args=arguments, msg=msg),
+                             msg)
---
+++ packages/robotframework-flaui/robotframework_flaui-2.0.16-cp39-cp39-win32.whl/FlaUILibrary/keywords/mouse.py
@@ -0,0 +1,214 @@
+from robotlibcore import keyword
+from FlaUILibrary.flaui.module import Mouse
+from FlaUILibrary.flaui.util.automationinterfacecontainer import AutomationInterfaceContainer
+
+
+class MouseKeywords:
+    """
+    Interface implementation from robotframework usage for mouse keywords.
+    """
+
+    def __init__(self, container: AutomationInterfaceContainer):
+        """
+        Constructor for mouse keywords.
+
+        ``container`` User automation container to handle element interaction.
+        """
+        self._container = container
+
+    @keyword
+    def click(self, identifier, msg=None):
+        """
+        Left click to element by an XPath.
+
+        XPaths syntax is explained in `XPath locator`.
+
+        If element could not be found by xpath an error message will be thrown.
+
+        Arguments:
+        | Argument   | Type   | Description                   |
+        | identifier | string | XPath identifier from element |
+        | msg        | string | Custom error message          |
+
+        Examples:
+        | Click  <XPATH> |
+
+        """
+        module = self._container.create_or_get_module()
+        element = module.get_element(identifier, msg=msg)
+        module.action(Mouse.Action.LEFT_CLICK,
+                      Mouse.create_value_container(element=element),
+                      msg)
+
+    @keyword
+    def click_hold(self, identifier, timeout_in_ms=1000, msg=None):
+        """
+        Left click and hold to element by XPath and release after timeout.
+
+        XPaths syntax is explained in `XPath locator`.
+
+        If element could not be found by xpath an error message will be thrown.
+
+        Arguments:
+        | Argument         | Type   | Description                   |
+        | identifier       | string | XPath identifier from element |
+        | timeout_in_ms    | int    | Holding time in ms            |
+        | msg              | string | Custom error message          |
+
+        Examples:
+        | Click Hold <XPATH>  5000 |
+
+        """
+        module = self._container.create_or_get_module()
+        element = module.get_element(identifier, msg=msg)
+        module.action(Mouse.Action.LEFT_CLICK_HOLD,
+                      Mouse.create_value_container(element=element, timeout_in_ms=int(timeout_in_ms)),
+                      msg)
+
+    @keyword
+    def double_click(self, identifier, msg=None):
+        """
+        Double click to element.
+
+        XPaths syntax is explained in `XPath locator`.
+
+        If element could not be found by xpath an error message will be thrown.
+
+        Arguments:
+        | Argument   | Type   | Description                   |
+        | identifier | string | XPath identifier from element |
+        | msg        | string | Custom error message          |
+
+        Examples:
+        | Double Click  <XPATH> |
+
+        """
+        module = self._container.create_or_get_module()
+        element = module.get_element(identifier, msg=msg)
+        module.action(Mouse.Action.DOUBLE_CLICK,
+                      Mouse.create_value_container(element=element),
+                      msg)
+
+    @keyword
+    def double_click_hold(self, identifier, timeout_in_ms=1000, msg=None):
+        """
+        Double click and hold to element by XPath and release after timeout.
+
+        XPaths syntax is explained in `XPath locator`.
+
+        If element could not be found by xpath an error message will be thrown.
+
+        Arguments:
+        | Argument         | Type   | Description                       |
+        | identifier       | string | XPath identifier from element     |
+        | timeout_in_ms    | int    | Holding time in ms                |
+        | msg              | string | Custom error message              |
+
+        Examples:
+        | Double Click Hold  <XPATH>  5000 |
+
+        """
+        module = self._container.create_or_get_module()
+        element = module.get_element(identifier, msg=msg)
+        module.action(Mouse.Action.DOUBLE_CLICK_HOLD,
+                      Mouse.create_value_container(element=element, timeout_in_ms=int(timeout_in_ms)),
+                      msg)
+
+    @keyword
+    def right_click(self, identifier, msg=None):
+        """
+        Right click to element.
+
+        XPaths syntax is explained in `XPath locator`.
+
+        If element could not be found by xpath an error message will be thrown.
+
+        Arguments:
+        | Argument   | Type   | Description                   |
+        | identifier | string | XPath identifier from element |
+        | msg        | string | Custom error message          |
+
+        Examples:
+        | Right Click  <XPATH> |
+
+        """
+        module = self._container.create_or_get_module()
+        element = module.get_element(identifier, msg=msg)
+        module.action(Mouse.Action.RIGHT_CLICK,
+                      Mouse.create_value_container(element=element),
+                      msg)
+
+    @keyword
+    def right_click_hold(self, identifier, timeout_in_ms=1000, msg=None):
+        """
+        Right click and hold to element by XPath and release after timeout.
+
+        XPaths syntax is explained in `XPath locator`.
+
+        If element could not be found by xpath an error message will be thrown.
+
+        Arguments:
+        | Argument      | Type   | Description                   |
+        | identifier    | string | XPath identifier from element |
+        | timeout_in_ms | int    | Holding time in ms            |
+        | msg           | string | Custom error message          |
+
+        Examples:
+        | Right Click Hold  <XPATH>  5000 |
+
+        """
+        module = self._container.create_or_get_module()
+        element = module.get_element(identifier, msg=msg)
+        module.action(Mouse.Action.RIGHT_CLICK_HOLD,
+                      Mouse.create_value_container(element=element, timeout_in_ms=int(timeout_in_ms)),
+                      msg)
+
+    @keyword
+    def move_to(self, identifier, msg=None):
+        """
+        Move mouse cursor to given element.
+
+        XPaths syntax is explained in `XPath locator`.
+
+        If element could not be found by xpath an error message will be thrown.
+
+        Arguments:
+        | Argument   | Type   | Description                   |
+        | identifier | string | XPath identifier from element |
+        | msg        | string | Custom error message          |
+
+        Examples:
+        | Move To  <XPATH> |
+
+        """
+        module = self._container.create_or_get_module()
+        element = module.get_element(identifier, msg=msg)
+        module.action(Mouse.Action.MOVE_TO,
+                      Mouse.create_value_container(element=element),
+                      msg)
+
+    @keyword
+    def drag_and_drop(self, start_identifier, end_identifier, msg=None):
+        """
+        Clicks and hold the item with start_identifier and drops it at item with end_identifier.
+
+        XPaths syntax is explained in `XPath locator`.
+
+        If element could not be found by xpath an error message will be thrown.
+
+        Arguments:
+        | Argument         | Type   | Description                                                        |
+        | start_identifier | string | XPath identifier of element which should be holded and draged from |
+        | end_identifier   | string | XPath identifier of element which should be holded and draged to   |
+        | msg              | string | Custom error message                                               |
+
+        Examples:
+        | Drag And Drop  <XPATH>  <XPATH> |
+
+        """
+        module = self._container.create_or_get_module()
+        start_element = module.get_element(start_identifier, msg=msg)
+        end_element = module.get_element(end_identifier, msg=msg)
+        module.action(Mouse.Action.DRAG_AND_DROP,
+                      Mouse.create_value_container(element=start_element, second_element=end_element),
+                      msg)
