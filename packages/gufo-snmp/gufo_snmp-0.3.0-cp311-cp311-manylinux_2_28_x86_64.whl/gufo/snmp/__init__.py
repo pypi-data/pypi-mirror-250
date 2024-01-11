964b546884c3ac19efb240281ea337e7f6c30701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 05:04:04 +0000

    Add package gufo-snmp/0.3.0/gufo_snmp-0.3.0-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/gufo-snmp/gufo_snmp-0.3.0-cp311-cp311-manylinux_2_28_x86_64.whl/gufo/snmp/__init__.py
@@ -0,0 +1,30 @@
+# ---------------------------------------------------------------------
+# Gufo SNMP: Python SNMP Library
+# ---------------------------------------------------------------------
+# Copyright (C) 2023, Gufo Labs
+# See LICENSE.md for details
+# ---------------------------------------------------------------------
+
+"""Gufo SNMP: The accelerated Python asyncio SNMP client library.
+
+Attributes:
+    __version__: Current version
+"""
+
+# Gufo Labs modules
+from ._fast import NoSuchInstance, SnmpDecodeError, SnmpEncodeError, SnmpError
+from .client import SnmpSession
+from .typing import ValueType
+from .version import SnmpVersion
+
+__version__: str = "0.3.0"
+__all__ = [
+    "__version__",
+    "SnmpSession",
+    "SnmpVersion",
+    "SnmpError",
+    "SnmpEncodeError",
+    "SnmpDecodeError",
+    "NoSuchInstance",
+    "ValueType",
+]
