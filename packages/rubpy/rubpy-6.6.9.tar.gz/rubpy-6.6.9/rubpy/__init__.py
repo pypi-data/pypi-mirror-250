f704c81bb8c45ba738af8c640b076f679cdacd02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 04:05:09 +0000

    Add package rubpy/6.6.9/rubpy-6.6.9.tar.gz

---
+++ packages/rubpy/rubpy-6.6.9.tar.gz/rubpy-6.6.9/rubpy/__init__.py
@@ -0,0 +1,7 @@
+from .sessions import SQLiteSession, StringSession
+from .client import Client
+from .rubino import Rubino
+from . import types, utils, filters, exceptions
+
+__author__ = 'Shayan Heidari'
+__version__ = '6.6.9'
