ae126a58d65e2584e0cf72fcca91897d86d84508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:04:53 +0000

    Add package diagrams_ext/0.23.5a7/diagrams_ext-0.23.5a7-py3-none-any.whl

---
+++ packages/diagrams_ext/diagrams_ext-0.23.5a7-py3-none-any.whl/diagrams_ext/outscale/__init__.py
@@ -0,0 +1,8 @@
+from diagrams_ext import Node
+
+
+class _Outscale(Node):
+    _provider = "outscale"
+    _icon_dir = "resources/outscale"
+
+    fontcolor = "#ffffff"
---
+++ packages/diagrams_ext/diagrams_ext-0.23.5a7-py3-none-any.whl/diagrams_ext/saas/__init__.py
@@ -0,0 +1,12 @@
+"""
+Saas provides a set of general saas services.
+"""
+
+from diagrams_ext import Node
+
+
+class _Saas(Node):
+    _provider = "saas"
+    _icon_dir = "resources/saas"
+
+    fontcolor = "#ffffff"
