6779d5f33e7af0b2ccfca6ddf3b5de84361afc54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 15:03:52 +0000

    Add package kokoridistributions/0.1/kokoridistributions-0.1.tar.gz

---
+++ packages/kokoridistributions/kokoridistributions-0.1.tar.gz/kokoridistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='kokoridistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['kokoridistributions'],
+      author='Ghislain Brice',
+      author_email='ghislainbrice@gmail.com',
+      zip_safe=False)
