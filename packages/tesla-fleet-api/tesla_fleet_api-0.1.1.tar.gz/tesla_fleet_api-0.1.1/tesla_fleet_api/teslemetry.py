31051c01ebeba38c8acdf360e779bdf9eff8aef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 15:03:55 +0000

    Add package tesla-fleet-api/0.1.1/tesla_fleet_api-0.1.1.tar.gz

---
+++ packages/tesla-fleet-api/tesla_fleet_api-0.1.1.tar.gz/tesla_fleet_api-0.1.1/tesla_fleet_api/teslemetry.py
@@ -0,0 +1,33 @@
+import aiohttp
+from .teslafleetapi import TeslaFleetApi
+from .const import Methods
+
+
+class Teslemetry(TeslaFleetApi):
+    def __init__(
+        self,
+        session: aiohttp.ClientSession,
+        access_token: str,
+        raise_for_status: bool = True,
+    ):
+        """Initialize the Teslemetry API."""
+        super().__init__(
+            session,
+            access_token,
+            use_command_protocol=False,
+            server="https://teslemetry.com",
+            raise_for_status=raise_for_status,
+            partner_scope=False,
+            user_scope=False,
+        )
+
+    async def subscription(self):
+        """Get the subscribed vehicles."""
+        return await self._request(
+            Methods.GET,
+            "/meta/subscription",
+        )
+
+    async def find_server(self):
+        """Find the server URL for the Tesla Fleet API."""
+        raise NotImplementedError("Do not use this function for Teslemetry.")
