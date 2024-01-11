ed1bcd283003bc359b6522872ba498de96a31eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 07:04:02 +0000

    Add package trontxsize/1.0.6/trontxsize-1.0.6.tar.gz

---
+++ packages/trontxsize/trontxsize-1.0.6.tar.gz/trontxsize-1.0.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="trontxsize",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    version="1.0.6",
+    license="MIT",
+    description="Calculate tron transaction size (bandwidth)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="MrNaif2018",
+    author_email="chuff184@gmail.com",
+    url="https://github.com/bitcart/trontxsize",
+    keywords=["tron", "trx", "bitcart", "bandwidth"],
+    install_requires=["protobuf", "base58"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8",
+)
