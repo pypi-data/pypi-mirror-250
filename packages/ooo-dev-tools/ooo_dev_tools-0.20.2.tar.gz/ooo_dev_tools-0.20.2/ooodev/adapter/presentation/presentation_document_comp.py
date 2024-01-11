7d9c890040131b4c03d157beed71c207ee7082b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 15:04:04 +0000

    Add package ooo-dev-tools/0.20.2/ooo_dev_tools-0.20.2.tar.gz

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.20.2.tar.gz/ooo_dev_tools-0.20.2/ooodev/adapter/presentation/presentation_document_comp.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+from typing import cast, TYPE_CHECKING
+from ooodev.adapter.drawing.generic_drawing_document_comp import GenericDrawingDocumentComp
+
+if TYPE_CHECKING:
+    from com.sun.star.presentation import PresentationDocument  # service
+    from com.sun.star.lang import XComponent
+
+
+class PresentationDocumentComp(GenericDrawingDocumentComp):
+    """
+    Class for managing PresentationDocument Component.
+    """
+
+    # pylint: disable=unused-argument
+
+    def __init__(self, component: XComponent) -> None:
+        """
+        Constructor
+
+        Args:
+            component (XComponent): UNO component that supports ``com.sun.star.presentation.PresentationDocument`` service.
+        """
+
+        super().__init__(component)
+
+    # region Overrides
+    def _ComponentBase__get_supported_service_names(self) -> tuple[str, ...]:
+        """Returns a tuple of supported service names."""
+        return ("com.sun.star.presentation.PresentationDocument",)
+
+    # endregion Overrides
+    # region Properties
+    if TYPE_CHECKING:
+
+        @property
+        def component(self) -> PresentationDocument:
+            """PresentationDocument Component"""
+            return cast("PresentationDocument", self._ComponentBase__get_component())  # type: ignore
+
+    # endregion Properties
