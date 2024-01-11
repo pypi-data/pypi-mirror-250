5f6c7aee7c4efc138f922bc4aceb9210eea9e569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 03:05:02 +0000

    Add package abhaysuvagiya/0.1.0/abhaysuvagiya-0.1.0-py3-none-any.whl

---
+++ packages/abhaysuvagiya/abhaysuvagiya-0.1.0-py3-none-any.whl/abhaysuvagiya/main.py
@@ -0,0 +1,60 @@
+def square (n :int ) -> int :
+    """
+    Calculates the square of a given number.
+
+    Parameters:
+        n (int): The number to be squared.
+
+    Returns:
+        int: The square of the given number.
+    """
+    return n*n
+
+def addition(a :int ,b :int ) -> int :
+    """
+    Adds two integers and returns the sum.
+
+    Parameters:
+        a (int): The first integer.
+        b (int): The second integer.
+
+    Returns:
+        int: The sum of the two integers.
+    """
+    return a+b
+
+def multiplication(a :int ,b :int ) -> int :
+    """
+    Multiply two integers and return the result.
+
+    Parameters:
+        a (int): The first integer.
+        b (int): The second integer.
+
+    Returns:
+        int: The product of the two integers.
+    """
+    return a*b
+
+def subtraction(a :int ,b :int ) -> int :
+    """
+    Perform subtraction between two integers and return the result.
+
+    Parameters:
+        a (int): The first integer.
+        b (int): The second integer.
+
+    Returns:
+        int: The result of subtracting b from a.
+    """
+    return a-b
+
+def division(a :int ,b :int ) -> int :
+    """
+    A function that performs division between two integers and returns the result.
+
+    :param a: An integer representing the dividend.
+    :param b: An integer representing the divisor.
+    :return: An integer representing the quotient of the division.
+    """
+    return a/b
