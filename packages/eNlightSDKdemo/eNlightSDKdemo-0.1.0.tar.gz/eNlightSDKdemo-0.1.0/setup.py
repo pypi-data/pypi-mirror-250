46a716d49fe5a639e62def9b4aa34655d30de710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:05:51 +0000

    Add package eNlightSDKdemo/0.1.0/eNlightSDKdemo-0.1.0.tar.gz

---
+++ packages/eNlightSDKdemo/eNlightSDKdemo-0.1.0.tar.gz/eNlightSDKdemo-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eNlightSDKdemo',
+    version='0.1.0',
+    description='VM create',
+    author='Akshay Ghatol',
+    author_email='akshay.ghatol@esds.co.in',
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+    ],
+)
+
