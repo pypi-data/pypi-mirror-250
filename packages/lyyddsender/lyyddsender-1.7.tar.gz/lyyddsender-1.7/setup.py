68ac3b6a23b5b399aada173379508c3f3b4a9f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 03:03:53 +0000

    Add package lyyddsender/1.7/lyyddsender-1.7.tar.gz

---
+++ packages/lyyddsender/lyyddsender-1.7.tar.gz/lyyddsender-1.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lyyddsender',
+    version='1.7',
+    author='Devin & lyy',
+    author_email='',
+    description='lyyddsendertools for lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+    ],
+)
