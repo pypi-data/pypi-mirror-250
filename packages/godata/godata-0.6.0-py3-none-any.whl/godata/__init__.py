8bfcea087e70abb3d30a2db3571c0695142f85d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 14:03:59 +0000

    Add package godata/0.6.0/godata-0.6.0-py3-none-any.whl

---
+++ packages/godata/godata-0.6.0-py3-none-any.whl/godata/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "0.6.0"
+__minimum_server_version__ = "0.6.0"
+
+from .project import (
+    create_project,
+    delete_project,
+    has_collection,
+    has_project,
+    list_collections,
+    list_projects,
+    load_project,
+)
+
+__all__ = [
+    "load_project",
+    "list_projects",
+    "create_project",
+    "list_collections",
+    "delete_project",
+    "has_project",
+    "has_collection",
+]
