9f4005c80c7d042637f834b4c528c683fd4c21c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 06:03:51 +0000

    Add package azure-quantum/1.0.0.dev1/azure-quantum-1.0.0.dev1.tar.gz

---
+++ packages/azure-quantum/azure-quantum-1.0.0.dev1.tar.gz/azure-quantum-1.0.0.dev1/azure/quantum/job/__init__.py
@@ -0,0 +1,11 @@
+##
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+##
+
+from azure.quantum.job.job import Job
+from azure.quantum.job.job_failed_with_results_error import JobFailedWithResultsError
+from azure.quantum.job.workspace_item import WorkspaceItem
+from azure.quantum.job.workspace_item_factory import WorkspaceItemFactory
+from azure.quantum.job.session import Session, SessionHost
+from azure.quantum._client.models import JobDetails
