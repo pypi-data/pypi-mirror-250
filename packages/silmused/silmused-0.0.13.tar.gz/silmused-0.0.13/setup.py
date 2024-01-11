03f2ae3b723a436a06cdf4340affc29b6ff438e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:06:04 +0000

    Add package silmused/0.0.13/silmused-0.0.13.tar.gz

---
+++ packages/silmused/silmused-0.0.13.tar.gz/silmused-0.0.13/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_namespace_packages
+exec(open('silmused/version.py').read())
+
+setup(
+    name='silmused',
+    version=__version__,
+    license='MIT License',
+    author='Karel Paan, Martti Kakk',
+    author_email='paan.karel@gmail.com',
+    packages=find_namespace_packages(),
+    url='https://github.com/kare22/silmused',
+    install_requires=[
+        'psycopg2',
+    ],
+)
