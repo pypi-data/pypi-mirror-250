7b03c7e851e7f218387e0416a15e406bed34048c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:06:04 +0000

    Add package netbox-bgp/0.12.0/netbox_bgp-0.12.0-py3-none-any.whl

---
+++ packages/netbox-bgp/netbox_bgp-0.12.0-py3-none-any.whl/netbox_bgp/graphql.py
@@ -0,0 +1,52 @@
+from graphene import ObjectType, Field
+
+from netbox.graphql.scalars import BigInt
+from netbox.graphql.types import NetBoxObjectType
+from netbox.graphql.fields import ObjectField, ObjectListField
+
+from . import models, filters
+
+
+class CommunityType(NetBoxObjectType):
+    class Meta:
+        model = models.Community
+        fields = '__all__'
+        filterset_class = filters.CommunityFilterSet
+
+
+class BgpSessionType(NetBoxObjectType):
+    class Meta:
+        model = models.BGPSession
+        fields = '__all__'
+        filterset_class = filters.BGPSessionFilterSet
+
+
+class PeerGroupType(NetBoxObjectType):
+    class Meta:
+        model = models.BGPPeerGroup
+        fields = '__all__'
+        filterset_class = filters.BGPPeerGroupFilterSet
+
+
+class RoutingPolicyType(NetBoxObjectType):
+    class Meta:
+        model = models.RoutingPolicy
+        fields = '__all__'
+        filterset_class = filters.RoutingPolicyFilterSet
+
+
+class BGPQuery(ObjectType):
+    community = ObjectField(CommunityType)
+    community_list = ObjectListField(CommunityType)
+
+    bgp_session = ObjectField(BgpSessionType)
+    bgp_session_list = ObjectListField(BgpSessionType)
+
+    peer_group = ObjectField(PeerGroupType)
+    peer_group_list = ObjectListField(PeerGroupType)
+
+    routing_policy = ObjectField(RoutingPolicyType)
+    routing_policy_list = ObjectListField(RoutingPolicyType)
+
+
+schema = BGPQuery
