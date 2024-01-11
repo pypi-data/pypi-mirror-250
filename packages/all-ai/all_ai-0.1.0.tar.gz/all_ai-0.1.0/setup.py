045711601bd87de53d6ab9da85c3b29145fdd84a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 04:05:13 +0000

    Add package all-ai/0.1.0/all_ai-0.1.0.tar.gz

---
+++ packages/all-ai/all_ai-0.1.0.tar.gz/all_ai-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = ''
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+    name="all_ai",
+    version="0.1.0",
+    author="Daku",
+    author_email="<avirakshit2002@gmail.com>",
+    description=DESCRIPTION,
+    keywords=[],
+    include_package_data=True,
+    packages=['all_ai'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
