2c9c226d3de5839f61034da24638f7ea88d9ad67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 14:04:15 +0000

    Add package pyhOn/0.15.15/pyhOn-0.15.15-py3-none-any.whl

---
+++ packages/pyhOn/pyhOn-0.15.15-py3-none-any.whl/pyhon/const.py
@@ -0,0 +1,13 @@
+AUTH_API = "https://account2.hon-smarthome.com"
+API_URL = "https://api-iot.he.services"
+API_KEY = "GRCqFhC6Gk@ikWXm1RmnSmX1cm,MxY-configuration"
+APP = "hon"
+CLIENT_ID = (
+    "3MVG9QDx8IX8nP5T2Ha8ofvlmjLZl5L_gvfbT9."
+    "HJvpHGKoAS_dcMN8LYpTSYeVFCraUnV.2Ag1Ki7m4znVO6"
+)
+APP_VERSION = "2.4.7"
+OS_VERSION = 31
+OS = "android"
+DEVICE_MODEL = "exynos9820"
+USER_AGENT = "Chrome/110.0.5481.153"
