30d00cac16257cd8a70e1c9b4c49506357a54c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:05:21 +0000

    Add package wikipedia2vec/2.0.0/wikipedia2vec-2.0.0-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/wikipedia2vec/wikipedia2vec-2.0.0-cp38-cp38-macosx_10_9_x86_64.whl/wikipedia2vec/utils/sentence_detector/icu_sentence_detector.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from icu import BreakIterator, Locale
+
+from .base_sentence_detector import BaseSentenceDetector
+from .sentence import Sentence
+
+
+class ICUSentenceDetector(BaseSentenceDetector):
+    def __init__(self, locale: str):
+        self._locale = locale
+        self._breaker = BreakIterator.createSentenceInstance(Locale(locale))
+
+    def detect_sentences(self, text: str) -> List[Sentence]:
+        self._breaker.setText(text)
+
+        ret = []
+        start = self._breaker.first()
+        for end in self._breaker:
+            ret.append(Sentence(text[start:end], start, end))
+            start = end
+
+        return ret
+
+    def __reduce__(self):
+        return (self.__class__, (self._locale,))
