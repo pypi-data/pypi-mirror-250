decd3185b4fbd5e7ca178cb24a02ff8fef57269f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:04:49 +0000

    Add package edgeiq/2.2.1/edgeiq-2.2.1-cp38-none-win_amd64.whl

---
+++ packages/edgeiq/edgeiq-2.2.1-cp38-none-win_amd64.whl/edgeiq/zones/__init__.pyi
@@ -0,0 +1,4 @@
+from .zone import Zone as Zone, ZoneType as ZoneType
+from .zone_list import RESULT_TYPE as RESULT_TYPE, ZoneList as ZoneList, ZoneResults as ZoneResults
+
+__all__ = ['Zone', 'ZoneType', 'ZoneList', 'ZoneResults', 'RESULT_TYPE']
