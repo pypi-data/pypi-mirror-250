d6644be367b97f1ad1f6dc788fc705556900623a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 06:03:30 +0000

    Add package deepgram-sdk/3.0.3/deepgram-sdk-3.0.3.tar.gz

---
+++ packages/deepgram-sdk/deepgram-sdk-3.0.3.tar.gz/deepgram-sdk-3.0.3/deepgram/clients/__init__.py
@@ -0,0 +1,67 @@
+# Copyright 2023 Deepgram SDK contributors. All Rights Reserved.
+# Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+# SPDX-License-Identifier: MIT
+
+# listen
+from .listen import ListenClient
+
+# live
+from .live import LiveClient, AsyncLiveClient
+from .live import LiveOptions
+from .live import LiveTranscriptionEvents
+from .live import (
+    LiveResultResponse,
+    MetadataResponse,
+    UtteranceEndResponse,
+    ErrorResponse,
+)
+
+# prerecorded
+from .prerecorded import PreRecordedClient, AsyncPreRecordedClient
+from .prerecorded import PrerecordedOptions
+from .prerecorded import (
+    PrerecordedSource,
+    FileSource,
+    UrlSource,
+    BufferSource,
+    ReadStreamSource,
+)
+from .prerecorded import (
+    AsyncPrerecordedResponse,
+    PrerecordedResponse,
+)
+
+# manage
+from .manage import ManageClient, AsyncManageClient
+from .manage import (
+    ProjectOptions,
+    KeyOptions,
+    ScopeOptions,
+    InviteOptions,
+    UsageRequestOptions,
+    UsageSummaryOptions,
+    UsageFieldsOptions,
+)
+from .manage import (
+    Message,
+    Project,
+    ProjectsResponse,
+    MembersResponse,
+    Key,
+    KeyResponse,
+    KeysResponse,
+    ScopesResponse,
+    InvitesResponse,
+    UsageRequest,
+    UsageRequestsResponse,
+    UsageSummaryResponse,
+    UsageFieldsResponse,
+    Balance,
+    BalancesResponse,
+)
+
+# onprem
+from .onprem import OnPremClient, AsyncOnPremClient
+
+# client options
+from ..options import DeepgramClientOptions
