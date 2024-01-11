e26c5354051dad5a8f14d4c30ac60aa3aa9e6a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 03:04:18 +0000

    Add package DashBoardUtils-DataScience/1.24/DashBoardUtils-DataScience-1.24.tar.gz

---
+++ packages/DashBoardUtils-DataScience/DashBoardUtils-DataScience-1.24.tar.gz/DashBoardUtils-DataScience-1.24/package/dashboardutil.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+from typing import List
+import pandas as pd
+
+
+class DashboardElementsBuilder:
+    """DashboardElementsBuilder build data visualisation elements on behalf of DataFrame"""
+
+    def __init__(self, data, DataClassifier) -> None:
+        if isinstance(data, pd.DataFrame):
+            self.data = data
+        else:
+            raise ValueError("data should be instance of Dataframe")
+        self.classifier = DataClassifier
+
+    def prepare_sections(self, columns):
+        """selects required cols"""
+        filters = self.data[columns]
+        return filters
+
+    def get_columns_groups(self, columns):
+        """Builds Groups on Behalf of all possible Catagories of Data"""
+        alldata_groups = []
+        for x in columns:
+            if type(self.prepare_sections(columns)[x]) != datetime:
+                alldata_groups.append(self.prepare_sections(columns).groupby([x]))
+            else:
+                alldata_groups.append(
+                    self.prepare_sections(columns).groupby(
+                        by=[
+                            getattr(self.prepare_sections(columns), x).month,
+                            getattr(self.prepare_sections(columns), x).year,
+                        ]
+                    )
+                )
+                alldata_groups.append(
+                    self.prepare_sections(columns).groupby(
+                        by=[
+                            getattr(self.prepare_sections(columns), x).day,
+                            getattr(self.prepare_sections(columns), x).month,
+                        ]
+                    )
+                )
+                alldata_groups.append(
+                    self.prepare_sections(columns).groupby(
+                        by=[getattr(self.prepare_sections(columns), x).month]
+                    )
+                )
+                alldata_groups.append(
+                    self.prepare_sections(columns).groupby(
+                        by=[getattr(self.prepare_sections(columns), x).day]
+                    )
+                )
+                alldata_groups.append(
+                    self.prepare_sections(columns).groupby(
+                        by=[getattr(self.prepare_sections(columns), x).year]
+                    )
+                )
+                alldata_groups.append(
+                    self.prepare_sections(columns).groupby(
+                        by=[
+                            getattr(self.prepare_sections(columns), x).day,
+                            getattr(self.prepare_sections(columns), x).year,
+                        ]
+                    )
+                )
+        return alldata_groups
+
+    def build_ploats(self, algorithm, columns):
+        """generate ploat data for user provided algorithm,columns"""
+        for x in self.get_columns_groups(columns):
+            yield self.classifier.build_ploats(algorithm, x)
