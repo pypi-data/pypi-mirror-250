2631f9d0ccf54703589ef7af6c24cc56eb8d97d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 04:04:33 +0000

    Add package zappend/0.1.0/zappend-0.1.0.tar.gz

---
+++ packages/zappend/zappend-0.1.0.tar.gz/zappend-0.1.0/zappend/__init__.py
@@ -0,0 +1,7 @@
+# Copyright © 2024 Norman Fomferra
+# Permissions are hereby granted under the terms of the MIT License:
+# https://opensource.org/licenses/MIT.
+
+from .api import zappend
+
+version = "0.1.0"
