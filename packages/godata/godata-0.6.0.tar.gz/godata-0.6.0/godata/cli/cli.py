fc5951e9a7422d34ac8937cd81bc875629975def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 14:03:59 +0000

    Add package godata/0.6.0/godata-0.6.0.tar.gz

---
+++ packages/godata/godata-0.6.0.tar.gz/godata-0.6.0/godata/cli/cli.py
@@ -0,0 +1,22 @@
+import click
+
+from . import project
+from .server import server
+
+
+@click.group()
+def main():
+    """Command line interface for GoData."""
+    pass
+
+
+main.add_command(project.create)
+main.add_command(project.link)
+main.add_command(project.ls)
+main.add_command(project.list)
+main.add_command(project.get)
+main.add_command(server)
+
+
+if __name__ == "__main__":
+    main()
