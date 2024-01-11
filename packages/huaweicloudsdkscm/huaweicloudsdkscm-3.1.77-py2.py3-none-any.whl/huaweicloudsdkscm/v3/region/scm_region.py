54e896f03fec93f3242c3012de69e9d2a9c69965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:05:00 +0000

    Add package huaweicloudsdkscm/3.1.77/huaweicloudsdkscm-3.1.77-py2.py3-none-any.whl

---
+++ packages/huaweicloudsdkscm/huaweicloudsdkscm-3.1.77-py2.py3-none-any.whl/huaweicloudsdkscm/v3/region/scm_region.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+import types
+import six
+
+from huaweicloudsdkcore.region.region import Region
+from huaweicloudsdkcore.region.provider import RegionProviderChain
+
+class ScmRegion:
+    _PROVIDER = RegionProviderChain.get_default_region_provider_chain("SCM")
+
+    CN_NORTH_4 = Region("cn-north-4",
+                        "https://scm.cn-north-4.myhuaweicloud.com")
+    AP_SOUTHEAST_1 = Region("ap-southeast-1",
+                        "https://scm.ap-southeast-1.myhuaweicloud.com")
+
+    static_fields = {
+        "cn-north-4": CN_NORTH_4,
+        "ap-southeast-1": AP_SOUTHEAST_1,
+    }
+
+    @classmethod
+    def value_of(cls, region_id, static_fields=None):
+        if not region_id:
+            raise KeyError("Unexpected empty parameter: region_id.")
+
+        fields = static_fields if static_fields else cls.static_fields
+
+        region = cls._PROVIDER.get_region(region_id)
+        if region:
+            return region
+
+        if region_id in fields:
+            return fields.get(region_id)
+
+        raise KeyError("Unexpected region_id: " + region_id)
+
+
