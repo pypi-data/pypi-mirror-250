d97e9e9710dd1252050436583934041e1d257893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:04:55 +0000

    Add package distributions-test-gb/0.3/distributions_test_gb-0.3.tar.gz

---
+++ packages/distributions-test-gb/distributions_test_gb-0.3.tar.gz/distributions_test_gb-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_test_gb',
+      version='0.3',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author='Julio_Barrios',
+      author_email='juliobarrios0296@gmail.com',
+      zip_safe=False)
