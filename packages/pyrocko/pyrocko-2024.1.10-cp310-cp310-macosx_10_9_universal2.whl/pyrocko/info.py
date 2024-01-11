9197c0ecbe6bb8cd42a72482ae376c78ed212897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 03:04:10 +0000

    Add package pyrocko/2024.1.10/pyrocko-2024.1.10-cp310-cp310-macosx_10_9_universal2.whl

---
+++ packages/pyrocko/pyrocko-2024.1.10-cp310-cp310-macosx_10_9_universal2.whl/pyrocko/info.py
@@ -0,0 +1,10 @@
+# This module is automatically created from setup.py
+"""
+Version information (generated from setup.py).
+"""
+git_sha1 = 'ae7c5b949d0720ea5170bb8ccc24d2a61fd8504e'
+local_modifications = False
+version = '2024.01.10'
+long_version = 'pyrocko-2024.01.10-ae7c5b949d0720ea5170bb8ccc24d2a61fd8504e-2024-01-10_11:49:05'  # noqa
+installed_date = '2024-01-10_11:49:05'
+src_path = '/private/var/folders/kk/j306053d5dv1dqgvxjw8x7300000gp/T/drone-pzMjN8K9p0zkya3X/drone/src'
---
+++ packages/pyrocko/pyrocko-2024.1.10-cp310-cp310-macosx_10_9_universal2.whl/pyrocko/obspy_compat/__init__.py
@@ -0,0 +1,9 @@
+# http://pyrocko.org - GPLv3
+#
+# The Pyrocko Developers, 21st Century
+# ---|P------/S----------~Lg----------
+
+from . import base
+from .base import *  # noqa
+
+__all__ = base.__all__
