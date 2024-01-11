b132320da613469c0dd9071ccd67fe886fe794e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 14:04:00 +0000

    Add package rdt-identity/1.9.1/rdt_identity-1.9.1.tar.gz

---
+++ packages/rdt-identity/rdt_identity-1.9.1.tar.gz/rdt_identity-1.9.1/tests/unit/transformers/test_utils.py
@@ -0,0 +1,68 @@
+import sre_parse
+from sre_constants import MAXREPEAT
+
+from rdt.transformers.utils import _any, _max_repeat, flatten_column_list, strings_from_regex
+
+
+def test_strings_from_regex_literal():
+    generator, size = strings_from_regex('abcd', max_repeat=16)
+
+    assert size == 1
+    assert list(generator) == ['abcd']
+
+
+def test_strings_from_regex_digit():
+    generator, size = strings_from_regex('[0-9]')
+
+    assert size == 10
+    assert list(generator) == ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
+def test_strings_from_regex_repeat_literal():
+    generator, size = strings_from_regex('a{1,3}')
+
+    assert size == 3
+    assert list(generator) == ['a', 'aa', 'aaa']
+
+
+def test_strings_from_regex_repeat_digit():
+    generator, size = strings_from_regex(r'\d{1,3}')
+
+    assert size == 1110
+
+    strings = list(generator)
+    assert strings[0] == '0'
+    assert strings[-1] == '999'
+
+
+def test__any():
+    options = {'a': 1}
+    max_repeat = 5
+    _any(options, max_repeat)
+
+
+def test___max_repeat():
+    options = (0, MAXREPEAT, [(sre_parse.LITERAL, 10)])
+    _max_repeat(options, 16)
+
+
+def test_strings_from_regex_very_large_regex():
+    """Ensure that ``size`` of a very large regex is still computable."""
+    very_large_regex = '[0-9a-zA-Z]{9}-[0-9a-zA-Z]{4}-[0-9a-zA-Z]{9}-[0-9a-zA-Z]{9}-[0-9a-z]{12}'
+    generator, size = strings_from_regex(very_large_regex, max_repeat=16)
+
+    assert size == 173689027553046619421110743915454114823342474255318764491341273608665169920
+    [next(generator) for _ in range(100_000)]
+
+
+def test_flatten_column_list():
+    """Test `flatten_column_list` function."""
+    # Setup
+    column_list = ['column1', ('column2', 'column3'), 'column4', ('column5',), 'column6']
+
+    # Run
+    flattened_list = flatten_column_list(column_list)
+
+    # Assert
+    expected_flattened_list = ['column1', 'column2', 'column3', 'column4', 'column5', 'column6']
+    assert flattened_list == expected_flattened_list
