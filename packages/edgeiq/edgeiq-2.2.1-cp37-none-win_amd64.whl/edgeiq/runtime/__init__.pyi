67f09b120bdb83cb6952e8c66865e7f4cf0a1958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:04:49 +0000

    Add package edgeiq/2.2.1/edgeiq-2.2.1-cp37-none-win_amd64.whl

---
+++ packages/edgeiq/edgeiq-2.2.1-cp37-none-win_amd64.whl/edgeiq/runtime/__init__.pyi
@@ -0,0 +1,5 @@
+from .hailo_rt import HailoRT as HailoRT
+from .ocv import OpenCV as OpenCV
+from .qaic_rt import QaicRT as QaicRT
+from .runtime import Runtime as Runtime
+from .trt import TensorRT as TensorRT
---
+++ packages/edgeiq/edgeiq-2.2.1-cp37-none-win_amd64.whl/edgeiq/zones/__init__.pyi
@@ -0,0 +1,2 @@
+from .zone import Zone as Zone, ZoneType as ZoneType
+from .zone_list import RESULT_TYPE as RESULT_TYPE, ZoneList as ZoneList, ZoneResults as ZoneResults
