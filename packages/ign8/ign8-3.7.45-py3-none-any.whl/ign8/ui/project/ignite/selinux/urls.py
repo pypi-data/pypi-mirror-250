b07c2e38a0b14e58cb334e9655e2108a22fabbaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 04:04:58 +0000

    Add package ign8/3.7.45/ign8-3.7.45-py3-none-any.whl

---
+++ packages/ign8/ign8-3.7.45-py3-none-any.whl/ign8/ui/project/ignite/selinux/urls.py
@@ -0,0 +1,18 @@
+
+from django.urls import path, include
+from .views import selinux_list, selinux_event_list , UploadSelinuxDataView, UploadSElinuxEventView, SetroubleshootEntry_list, SetroubleshootEntry_host, SetroubleshootEntryAPIView
+from rest_framework.routers import DefaultRouter
+
+
+
+urlpatterns = [
+    path('', selinux_list, name='selinux_list'),
+    path('selinux_list/', selinux_list, name='selinux_list'),
+    path('selinux_event_list/', selinux_event_list, name='selinux_event_list'),
+    path('upload_selinux_data/', UploadSelinuxDataView.as_view(), name='upload_selinux_data'),
+    path('upload_selinux_event/', UploadSElinuxEventView.as_view(), name='upload_selinux_event'),
+    path('api/setroubleshoot/upload/', SetroubleshootEntryAPIView.as_view(), name='upload_setroubleshoot_entry'),
+    path('SetroubleshootEntry/<str:hostname>/', SetroubleshootEntry_host, name='SetroubleshootEntry_host'),
+    path('SetroubleshootEntry/', SetroubleshootEntry_list, name='SetroubleshootEntry_list'),
+]
+
