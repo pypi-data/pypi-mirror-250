55639229b16990fb5a0e0039cc557a4a6e0ef847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 05:04:07 +0000

    Add package noggin-aaa/1.9.0/noggin_aaa-1.9.0.tar.gz

---
+++ packages/noggin-aaa/noggin_aaa-1.9.0.tar.gz/noggin_aaa-1.9.0/noggin/representation/group.py
@@ -0,0 +1,20 @@
+from .base import Representation
+
+
+class Group(Representation):
+    attr_names = {
+        "name": "cn",
+        "description": "description",
+        "members": "member_user",
+        "sponsors": "membermanager_user",
+        "urls": "fasurl",
+        "irc_channel": "fasircchannel",
+        "mailing_list": "fasmailinglist",
+    }
+    attr_types = {
+        "members": "list",
+        "sponsors": "list",
+        "urls": "list",
+    }
+    pkey = "name"
+    ipa_object = "group"
