6eedd365c456e099c996708c62c826d366913ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 05:04:20 +0000

    Add package ostorlab/0.18.18/ostorlab-0.18.18.tar.gz

---
+++ packages/ostorlab/ostorlab-0.18.18.tar.gz/ostorlab-0.18.18/src/ostorlab/cli/agent/__init__.py
@@ -0,0 +1,8 @@
+"""Module for the root command: agent."""
+from ostorlab.cli.agent.agent import agent
+from ostorlab.cli.agent import build
+from ostorlab.cli.agent import install
+from ostorlab.cli.agent import healthcheck
+from ostorlab.cli.agent import delete
+from ostorlab.cli.agent import list as list_cli
+from ostorlab.cli.agent import search as search_cli
