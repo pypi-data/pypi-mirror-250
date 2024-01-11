cfd6ed7d7fd3c0ec5a465dad2e6cbd5a1f65478c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 15:04:22 +0000

    Add package tensrflwo/2.5/tensrflwo-2.5.tar.gz

---
+++ packages/tensrflwo/tensrflwo-2.5.tar.gz/tensrflwo-2.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tensrflwo',
+    version='2.5',
+    packages=find_packages(),
+    description='Do not install, just for security test',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # This is important if you have a Markdown README
+    author='impact777',
+    author_email='your_email@example.com',
+    url='https://github.com/impact777/tensrflwo',
+    install_requires=[
+        # 依赖列表
+        # 例如: 'requests >= 2.20.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
+print("111111111111111111111111111111111111111111111111111111111111111111111111")
+print("111111111111111111111111111111111111111111111111111111111111111111111111")
+print("111111111111111111111111111111111111111111111111111111111111111111111111")
+print("111111111111111111111111111111111111111111111111111111111111111111111111")
