33d431fbf94590f558bae4e06f5c1d061598ad4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:05:28 +0000

    Add package sorrek-salesforce/1.0.0/sorrek_salesforce-1.0.0-py3-none-any.whl

---
+++ packages/sorrek-salesforce/sorrek_salesforce-1.0.0-py3-none-any.whl/sorrek-salesforce/__version__.py
@@ -0,0 +1,11 @@
+"Version details for sorrek-salesforce"
+
+__title__ = 'sorrek-salesforce'
+__url__ = 'https://github.com/sorrek/sorrek-salesforce'
+__version__ = '1.0.0'
+__author__ = 'Ariel Lavi'
+__author_email__ = 'ariel@sorrek.io'
+__license__ = 'MIT'
+__maintainer__ = 'Ariel Lavi'
+__maintainer_email__ = 'ariel@sorrek.io'
+__keywords__ = 'python sql salesforce salesforce.com sorrek sorrek.io'
