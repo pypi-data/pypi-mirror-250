12a4818ee0de445d82beed4e01d3bbf60b888ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 15:04:42 +0000

    Add package mindpet/1.0.3/mindpet-1.0.3-py3-none-any.whl

---
+++ packages/mindpet/mindpet-1.0.3-py3-none-any.whl/mindpet/utils/version_control.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright © Huawei Technologies Co., Ltd. 2022-2023. All rights reserved.
+"""Version Control APIs"""
+import mindspore as ms
+from mindspore import nn
+from .version_utils import is_version_ge
+if is_version_ge(ms.__version__, '2.0.0'):
+    from ..layers.activation import get_activation, _activation
+else:
+    from mindspore.nn.layer.activation import get_activation, _activation
+
+# pylint: disable=W0127
+_activation = _activation
+# pylint: disable=W0127
+get_activation = get_activation
+
+
+def get_dropout(dropout_prob):
+    if is_version_ge(ms.__version__, '1.11.0'):
+        dropout = nn.Dropout(p=dropout_prob)
+    else:
+        dropout = nn.Dropout(keep_prob=1 - dropout_prob)
+    return dropout
