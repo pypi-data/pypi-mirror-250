637d48d9ab6b0d2e6a5f2f1f3c6f483d1845643d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 07:03:36 +0000

    Add package bstq/1.0.0/bstq-1.0.0.tar.gz

---
+++ packages/bstq/bstq-1.0.0.tar.gz/bstq-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+with open("README.md" , "r") as f:
+  long_description = f.read()
+  
+setup(
+  name='bstq',
+  version='1.0.0',
+  description='Comphrensive Library for Binary Search Trees.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url='',  
+  author='Om Kashyap',
+  author_email='omkashyapcric@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='bst,binary search tree,binarytree,binary tree', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
