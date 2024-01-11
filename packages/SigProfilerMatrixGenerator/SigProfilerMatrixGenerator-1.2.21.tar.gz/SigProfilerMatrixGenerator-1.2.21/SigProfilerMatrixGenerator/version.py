16ec18d6e10e3d9fec63666ab16a885cc4c23dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 14:04:09 +0000

    Add package SigProfilerMatrixGenerator/1.2.21/SigProfilerMatrixGenerator-1.2.21.tar.gz

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.21.tar.gz/SigProfilerMatrixGenerator-1.2.21/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.21'
+version = '1.2.21'
+Update = 'v1.2.21: Add reference_genome_manager and remove site-package benchmarking.'
+
+	
