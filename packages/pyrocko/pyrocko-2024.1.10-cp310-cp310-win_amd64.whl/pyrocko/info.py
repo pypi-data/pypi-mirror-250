b0afb309bb7d57352942e5882e94ba1550ab9ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 03:05:10 +0000

    Add package pyrocko/2024.1.10/pyrocko-2024.1.10-cp310-cp310-win_amd64.whl

---
+++ packages/pyrocko/pyrocko-2024.1.10-cp310-cp310-win_amd64.whl/pyrocko/info.py
@@ -0,0 +1,10 @@
+# This module is automatically created from setup.py
+"""
+Version information (generated from setup.py).
+"""
+git_sha1 = 'ae7c5b949d0720ea5170bb8ccc24d2a61fd8504e'
+local_modifications = False
+version = '2024.01.10'
+long_version = 'pyrocko-2024.01.10-ae7c5b949d0720ea5170bb8ccc24d2a61fd8504e-2024-01-10_02:58:34'  # noqa
+installed_date = '2024-01-10_02:58:34'
+src_path = 'C:\\Windows\\TEMP\\drone-mfJetzC9cwkEm8HD\\drone\\src'
