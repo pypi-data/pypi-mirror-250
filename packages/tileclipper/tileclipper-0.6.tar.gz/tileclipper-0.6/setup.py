f51904c6adf20cb17539b358d6a1449d2e7ac2a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 03:04:06 +0000

    Add package tileclipper/0.6/tileclipper-0.6.tar.gz

---
+++ packages/tileclipper/tileclipper-0.6.tar.gz/tileclipper-0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='tileclipper',
+    version='0.6',
+    packages=find_packages(),
+    package_data={'tileclipper': ['README.md', 'docs/*.md', 'LICENSE']},
+    install_requires=[
+        'requests',
+        'pyproj',
+        'tqdm'
+        # Add other dependencies here if needed
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
