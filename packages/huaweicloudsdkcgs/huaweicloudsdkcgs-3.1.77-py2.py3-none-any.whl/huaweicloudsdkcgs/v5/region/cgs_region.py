1b468499e58748a8e531dd112830ba09b701ca25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:05:38 +0000

    Add package huaweicloudsdkcgs/3.1.77/huaweicloudsdkcgs-3.1.77-py2.py3-none-any.whl

---
+++ packages/huaweicloudsdkcgs/huaweicloudsdkcgs-3.1.77-py2.py3-none-any.whl/huaweicloudsdkcgs/v5/region/cgs_region.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+import types
+import six
+
+from huaweicloudsdkcore.region.region import Region
+from huaweicloudsdkcore.region.provider import RegionProviderChain
+
+class CgsRegion:
+    _PROVIDER = RegionProviderChain.get_default_region_provider_chain("CGS")
+
+    CN_SOUTH_4 = Region("cn-south-4",
+                        "https://cgs.cn-south-4.myhuaweicloud.com")
+    AE_AD_1 = Region("ae-ad-1",
+                        "https://cgs.ae-ad-1.myhuaweicloud.com")
+
+    static_fields = {
+        "cn-south-4": CN_SOUTH_4,
+        "ae-ad-1": AE_AD_1,
+    }
+
+    @classmethod
+    def value_of(cls, region_id, static_fields=None):
+        if not region_id:
+            raise KeyError("Unexpected empty parameter: region_id.")
+
+        fields = static_fields if static_fields else cls.static_fields
+
+        region = cls._PROVIDER.get_region(region_id)
+        if region:
+            return region
+
+        if region_id in fields:
+            return fields.get(region_id)
+
+        raise KeyError("Unexpected region_id: " + region_id)
+
+
