78c5996c400b958c403d25882f58aa24475d90d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 14:03:47 +0000

    Add package spei-python/0.28.1/spei_python-0.28.1.tar.gz

---
+++ packages/spei-python/spei_python-0.28.1.tar.gz/spei_python-0.28.1/spei/resources/__init__.py
@@ -0,0 +1,9 @@
+from spei.resources.acuse import (  # noqa: F401
+    Acuse,
+    ResultadoBanxico,
+    ResultadoServidor,
+)
+from spei.resources.cda import CDA  # noqa: F401
+from spei.resources.mensaje import Mensaje  # noqa: F401
+from spei.resources.orden import Orden, TerceroATercero  # noqa: F401
+from spei.resources.respuesta import Respuesta  # noqa: F401
