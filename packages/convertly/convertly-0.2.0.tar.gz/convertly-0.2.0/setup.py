030dd359cbddfa233ff291dae56c063fffa4e361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:05:27 +0000

    Add package convertly/0.2.0/convertly-0.2.0.tar.gz

---
+++ packages/convertly/convertly-0.2.0.tar.gz/convertly-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="convertly",
+    version="0.2.0",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["conv=file_converter.convertly:main"]},
+    install_requires=[
+        "openai",
+    ],
+    python_requires=">=3.6",
+    author="HudZah",
+    author_email="hudzah@hudzah.com",
+    description="A tool to convert and manipulate media files",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/HudZah/Convertly",
+    license="MIT",
+)
