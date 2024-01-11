f3ca47a4c05b11d6f040a736e13d58e61c446f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 04:05:59 +0000

    Add package python-policy-demo/1.2.0/python_policy_demo-1.2.0-py3-none-any.whl

---
+++ packages/python-policy-demo/python_policy_demo-1.2.0-py3-none-any.whl/demo.py
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+# Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+# "Sonatype" is a trademark of Sonatype, Inc.
+#
+
+def print_hello():
+    print(f'Hello world from v1.2.0')
