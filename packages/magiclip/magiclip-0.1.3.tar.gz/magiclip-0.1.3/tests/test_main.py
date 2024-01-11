aae4d3a61f5a5a2dd7fdb4ac88595a04ceac023e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 14:03:59 +0000

    Add package magiclip/0.1.3/magiclip-0.1.3.tar.gz

---
+++ packages/magiclip/magiclip-0.1.3.tar.gz/magiclip-0.1.3/tests/test_main.py
@@ -0,0 +1,11 @@
+"""test the main file"""
+from docopt import docopt
+from main import entry_point, __doc__
+
+
+def test_main():
+    """test the main file with arguments"""
+    entry_point(docopt(__doc__, argv=["--dt=0.01", "--n=2", "--lambda-lower"]))
+    entry_point(docopt(__doc__, argv=["--dt=0.01", "--n=2", "--lambda-excel"]))
+    entry_point(
+        docopt(__doc__, argv=["--dt=0.01", "--n=2", "--lambda=lambda x:x"]))
