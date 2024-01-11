34f258fd5fc9aba790fc1f645d4b779c7a234c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 04:05:07 +0000

    Add package vtarget/0.5.3/vtarget-0.5.3.tar.gz

---
+++ packages/vtarget/vtarget-0.5.3.tar.gz/vtarget-0.5.3/vtarget/__init__.py
@@ -0,0 +1,5 @@
+from vtarget.dataprep import run_flow  # pyright: ignore
+from vtarget.utils.modeling import Modeling1  # pyright: ignore
+from vtarget.utils.modeling import Modeling2  # pyright: ignore
+
+hot_storage = dict()
