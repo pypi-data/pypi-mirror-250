2798955bfbc9a6cb961b882b09c88dd6502f50b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:05:49 +0000

    Add package addsum/0.1.0/addsum-0.1.0.tar.gz

---
+++ packages/addsum/addsum-0.1.0.tar.gz/addsum-0.1.0/addsum/main.py
@@ -0,0 +1,8 @@
+def sum(a,b):
+    return a+b
+
+a=sum(2,3)
+print(f"Sum of 2 and 3 is {a}")
+
+
+# pypi-AgEIcHlwaS5vcmcCJGUxMDljZDdjLWM1ODktNDQ3Ni1iNTI3LTFjNTBkZTdmZjY0YgACKlszLCI0MWNiMGZjYi01NThiLTQwYzQtOWY3MS0zOWIyYWQxZDVlY2YiXQAABiDojl4sbwTKV7NKvmMo0QjlxEpUJoeVjUv26Fl1RerHkQ
