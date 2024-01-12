3ad3b0f2f348f01b1c068a1074b69cfd77e2f5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 03:04:23 +0000

    Add package EFCrud/1.0.5.1/EFCrud-1.0.5.1.tar.gz

---
+++ packages/EFCrud/EFCrud-1.0.5.1.tar.gz/EFCrud-1.0.5.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='EFCrud',
+    version='1.0.5.1',
+    description='API client for EFCrud',
+    author='Devi Prakash',
+    author_email='dprakash@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/dprakash2101/EFCrudPython.git',
+    packages=find_packages(),
+    install_requires=[
+        'requests',  # Add any other dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    
+)
