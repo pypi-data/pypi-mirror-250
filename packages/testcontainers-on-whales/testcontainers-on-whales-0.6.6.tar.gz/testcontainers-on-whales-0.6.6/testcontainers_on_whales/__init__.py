48055a75a6f11c24d065b1b1bf9dafb4756f3523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 04:05:21 +0000

    Add package testcontainers-on-whales/0.6.6/testcontainers-on-whales-0.6.6.tar.gz

---
+++ packages/testcontainers-on-whales/testcontainers-on-whales-0.6.6.tar.gz/testcontainers-on-whales-0.6.6/testcontainers_on_whales/__init__.py
@@ -0,0 +1,2 @@
+from testcontainers_on_whales.__version__ import __version__
+from testcontainers_on_whales.core.container import Container
---
+++ packages/testcontainers-on-whales/testcontainers-on-whales-0.6.6.tar.gz/testcontainers-on-whales-0.6.6/testcontainers_on_whales/nginx.py
@@ -0,0 +1,26 @@
+import requests
+
+from testcontainers_on_whales import Container
+
+
+class NginxContainer(Container):
+    NGINX_PORT = 8080
+
+    def __init__(
+        self,
+        image: str = "docker.io/nginxinc/nginx-unprivileged:alpine",
+    ) -> None:
+        super().__init__(image=image)
+
+    def get_connection_url(self) -> str:
+        ip = self.get_container_ip()
+        port = self.get_container_port(self.NGINX_PORT)
+        return f"http://{ip}:{port}"
+
+    def readiness_probe(self) -> bool:
+        try:
+            requests.get(self.get_connection_url())
+            return True
+        except requests.ConnectionError:
+            pass
+        return False
