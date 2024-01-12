8220b355c1be3fcc47f803a1c6abbc29f623093a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 15:04:17 +0000

    Add package ezsn/0.1.0/ezsn-0.1.0.tar.gz

---
+++ packages/ezsn/ezsn-0.1.0.tar.gz/ezsn-0.1.0/ezsn/__init__.py
@@ -0,0 +1,2 @@
+from .main import EZSN
+from .iot import iot
---
+++ packages/ezsn/ezsn-0.1.0.tar.gz/ezsn-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ezsn',
+    version='0.1.0',
+    author='Ezhilarasan',
+    author_email='nec0914014@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'ezsn = ezsn.iot:iot',
+        ],
+    },
+)
