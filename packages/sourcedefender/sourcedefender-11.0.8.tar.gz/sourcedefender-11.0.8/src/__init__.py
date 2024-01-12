b076a60b1c0eb3a6f77095115ffb10288e676919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 04:04:52 +0000

    Add package sourcedefender/11.0.8/sourcedefender-11.0.8.tar.gz

---
+++ packages/sourcedefender/sourcedefender-11.0.8.tar.gz/sourcedefender-11.0.8/src/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "11.0.8"
+import msgpack
+import tgcrypto
+import boltons.timeutils
+import datetime
+import environs
+try:
+    from . import loader, tools
+except ModuleNotFoundError:
+    pass
