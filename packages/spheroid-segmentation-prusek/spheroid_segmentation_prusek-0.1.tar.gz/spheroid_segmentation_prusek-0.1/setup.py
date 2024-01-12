adc549cd0ff70f8805bd89e37432b25fec38af6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 05:04:18 +0000

    Add package spheroid-segmentation-prusek/0.1/spheroid_segmentation_prusek-0.1.tar.gz

---
+++ packages/spheroid-segmentation-prusek/spheroid_segmentation_prusek-0.1.tar.gz/spheroid_segmentation_prusek-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="spheroid_segmentation_prusek",
+    version="0.1",
+    description="Your package description",
+    author="Michal Prusek",
+    author_email="prusemic@cvut.cz",
+    url="https://github.com/michalprusek/Spheroid-segmentation",
+    packages=["spheroid_segmentation_prusek"],
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
