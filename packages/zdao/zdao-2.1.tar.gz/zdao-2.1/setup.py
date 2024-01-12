df792acb96cd5b55a0c67f7a9b08ed78aea20839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 16:05:05 +0000

    Add package zdao/2.1/zdao-2.1.tar.gz

---
+++ packages/zdao/zdao-2.1.tar.gz/zdao-2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zdao",
+    version="2.1",
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    author="zhstack",
+    author_email="zhstack@163.com",
+    description="A useless database access model",
+    license="MIT",
+    keywords="zdao",
+    url="https://github.com/zzt/w-dao.git"
+)
