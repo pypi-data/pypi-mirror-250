69cfea98a2424a3b767d520b2e66426e79dbb6d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 15:03:55 +0000

    Add package phoenix-ws/2.0.3/phoenix-ws-2.0.3.tar.gz

---
+++ packages/phoenix-ws/phoenix-ws-2.0.3.tar.gz/phoenix-ws-2.0.3/setup.py
@@ -0,0 +1,53 @@
+import pathlib
+from setuptools import setup
+from phoenix import VERSION
+
+print(f"Packaging Phoenix version {VERSION}...")
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+  name="phoenix-ws",
+  version=VERSION,
+  description="An alternative web server and package manager",
+  long_description=README,
+  long_description_content_type="text/markdown",
+  url="https://github.com/Froggo8311/Phoenix",
+  author="Froggo",
+  # author_email="",
+  license="MIT",
+  classifiers=[
+    "Programming Language :: Python :: 3"
+  ],
+  packages=[
+    "phoenix"
+  ],
+  include_package_data=True,
+  install_requires=[
+    "flask",
+    "waitress"
+  ],
+  entry_points={
+    "console_scripts": [
+      "phoenix=phoenix.__init__:main"
+    ]
+  },
+  license_files = ("LICENSE.md",),
+  keywords=[
+    "Phoenix",
+    "PPM",
+    "NoJS",
+    "NoPM",
+    "Website",
+    "Web",
+    "Webserver",
+    "Server",
+    "Package Manager",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "JS",
+    "Fast"
+  ]
+)
