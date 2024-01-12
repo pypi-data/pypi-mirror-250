8be7dc3102624338ccf034b3709f8d086321c4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 05:04:12 +0000

    Add package snap-http/1.1.0/snap_http-1.1.0.tar.gz

---
+++ packages/snap-http/snap_http-1.1.0.tar.gz/snap_http-1.1.0/snap_http/types.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any, Dict, List, Type, Union
+
+
+# For the below, refer to https://snapcraft.io/docs/snapd-api#heading--changes
+COMPLETE_STATUSES = {"Done", "Error", "Hold", "Abort"}
+INCOMPLETE_STATUSES = {"Do", "Doing", "Undo", "Undoing"}
+SUCCESS_STATUSES = {"Done"}
+ERROR_STATUSES = {"Error", "Hold", "Unknown"}
+
+
+@dataclass
+class SnapdResponse:
+    """A response received from snapd's REST API.
+
+    See https://snapcraft.io/docs/snapd-api
+    """
+
+    type: str
+    status_code: int
+    status: str
+    result: Union[Dict[str, Any], List[Any]]
+    sources: Union[List[str], None] = None
+    change: Union[str, None] = None
+
+    @classmethod
+    def from_http_response(cls: Type, response: Dict[str, Any]) -> SnapdResponse:
+        return cls(**{k.replace("-", "_"): v for k, v in response.items()})
