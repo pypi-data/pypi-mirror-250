fe40f20dfad7fbe79242b708d6e939cc72e53c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 03:04:09 +0000

    Add package light-database/0.1.3/light_database-0.1.3-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/light-database/light_database-0.1.3-cp311-cp311-macosx_11_0_arm64.whl/light_database/hive/db.pyi
@@ -0,0 +1,11 @@
+# (generated with --quick)
+
+import light_database.base
+import impala
+from typing import Type
+
+RegularDB: Type[light_database.base.RegularDB]
+
+class HiveDB(light_database.base.RegularDB):
+    _creator: module
+    _section: str
