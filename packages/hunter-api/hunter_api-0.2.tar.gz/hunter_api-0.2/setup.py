b988f758f09b88c7eb693b1fca9fade7fa6667c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 16:05:37 +0000

    Add package hunter-api/0.2/hunter_api-0.2.tar.gz

---
+++ packages/hunter-api/hunter_api-0.2.tar.gz/hunter_api-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hunter_api',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+)
