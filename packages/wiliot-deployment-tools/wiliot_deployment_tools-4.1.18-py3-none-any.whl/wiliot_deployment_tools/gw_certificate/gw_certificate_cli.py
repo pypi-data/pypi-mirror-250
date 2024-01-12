5c1d538e4ba8dc0a1d549d711966e0a1c831dcc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 05:04:14 +0000

    Add package wiliot-deployment-tools/4.1.18/wiliot_deployment_tools-4.1.18-py3-none-any.whl

---
+++ packages/wiliot-deployment-tools/wiliot_deployment_tools-4.1.18-py3-none-any.whl/wiliot_deployment_tools/gw_certificate/gw_certificate_cli.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser
+from wiliot_deployment_tools.gw_certificate.gw_certificate import GWCertificate
+
+def main():
+    parser = ArgumentParser(prog='wlt-gw-certificate',
+                            description='Gateway Certificate - CLI Tool to test Wiliot GWs')
+    required = parser.add_argument_group('required arguments')
+    required.add_argument('-owner', type=str, help="Owner ID", required=True)
+    required.add_argument('-gw', type=str, help="Gateway ID", required=True)
+    
+    optional = parser.add_argument_group('optional arguments')
+    optional.add_argument('-suffix', type=str, help="Topic suffix", default='', required=False)
+    optional.add_argument('-coupled', action='store_true', help="GW Metadata Coupling Enabled", default=False, required=False)
+
+    
+    args = parser.parse_args()
+    topic_suffix = '' if args.suffix == '' else '-'+args.suffix
+    gwc = GWCertificate(gw_id=args.gw, owner_id=args.owner, coupled=args.coupled,topic_suffix=topic_suffix)
+    gwc.run_tests()
+    gwc.create_results_html()
+
+def main_cli():
+    main()
+
+if __name__ == '__main__':
+    main()
