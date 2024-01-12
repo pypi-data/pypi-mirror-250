ca488b0682bd40447444819f53acdfab1f9dfe92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 03:04:31 +0000

    Add package Rocket-Store/0.0.10/Rocket_Store-0.0.10-py3-none-any.whl

---
+++ packages/Rocket-Store/Rocket_Store-0.0.10-py3-none-any.whl/Rocketstore/__init__.py
@@ -0,0 +1,25 @@
+"""
+█▀ █▄█ █▀▀ █░█ █▀▀ █░█
+▄█ ░█░ █▄▄ █▀█ ██▄ ▀▄▀
+
+Author: <Anton Sychev> (anton at sychev dot xyz) 
+__init__.py (c) 2024 
+Created:  2024-01-02 17:37:05 
+Desc: Inilization of RocketStore
+Docs: documentation
+"""
+
+__all__ = ["Rocketstore"]
+
+from .__version__ import (
+    __author__,
+    __author_email__,
+    __build__,
+    __copyright__,
+    __description__,
+    __title__,
+    __url__,
+    __version__,
+)
+
+from .Rocketstore import Rocketstore
