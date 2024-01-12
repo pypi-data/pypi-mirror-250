f43b3fed1f7012dbafce3141537ed9c59a5d44b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 06:04:33 +0000

    Add package hspylib-askai/0.9.6/hspylib-askai-0.9.6.tar.gz

---
+++ packages/hspylib-askai/hspylib-askai-0.9.6.tar.gz/hspylib-askai-0.9.6/askai/core/engine/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-ask-ai v0.9.6
+#
+# Package: main.askai.core.engine
+"""Package initialization."""
+
+__all__ = [
+    'ai_engine', 
+    'openai_engine'
+]
+__version__ = '0.9.6'
---
+++ packages/hspylib-askai/hspylib-askai-0.9.6.tar.gz/hspylib-askai-0.9.6/askai/utils/__init__.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-ask-ai v0.9.6
+#
+# Package: main.askai.utils
+"""Package initialization."""
+
+__all__ = [
+    'utilities'
+]
+__version__ = '0.9.6'
