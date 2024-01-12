46ab1657539e02a2c51ac278558facdb868eb04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 06:04:46 +0000

    Add package stanford-stk/0.0.7/stanford-stk-0.0.7.tar.gz

---
+++ packages/stanford-stk/stanford-stk-0.0.7.tar.gz/stanford-stk-0.0.7/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import torch
+
+extra_deps = {}
+
+extra_deps['dev'] = [
+    'absl-py',
+]
+
+extra_deps['all'] = set(dep for deps in extra_deps.values() for dep in deps)
+
+setup(
+    name="stanford-stk",
+    version="0.0.7",
+    author="Trevor Gale",
+    author_email="tgale@stanford.edu",
+    description="Sparse Toolkit",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/stanford-futuredata/stk",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Unix",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "triton>=2.1.0",
+    ],
+    extras_require=extra_deps,
+)
