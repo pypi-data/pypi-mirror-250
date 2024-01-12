17b92743ecac5edbec2badeb0f80592bf847241f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 05:04:40 +0000

    Add package wunderkafka/0.15.0/wunderkafka-0.15.0.tar.gz

---
+++ packages/wunderkafka/wunderkafka-0.15.0.tar.gz/wunderkafka-0.15.0/wunderkafka/schema_registry/abc.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+from wunderkafka.structures import SRMeta, SchemaMeta
+
+
+class AbstractHTTPClient(ABC):
+
+    @abstractmethod
+    def make_request(self, relative_url: str, method: str = 'GET', body: Any = None, query: Any = None) -> Any: ...
+
+    @property
+    def base_url(self) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def close(self) -> None: ...
+
+
+class AbstractSchemaRegistry(ABC):
+
+    @abstractmethod
+    def get_schema_text(self, schema_meta: SchemaMeta) -> str: ...
+
+    @abstractmethod
+    def register_schema(self, topic: str, schema_text: str, *, is_key: bool = True) -> SRMeta: ...
---
+++ packages/wunderkafka/wunderkafka-0.15.0.tar.gz/wunderkafka-0.15.0/wunderkafka/serdes/store.py
@@ -0,0 +1,42 @@
+from typing import Type, Union, Optional
+from pathlib import Path
+
+from wunderkafka.types import TopicName, KeySchemaDescription, ValueSchemaDescription
+from wunderkafka.serdes.abc import AbstractDescriptionStore
+from dataclasses_avroschema import AvroModel
+from wunderkafka.serdes.avromodel import derive
+
+
+class SchemaTextRepo(AbstractDescriptionStore):
+
+    def add(self, topic: TopicName, value: str, key: str) -> None:
+        self._values[topic] = ValueSchemaDescription(text=value)
+        if key is not None:
+            self._keys[topic] = KeySchemaDescription(text=key)
+
+
+def _load_from_file(filename: Path) -> str:
+    with open(filename) as fl:
+        return fl.read()
+
+
+# ToDo (tribunsky.kir): refactor it, maybe add hooks to parent class.
+#                       Barbara, forgive us. Looks like AbstractDescriptionStore should be generic.
+class SchemaFSRepo(AbstractDescriptionStore):
+
+    def add(self, topic: TopicName, value: Union[str, Path], key: Union[str, Path]) -> None:
+        self._values[topic] = ValueSchemaDescription(text=_load_from_file(Path(value)))
+        if key is not None:
+            self._keys[topic] = KeySchemaDescription(text=_load_from_file(Path(key)))
+
+
+class AvroModelRepo(AbstractDescriptionStore):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    # ToDo (tribunsky.kir): change Type[AvroModel] to more general alias + check derivation from python built-ins
+    def add(self, topic: TopicName, value: Type[AvroModel], key: Optional[Type[AvroModel]]) -> None:
+        self._values[topic] = ValueSchemaDescription(text=derive(value, topic))
+        if key is not None:
+            self._keys[topic] = KeySchemaDescription(text=derive(key, topic, is_key=True))
