a0ff4c442289c943775c811fdc67de25316db295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 04:05:20 +0000

    Add package teomim/0.0.146/teomim-0.0.146.tar.gz

---
+++ packages/teomim/teomim-0.0.146.tar.gz/teomim-0.0.146/teomim/__init__.py
@@ -0,0 +1,16 @@
+"""                                                                                                                        
+# teomim                                                                                                                   
+                                                                                                                           
+Description:                                                                                                               
+    Digital twin for human disease risk. Generate realistic diagnostic histories for ptients nearly indistinguishable from real population using deep comorbidity signatures.                                                                  
+                                                                                                                           
+---                                                                                                                        
+                                                                                                                           
+Installation:                                                                                                              
+   ```                                                                                                                     
+      pip install teomim                                                                                                   
+   ```                                                                                                                                                                                                      
+Notes:                                                                                                                     
+   Models are contextual
+
+"""
