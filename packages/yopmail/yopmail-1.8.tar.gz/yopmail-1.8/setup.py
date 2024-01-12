b40b8d738df184560242f674698993f8894039db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 05:04:40 +0000

    Add package yopmail/1.8/yopmail-1.8.tar.gz

---
+++ packages/yopmail/yopmail-1.8.tar.gz/yopmail-1.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='yopmail',
+    version='1.8',
+    description="A Python module to get mails from a Yopmail inbox, save them",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    readme = "README.md",
+    license='MIT',
+    author="rklf",
+    packages = find_packages(),
+    python_requires='>=3.10',
+    url='https://github.com/rklf/yopmail',
+    keywords='yopmail get mails retrieve scrap emails',
+    install_requires=[
+          'beautifulsoup4',
+          'requests',
+      ],
+
+)
