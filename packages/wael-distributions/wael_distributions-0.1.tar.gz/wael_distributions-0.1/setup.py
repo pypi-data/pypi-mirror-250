90eb2fd5c1947f8b807251ec3b0b7f2aa04fede2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 05:04:36 +0000

    Add package wael-distributions/0.1/wael_distributions-0.1.tar.gz

---
+++ packages/wael-distributions/wael_distributions-0.1.tar.gz/wael_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='wael_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['wael_distributions'],
+      zip_safe=False)
