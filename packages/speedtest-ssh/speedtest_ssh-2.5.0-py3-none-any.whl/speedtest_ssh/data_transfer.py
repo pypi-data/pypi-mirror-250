249d6476adc919c80fecc74a127a346c56c41342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 06:04:45 +0000

    Add package speedtest-ssh/2.5.0/speedtest_ssh-2.5.0-py3-none-any.whl

---
+++ packages/speedtest-ssh/speedtest_ssh-2.5.0-py3-none-any.whl/speedtest_ssh/data_transfer.py
@@ -0,0 +1,153 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from datetime import datetime
+from pathlib import Path
+import subprocess
+import random
+import string
+import re
+import os
+
+from tqdm import tqdm
+
+from .sftp_wrapper import sftp_wrapper
+from .config import Config
+from .util import find_exe
+
+if TYPE_CHECKING:
+    from paramiko import SFTPClient
+
+
+__all__ = ("DataTransfer", "SFTP", "Rsync")
+
+
+class ProgressBar(tqdm):
+    """
+    A small tqdm wrapper for paramiko's SFTP callbacks to use
+    """
+
+    def __init__(self, desc: str):
+        super().__init__(desc=desc, unit="B", unit_scale=True, unit_divisor=1024, dynamic_ncols=True)
+
+    def __call__(self, bytes_done: int, bytes_remaining: int):
+        """
+        :param bytes_done: The number of bytes transferred
+        :param bytes_remaining: The number of bytes yet to be transferred
+        """
+        self.total: int | None
+        if self.total is None:
+            self.total = bytes_remaining
+        self.update(bytes_done - self.n)
+
+
+class DataTransfer:
+    """
+    A context mananger used to send and receive data to and from the remote client
+    This class is a context manager; on exit will remove the remote file on deletion
+    """
+
+    def __init__(self, config: Config):
+        """
+        :param config: The Config object the DataTransfer instance should use
+        """
+        rand = lambda: random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)
+        self._remote_f: str = f"/tmp/speedtest-ssh_{datetime.now()}_{''.join(rand() for _ in range(8))}.tmp"
+        self._remote_f = self._remote_f.replace(":", "-").replace(" ", "_")
+        # We promise that _remote_f components match: ^[a-zA-Z\d_.-]+$ (old rsync args suck)
+        self._sftp_cm = sftp_wrapper(config)
+        self._sftp: SFTPClient  # Defined in __enter__
+
+    def __enter__(self) -> DataTransfer:
+        self._sftp = self._sftp_cm.__enter__()
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.clean_remote()
+        self._sftp_cm.__exit__(*args, **kwargs)
+
+    def put(self, local: Path) -> None:
+        """
+        Send the local file to be saved at the remote path
+        """
+        raise NotImplementedError
+
+    def get(self, local: Path) -> None:
+        """
+        Get the remote file to and save it at the local path
+        """
+        raise NotImplementedError
+
+    def clean_remote(self) -> None:
+        """
+        Removes the remote file
+        """
+        try:
+            self._sftp.remove(self._remote_f)
+        except FileNotFoundError:
+            pass
+
+
+class SFTP(DataTransfer):
+    """
+    A DataTransfer classes that uses SFTP
+    """
+
+    def put(self, local: Path) -> None:
+        with ProgressBar("Upload   ") as p:
+            self._sftp.put(str(local), self._remote_f, callback=p)
+
+    def get(self, local: Path) -> None:
+        with ProgressBar("Download ") as p:
+            self._sftp.get(self._remote_f, str(local), callback=p)
+
+
+class Rsync(DataTransfer):
+    """
+    A DataTransfer that uses rsync
+    Requires rsync be installed
+    """
+
+    def __init__(self, config: Config):
+        """
+        :param host: The hostname ssh uses for the remote client
+        """
+        super().__init__(config)
+        self._remote_f = re.sub("[^a-zA-Z\\d_-]", "_", self._remote_f)  # Old rsync args suck
+        rsync = find_exe("rsync")
+        # Determine rsync version
+        try:
+            output: str = subprocess.check_output((rsync, "--version")).decode()
+            output = output.split("version")[1].split("protocol")[0].strip()
+            version: tuple[int, ...] = tuple(int(i) for i in output.split("."))
+            self._old = version <= (3, 1, 0)  # macOS has an old version by default
+            if self._old:
+                print("\tOld version of rsync detected. Output will be more verbose.")
+        except (subprocess.CalledProcessError, KeyError) as e:
+            raise RuntimeError(f"Could not determine rsync version from {rsync}") from e
+        # Determine rsync command
+        self._cmd: list[str | Path] = [rsync]
+        self._env = os.environ.copy()
+        if config.password is not None:
+            sshpass = find_exe("sshpass")
+            self._cmd = [sshpass, "-e"] + self._cmd  # type: ignore
+            self._env["SSHPASS"] = config.password
+        self._cmd.extend(("-hh", "--progress" if self._old else "--info=progress2"))
+        if config.port is not None:
+            self._cmd.append(f"--port={config.port}")
+        # Determine host info
+        self._target: str = ("" if config.user is None else f"{config.user}@") + f"{config.host}:{self._remote_f}"
+
+    def _transfer(self, src: str | Path, dst: str | Path) -> None:
+        """
+        rsync src to dst
+        """
+        try:
+            _ = subprocess.run((*self._cmd, src, dst), env=self._env, check=True)
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError("Failed to transfer data during speed test") from e
+
+    def put(self, local: Path) -> None:
+        self._transfer(local, self._target)
+
+    def get(self, local: Path) -> None:
+        self._transfer(self._target, local)
---
+++ packages/speedtest-ssh/speedtest_ssh-2.5.0-py3-none-any.whl/speedtest_ssh/speedtest_ssh.py
@@ -0,0 +1,119 @@
+from tempfile import NamedTemporaryFile as NTF
+from pathlib import Path
+from math import log
+import argparse
+import time
+import sys
+import os
+
+from .data_transfer import Config, DataTransfer, SFTP, Rsync
+from .ping import ping as ping_test
+from ._version import __version__
+
+
+base_size = 2 * (1024**2)
+_password_env_name = "SPEEDTEST_SSH_PASSWORD"
+
+
+def _iteration(temp: Path, client: DataTransfer, size: int) -> tuple[int, int]:
+    """
+    Time uploading and downloading a file of size bytes
+    :param temp: A temporary file we have ownership of
+    :param client: A client used to transfer the data
+    :param size: how much data to transfer
+    :return: <nanoseconds to upload>, <nanoseconds to download>
+    """
+    # Create the file to upload; sendfile not always supported so we do this the hard way
+    block: int = min(base_size, 1024**2)
+    with temp.open("ab") as f:
+        done: int = os.fstat(f.fileno()).st_size  # .tell() can be odd in append mode
+        with open("/dev/urandom", "rb") as ur:
+            while done < size:
+                f.write(ur.read(block))
+                done += block
+        f.truncate(size)
+    # Time uploading the file
+    client.clean_remote()
+    start: int = time.time_ns()
+    client.put(temp)
+    up: int = time.time_ns() - start
+    # Time downloading the file
+    temp.unlink()
+    start = time.time_ns()
+    client.get(temp)
+    down: int = time.time_ns() - start
+    return up, down
+
+
+def _print_results(ping_delay: float | None, up_ns: int, down_ns: int, size: int) -> None:
+    """
+    Print the speed test results
+    """
+    prefix = "\n" + ("" if ping_delay is None else f"Ping: {ping_delay} ms\n")
+    fmt = lambda s: f"{8 * (size/1024**2) / (s/1000**3):.2f} Mbit/s"
+    print(f"{prefix}Upload Speed: {fmt(up_ns)}\nDownload Speed: {fmt(down_ns)}")
+
+
+def speedtest_ssh(
+    host: str, num_seconds: int, ping: bool, verbose: bool, mode: str, **kwargs: int | str | None
+) -> None:
+    """
+    Run speedtest_ssh
+    """
+    kwargs["password"] = None
+    if kwargs.pop("password_env"):
+        kwargs["password"] = os.environ.get(_password_env_name, None)
+        if kwargs["password"] is None:
+            raise RuntimeError(f"{_password_env_name} is not set")
+    ping_delay: float | None = ping_test(host, verbose) if ping else None
+    print("Connecting...")
+    with (Rsync if mode == "rsync" else SFTP)(Config(host=host, **kwargs)) as remote:  # type: ignore
+        with NTF(prefix="speedtest_ssh.", dir="/tmp", delete_on_close=False) as ntf:
+            ntf.close()
+            temp = Path(ntf.name)
+            print("Testing...")
+            nano_sec: int = 0
+            size: int = base_size
+            remaining: int = int(1e9) * num_seconds
+            while int(1.5 * nano_sec) < remaining - 1:  # Try to get close to seconds
+                size *= 2 ** (0 if not nano_sec else max(1, int(log(remaining / nano_sec, 2))))
+                # ^ Faster than just *=2
+                up_t, down_t = _iteration(temp, remote, size)
+                nano_sec = up_t + down_t
+                remaining -= nano_sec
+    _print_results(ping_delay, up_t, down_t, size)
+
+
+def main(argv: list[str]) -> None:
+    """
+    speedtest_ssh from arguments
+    """
+    base: str = os.path.basename(argv[0])
+    parser = argparse.ArgumentParser(prog=base)
+    parser.add_argument("--version", action="version", version=f"{base} {__version__}")
+    parser.add_argument("host", help="The host to speedtest the conection to")
+    parser.add_argument("--ping", action="store_true", help="Ping test the host as well")
+    parser.add_argument("--verbose", action="store_true", help="Be verbose")
+    parser.add_argument("-u", "--user", help="The user used for ssh")
+    parser.add_argument(
+        "--password-env", action="store_true", help=f"Read password from {_password_env_name} environment variable"
+    )
+    parser.add_argument("-p", "--port", type=int, help="The port used for ssh")
+    parser.add_argument(
+        "--num-seconds", type=int, default=20, help="An approximate number of seconds the speed tests should take"
+    )
+    parser.add_argument(
+        "-m", "--mode", choices=["rsync", "sftp"], default="rsync", help="The speedtest method. Defaults to rsync"
+    )
+    return speedtest_ssh(**vars(parser.parse_args(argv[1:])))
+
+
+def cli() -> None:
+    """
+    speedtest_ssh CLI
+    """
+    main(sys.argv)
+
+
+if __name__ == "__main__":
+    cli()
