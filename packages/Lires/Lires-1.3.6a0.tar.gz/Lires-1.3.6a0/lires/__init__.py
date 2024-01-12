88d70093eed7dcf8f02162a119ce0fbfcdf7af0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 05:04:38 +0000

    Add package Lires/1.3.6a0/Lires-1.3.6a0.tar.gz

---
+++ packages/Lires/Lires-1.3.6a0.tar.gz/Lires-1.3.6a0/lires/__init__.py
@@ -0,0 +1,5 @@
+from .core import globalVar as G
+from .version import VERSION
+
+G.init()
+__version__ = VERSION
---
+++ packages/Lires/Lires-1.3.6a0.tar.gz/Lires-1.3.6a0/lires/api/__init__.py
@@ -0,0 +1,6 @@
+"""
+Connect to peripherals and provide an API to interact with them.
+"""
+from .iserver import IServerConn
+
+__all__ = ["IServerConn"]
