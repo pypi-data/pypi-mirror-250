041ec8609201ce7f22913f57a844f38d9a11f1cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 05:04:42 +0000

    Add package prusek-spheroid/0.7/prusek_spheroid-0.7.tar.gz

---
+++ packages/prusek-spheroid/prusek_spheroid-0.7.tar.gz/prusek_spheroid-0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="prusek_spheroid",
+    version="0.7",
+    description="Your package description",
+    author="Michal Prusek",
+    author_email="prusemic@cvut.cz",
+    url="https://github.com/michalprusek/Spheroid-segmentation",
+    packages=["prusek_spheroid"],
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'scikit-image',
+        'scikit-learn',
+        'shapely',
+        'threadpoolctl',
+        'matplotlib',
+        'rasterio'
+    ],
+)
