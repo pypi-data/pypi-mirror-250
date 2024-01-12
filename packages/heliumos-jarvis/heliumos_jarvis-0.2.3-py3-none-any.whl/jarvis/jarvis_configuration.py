79eae9ba5cf33918bd61299361e18c76f2c9f855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 16:05:54 +0000

    Add package heliumos-jarvis/0.2.3/heliumos_jarvis-0.2.3-py3-none-any.whl

---
+++ packages/heliumos-jarvis/heliumos_jarvis-0.2.3-py3-none-any.whl/jarvis/jarvis_configuration.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings
+
+
+class JarvisSettings(BaseSettings):
+    INFERENCE_MODEL_HOST: str = "129.146.121.185"
+    INFERENCE_MODEL_PORT: str = "8001"
+
+    EMBEDDING_MODEL_HOST: str = "192.168.1.201"
+    EMBEDDING_MODEL_PORT: str = "17880"
+
+    MILVUS_HOST: str = "192.168.1.201"
+    MILVUS_PORT: str = "19530"
+    MILVUS_USERNAME: str = ""
+    MILVUS_PASSWORD: str = ""
+    MILVUS_DB: str = ""
+
+    POSTGRES_HOST: str = "192.168.1.201"
+    POSTGRES_PORT: str = "5432"
+    POSTGRES_USERNAME: str = "postgres"
+    POSTGRES_PASSWORD: str = "1qaz2wsx"
+    POSTGRES_DB: str = "postgres"
+
+
+settings = JarvisSettings()
+print(settings)
