6cfa7c2063de8845fa3f10ddf03661f4fe7772f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 05:04:10 +0000

    Add package legend-pydataobj/1.5.0a3/legend_pydataobj-1.5.0a3.tar.gz

---
+++ packages/legend-pydataobj/legend_pydataobj-1.5.0a3.tar.gz/legend_pydataobj-1.5.0a3/tests/compression/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+
+import lgdo.lh5 as lh5
+
+
+@pytest.fixture()
+def wftable(lgnd_test_data):
+    store = lh5.LH5Store()
+    wft, _ = store.read(
+        "/geds/raw/waveform",
+        lgnd_test_data.get_path("lh5/LDQTA_r117_20200110T105115Z_cal_geds_raw.lh5"),
+    )
+    return wft
