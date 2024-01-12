596b93b2d336948ec54042443b2cb7c8f7bd5c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 03:04:27 +0000

    Add package whisperplus/0.2.4/whisperplus-0.2.4.tar.gz

---
+++ packages/whisperplus/whisperplus-0.2.4.tar.gz/whisperplus-0.2.4/whisperplus/__init__.py
@@ -0,0 +1,10 @@
+from whisperplus.pipelines.summarization import TextSummarizationPipeline
+from whisperplus.pipelines.whisper import SpeechToTextPipeline
+from whisperplus.pipelines.whisper_diarize import ASRDiarizationPipeline
+from whisperplus.utils.download_utils import download_and_convert_to_mp3
+from whisperplus.utils.text_utils import format_speech_to_dialogue
+
+__version__ = '0.2.4'
+__author__ = 'kadirnar'
+__license__ = 'Apache License 2.0'
+__all__ = ['']
