ab59e0990b8d07a489e7516742eef32761ae5a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 03:04:30 +0000

    Add package EFCrud/1.0.5.3/EFCrud-1.0.5.3-py3-none-any.whl

---
+++ packages/EFCrud/EFCrud-1.0.5.3-py3-none-any.whl/EFCrud/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from EFCrud.api.auth_api import AuthApi
+from EFCrud.api.employees_api import EmployeesApi
+from EFCrud.api.features_api import FeaturesApi
+from EFCrud.api.roles_api import RolesApi
+
