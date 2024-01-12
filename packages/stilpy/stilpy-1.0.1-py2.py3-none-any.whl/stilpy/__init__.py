b08c99b0d07e0147d0fa5076d4035e7597e3f465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 05:04:32 +0000

    Add package stilpy/1.0.1/stilpy-1.0.1-py2.py3-none-any.whl

---
+++ packages/stilpy/stilpy-1.0.1-py2.py3-none-any.whl/stilpy/__init__.py
@@ -0,0 +1,10 @@
+"""Sorted Time Intervals Library for Python (stilpy).
+
+"""
+
+__author__ = """Felipe Santa-Cruz"""
+__email__ = 'fesanmar@gmail.com'
+__version__ = '1.0.1'
+__all__ = ['timeinterval', 'timegaps']
+
+from stilpy.timegaps import TimeGaps, TimeInterval
