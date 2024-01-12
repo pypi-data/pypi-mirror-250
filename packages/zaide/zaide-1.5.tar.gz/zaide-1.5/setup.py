76a0ea8208cafa1e87352524267d3f18142ab02a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 15:04:56 +0000

    Add package zaide/1.5/zaide-1.5.tar.gz

---
+++ packages/zaide/zaide-1.5.tar.gz/zaide-1.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zaide",
+    version="1.5",
+    packages=find_packages(),
+    install_requires=["datetime", "time", "os", "json", "socket", "inspect"],
+    author="zaide",
+    author_email="zztaide@163.com",
+    description="A useless model",
+    license="MIT",
+    keywords="rst",
+    url="https://github.com/waide/waide.git"
+)
