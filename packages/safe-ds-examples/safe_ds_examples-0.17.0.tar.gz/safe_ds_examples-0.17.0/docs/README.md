e1c4e95c18204975818c46170d4a40e0c2157d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 03:04:49 +0000

    Add package safe-ds-examples/0.17.0/safe_ds_examples-0.17.0.tar.gz

---
+++ packages/safe-ds-examples/safe_ds_examples-0.17.0.tar.gz/safe_ds_examples-0.17.0/docs/README.md
@@ -0,0 +1,32 @@
+# Safe-DS Python Library - Examples
+
+[![PyPI](https://img.shields.io/pypi/v/safe-ds-examples)](https://pypi.org/project/safe-ds-examples)
+[![Main](https://github.com/Safe-DS/Library-Examples/actions/workflows/main.yml/badge.svg)](https://github.com/Safe-DS/Library-Examples/actions/workflows/main.yml)
+[![codecov](https://codecov.io/gh/Safe-DS/Library-Examples/branch/main/graph/badge.svg?token=X5CU9V952H)](https://codecov.io/gh/Safe-DS/Library-Examples)
+[![Documentation Status](https://readthedocs.org/projects/stdlib-examples/badge/?version=stable)](https://library-examples.safeds.com)
+
+Ready-to-use examples for the [Safe-DS Python Library](https://github.com/Safe-DS/Library).
+
+## Installation
+
+Get the latest version from [PyPI](https://pypi.org/project/safe-ds-examples):
+
+```shell
+pip install safe-ds-examples
+```
+
+## Documentation
+
+You can find the full documentation [here](https://library-examples.safeds.com).
+
+## Contributing
+
+We welcome contributions from everyone. As a starting point, check the following resources:
+
+* [Setting up a development environment](https://library-examples.safeds.com/en/latest/development/environment/)
+* [Project guidelines](https://library-examples.safeds.com/en/latest/development/project_guidelines/)
+* [Contributing page](https://github.com/Safe-DS/Library-Examples/contribute)
+
+If you need further help, please [use our discussion forum][forum].
+
+[forum]: https://github.com/orgs/Safe-DS/discussions
---
+++ packages/safe-ds-examples/safe_ds_examples-0.17.0.tar.gz/safe_ds_examples-0.17.0/src/safeds_examples/tabular/containers/_example_table.py
@@ -0,0 +1,69 @@
+from safeds.data.tabular.containers import Table
+from safeds.exceptions import UnknownColumnNameError
+
+
+class ExampleTable(Table):
+    """
+    A `Table` with descriptions for its columns.
+
+    Parameters
+    ----------
+    table : Table
+        The table.
+    column_descriptions : dict[str, str]
+        A dictionary mapping column names to their descriptions.
+
+    Raises
+    ------
+    UnknownColumnNameError
+        If a column name in `descriptions` does not exist in `table`.
+    """
+
+    # noinspection PyMissingConstructor
+    def __init__(self, table: Table, column_descriptions: dict[str, str]) -> None:
+        # Check that all column names in `descriptions` exist in `table`
+        invalid_column_names = set(column_descriptions.keys()) - set(table.column_names)
+        if invalid_column_names:
+            raise UnknownColumnNameError(list(invalid_column_names))
+
+        self._data = table._data
+        self._schema = table.schema
+        self._descriptions = column_descriptions
+
+    @property
+    def column_descriptions(self) -> Table:
+        """
+        Return a `Table` contain the name of a column and its description.
+
+        The name is stored in a column called `"Name"` and the description in a column called `"Description"`.
+        """
+        return Table(
+            {
+                "Name": self.column_names,
+                "Description": [self.get_column_description(column_name) for column_name in self.column_names],
+            },
+        )
+
+    def get_column_description(self, column_name: str) -> str:
+        """
+        Get the description of a column. If no description exists, an empty string is returned.
+
+        Parameters
+        ----------
+        column_name : str
+            The name of the column.
+
+        Returns
+        -------
+        description : str
+            The description of the column.
+
+        Raises
+        ------
+        UnknownColumnNameError
+            If no column with the given name exists.
+        """
+        if column_name not in self.column_names:
+            raise UnknownColumnNameError([column_name])
+
+        return self._descriptions.get(column_name, "")
