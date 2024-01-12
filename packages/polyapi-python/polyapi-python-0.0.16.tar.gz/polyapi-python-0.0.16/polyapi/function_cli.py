7a61bcb472d43aa20f5faf4e5660f95035d501b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 05:04:19 +0000

    Add package polyapi-python/0.0.16/polyapi-python-0.0.16.tar.gz

---
+++ packages/polyapi-python/polyapi-python-0.0.16.tar.gz/polyapi-python-0.0.16/polyapi/function_cli.py
@@ -0,0 +1,186 @@
+import ast
+import argparse
+import json
+import types
+import sys
+from typing import Dict, List, Tuple
+from typing_extensions import _TypedDictMeta  # type: ignore
+import requests
+from pydantic import TypeAdapter
+from polyapi.generate import generate
+from polyapi.config import get_api_key_and_url
+from polyapi.constants import PYTHON_TO_JSONSCHEMA_TYPE_MAP
+from polyapi.utils import get_auth_headers
+
+
+def _get_schemas(code: str) -> List[Dict]:
+    schemas = []
+    user_code = types.SimpleNamespace()
+    exec(code, user_code.__dict__)
+    for name, obj in user_code.__dict__.items():
+        if (
+            isinstance(obj, type) and
+            isinstance(obj, _TypedDictMeta)
+            and name != "TypedDict"
+        ):
+            schemas.append(TypeAdapter(obj).json_schema())
+    return schemas
+
+
+def _get_jsonschema_type(python_type: str):
+    if python_type == "Any":
+        return "Any"
+
+    if python_type == "List":
+        return "array"
+
+    if python_type.startswith("List["):
+        # the actual type will be returned as return_type_schema
+        subtype = python_type[5:-1]
+        if subtype == "Any":
+            return "any[]"
+        elif subtype in ["int", "float", "str", "bool"]:
+            jsonschema_type = PYTHON_TO_JSONSCHEMA_TYPE_MAP.get(subtype)
+            return f"{jsonschema_type}[]"
+        else:
+            # the schema will handle it!
+            return "object"
+
+    if python_type.startswith("Dict"):
+        return "object"
+
+    rv = PYTHON_TO_JSONSCHEMA_TYPE_MAP.get(python_type)
+    if rv:
+        return rv
+
+    # should be custom type
+    return python_type
+
+
+def get_python_type_from_ast(expr: ast.expr) -> str:
+    if isinstance(expr, ast.Name):
+        return str(expr.id)
+    elif isinstance(expr, ast.Subscript):
+        assert isinstance(expr, ast.Subscript)
+        name = getattr(expr.value, "id", "")
+        if name == "List":
+            slice = getattr(expr.slice, "id", "Any")
+            return f"List[{slice}]"
+        return "Any"
+    else:
+        return "Any"
+
+
+def _get_type_schema(json_type: str, python_type: str, schemas: List[Dict]):
+    if python_type.startswith("List["):
+        subtype = python_type[5:-1]
+        for schema in schemas:
+            if schema["title"] == subtype:
+                return {"type": "array", "items": schema}
+
+        # subtype somehow not in schema, just call it any
+        return None
+    else:
+        for schema in schemas:
+            if schema["title"] == json_type:
+                return schema
+
+
+def _get_type(expr: ast.expr | None, schemas: List[Dict]) -> Tuple[str, Dict | None]:
+    if not expr:
+        return "Any", None
+    python_type = get_python_type_from_ast(expr)
+    json_type = _get_jsonschema_type(python_type)
+    return json_type, _get_type_schema(json_type, python_type, schemas)
+
+
+
+def _get_args_and_return_type_from_code(code: str, function_name: str):
+    parsed_args = []
+    return_type = None
+    return_type_schema = None
+
+    schemas = _get_schemas(code)
+
+    parsed_code = ast.parse(code)
+    for node in ast.iter_child_nodes(parsed_code):
+        if isinstance(node, ast.FunctionDef) and node.name == function_name:
+            function_args = [arg for arg in node.args.args]
+            for arg in function_args:
+                json_type, type_schema = _get_type(arg.annotation, schemas)
+                parsed_args.append(
+                    {
+                        "key": arg.arg,
+                        "name": arg.arg,
+                        "type": json_type,
+                        "typeSchema": json.dumps(type_schema),
+                    }
+                )
+            if node.returns:
+                return_type, return_type_schema = _get_type(node.returns, schemas)
+            else:
+                return_type = "Any"
+            break
+
+    if not return_type:
+        print(
+            f"Error: function named {function_name} not found as top-level function in file. Exiting."
+        )
+        sys.exit(1)
+
+
+    return parsed_args, return_type, return_type_schema
+
+
+def function_add_or_update(
+    context: str, description: str, server: bool, logs_enabled: bool, subcommands: List
+):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("subcommand", choices=["add"])
+    parser.add_argument("function_name")
+    parser.add_argument("filename")
+    args = parser.parse_args(subcommands)
+
+    with open(args.filename, "r") as f:
+        code = f.read()
+
+    # OK! let's parse the code and generate the arguments
+    (
+        arguments,
+        return_type,
+        return_type_schema,
+    ) = _get_args_and_return_type_from_code(code, args.function_name)
+
+    data = {
+        "context": context,
+        "name": args.function_name,
+        "description": description,
+        "code": code,
+        "language": "python",
+        "returnType": return_type,
+        "returnTypeSchema": return_type_schema,
+        "arguments": arguments,
+        "logsEnabled": logs_enabled,
+    }
+
+    api_key, api_url = get_api_key_and_url()
+    assert api_key
+    if server:
+        url = f"{api_url}/functions/server"
+    else:
+        raise NotImplementedError("Client functions not yet implemented.")
+        # url = f"{base_url}/functions/client"
+
+    headers = get_auth_headers(api_key)
+    print("Adding function...")
+    resp = requests.post(url, headers=headers, json=data)
+    if resp.status_code == 201:
+        function_id = resp.json()["id"]
+        print(f"Function added successfully. Function id is {function_id}")
+        print("Regenerating library...")
+        generate()
+    else:
+        print("Error adding function.")
+        print(resp.status_code)
+        print(resp.content)
+        sys.exit(1)
