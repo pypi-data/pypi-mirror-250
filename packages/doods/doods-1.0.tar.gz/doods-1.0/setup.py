ec5ffdf685f3595855a0218dd20c0d00fee4f9a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 04:04:50 +0000

    Add package doods/1.0/doods-1.0.tar.gz

---
+++ packages/doods/doods-1.0.tar.gz/doods-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='doods',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+    'requests',
+    'PySocks'],
+    description="read docs in https://github.com/Kaii-Devv/ds2play",
+    author='Tamsis X Code',
+    author_email='ceeskamu@gmail.com',
+    entry_points={
+        'console_scripts': [
+            'module = ds2play.ds:main',
+        ],
+    },
+)
