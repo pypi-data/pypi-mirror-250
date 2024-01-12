5611d40ba5d2b23906b697349c44431d6da1390a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 04:05:38 +0000

    Add package oda-wd-client/0.0.18/oda_wd_client-0.0.18.tar.gz

---
+++ packages/oda-wd-client/oda_wd_client-0.0.18.tar.gz/oda_wd_client-0.0.18/oda_wd_client/types/resource_management.py
@@ -0,0 +1,29 @@
+from oda_wd_client.service.resource_management.types import (
+    CostCenterWorktag,
+    FinancialAttachmentData,
+    SpendCategory,
+    Supplier,
+    SupplierInvoice,
+    SupplierInvoiceLine,
+    TaxApplicability,
+    TaxCode,
+    TaxOption,
+    TaxRate,
+    TaxRateOptionsData,
+    TaxRecoverability,
+)
+
+__all__ = [
+    "CostCenterWorktag",
+    "SpendCategory",
+    "Supplier",
+    "SupplierInvoice",
+    "SupplierInvoiceLine",
+    "TaxApplicability",
+    "TaxCode",
+    "TaxOption",
+    "TaxRate",
+    "TaxRateOptionsData",
+    "TaxRecoverability",
+    "FinancialAttachmentData",
+]
