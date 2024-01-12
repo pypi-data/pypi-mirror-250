022ea1a1b1fc93d20a23f4ee83643249e24e2713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 16:05:23 +0000

    Add package MedShapeNetCore/0.1.0/MedShapeNetCore-0.1.0.tar.gz

---
+++ packages/MedShapeNetCore/MedShapeNetCore-0.1.0.tar.gz/MedShapeNetCore-0.1.0/setup.py
@@ -0,0 +1,44 @@
+# To build and upload to PyPI:
+#
+#     python3 setup.py sdist bdist_wheel
+#     python3 -m twine upload dist/*
+#
+import MedShapeNetCore
+from setuptools import setup, find_packages
+
+
+setup(
+    name='MedShapeNetCore',
+    version=MedShapeNetCore.__version__,
+    url='https://github.com/Jianningli/medshapenet-feedback/',
+    license='Apache-2.0 License',
+    author='Jianning Li',
+    author_email='jianningli.me@gmail.com',
+    python_requires=">=3.8.0",
+    description='MedShapeNetCore: A Lightweight 3D Repository for Computer Vision and Machine Learning',
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "scikit-image",
+        "tqdm",
+        "Pillow",
+        "fire",
+        "trimesh",
+        "SimpleITK",
+        "open3d",
+        "scipy",
+        "matplotlib",
+        "clint",
+        "requests",
+        "argparse"
+    ],
+    zip_safe=True,
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only"
+    ]
+)
