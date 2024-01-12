6e113001191b95d1db4fa23d0bc0a3e45c86f6a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 15:04:34 +0000

    Add package scrapeomatic/0.1.5/scrapeomatic-0.1.5-py3-none-any.whl

---
+++ packages/scrapeomatic/scrapeomatic-0.1.5-py3-none-any.whl/scrapeomatic/utils/constants.py
@@ -0,0 +1,15 @@
+DEFAULT_BROWSER = "chrome"
+DEFAULT_TIMEOUT = 5
+DEFAULT_VIDEO_LIMIT = 10
+DEFAULT_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+
+
+# Base URLs
+GITHUB_BASE_URL = "https://github.com"
+INSTAGRAM_BASE_URL =  "https://www.instagram.com"
+INSTAGRAM_QUERY_HASH = "b3055c01b4b222b8a47dc12b090e4e64"
+INSTAGRAM_PROFILE_URL = f"{INSTAGRAM_BASE_URL}/api/v1/users/web_profile_info/"
+INSTAGRAM_VIDEO_URL = f"{INSTAGRAM_BASE_URL}/graphql/query/?query_hash={INSTAGRAM_QUERY_HASH}&variables="
+INSTAGRAM_APP_ID = "936619743392459"
+TIKTOK_BASE_URL = "https://tiktok.com/@"
+YOUTUBE_BASE_URL = "https://www.youtube.com/@"
