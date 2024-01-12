9a01fcfc19b47d1858b2cd03d91b7eadad515f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 03:04:27 +0000

    Add package video-streaming-api/0.1.0/video-streaming-api-0.1.0.tar.gz

---
+++ packages/video-streaming-api/video-streaming-api-0.1.0.tar.gz/video-streaming-api-0.1.0/video_streaming/processing/datasets.py
@@ -0,0 +1,176 @@
+import os
+import sys
+import glob
+import numpy as np
+
+from pathlib import Path
+
+if Path(__file__).parent not in sys.path:
+    sys.path.append(Path(__file__).parent)
+from .manager import VideoManagers
+
+
+class LoadBatchVideos:
+    # include image suffixes
+    IMG_FORMATS = 'bmp', 'dng', 'jpeg', 'jpg', 'mpo', 'png', 'tif', 'tiff', 'webp'
+    # include video suffixes
+    VID_FORMATS = 'asf', 'avi', 'gif', 'm4v', 'mkv', 'mov', 'mp4', 'mpeg', 'mpg', 'ts', 'wmv'
+
+    def __init__(
+        self, path, define, vid_batch=1, div_fps=1, save_dir='', preproc=None, img_size=(640, 640), many_folder=False,
+        video_sec=70, vis_mode='write', SYSDTFORMAT='%Y%m%d%H%M%S', YMDFORMAT='%Y%m%d000000', visualizer=None,
+        queue_maxsize=10, vid_queue_maxsize=200, close_prev_window=True
+    ):
+        # Initialize variables
+        self.div_fps = div_fps
+        self.preproc = preproc
+        self.img_size = img_size
+        self.vid_batch = vid_batch
+        self.visualizer = visualizer
+        self.end_title = '\n------------------------------------------------' + (
+            '\n' + '%20s' * 2) % ('Infer Time', 'Total Time')
+        # get video paths
+        self.files, self.defines = LoadBatchVideos.build_videos_dict(path, define, many_folder=many_folder)
+
+        # Create video managers
+        self.video_managers = VideoManagers.create(
+            self.files, self.defines, div_fps, save_dir, vis_mode, video_sec=video_sec, visualizer=visualizer,
+            end_title=self.end_title, SYSDTFORMAT=SYSDTFORMAT, YMDFORMAT=YMDFORMAT, queue_maxsize=queue_maxsize,
+            vid_queue_maxsize=vid_queue_maxsize, close_prev_window=close_prev_window
+        )
+        self._init_from_manager()
+
+    @staticmethod
+    def get_video_paths(p, many_folder):
+        if '*' in p:
+            files = sorted(glob.glob(p, recursive=True))  # glob
+        elif os.path.isdir(p):
+            if many_folder:
+                files = sorted(glob.glob(os.path.join(p, '*/img/*.*')))  # dir
+            else:
+                if glob.glob(os.path.join(p, '*.*')):
+                    files = sorted(glob.glob(os.path.join(p, '*.*')))  # dir
+                else:
+                    files = sorted(glob.glob(os.path.join(p, '*/*.*')))  # dir
+        elif os.path.isfile(p):
+            files = [p]  # files
+        else:
+            raise Exception(f'ERROR: {p} does not exist')
+        return [x for x in files if x.split('.')[-1].lower() in LoadBatchVideos.VID_FORMATS]
+
+    @staticmethod
+    def build_videos_dict(path, define, many_folder=False):
+        # process str
+        if not isinstance(path, list):
+            p = str(Path(path).resolve())  # os-agnostic absolute path
+            path = LoadBatchVideos.get_video_paths(p, many_folder)
+            define = [define for _ in path]
+
+        # build
+        files, defines = {}, {}
+        for i, p in enumerate(path):
+            if isinstance(path, str):
+                p = str(Path(p).resolve())  # os-agnostic absolute path
+                files[i] = (p, many_folder)
+            else:
+                files[i] = p
+            defines[i] = define[i]
+        return files, defines
+
+    @staticmethod
+    def frame_counter(curframe, epochframe):
+        """ Transfer the stream real frame to curframe in an epoch """
+        frame = curframe % epochframe
+        if curframe % epochframe == 0:
+            frame = epochframe
+        return frame
+
+    def transfer_images_info(self, stream_info, img0s):
+        imgs_info = []
+        for i, (_in, frame, dt, cur_sec, cur_time, vid_time, info) in enumerate(stream_info):
+            imgs_info.append({
+                "id": _in,
+                "frame": frame,
+                "height": img0s[i].shape[1],
+                "width": img0s[i].shape[2],
+                "ratio": min(self.img_size[0] / img0s[i].shape[1], self.img_size[1] / img0s[i].shape[2]),
+                "raw_img": img0s[i],
+                "dt": dt, "cur_sec": cur_sec, "cur_time": cur_time, "vid_time": vid_time,
+                "is_newepoch": self.video_managers[_in].epoch['n'],
+                "is_epochfinal": self.video_managers[_in].epoch['f'],
+            })
+        return imgs_info
+
+    def _init_from_manager(self):
+        self.stop_signals = {k: False for k in self.video_managers.keys()}
+        self.batch = len(self.video_managers)
+
+        # setting the title for show
+        self.title = ('\n' + '%15s' * 2) % ('Parrent Folder', 'Video')
+        for _, m in self.video_managers.items():
+            self.title += ('\n' + '%15s' * 2) % (os.path.join(*m.stream.video_define), m.vid_writer.start_time)
+
+    def _update_epoch(self, k, manager):
+        epoch = int(self.frames[k] / manager.stream.epochframes)
+        if epoch > manager.epoch['e']:
+            if self.frames[k] % manager.stream.epochframes == 0:
+                manager.epoch['n'], manager.epoch['f'] = False, True
+            else:
+                manager.epoch = {'e': epoch, 'n': True, 'f': False}
+        else:
+            manager.epoch = {'e': epoch, 'n': False, 'f': False}
+
+    def stop(self, k):
+        self.stop_signals[k] = True
+        self.video_managers[k].stop()
+
+    def __iter__(self):
+        self.frames = {}
+        self.finalframes = {}
+        # start video manager
+        for k, video_manager in self.video_managers.items():
+            self.frames[k] = 0
+            self.finalframes[k] = 0
+            video_manager.start()
+
+        # show image
+        if self.visualizer is not None:
+            self.visualizer.run_start()
+        return self
+
+    def __next__(self):
+        if self.batch == 0 or all([self.stop_signals[k] for k in self.video_managers.keys()]):
+            raise StopIteration
+
+        img0s, imgs, stream_info = [], None, []
+        for k, manager in self.video_managers.items():
+            if not self.stop_signals[k]:
+                for _ in range(self.vid_batch):
+                    # Load Image
+                    ret, self.frames[k], img, info = manager.stream.read(self.frames[k])
+                    if not len(info):
+                        self.stop_signals[k] = True
+                    if not ret or img is None:
+                        continue
+
+                    # Concatenate Image Information
+                    img0s.append(img)
+                    if self.preproc is not None:
+                        img, _ = self.preproc(img, None, self.img_size)
+                    if imgs is None:
+                        imgs = np.expand_dims(img, 0)
+                    else:
+                        imgs = np.concatenate((imgs, np.expand_dims(img, 0)), axis=0)
+
+                    # Calculate Epoch Frame
+                    self._update_epoch(k, manager)
+
+                    # Record Information
+                    self.finalframes[k] = LoadBatchVideos.frame_counter(self.frames[k], manager.stream.epochframes)
+                    stream_info.append((k, self.finalframes[k], *manager.stream.get_cur_info(info['sec']), info))
+        return self.transfer_images_info(stream_info, img0s), imgs
+
+    def __len__(self):
+        return int(np.ceil(max([
+            m.stream.maxframes for m in self.video_managers.values()]) / self.vid_batch / self.div_fps)
+        ) if len(self.video_managers.values()) else 0
---
+++ packages/video-streaming-api/video-streaming-api-0.1.0.tar.gz/video-streaming-api-0.1.0/video_streaming/processing/manager.py
@@ -0,0 +1,154 @@
+import os
+
+from loguru import logger
+from threading import Thread
+
+from .stream import Stream
+from .writer import VideoWriter
+
+
+class VideoManagers:
+    _instances = {}
+
+    def __init__(
+        self, mode, vis_mode, stream, stream_thread, save_dir, visualizer, end_title, vid_queue_maxsize=200,
+        close_prev_window=True
+    ):
+        # update parameters
+        self.mode = mode
+        self.vis_mode = vis_mode
+        self.stream = stream
+        self.stream_thread = stream_thread
+        self.save_dir = save_dir
+        self.visualizer = visualizer
+        self.vid_writer = VideoWriter(
+            self.save_dir, stream.video_define, stream.start_time, stream.infer_fps,
+            stream.width, stream.height, vid_reload=True if mode == 'webcam' else False, title=end_title,
+            queue_maxsize=vid_queue_maxsize, visualizer=self.visualizer,
+            keepdate=False if mode == 'webcam' else stream.video_define['start_time'] == 'videoname',
+            keepname=False if mode == 'webcam' else stream.video_define['start_time'] == 'videoname',
+            close_prev_window=close_prev_window
+        )
+        # vis mode thread
+        if self.vis_mode == 'write':
+            self.vid_thread = Thread(target=self.vid_writer.process_video, args=(True, False), daemon=True)
+        elif self.vis_mode == 'show':
+            self.vid_thread = Thread(target=self.vid_writer.process_video, args=(False, True), daemon=True)
+        elif self.vis_mode == 'all':
+            self.vid_thread = Thread(target=self.vid_writer.process_video, args=(True, True), daemon=True)
+        else:
+            self.vid_thread = None
+            logger.warning("Current vis_mode is %s, will ignore to build video writer !!" % self.vis_mode)
+        self.epoch = {'e': 0, 'n': True, 'f': False}
+
+    @staticmethod
+    def get_mode(video_path):
+        mode = 'video' if isinstance(video_path, list) or isinstance(video_path, str) else 'webcam'
+        return mode
+
+    def stop(self):
+        self.stream.stop_signal = True
+
+        if self.vid_writer is not None and not self.vid_writer.stop_flag:
+            self.vid_writer.put_frame(None, '', '', -1)
+
+    def start(self):
+        # stream thread
+        if self.stream_thread is not None:
+            self.stream_thread.start()
+
+        # writer threads
+        if self.vid_thread is not None:
+            self.vid_thread.start()
+
+    @classmethod
+    def create(
+        cls, video_sources: dict, video_defines: dict, div_fps, save_dir, vis_mode, video_sec=0,
+        visualizer=None, end_title='', SYSDTFORMAT='', YMDFORMAT='', warn=True, queue_maxsize=10, vid_queue_maxsize=200,
+        close_prev_window=True
+    ):
+        """
+        Args:
+            video_sources: {id: "video path", ...}
+            video_defines: {id: {"parent_folder": [None, ...], "start_time": "current"}}
+        """
+        initialized_video_source = set()
+        for k, video_source in video_sources.items():
+            video_define = video_defines[k]
+            mode = cls.get_mode(video_source)
+            # check path
+            if str(video_source) in initialized_video_source or (
+                isinstance(video_source, str) and (not video_source or not os.path.exists(video_source))
+            ):
+                continue
+
+            # load stream
+            stream, stream_thread = Stream.load(
+                mode, video_source, video_define, div_fps, save_dir, video_sec=video_sec,
+                SYSDTFORMAT=SYSDTFORMAT, YMDFORMAT=YMDFORMAT, warn=warn, queue_maxsize=queue_maxsize
+            )
+            initialized_video_source.add(str(video_source))
+
+            # check stream was opened
+            if not stream.capture.isOpened():
+                continue
+
+            # get manager
+            cls._instances[k] = VideoManagers(
+                mode, vis_mode, stream, stream_thread, save_dir, visualizer, end_title,
+                vid_queue_maxsize=vid_queue_maxsize, close_prev_window=close_prev_window
+            )
+        return cls._instances
+
+
+if __name__ == '__main__':
+    # Create VideoManager
+    video_managers = VideoManagers.create(
+        {0: {
+            'ip': "192.168.66.28", 'port': "554", 'username': "Admin", 'password': "1234",
+            'stream_name': "ch1", 'group': "TR", 'channel': 'Vivocam1'
+        }},
+        {0: {"parent_folder": [None, None, None], "start_time": "videoname"}},
+        div_fps=2, save_dir='', vis_mode='show', video_sec=600, visualizer=None, end_title='',
+        SYSDTFORMAT='%Y%m%d%H%M%S', YMDFORMAT='%Y%m%d000000'
+    )
+
+    # Start the video manager
+    for k, manager in video_managers.items():
+        manager.start()
+
+    # Show video streaming
+    frames = {}
+    while not all([m.stream.stop_stream for m in video_managers.values()]):
+        for k, manager in video_managers.items():
+            # init frame
+            if k not in frames:
+                frames[k] = 1
+
+            # read new frame
+            ret, frames[k], img, info = manager.stream.read(frames[k])
+            if not ret or img is None:
+                continue
+
+            # TODO: you can add your image recongnitionin in here
+            """
+            output = model(img)
+            run_event_generator(img, output)
+            img = visualize(output)
+            ...
+            """
+            # TODO: you can set stop rules by yourself
+            if manager.vid_writer.stop_flag:
+                manager.stop()
+                continue
+
+            # show
+            cur_date_time, cur_second, cur_time, vid_time = manager.stream.get_cur_info(info['sec'])
+            manager.vid_writer.put_frame(img, cur_date_time, cur_time, cur_second, vis='a')
+
+    # Stop the video manager
+    for k, manager in video_managers.items():
+        manager.stop()
+        if not manager.vid_writer.stop_flag:
+            manager.vid_writer.put_frame(None, '', '', -1)
+            manager.vid_thread.join()
