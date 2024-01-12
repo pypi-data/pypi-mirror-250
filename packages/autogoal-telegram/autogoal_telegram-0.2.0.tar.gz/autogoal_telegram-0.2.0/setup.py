d25d0d93abb8d9b3bdc9634d167577c1a9fd4cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 14:04:22 +0000

    Add package autogoal-telegram/0.2.0/autogoal_telegram-0.2.0.tar.gz

---
+++ packages/autogoal-telegram/autogoal_telegram-0.2.0.tar.gz/autogoal_telegram-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['autogoal_telegram']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['autogoal==1.0.1a0', 'python-telegram-bot==13', 'telegram>=0.0.1,<0.0.2']
+
+setup_kwargs = {
+    'name': 'autogoal-telegram',
+    'version': '0.2.0',
+    'description': 'telegram library wrapper for AutoGOAL',
+    'long_description': '# AutoGOAL TELEGRAM Algorithm Library',
+    'author': 'Suilan Estevez-Velarde',
+    'author_email': 'suilanestevez@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://autogoal.github.io',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '==3.9.16',
+}
+
+
+setup(**setup_kwargs)
