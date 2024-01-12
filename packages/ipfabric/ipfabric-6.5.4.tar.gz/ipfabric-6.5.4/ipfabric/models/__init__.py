4f24d493b9e8bf0ef6eee37f803b49ba8198c1b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 04:04:16 +0000

    Add package ipfabric/6.5.4/ipfabric-6.5.4.tar.gz

---
+++ packages/ipfabric/ipfabric-6.5.4.tar.gz/ipfabric-6.5.4/ipfabric/models/__init__.py
@@ -0,0 +1,20 @@
+from .device import Device, Devices
+from .intent import Intent
+from .inventory import Inventory
+from .jobs import Jobs
+from .snapshot import Snapshot, SNAPSHOT_COLUMNS
+from .table import BaseTable, Table
+from .technology import Technology
+
+__all__ = [
+    "BaseTable",
+    "Table",
+    "Inventory",
+    "Technology",
+    "Jobs",
+    "Snapshot",
+    "Intent",
+    "SNAPSHOT_COLUMNS",
+    "Device",
+    "Devices",
+]
