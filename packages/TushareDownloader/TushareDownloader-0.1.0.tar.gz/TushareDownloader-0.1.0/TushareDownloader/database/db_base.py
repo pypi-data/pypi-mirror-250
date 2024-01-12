77cb4cf8183147a751f6c8403c042f5fc01fc065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 15:04:07 +0000

    Add package TushareDownloader/0.1.0/TushareDownloader-0.1.0.tar.gz

---
+++ packages/TushareDownloader/TushareDownloader-0.1.0.tar.gz/TushareDownloader-0.1.0/TushareDownloader/database/db_base.py
@@ -0,0 +1,50 @@
+"""
+database base module
+
+Author: Yanzhong Huang
+Email: yanzhong.huang@outlook.com
+"""
+
+import json
+from sqlalchemy import create_engine
+
+from sqlalchemy import Engine
+
+
+CONFIG_PATH = 'TushareDownloader/database/database_config.json'
+
+
+def get_db_config() -> dict[str, str|int]:
+    """
+    get database configuration
+    :return: database configuration
+    """
+    with open(CONFIG_PATH, 'r') as f:
+        config = json.load(f)
+    return config 
+
+
+def update_db_config(config: dict[str, str|int]) -> None:
+    """
+    update database configuration
+    :param config: database configuration
+    :return: None
+    """
+    with open(CONFIG_PATH, 'w') as f:
+        json.dump(config, f)
+
+
+def get_db_engine() -> Engine:
+    """
+    :return: database engine
+    """
+    config = get_db_config()
+    engine = create_engine(
+        f"mysql+pymysql://{config['user']}:{config['password']}@{config['host']}:{config['port']}/{config['database']}",
+        echo=False
+    )
+    return engine
+
+
+if __name__ == '__main__':
+    print(get_db_config())
