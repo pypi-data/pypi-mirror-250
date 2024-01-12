d2180199ecee581edbb50f25ace75c17e6a78f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 14:04:39 +0000

    Add package pyenphase/1.17.0/pyenphase-1.17.0-py3-none-any.whl

---
+++ packages/pyenphase/pyenphase-1.17.0-py3-none-any.whl/pyenphase/const.py
@@ -0,0 +1,68 @@
+import enum
+
+import httpx
+from awesomeversion import AwesomeVersion
+
+# Versions
+LEGACY_ENVOY_VERSION = AwesomeVersion("3.9.0")
+ENSEMBLE_MIN_VERSION = AwesomeVersion("5.0.0")
+AUTH_TOKEN_MIN_VERSION = AwesomeVersion("7.0.0")
+
+# System Production
+URL_PRODUCTION_INVERTERS = "/api/v1/production/inverters"
+URL_PRODUCTION_V1 = "/api/v1/production"
+URL_PRODUCTION_JSON = "/production.json"
+URL_PRODUCTION = "/production"
+
+# Authentication
+URL_AUTH_CHECK_JWT = "/auth/check_jwt"
+
+# Battery and Enpower Status
+URL_DRY_CONTACT_STATUS = "/ivp/ensemble/dry_contacts"
+URL_DRY_CONTACT_SETTINGS = "/ivp/ss/dry_contact_settings"
+URL_ENSEMBLE_INVENTORY = "/ivp/ensemble/inventory"
+URL_ENSEMBLE_STATUS = "/ivp/ensemble/status"
+URL_ENSEMBLE_SECCTRL = "/ivp/ensemble/secctrl"
+URL_ENCHARGE_BATTERY = "/ivp/ensemble/power"
+URL_GRID_RELAY = "/ivp/ensemble/relay"
+URL_POWER_EXPORT = "/uvp/ss/pel_settings"
+URL_TARIFF = "/admin/lib/tariff"
+
+# Generator Configuration
+URL_GEN_CONFIG = "/ivp/ss/gen_config"
+URL_GEN_MODE = "/ivp/ss/gen_mode"
+URL_GEN_SCHEDULE = "/ivp/ss/gen_schedule"
+
+# Meters data
+ENDPOINT_URL_METERS = "/ivp/meters"
+ENDPOINT_URL_METERS_READINGS = "/ivp/meters/readings"
+
+LOCAL_TIMEOUT = httpx.Timeout(
+    # The envoy can be slow to respond but fast to connect to we
+    # need to set a long timeout for the read and a short timeout
+    # for the connect
+    timeout=10.0,
+    read=60.0,
+)
+
+
+class SupportedFeatures(enum.IntFlag):
+    INVERTERS = 1
+    METERING = 2
+    TOTAL_CONSUMPTION = 4
+    NET_CONSUMPTION = 8
+    ENCHARGE = 16
+    ENPOWER = 32
+    PRODUCTION = 64
+    TARIFF = 128
+    DUALPHASE = 256
+    THREEPHASE = 512
+
+
+class PhaseNames(enum.StrEnum):
+    PHASE_1 = "L1"
+    PHASE_2 = "L2"
+    PHASE_3 = "L3"
+
+
+PHASENAMES = list(PhaseNames)
