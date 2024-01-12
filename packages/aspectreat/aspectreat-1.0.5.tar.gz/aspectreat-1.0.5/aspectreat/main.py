c0a9a9a5e09ced82bf4bf66bc786324f2f9590a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 15:04:22 +0000

    Add package aspectreat/1.0.5/aspectreat-1.0.5.tar.gz

---
+++ packages/aspectreat/aspectreat-1.0.5.tar.gz/aspectreat-1.0.5/aspectreat/main.py
@@ -0,0 +1,4 @@
+# -*- coding = utf-8 -*-
+from .aspectreat_pcs.et import np as nps
+from .aspectreat_pcs.et import aspects as aspects
+from .aspectreat_pcs.et import aspectPerson_s as aspect_person_s
