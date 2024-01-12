f516e30d2bff247b793047fca99afbf1746f6260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 05:04:30 +0000

    Add package c4dynamics/1.0.70/c4dynamics-1.0.70-py3-none-any.whl

---
+++ packages/c4dynamics/c4dynamics-1.0.70-py3-none-any.whl/c4dynamics/src/main/py/__init__.py
@@ -0,0 +1,5 @@
+#
+# py  
+##
+
+# from . import eqm 
---
+++ packages/c4dynamics/c4dynamics-1.0.70-py3-none-any.whl/c4dynamics/utils/params.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+''' global quantities '''
+pi       = np.pi
+g_ms2    = 9.80665  # 
+g_fts2   = 32.2        # 
+
+''' conversion variables '''
+ft2m        = 0.3048    # 1             # 
+lbft2kgm    = 4.88243 # 47.8803 to include gravity        # 1      
+r2d         = 180 / np.pi 
+d2r         = np.pi / 180 
+
+''' math functions  '''
+sin       = np.sin
+cos       = np.cos
+tan       = np.tan
+asin      = np.arcsin
+acos      = np.arccos
+atan      = np.arctan
+
+
+
+
