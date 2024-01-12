75790dfaac4bf715efc6e48efe71d73cdbecb3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 04:05:40 +0000

    Add package motrack/0.3.1/motrack-0.3.1-py3-none-any.whl

---
+++ packages/motrack/motrack-0.3.1-py3-none-any.whl/motrack/common/project.py
@@ -0,0 +1,14 @@
+"""
+Set of path constants relative to project source (independent of project location on system).
+"""
+import os
+
+
+ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+SRC_PATH = os.path.join(ROOT_PATH, 'motrack')
+CONFIGS_PATH = os.path.join(ROOT_PATH, 'configs')
+OUTPUTS_PATH = os.path.join(ROOT_PATH, 'outputs')
+PLAYGROUND_PATH = os.path.join(ROOT_PATH, 'playground')
+
+ASSETS_PATH = '/media/home/motrack-outputs/'
+MASTER_PATH = '/media/home/'
