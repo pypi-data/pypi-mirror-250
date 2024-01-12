b7192a9873deee94a4dac190e4ffb81c9461a80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 04:04:09 +0000

    Add package aioretry-decorator/1.0.4/aioretry-decorator-1.0.4.tar.gz

---
+++ packages/aioretry-decorator/aioretry-decorator-1.0.4.tar.gz/aioretry-decorator-1.0.4/setup.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+from setuptools import setup
+
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name='aioretry-decorator',
+    version='1.0.4',
+    description='Handy decorator to set retry policies for async callables with some useful features',
+
+    url='https://github.com/remort/aioretry',
+    author='Vadim Bazhov',
+    author_email='master@remort.net',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    packages=['aioretry_decorator'],
+    package_data={
+        'aioretry_decorator': ['py.typed'],
+    },
+)
