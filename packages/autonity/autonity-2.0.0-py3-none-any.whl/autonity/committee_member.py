4cd7213d6d25ff726b553680894aae240539ddde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 05:04:14 +0000

    Add package autonity/2.0.0/autonity-2.0.0-py3-none-any.whl

---
+++ packages/autonity/autonity-2.0.0-py3-none-any.whl/autonity/committee_member.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2015-2022 Clearmatics Technologies Ltd - All Rights Reserved.
+
+"""
+Committee member model class
+"""
+
+from __future__ import annotations
+
+from typing import Tuple, TypedDict
+
+from eth_typing import ChecksumAddress
+
+
+class CommitteeMember(TypedDict):
+    """
+    Dictionary representing a member of the consensus committee.
+    """
+
+    address: ChecksumAddress
+    voting_power: int
+
+
+def committee_member_from_tuple(value: Tuple[ChecksumAddress, int]) -> CommitteeMember:
+    """
+    From Web3 tuple
+    """
+    assert len(value) == 2
+    assert isinstance(value[0], str)
+    assert isinstance(value[1], int)
+    return CommitteeMember({"address": value[0], "voting_power": value[1]})
