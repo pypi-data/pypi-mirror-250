bbf74182d0ad223f206b9716155180f979c5dfb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 04:05:14 +0000

    Add package pygpt-net/2.0.96/pygpt-net-2.0.96.tar.gz

---
+++ packages/pygpt-net/pygpt-net-2.0.96.tar.gz/pygpt-net-2.0.96/src/pygpt_net/controller/settings/editor.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2023.12.30 21:00:00                  #
+# ================================================== #
+
+import copy
+
+from pygpt_net.utils import trans
+
+
+class Editor:
+    def __init__(self, window=None):
+        """
+        Settings controller
+
+        :param window: Window instance
+        """
+        self.window = window
+        self.options = {}
+        self.before_config = {}
+        self.initialized = False
+
+    def init(self, id: str):
+        """
+        Initialize settings
+
+        :param id: settings window id
+        """
+        # add hooks for config update in real-time
+        self.window.ui.add_hook("update.config.font_size", self.hook_update)
+        self.window.ui.add_hook("update.config.font_size.input", self.hook_update)
+        self.window.ui.add_hook("update.config.font_size.ctx", self.hook_update)
+        self.window.ui.add_hook("update.config.font_size.toolbox", self.hook_update)
+        self.window.ui.add_hook("update.config.theme.markdown", self.hook_update)
+        self.window.ui.add_hook("update.config.render.plain", self.hook_update)
+        self.window.ui.add_hook("update.config.vision.capture.enabled", self.hook_update)
+        self.window.ui.add_hook("update.config.vision.capture.auto", self.hook_update)
+        self.window.ui.add_hook("update.config.ctx.records.limit", self.hook_update)
+        self.window.ui.add_hook("update.config.layout.density", self.hook_update)
+
+        if id == 'settings':
+            options = {}
+            for key in self.options:
+                if 'type' not in self.options[key]:
+                    continue
+                options[key] = self.options[key]
+                options[key]['value'] = self.window.core.config.get(key)  # append current config value
+            self.window.controller.config.load_options('config', options)
+
+    def load(self):
+        """Load settings options from config file"""
+        self.load_config_options()
+
+        # store copy of loaded config data
+        self.before_config = copy.deepcopy(self.window.core.config.all())
+
+    def load_config_options(self, initialize: bool = True):
+        """
+        Load settings options from config file
+
+        :param initialize: True if marks settings as initialized
+        """
+        self.options = self.window.core.settings.get_options()
+        if initialize:
+            self.initialized = True
+
+    def save(self, id: str = None):
+        """
+        Save settings
+
+        :param id: settings id
+        """
+        for key in self.options:
+            if 'type' not in self.options[key]:
+                continue
+            value = self.window.controller.config.get_value('config', key, self.options[key])
+            self.window.core.config.set(key, value)
+
+            # update preset temperature
+            if key == "temperature":
+                preset_id = self.window.core.config.get('preset')
+                if preset_id is not None and preset_id != "":
+                    if preset_id in self.window.core.presets.items:
+                        preset = self.window.core.presets.items[preset_id]
+                        preset.temperature = value
+                        self.window.core.presets.save(preset_id)
+                        self.window.controller.mode.update_temperature(value)  # update current temperature
+
+        self.window.core.config.save()
+        self.window.ui.status(trans('info.settings.saved'))
+        self.window.controller.ui.update_font_size()
+        self.window.controller.ui.update()
+
+        # update layout if needed
+        if self.before_config['layout.density'] != self.window.core.config.get('layout.density'):
+            self.window.controller.theme.reload()
+
+        self.before_config = copy.deepcopy(self.window.core.config.all())
+        self.window.controller.settings.close_window(id)
+
+    def hook_update(self, key, value, caller, *args, **kwargs):
+        """
+        Hook: on settings update
+        """
+        if self.window.core.config.get(key) == value:
+            return
+
+        # update font size
+        if key.startswith('font_size') and caller == "slider":
+            self.window.core.config.set(key, value)
+            self.window.controller.ui.update_font_size()
+
+        # update markdown
+        elif key == "theme.markdown":
+            self.window.core.config.set(key, value)
+            self.window.controller.theme.update_markdown(force=True)
+
+        # update raw output
+        elif key == "render.plain":
+            self.window.core.config.set(key, value)
+            if not value:
+                self.window.controller.ctx.refresh()
+                self.window.controller.theme.update_markdown(force=True)
+                self.window.ui.nodes['output.raw'].setChecked(False)
+            else:
+                self.window.controller.theme.clear_markdown()
+                self.window.ui.nodes['output.raw'].setChecked(True)
+
+        # call vision checkboxes events
+        elif key == "vision.capture.enabled":
+            self.window.core.config.set(key, value)
+            self.window.ui.nodes['vision.capture.enable'].setChecked(value)
+
+        elif key == "vision.capture.auto":
+            self.window.core.config.set(key, value)
+            self.window.ui.nodes['vision.capture.auto'].setChecked(value)
+
+        # update ctx limit
+        elif key.startswith('ctx.records.limit') and caller == "slider":
+            self.window.core.config.set(key, value)
+            self.window.controller.ctx.update(True, False)
+
+        # update layout density
+        elif key == 'layout.density' and caller == "slider":
+            self.window.core.config.set(key, value)
+            self.window.controller.theme.reload()
+
+    def toggle_collapsed(self, id: str, value: any, section: str):
+        """
+        Toggle collapsed state of section
+
+        :param id: section
+        :param value: value
+        :param section: section
+        """
+        if id not in self.window.ui.groups:
+            return
+
+        self.window.ui.groups[id].collapse(value)
+
+    def load_defaults_user(self, force: bool = False):
+        """
+        Load default user config
+
+        :param force: force load
+        """
+        if not force:
+            self.window.ui.dialogs.confirm('settings.defaults.user', -1, trans('settings.defaults.user.confirm'))
+            return
+
+        # load default user config
+        self.window.core.settings.load_user_settings()
+
+        # re-init settings
+        self.init('settings')
+        # self.window.ui.dialogs.alert(trans('dialog.settings.defaults.user.result'))
+
+    def load_defaults_app(self, force: bool = False):
+        """
+        Load default app config
+
+        :param force: force load
+        """
+        if not force:
+            self.window.ui.dialogs.confirm('settings.defaults.app', -1, trans('settings.defaults.app.confirm'))
+            return
+
+        # load default user config
+        self.window.core.settings.load_app_settings()
+
+        # re-init settings
+        self.init('settings')
+        self.window.ui.dialogs.alert(trans('dialog.settings.defaults.app.result'))
+
+    def delete_item(self, parent_object, id: str, force: bool = False):
+        """
+        Load delete item (from dict list) confirmation dialog or executes delete
+
+        :param parent_object: parent object
+        :param id: item id
+        :param force: force delete
+        """
+        if not force:
+            self.window.ui.dialogs.confirm('settings.dict.delete', id, trans('settings.dict.delete.confirm'),
+                                           parent_object)
+            return
+
+        # delete item
+        if parent_object is not None:
+            parent_object.delete_item_execute(id)
+
+    def get_options(self) -> dict:
+        """
+        Return settings options dict
+
+        :return: dict Options dict
+        """
+        return self.options
+
+    def get_option(self, key: str) -> dict:
+        """
+        Return settings option
+
+        :param key: option key
+        :return: dict Option dict
+        """
+        if key not in self.options:
+            return {}
+
+        return self.options[key]
---
+++ packages/pygpt-net/pygpt-net-2.0.96.tar.gz/pygpt-net-2.0.96/src/pygpt_net/ui/widget/filesystem/explorer.py
@@ -0,0 +1,257 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.01.11 09:00:00                  #
+# ================================================== #
+
+import datetime
+from pathlib import Path
+
+from PySide6.QtCore import Qt, QModelIndex
+from PySide6.QtGui import QAction, QIcon, QCursor, QResizeEvent
+from PySide6.QtWidgets import QTreeView, QMenu, QWidget, QVBoxLayout, QFileSystemModel, QLabel, QHBoxLayout, \
+    QPushButton, QSizePolicy
+
+from pygpt_net.utils import trans
+
+
+class FileExplorer(QWidget):
+    def __init__(self, window, directory, index_data):
+        """
+        File explorer widget
+
+        :param window: Window instance
+        :param directory: directory to explore
+        :param index_data: index data
+        """
+        super().__init__()
+
+        self.window = window
+        self.index_data = index_data
+        self.directory = directory
+        self.model = IndexedFileSystemModel(self.window, self.index_data)
+        self.model.setRootPath(directory)
+
+        self.treeView = QTreeView()
+        self.treeView.setModel(self.model)
+        self.treeView.setRootIndex(self.model.index(directory))
+
+        header = QHBoxLayout()
+
+        self.btn_upload = QPushButton(trans('files.local.upload'))
+        self.btn_upload.setMaximumHeight(40)
+        self.btn_upload.clicked.connect(self.window.controller.files.upload_local)
+        self.btn_upload.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+
+        self.btn_idx = QPushButton(trans('idx.btn.index_all'))
+        self.btn_idx.setMaximumHeight(40)
+        self.btn_idx.clicked.connect(self.window.controller.idx.index_all)
+        self.btn_idx.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+
+        self.btn_clear = QPushButton(trans('idx.btn.clear'))
+        self.btn_clear.setMaximumHeight(40)
+        self.btn_clear.clicked.connect(self.window.controller.idx.clear)
+        self.btn_clear.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+
+        self.path_label = QLabel(trans('files.local.dir.prefix') + ': ' + self.directory)
+        self.path_label.setMaximumHeight(40)
+        self.path_label.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        self.path_label.setStyleSheet(self.window.controller.theme.get_style('text_small'))
+
+        header.addWidget(self.btn_upload)
+        header.addWidget(self.btn_idx)
+        header.addWidget(self.btn_clear)
+        header.addStretch()
+        header.addWidget(self.path_label)
+
+        layout = QVBoxLayout()
+
+        layout.addWidget(self.treeView)
+        layout.addLayout(header)
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+
+        self.treeView.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.treeView.customContextMenuRequested.connect(self.openContextMenu)
+        self.treeView.setColumnWidth(0, self.width() / 2)
+
+        self.header = self.treeView.header()
+        self.header.setStretchLastSection(True)
+        self.header.setContentsMargins(0, 0, 0, 0)
+        self.header.setMaximumHeight(40)
+
+        self.column_proportion = 0.5
+        self.adjustColumnWidths()
+
+        self.header.setStyleSheet("""
+           QHeaderView::section {
+               padding-top: 0.8em;
+               text-align: center;
+               vertical-align: middle;
+           }
+       """)
+
+    def adjustColumnWidths(self):
+        """
+        Adjust column widths
+        """
+        total_width = self.treeView.width()
+        first_column_width = int(total_width * self.column_proportion)
+        self.treeView.setColumnWidth(0, first_column_width)
+        for column in range(1, self.model.columnCount()):
+            self.treeView.setColumnWidth(column, (total_width - first_column_width) // (self.model.columnCount() - 1))
+
+    def resizeEvent(self, event: QResizeEvent):
+        """
+        Resize event
+        :param event: Event object
+        """
+        super().resizeEvent(event)
+        self.adjustColumnWidths()
+
+    def openContextMenu(self, position):
+        """
+        Open context menu
+        :param position: position
+        """
+        indexes = self.treeView.selectedIndexes()
+        if indexes:
+            index = indexes[0]
+            path = self.model.filePath(index)
+
+            actions = {}
+            actions['open'] = QAction(QIcon.fromTheme("document-open"), trans('action.open'), self)
+            actions['open'].triggered.connect(
+                lambda: self.action_open(path))
+
+            actions['open_dir'] = QAction(QIcon.fromTheme("system-file-manager"), trans('action.open_dir'), self)
+            actions['open_dir'].triggered.connect(
+                lambda: self.action_open_dir(path))
+
+            actions['index_path'] = QAction(QIcon.fromTheme("search"), trans('action.idx'), self)
+            actions['index_path'].triggered.connect(
+                lambda: self.action_idx(path))
+
+            actions['rename'] = QAction(QIcon.fromTheme("edit-edit"), trans('action.rename'), self)
+            actions['rename'].triggered.connect(
+                lambda: self.action_rename(path))
+
+            actions['delete'] = QAction(QIcon.fromTheme("edit-delete"), trans('action.delete'), self)
+            actions['delete'].triggered.connect(
+                lambda: self.action_delete(path))
+
+            menu = QMenu(self)
+            menu.addAction(actions['open'])
+            menu.addAction(actions['open_dir'])
+            menu.addAction(actions['rename'])
+            menu.addAction(actions['index_path'])
+            menu.addAction(actions['delete'])
+
+            menu.exec(QCursor.pos())
+
+    def action_open(self, path):
+        """
+        Open action handler
+
+        :param path: path to open
+        """
+        self.window.controller.files.open(path)
+
+    def action_idx(self, path):
+        """
+        Index file or dir handler
+
+        :param path: path to open
+        """
+        self.window.controller.idx.index_path(path)
+
+    def action_open_dir(self, path):
+        """
+        Open dir action handler
+
+        :param path: path to open
+        """
+        self.window.controller.files.open_dir(path)
+
+    def action_rename(self, path):
+        """
+        Rename action handler
+
+        :param path: path to open
+        """
+        self.window.controller.files.rename(path)
+
+    def action_delete(self, path):
+        """
+        Delete action handler
+
+        :param path: path to open
+        """
+        self.window.controller.files.delete(path)
+
+
+class IndexedFileSystemModel(QFileSystemModel):
+    def __init__(self, window, index_dict, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.window = window
+        self.index_dict = index_dict
+
+    def columnCount(self, parent=QModelIndex()):
+        """
+        Return column count
+
+        :param parent: parent
+        :return: column count
+        """
+        return super().columnCount(parent) + 1
+
+    def data(self, index, role=Qt.DisplayRole):
+        """
+        Data handler
+
+        :param index: row index
+        :param role: role
+        :return: data
+        """
+        if index.column() == self.columnCount() - 1:
+            if role == Qt.DisplayRole:
+                file_path = self.filePath(index.siblingAtColumn(0))
+                file_id = self.window.core.idx.to_file_id(file_path)
+                if self.index_dict.get(file_id):
+                    # show status and date from timestamp:
+                    return "Yes" + " (" + datetime.datetime.fromtimestamp(self.index_dict[file_id]['indexed_ts']).strftime(
+                        "%Y-%m-%d %H:%M") + ")"
+                else:
+                    return "No"
+        return super().data(index, role)
+
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        """
+        Prepare Header data (append Indexed column)
+
+        :param section: Section
+        :param orientation: Orientation
+        :param role: Role
+        :return: Header data
+        """
+        if section == self.columnCount() - 1 and orientation == Qt.Horizontal:
+            if role == Qt.DisplayRole:
+                return "Llama-indexed"
+        return super().headerData(section, orientation, role)
+
+    def update_idx_status(self, new_index_dict):
+        """
+        Update index status
+
+        :param new_index_dict: new index dict
+        """
+        self.index_dict = new_index_dict
+        top_left_index = self.index(0, 0)
+        bottom_right_index = self.index(self.rowCount() - 1, self.columnCount() - 1)
+        self.dataChanged.emit(top_left_index, bottom_right_index, [Qt.DisplayRole])
+        self.layoutChanged.emit()
