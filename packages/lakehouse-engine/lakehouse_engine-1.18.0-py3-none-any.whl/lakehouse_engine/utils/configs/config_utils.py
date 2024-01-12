14b8796b14fe021f5c967b2e4c232678396301ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 06:04:23 +0000

    Add package lakehouse-engine/1.18.0/lakehouse_engine-1.18.0-py3-none-any.whl

---
+++ packages/lakehouse-engine/lakehouse_engine-1.18.0-py3-none-any.whl/lakehouse_engine/utils/configs/config_utils.py
@@ -0,0 +1,98 @@
+"""Module to read configurations."""
+import importlib.resources
+from typing import Any, Optional
+from urllib.parse import urlparse
+
+import pkg_resources
+import yaml
+
+from lakehouse_engine.utils.logging_handler import LoggingHandler
+from lakehouse_engine.utils.storage.file_storage_functions import (
+    FileStorageFunctions,
+    LocalFSStorage,
+    S3Storage,
+)
+
+
+class ConfigUtils(object):
+    """Config utilities class."""
+
+    _logger = LoggingHandler(__name__).get_logger()
+
+    @classmethod
+    def get_acon(
+        cls,
+        acon_path: Optional[str] = None,
+        acon: Optional[dict] = None,
+    ) -> dict:
+        """Get acon based on a filesystem path or on a dict.
+
+        Args:
+            acon_path: path of the acon (algorithm configuration) file.
+            acon: acon provided directly through python code (e.g., notebooks
+                or other apps).
+
+        Returns:
+            Dict representation of an acon.
+        """
+        acon = acon if acon else ConfigUtils.read_json_acon(acon_path)
+        cls._logger.info(f"Read Algorithm Configuration: {str(acon)}")
+        return acon
+
+    @staticmethod
+    def get_config(package: str = "lakehouse_engine.configs") -> Any:
+        """Get the lakehouse engine configuration file.
+
+        Returns:
+            Configuration dictionary
+        """
+        with importlib.resources.open_binary(package, "engine.yaml") as config:
+            config = yaml.safe_load(config)
+        return config
+
+    @classmethod
+    def get_engine_version(cls) -> str:
+        """Get Lakehouse Engine version from the installed packages.
+
+        Returns:
+            String of engine version.
+        """
+        try:
+            version = pkg_resources.get_distribution("lakehouse-engine").version
+        except pkg_resources.DistributionNotFound:
+            cls._logger.info("Could not identify Lakehouse Engine version.")
+            version = ""
+        return str(version)
+
+    @staticmethod
+    def read_json_acon(path: str) -> Any:
+        """Read an acon (algorithm configuration) file.
+
+        Args:
+            path: path to the acon file.
+
+        Returns:
+            The acon file content as a dict.
+        """
+        return FileStorageFunctions.read_json(path)
+
+    @staticmethod
+    def read_sql(path: str) -> Any:
+        """Read a DDL file in Spark SQL format from a cloud object storage system.
+
+        Args:
+            path: path to the acon (algorithm configuration) file.
+
+        Returns:
+            Content of the SQL file.
+        """
+        url = urlparse(path, allow_fragments=False)
+        if url.scheme == "s3":
+            return S3Storage.get_file_payload(url).read().decode("utf-8")
+        elif url.scheme == "file":
+            local = LocalFSStorage.get_file_payload(url).read()
+            return local
+        else:
+            raise NotImplementedError(
+                f"Object storage protocol not implemented for {path}."
+            )
