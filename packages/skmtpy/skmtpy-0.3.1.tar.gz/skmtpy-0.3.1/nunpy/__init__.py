2cfb647e5c8dac6cf4615094d6303f7517585c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 03:04:35 +0000

    Add package skmtpy/0.3.1/skmtpy-0.3.1.tar.gz

---
+++ packages/skmtpy/skmtpy-0.3.1.tar.gz/skmtpy-0.3.1/nunpy/__init__.py
@@ -0,0 +1,1 @@
+from .rechape import *
---
+++ packages/skmtpy/skmtpy-0.3.1.tar.gz/skmtpy-0.3.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='skmtpy',
+    version='0.3.1',
+    url='https://github.com/usuario/nombre_del_paquete',
+    author='Nombre Autor',
+    author_email='autor@gmail.com',
+    description='Descripción del paquete',
+    packages=find_packages(),
+    install_requires=[],
+)
