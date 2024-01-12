8a282c742d14290ca72e1e7c8f50c355c5c6f954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 16:05:06 +0000

    Add package zpgs/1.7/zpgs-1.7.tar.gz

---
+++ packages/zpgs/zpgs-1.7.tar.gz/zpgs-1.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zpgs",
+    version="1.7",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author="zaide",
+    author_email="zhstack@163.com",
+    description="A useless database connection based on psycopg2",
+    license="MIT",
+    keywords="zpgs",
+    url="https://github.com/zzt/w-rst.git"
+)
