11b287f72106a4a726f1d60ce33e1b9ac2c21304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 05:04:42 +0000

    Add package mlxin-data/0.0.2/mlxin_data-0.0.2-cp311-cp311-macosx_14_0_arm64.whl

---
+++ packages/mlxin-data/mlxin_data-0.0.2-cp311-cp311-macosx_14_0_arm64.whl/compare.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import numpy as np
+
+import mlx.core as mx
+import mlxin as mxi
+
+def compare(x: np.ndarray, cmp: mx.array):
+    np.testing.assert_allclose(x, cmp)
+
+# wget -O dog.png https://static.vecteezy.com/system/resources/previews/018/871/732/original/cute-and-happy-dog-png.png
+compare(np.array(Image.open("./dog.png")), mxi.load_png("./dog.png"))
+
+# wget -O dog.jpg https://upload.wikimedia.org/wikipedia/commons/4/43/Cute_dog.jpg
+compare(np.array(Image.open("./dog.jpg")), mxi.load_jpeg("./dog.jpg"))
+
+x = mxi.load_png("./dog.png")
+mxi.save_png("./dog2.png", x)
+compare(mxi.load_png("./dog2.png"), mxi.load_png("./dog.png"))
+
+y = mxi.load_jpeg("./dog.jpg")
+mxi.save_jpeg("./dog2.jpg", y)
+# jpeg compression makes this comparison fail
+# compare(mxi.load_jpeg("./dog2.jpg"), mxi.load_jpeg("./dog.jpg"))
