217ed5cff34cbe80399c230c32dcd1f5fefd1f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 16:05:44 +0000

    Add package calculate-triangle-area/0.1.0/calculate_triangle_area-0.1.0.tar.gz

---
+++ packages/calculate-triangle-area/calculate_triangle_area-0.1.0.tar.gz/calculate_triangle_area-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='calculate_triangle_area',
+    version='0.1.0',
+    py_modules=['triangle_area'],
+    install_requires=[
+        # List any dependencies here if needed
+    ],
+)
