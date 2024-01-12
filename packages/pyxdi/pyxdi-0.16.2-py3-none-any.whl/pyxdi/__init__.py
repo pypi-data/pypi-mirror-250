e692fa31e93b396b4182bc2e1f7e60008f3dbbf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-12 04:05:56 +0000

    Add package pyxdi/0.16.2/pyxdi-0.16.2-py3-none-any.whl

---
+++ packages/pyxdi/pyxdi-0.16.2-py3-none-any.whl/pyxdi/__init__.py
@@ -0,0 +1,14 @@
+"""PyxDI public objects and functions."""
+from .core import Module, Named, Provider, PyxDI, Scope, dep
+from .decorators import inject, provider
+
+__all__ = [
+    "dep",
+    "inject",
+    "Module",
+    "Named",
+    "provider",
+    "Provider",
+    "PyxDI",
+    "Scope",
+]
