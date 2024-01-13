f06f2fe785b81f8b40e8bc9ec1d042fc4ff2d59b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 03:04:32 +0000

    Add package faas-cache-dict/0.4.0/faas_cache_dict-0.4.0-py3-none-any.whl

---
+++ packages/faas-cache-dict/faas_cache_dict-0.4.0-py3-none-any.whl/faas_cache_dict/__init__.py
@@ -0,0 +1,7 @@
+import sys
+
+__version__ = "0.4.0"
+
+
+from faas_cache_dict.faas_cache_dict import FaaSCacheDict  # noqa
+from faas_cache_dict.file_faas_cache_dict import FileBackedFaaSCache  # noqa
