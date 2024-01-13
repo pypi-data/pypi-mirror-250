f194592101178516901f9e9e57998df76ed558b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 04:04:33 +0000

    Add package proalgotrader-core/0.0.2/proalgotrader_core-0.0.2.tar.gz

---
+++ packages/proalgotrader-core/proalgotrader_core-0.0.2.tar.gz/proalgotrader_core-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+ext_modules = Extension('core.*', ['core/*.py']),
+
+setup(
+    name='proalgotrader_core',
+    version='0.0.2',
+    ext_modules=cythonize(ext_modules),
+    package_data={'core': ['*.so']},
+    include_package_data=True,
+    exclude_package_data={'core': ['*.py']},
+    install_requires=[
+        "python-dotenv"
+    ],
+    entry_points={
+        'console_scripts': [
+            'proalgotrader_core = core.main:main',
+        ],
+    },
+)
