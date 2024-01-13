6998dd37815eecd4c8c30275258cefdbf43c7a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 03:04:41 +0000

    Add package dj-py/0.1.9/dj_py-0.1.9.tar.gz

---
+++ packages/dj-py/dj_py-0.1.9.tar.gz/dj_py-0.1.9/djpay/utils/client.py
@@ -0,0 +1,31 @@
+import requests
+from dataclasses import dataclass
+from abc import ABC, abstractclassmethod
+
+
+class PaypalClient(ABC):
+    @abstractclassmethod
+    def post():
+        pass
+
+
+@dataclass
+class AuthorizationAPI(PaypalClient):
+    api_client: str
+    api_secret: str
+
+    def post(self, base_url, data):
+        try:
+            response = requests.post(
+                base_url, data, auth=(self.api_client, self.api_secret)
+            )
+            response.raise_for_status()
+
+        except requests.exceptions.Timeout as e:
+            return f"Timed Out {e}"
+        except requests.exceptions.ConnectionError as e:
+            return f"Connection Error {e}"
+        except requests.exceptions.HTTPError as e:
+            return f"HttpError raised {e}"
+        else:
+            return response
