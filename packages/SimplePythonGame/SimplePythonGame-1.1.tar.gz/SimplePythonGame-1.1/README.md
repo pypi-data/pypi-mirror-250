df81c8527a3ce09890ea59bd3bea178526820386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 06:04:38 +0000

    Add package SimplePythonGame/1.1/SimplePythonGame-1.1.tar.gz

---
+++ packages/SimplePythonGame/SimplePythonGame-1.1.tar.gz/SimplePythonGame-1.1/README.md
@@ -0,0 +1,8 @@
+# EN
+## This project is a simple game module for Python, listed below are the games and their features.
+
+## NOTE: This project uses [TimePrint](https://pypi.org/project/TimePrintOnPYPI/) module. but you don't have to install that. it comes with code
+
+### 1) Number Guessing Game:
+* Ability to choose the range of the generated number <br>
+* Ability to choose the number of lives
---
+++ packages/SimplePythonGame/SimplePythonGame-1.1.tar.gz/SimplePythonGame-1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+class CustomInstallCommand(install):
+    def run(self):
+        install.run(self)
+        print("Need Help ? Contact Me From e-mail: osmntn08@gmail.com")
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='SimplePythonGame',
+    version='1.1',
+    description='A Simple Python Game!',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/SForces/TimePrint',
+    author='Osman TUNA',
+    author_email='osmntn08@gmail.com',
+    license='MIT',
+    packages=['SimplePythonGame'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
+)
