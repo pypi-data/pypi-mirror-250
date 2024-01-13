87c8ccbc228c1499e99ffc124d62043f914a0e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 05:04:10 +0000

    Add package odoo14-addon-ssi-l10n-id-taxform-bukti-potong-pph-f113308/14.0.1.4.0/odoo14_addon_ssi_l10n_id_taxform_bukti_potong_pph_f113308-14.0.1.4.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-l10n-id-taxform-bukti-potong-pph-f113308/odoo14_addon_ssi_l10n_id_taxform_bukti_potong_pph_f113308-14.0.1.4.0-py3-none-any.whl/odoo/addons/ssi_l10n_id_taxform_bukti_potong_pph_f113308/models/bukti_potong_pph_f113308_out.py
@@ -0,0 +1,34 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import Warning as UserError
+
+
+class BuktiPotongPPhf113308Out(models.Model):
+    _name = "l10n_id.bukti_potong_pph_f113308_out"
+    _inherit = "l10n_id.bukti_potong_pph_mixin"
+    _description = "Bukti Potong PPh 26 (f.1.1.33.08) Out"
+
+    def _get_type_id(self):
+        type_id = self.env.ref(
+            "ssi_l10n_id_taxform_bukti_potong_pph_f113308."
+            "bukti_potong_pph_type_f113308_out"
+        )
+        if not type_id.id:
+            err_msg = _("Bukti Potong PPh 26 f113308 Out type hasn't defined")
+            raise UserError(err_msg)
+        return type_id.id
+
+    @api.model
+    def _default_type_id(self):
+        return self._get_type_id()
+
+    type_id = fields.Many2one(
+        default=lambda self: self._default_type_id(),
+    )
+
+    line_ids = fields.One2many(
+        comodel_name="l10n_id.bukti_potong_pph_f113308_out_line",
+    )
