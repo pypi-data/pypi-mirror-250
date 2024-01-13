b942a8f565696c56c15794a7a0ca676da228ac25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 04:04:23 +0000

    Add package reyworm/0.12/reyworm-0.12-py3-none-any.whl

---
+++ packages/reyworm/reyworm-0.12-py3-none-any.whl/reyapi/rbaidu/__init__.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2024-01-11 21:55:48
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Baidu API methods.
+
+Modules
+-------
+rbaidu_base : Baidu API base methods.
+rbaidu_voice : Baidu API voice methods.
+"""
+
+
+from .rbaidu_base import *
+from .rbaidu_voice import *
+
+
+__all__ = (
+    "RAPIBaidu",
+    "RAPIBaiduVoice"
+)
