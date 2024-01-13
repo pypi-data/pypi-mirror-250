d399f70a447575f30e99df76a0b9d0deb696b0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 03:05:19 +0000

    Add package zpgs/2.3/zpgs-2.3.tar.gz

---
+++ packages/zpgs/zpgs-2.3.tar.gz/zpgs-2.3/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+
+# in windows slash is \,use \\ in code
+def next_version():
+    file_path = f"{os.getcwd()}\\setup.version"
+
+    f = open(file_path, 'r')
+
+    text = f.read()
+
+    f.close()
+
+    version_now = text.replace("VERSION=", '')
+
+    if version_now in [None, ""]:
+        version_next = 1.0
+    else:
+        version_next = float(version_now) + 0.1
+
+        version_next = round(version_next, 2)
+
+    f = open(file_path, 'w', encoding='utf-8')
+
+    f.write(f"VERSION={version_next}")
+
+    f.close()
+
+    return version_next
+
+
+setup(
+    name="zpgs",
+    version=next_version(),
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author="zhstack",
+    author_email="zhstack@163.com",
+    description="A useless database connection based on psycopg2",
+    license="MIT",
+    keywords="zpgs",
+    url="https://github.com/zzt/w-rst.git"
+)
