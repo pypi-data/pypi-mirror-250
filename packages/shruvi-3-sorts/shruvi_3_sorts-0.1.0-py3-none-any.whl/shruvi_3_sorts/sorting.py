3208b3a941e3f57f4063402c2d5e15b4f9c217eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 03:05:45 +0000

    Add package shruvi-3-sorts/0.1.0/shruvi_3_sorts-0.1.0-py3-none-any.whl

---
+++ packages/shruvi-3-sorts/shruvi_3_sorts-0.1.0-py3-none-any.whl/shruvi_3_sorts/sorting.py
@@ -0,0 +1,66 @@
+def bubble_sort(arr):
+    """
+    Sorts an array using the bubble sort algorithm.
+
+    Parameters:
+        arr (list): The input list to be sorted.
+
+    Returns:
+        list: The sorted list.
+    """
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+def quick_sort(arr):
+    """
+    Sorts an array using the quicksort algorithm.
+
+    Parameters:
+        arr (List): The array to be sorted.
+
+    Returns:
+        List: The sorted array.
+    """
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+# Take input from the user
+# input_list = input("Enter a list of numbers separated by spaces: ").split()
+
