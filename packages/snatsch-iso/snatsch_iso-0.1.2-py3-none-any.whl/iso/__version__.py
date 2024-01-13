ed4c92ce59ed1826903c84156559dccd6be01777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 06:04:38 +0000

    Add package snatsch-iso/0.1.2/snatsch_iso-0.1.2-py3-none-any.whl

---
+++ packages/snatsch-iso/snatsch_iso-0.1.2-py3-none-any.whl/iso/__version__.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+current_year = datetime.utcnow().strftime("%Y")
+
+__title__ = "snatsch-iso"
+__description__ = "A lightweight python library to retrieve countries and currencies based on ISO_3166-1 and ISO_4217 standards."
+__version__ = "0.1.2"
+__author__ = "Victor Natschke"
+__author_email__ = "vnatschke@gmail.com"
+__license__ = "MIT"
+__copyright__ = f"Copyright {current_year} Snatsch"
