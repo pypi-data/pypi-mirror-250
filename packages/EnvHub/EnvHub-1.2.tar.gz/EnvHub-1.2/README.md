ef1ea1e3aebf419ded7707fd81dea7c3a0d72b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 04:04:27 +0000

    Add package EnvHub/1.2/EnvHub-1.2.tar.gz

---
+++ packages/EnvHub/EnvHub-1.2.tar.gz/EnvHub-1.2/README.md
@@ -0,0 +1,3 @@
+# EnvHub Python SDK
+> A Python SDK for EnvHub API
+> Website: [link](https://EnvHub.ryanbaig.vercel.app)
---
+++ packages/EnvHub/EnvHub-1.2.tar.gz/EnvHub-1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EnvHub',
+    version='1.2',
+    packages=find_packages(),
+    author="RyanBaig",
+    long_description="""
+    # EnvHub Python SDK
+    ## Hello, This is the **official** Python SDK for [EnvHub](https://envhub.ryanbaig.vercel.app).
+    """,
+    long_description_content_type="text/markdown",
+    url='https://github.com/RyanBaig/EnvHub-Python',
+)
