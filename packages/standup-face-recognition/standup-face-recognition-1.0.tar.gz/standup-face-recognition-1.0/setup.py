3032b2472a3827837fbad4c5bfb032066b52908f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 05:05:12 +0000

    Add package standup-face-recognition/1.0/standup-face-recognition-1.0.tar.gz

---
+++ packages/standup-face-recognition/standup-face-recognition-1.0.tar.gz/standup-face-recognition-1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+print('find packages: ', find_packages())
+setup(
+    name='standup-face-recognition',
+    version='1.0',
+    description='Standup helper: Detects and recognizes the person in the team.',
+    author='Timo',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    	'opencv-python',
+    	'numpy',
+    	'facenet-pytorch',
+    	'torch==2.0.0',
+    	'torchvision==0.15.1',
+    	
+    ],
+    entry_points={
+        'console_scripts': [
+            'standup_face_recognition=standup_face_recognition.main:main',
+        ],
+    },
+)
