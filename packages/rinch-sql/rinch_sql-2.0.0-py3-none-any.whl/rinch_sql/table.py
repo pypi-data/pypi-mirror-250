51c0a1079ab6ce9bd97ab35c3155afb58dccd040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 15:04:59 +0000

    Add package rinch-sql/2.0.0/rinch_sql-2.0.0-py3-none-any.whl

---
+++ packages/rinch-sql/rinch_sql-2.0.0-py3-none-any.whl/rinch_sql/table.py
@@ -0,0 +1,26 @@
+from functools import cache
+
+from .mysql import Mysql
+from .db_config import DbConfig
+
+
+class Table:
+    db_config: DbConfig = None
+    field_list_unique: list[str]
+
+    @classmethod
+    @cache
+    def db(cls, db_config: DbConfig = None):
+        db_config = db_config or cls.db_config
+        db = Mysql(cls, db_config)
+        return db
+
+    def __hash__(self):
+        return hash(tuple([self.__dict__[i] for i in self.field_list_unique]))
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            eq_list = [self.__dict__[i] == other.__dict__[i] for i in self.field_list_unique]
+            return all(eq_list)
+        else:
+            return False
