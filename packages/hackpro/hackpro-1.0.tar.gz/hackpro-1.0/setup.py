46ff9b9d1930c68fc5a649922ee313f26297cf7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 15:04:49 +0000

    Add package hackpro/1.0/hackpro-1.0.tar.gz

---
+++ packages/hackpro/hackpro-1.0.tar.gz/hackpro-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='hackpro',
+    version='1.0',
+    description='hackpro is tool ',
+    author='john',
+    author_email='junmaeda@email.com',
+    packages=[],
+    package_data={'': ['HACKPRO']},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
