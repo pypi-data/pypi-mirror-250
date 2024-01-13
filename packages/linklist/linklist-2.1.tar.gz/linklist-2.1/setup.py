014da3ae934f4c509c31d9fb81e39b442efe0af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 05:04:35 +0000

    Add package linklist/2.1/linklist-2.1.tar.gz

---
+++ packages/linklist/linklist-2.1.tar.gz/linklist-2.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, Extension, find_packages
+import os 
+
+name = os.name 
+if name == 'nt': 
+      args = ['/std:c++20']
+else: 
+      args = ['-std=c++20'] 
+
+module0 = Extension('linked_list',
+      sources = ['linked_list/pythonbind.cc'], 
+      include_dirs = ['linked_list'],
+      extra_compile_args=args
+) 
+
+setup(
+      name = 'linklist',
+      version = '2.1', 
+      description = 'Python package with a fast linked_list support. ',
+      ext_modules = [module0],
+      author = 'Cutie Deng',
+      author_email = 'Dhdreamer@126.com', 
+      url = 'https://github.com/CutieDeng/OrderStatisticTree',
+      license = 'MIT',
+      classifiers = [
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+      ],
+      python_requires = '>=3.6',
+      # py_modules=['fastlist'], 
+      packages= find_packages(),
+)
