b54071edb0845534d98c3ff85c60d11e5e4057d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 05:04:04 +0000

    Add package kraken-build/0.33.1/kraken_build-0.33.1-py3-none-any.whl

---
+++ packages/kraken-build/kraken_build-0.33.1-py3-none-any.whl/kraken/std/protobuf/__init__.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import subprocess as sp
+
+from kraken.core import Project, Task, TaskStatus
+
+
+class BufFormatTask(Task):
+    description = "Format Protobuf files with buf."
+
+    def execute(self) -> TaskStatus | None:
+        command = ["buf", "format", "-w"]
+        result = sp.call(command, cwd=self.project.directory / "proto")
+
+        return TaskStatus.from_exit_code(command, result)
+
+
+class BufLintTask(Task):
+    description = "Lint Protobuf files with buf."
+
+    def execute(self) -> TaskStatus | None:
+        command = ["buf", "lint"]
+        result = sp.call(command, cwd=self.project.directory / "proto")
+
+        return TaskStatus.from_exit_code(command, result)
+
+
+def buf_format(*, name: str = "buf.format", project: Project | None = None) -> BufFormatTask:
+    project = project or Project.current()
+    return project.task(name, BufFormatTask, group="fmt")
+
+
+def buf_lint(*, name: str = "buf.lint", project: Project | None = None) -> BufLintTask:
+    project = project or Project.current()
+    return project.task(name, BufLintTask, group="lint")
