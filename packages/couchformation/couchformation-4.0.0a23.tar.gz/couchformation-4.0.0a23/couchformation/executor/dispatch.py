51307bcccbedbb844d1afcfa73fc8c33c5f3a66f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 05:04:06 +0000

    Add package couchformation/4.0.0a23/couchformation-4.0.0a23.tar.gz

---
+++ packages/couchformation/couchformation-4.0.0a23.tar.gz/couchformation-4.0.0a23/couchformation/executor/dispatch.py
@@ -0,0 +1,41 @@
+##
+##
+
+import logging
+import concurrent.futures
+import couchformation.executor.worker as worker
+from couchformation.exception import NonFatalLogError
+from multiprocessing import get_context
+
+logger = logging.getLogger('couchformation.executor.dispatch')
+logger.addHandler(logging.NullHandler())
+
+
+class TaskError(NonFatalLogError):
+    pass
+
+
+class JobDispatch(object):
+
+    def __init__(self):
+        context = get_context('fork')
+        self.executor = concurrent.futures.ProcessPoolExecutor(mp_context=context)
+        self.tasks = set()
+
+    def dispatch(self, *args, **kwargs):
+        self.tasks.add(self.executor.submit(worker.main, *args, **kwargs))
+
+    @staticmethod
+    def foreground(*args, **kwargs):
+        return worker.main(*args, **kwargs)
+
+    def join(self):
+        while self.tasks:
+            done, self.tasks = concurrent.futures.wait(self.tasks, return_when=concurrent.futures.ALL_COMPLETED)
+            for task in done:
+                try:
+                    res = task.result()
+                    logger.debug(f"task result: {res}")
+                    yield res
+                except Exception as err:
+                    raise TaskError(f"task exception: {err}")
