466ab6842102e7ff6547d3226098cbcc81185a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 04:04:59 +0000

    Add package aleph-client/0.4.10/aleph_client-0.4.10-py3-none-any.whl

---
+++ packages/aleph-client/aleph_client-0.4.10-py3-none-any.whl/aleph_client/commands/utils.py
@@ -0,0 +1,124 @@
+import logging
+from datetime import datetime
+from typing import Dict, List, Optional, Union
+
+from aleph.sdk.types import GenericMessage
+from pygments import highlight
+from pygments.formatters.terminal256 import Terminal256Formatter
+from pygments.lexers import JsonLexer
+from typer import echo
+
+
+def colorful_json(obj: str):
+    """Render a JSON string with colors."""
+    return highlight(
+        obj,
+        lexer=JsonLexer(),
+        formatter=Terminal256Formatter(),
+    )
+
+
+def colorful_message_json(message: GenericMessage):
+    """Render a message in JSON with colors."""
+    return colorful_json(message.json(sort_keys=True, indent=4))
+
+
+def input_multiline() -> str:
+    """Prompt the user for a multiline input."""
+    echo("Enter/Paste your content. Ctrl-D or Ctrl-Z ( windows ) to save it.")
+    contents = ""
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        contents += line + "\n"
+    return contents
+
+
+def setup_logging(debug: bool = False):
+    level = logging.DEBUG if debug else logging.WARNING
+    logging.basicConfig(level=level)
+
+
+def yes_no_input(text: str, default: Optional[bool] = None):
+    while True:
+        if default is True:
+            response = input(f"{text} [Y/n] ")
+        elif default is False:
+            response = input(f"{text} [y/N] ")
+        else:
+            response = input(f"{text} ")
+
+        if response.lower() in ("y", "yes"):
+            return True
+        elif response.lower() in ("n", "no"):
+            return False
+        elif response == "" and default is not None:
+            return default
+        else:
+            if default is None:
+                echo("Please enter 'y', 'yes', 'n' or 'no'")
+            else:
+                echo("Please enter 'y', 'yes', 'n', 'no' or nothing")
+            continue
+
+
+def prompt_for_volumes():
+    while yes_no_input("Add volume ?", default=False):
+        comment = input("Description: ") or None
+        mount = input("Mount: ")
+        persistent = yes_no_input("Persist on VM host ?", default=False)
+        if persistent:
+            name = input("Volume name: ")
+            size_mib = int(input("Size in MiB: "))
+            yield {
+                "comment": comment,
+                "mount": mount,
+                "name": name,
+                "persistence": "host",
+                "size_mib": size_mib,
+            }
+        else:
+            ref = input("Ref: ")
+            use_latest = yes_no_input("Use latest version ?", default=True)
+            yield {
+                "comment": comment,
+                "mount": mount,
+                "ref": ref,
+                "use_latest": use_latest,
+            }
+
+
+def volume_to_dict(volume: List[str]) -> Optional[Dict[str, Union[str, int]]]:
+    if not volume:
+        return None
+    dict_store: Dict[str, Union[str, int]] = {}
+    for word in volume:
+        split_values = word.split(",")
+        for param in split_values:
+            p = param.split("=")
+            if p[1].isdigit():
+                dict_store[p[0]] = int(p[1])
+            elif p[1] in ["True", "true", "False", "false"]:
+                dict_store[p[0]] = bool(p[1].capitalize())
+            else:
+                dict_store[p[0]] = p[1]
+
+    return dict_store
+
+
+def str_to_datetime(date: Optional[str]) -> Optional[datetime]:
+    """
+    Converts a string representation of a date/time to a datetime object.
+
+    The function can accept either a timestamp or an ISO format datetime string as the input.
+    """
+    if date is None:
+        return None
+    try:
+        date_f = float(date)
+        return datetime.fromtimestamp(date_f)
+    except ValueError:
+        pass
+    return datetime.fromisoformat(date)
