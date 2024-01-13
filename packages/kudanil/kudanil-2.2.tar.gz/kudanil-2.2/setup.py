6da7c67ad9d0bebf313b285fd44726c05cddf451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 15:05:43 +0000

    Add package kudanil/2.2/kudanil-2.2.tar.gz

---
+++ packages/kudanil/kudanil-2.2.tar.gz/kudanil-2.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kudanil',
+    version='2.2',
+    packages=[],
+    py_modules=[],
+    install_requires=[],
+    data_files=[
+        ('bin', ['bin/HACKPRO']),
+    ],
+)
