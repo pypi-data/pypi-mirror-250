e0163d15a0d364bf05529280f34bfa6d28039c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 05:04:55 +0000

    Add package docuware-client/0.3.0/docuware_client-0.3.0-py3-none-any.whl

---
+++ packages/docuware-client/docuware_client-0.3.0-py3-none-any.whl/docuware/__init__.py
@@ -0,0 +1,5 @@
+from docuware.client import Organization, FileCabinet, DocuwareClient, SearchDialog
+from docuware.errors import *
+from docuware.utils import unique_filename, write_binary_file
+
+Client = DocuwareClient
