7a8fa28216344b65858e58f9437b0e3a31664a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 03:04:22 +0000

    Add package target365-sdk/1.8.3/target365_sdk-1.8.3.tar.gz

---
+++ packages/target365-sdk/target365_sdk-1.8.3.tar.gz/target365_sdk-1.8.3/target365_sdk/models/pincode.py
@@ -0,0 +1,13 @@
+from .model import Model
+
+class Pincode(Model):
+
+    def _accepted_params(self):
+        return [
+            'transactionId',
+            'recipient',
+            'sender',
+            'prefixText',
+            'suffixText',
+            'pincodeLength',
+        ]
