30a14049c0c6babe8da8811bc76379bca2237d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 05:04:16 +0000

    Add package finch-api/0.12.0/finch_api-0.12.0-py3-none-any.whl

---
+++ packages/finch-api/finch_api-0.12.0-py3-none-any.whl/finch/resources/__init__.py
@@ -0,0 +1,53 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from .hris import HRIS, AsyncHRIS, HRISWithRawResponse, AsyncHRISWithRawResponse
+from .jobs import Jobs, AsyncJobs, JobsWithRawResponse, AsyncJobsWithRawResponse
+from .account import Account, AsyncAccount, AccountWithRawResponse, AsyncAccountWithRawResponse
+from .sandbox import Sandbox, AsyncSandbox, SandboxWithRawResponse, AsyncSandboxWithRawResponse
+from .webhooks import Webhooks, AsyncWebhooks
+from .providers import Providers, AsyncProviders, ProvidersWithRawResponse, AsyncProvidersWithRawResponse
+from .access_tokens import (
+    AccessTokens,
+    AsyncAccessTokens,
+    AccessTokensWithRawResponse,
+    AsyncAccessTokensWithRawResponse,
+)
+from .request_forwarding import (
+    RequestForwarding,
+    AsyncRequestForwarding,
+    RequestForwardingWithRawResponse,
+    AsyncRequestForwardingWithRawResponse,
+)
+
+__all__ = [
+    "AccessTokens",
+    "AsyncAccessTokens",
+    "AccessTokensWithRawResponse",
+    "AsyncAccessTokensWithRawResponse",
+    "HRIS",
+    "AsyncHRIS",
+    "HRISWithRawResponse",
+    "AsyncHRISWithRawResponse",
+    "Providers",
+    "AsyncProviders",
+    "ProvidersWithRawResponse",
+    "AsyncProvidersWithRawResponse",
+    "Account",
+    "AsyncAccount",
+    "AccountWithRawResponse",
+    "AsyncAccountWithRawResponse",
+    "Webhooks",
+    "AsyncWebhooks",
+    "RequestForwarding",
+    "AsyncRequestForwarding",
+    "RequestForwardingWithRawResponse",
+    "AsyncRequestForwardingWithRawResponse",
+    "Jobs",
+    "AsyncJobs",
+    "JobsWithRawResponse",
+    "AsyncJobsWithRawResponse",
+    "Sandbox",
+    "AsyncSandbox",
+    "SandboxWithRawResponse",
+    "AsyncSandboxWithRawResponse",
+]
---
+++ packages/finch-api/finch_api-0.12.0-py3-none-any.whl/finch/resources/hris/benefits/benefits.py
@@ -0,0 +1,423 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from __future__ import annotations
+
+from typing import Optional
+
+import httpx
+
+from ...._types import NOT_GIVEN, Body, Query, Headers, NotGiven
+from ...._utils import maybe_transform
+from ...._compat import cached_property
+from .individuals import Individuals, AsyncIndividuals, IndividualsWithRawResponse, AsyncIndividualsWithRawResponse
+from ...._resource import SyncAPIResource, AsyncAPIResource
+from ...._response import to_raw_response_wrapper, async_to_raw_response_wrapper
+from ....pagination import SyncSinglePage, AsyncSinglePage
+from ....types.hris import (
+    BenefitType,
+    CompanyBenefit,
+    BenefitFrequency,
+    SupportedBenefit,
+    UpdateCompanyBenefitResponse,
+    CreateCompanyBenefitsResponse,
+    benefit_create_params,
+    benefit_update_params,
+)
+from ...._base_client import (
+    AsyncPaginator,
+    make_request_options,
+)
+
+__all__ = ["Benefits", "AsyncBenefits"]
+
+
+class Benefits(SyncAPIResource):
+    @cached_property
+    def individuals(self) -> Individuals:
+        return Individuals(self._client)
+
+    @cached_property
+    def with_raw_response(self) -> BenefitsWithRawResponse:
+        return BenefitsWithRawResponse(self)
+
+    def create(
+        self,
+        *,
+        description: str | NotGiven = NOT_GIVEN,
+        frequency: Optional[BenefitFrequency] | NotGiven = NOT_GIVEN,
+        type: Optional[BenefitType] | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> CreateCompanyBenefitsResponse:
+        """
+        **Availability: Automated and Assisted Benefits providers**
+
+        Creates a new company-wide benefit. Please use the `/meta` endpoint to view
+        available types for each provider.
+
+        Args:
+          type: Type of benefit.
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._post(
+            "/employer/benefits",
+            body=maybe_transform(
+                {
+                    "description": description,
+                    "frequency": frequency,
+                    "type": type,
+                },
+                benefit_create_params.BenefitCreateParams,
+            ),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=CreateCompanyBenefitsResponse,
+        )
+
+    def retrieve(
+        self,
+        benefit_id: str,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> CompanyBenefit:
+        """
+        **Availability: Automated Benefits providers only**
+
+        Lists benefit information for a given benefit
+
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._get(
+            f"/employer/benefits/{benefit_id}",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=CompanyBenefit,
+        )
+
+    def update(
+        self,
+        benefit_id: str,
+        *,
+        description: str | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> UpdateCompanyBenefitResponse:
+        """
+        **Availability: Automated and Assisted Benefits providers**
+
+        Updates an existing company-wide benefit
+
+        Args:
+          description: Updated name or description.
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._post(
+            f"/employer/benefits/{benefit_id}",
+            body=maybe_transform({"description": description}, benefit_update_params.BenefitUpdateParams),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=UpdateCompanyBenefitResponse,
+        )
+
+    def list(
+        self,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> SyncSinglePage[CompanyBenefit]:
+        """
+        **Availability: Automated Benefits providers only**
+
+        List all company-wide benefits.
+        """
+        return self._get_api_list(
+            "/employer/benefits",
+            page=SyncSinglePage[CompanyBenefit],
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            model=CompanyBenefit,
+        )
+
+    def list_supported_benefits(
+        self,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> SyncSinglePage[SupportedBenefit]:
+        """
+        **Availability: Automated and Assisted Benefits providers**
+
+        Lists available types and configurations for the provider associated with the
+        access token.
+        """
+        return self._get_api_list(
+            "/employer/benefits/meta",
+            page=SyncSinglePage[SupportedBenefit],
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            model=SupportedBenefit,
+        )
+
+
+class AsyncBenefits(AsyncAPIResource):
+    @cached_property
+    def individuals(self) -> AsyncIndividuals:
+        return AsyncIndividuals(self._client)
+
+    @cached_property
+    def with_raw_response(self) -> AsyncBenefitsWithRawResponse:
+        return AsyncBenefitsWithRawResponse(self)
+
+    async def create(
+        self,
+        *,
+        description: str | NotGiven = NOT_GIVEN,
+        frequency: Optional[BenefitFrequency] | NotGiven = NOT_GIVEN,
+        type: Optional[BenefitType] | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> CreateCompanyBenefitsResponse:
+        """
+        **Availability: Automated and Assisted Benefits providers**
+
+        Creates a new company-wide benefit. Please use the `/meta` endpoint to view
+        available types for each provider.
+
+        Args:
+          type: Type of benefit.
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return await self._post(
+            "/employer/benefits",
+            body=maybe_transform(
+                {
+                    "description": description,
+                    "frequency": frequency,
+                    "type": type,
+                },
+                benefit_create_params.BenefitCreateParams,
+            ),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=CreateCompanyBenefitsResponse,
+        )
+
+    async def retrieve(
+        self,
+        benefit_id: str,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> CompanyBenefit:
+        """
+        **Availability: Automated Benefits providers only**
+
+        Lists benefit information for a given benefit
+
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return await self._get(
+            f"/employer/benefits/{benefit_id}",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=CompanyBenefit,
+        )
+
+    async def update(
+        self,
+        benefit_id: str,
+        *,
+        description: str | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> UpdateCompanyBenefitResponse:
+        """
+        **Availability: Automated and Assisted Benefits providers**
+
+        Updates an existing company-wide benefit
+
+        Args:
+          description: Updated name or description.
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return await self._post(
+            f"/employer/benefits/{benefit_id}",
+            body=maybe_transform({"description": description}, benefit_update_params.BenefitUpdateParams),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=UpdateCompanyBenefitResponse,
+        )
+
+    def list(
+        self,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> AsyncPaginator[CompanyBenefit, AsyncSinglePage[CompanyBenefit]]:
+        """
+        **Availability: Automated Benefits providers only**
+
+        List all company-wide benefits.
+        """
+        return self._get_api_list(
+            "/employer/benefits",
+            page=AsyncSinglePage[CompanyBenefit],
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            model=CompanyBenefit,
+        )
+
+    def list_supported_benefits(
+        self,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> AsyncPaginator[SupportedBenefit, AsyncSinglePage[SupportedBenefit]]:
+        """
+        **Availability: Automated and Assisted Benefits providers**
+
+        Lists available types and configurations for the provider associated with the
+        access token.
+        """
+        return self._get_api_list(
+            "/employer/benefits/meta",
+            page=AsyncSinglePage[SupportedBenefit],
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            model=SupportedBenefit,
+        )
+
+
+class BenefitsWithRawResponse:
+    def __init__(self, benefits: Benefits) -> None:
+        self.individuals = IndividualsWithRawResponse(benefits.individuals)
+
+        self.create = to_raw_response_wrapper(
+            benefits.create,
+        )
+        self.retrieve = to_raw_response_wrapper(
+            benefits.retrieve,
+        )
+        self.update = to_raw_response_wrapper(
+            benefits.update,
+        )
+        self.list = to_raw_response_wrapper(
+            benefits.list,
+        )
+        self.list_supported_benefits = to_raw_response_wrapper(
+            benefits.list_supported_benefits,
+        )
+
+
+class AsyncBenefitsWithRawResponse:
+    def __init__(self, benefits: AsyncBenefits) -> None:
+        self.individuals = AsyncIndividualsWithRawResponse(benefits.individuals)
+
+        self.create = async_to_raw_response_wrapper(
+            benefits.create,
+        )
+        self.retrieve = async_to_raw_response_wrapper(
+            benefits.retrieve,
+        )
+        self.update = async_to_raw_response_wrapper(
+            benefits.update,
+        )
+        self.list = async_to_raw_response_wrapper(
+            benefits.list,
+        )
+        self.list_supported_benefits = async_to_raw_response_wrapper(
+            benefits.list_supported_benefits,
+        )
