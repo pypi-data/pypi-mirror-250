59a1474f1c33e43da412931d1bd4878fa0fce5b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 15:04:43 +0000

    Add package proalgotrader-core/0.0.9b11/proalgotrader_core-0.0.9b11-py3-none-any.whl

---
+++ packages/proalgotrader-core/proalgotrader_core-0.0.9b11-py3-none-any.whl/proalgotrader_core/run.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def run() -> None:
+    environment = os.environ.get("ENVIRONMENT", "development")
+
+    print("environment", environment)
