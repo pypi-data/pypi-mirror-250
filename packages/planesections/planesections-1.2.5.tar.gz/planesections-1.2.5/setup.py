0df9b527c25e2667005aee969c6079f500afd940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 15:04:43 +0000

    Add package planesections/1.2.5/planesections-1.2.5.tar.gz

---
+++ packages/planesections/planesections-1.2.5.tar.gz/planesections-1.2.5/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="planesections",
+    version="1.2.5",
+    author="Christian Slotboom",
+    author_email="christian.slotboom@gmail.com",
+    description="A light-weight FEM beam analyzer.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cslotboom/planesections",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    'numpy',
+    'matplotlib',
+    'PyniteFEA'
+    ],
+    extras_require={
+        "opensees": ["openseespy"],
+    },
+    python_requires='>=3.9',
+)
