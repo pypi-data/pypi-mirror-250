1fde78ab8c7caa871da228597e1e9f5d4ebf0b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 04:04:18 +0000

    Add package jinns/0.6.1/jinns-0.6.1.tar.gz

---
+++ packages/jinns/jinns-0.6.1.tar.gz/jinns-0.6.1/jinns/utils/__init__.py
@@ -0,0 +1,8 @@
+from ._utils import (
+    euler_maruyama,
+    euler_maruyama_density,
+    log_euler_maruyama_density,
+)
+from ._pinn import create_PINN
+from ._spinn import create_SPINN
+from ._optim import alternate_optimizer, delayed_optimizer
