48f920ed5784c0935ce588d82a360207160246fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 15:05:22 +0000

    Add package aiosqs/1.0.5/aiosqs-1.0.5-py3-none-any.whl

---
+++ packages/aiosqs/aiosqs-1.0.5-py3-none-any.whl/aiosqs/tests/test_get_queue_url.py
@@ -0,0 +1,32 @@
+import unittest
+
+from aiosqs.parser import parse_xml_result_response
+from aiosqs.tests.utils import load_fixture
+
+
+class GetQueueUrlTestCase(unittest.TestCase):
+    action = "GetQueueUrl"
+
+    def test_get_queue_url(self):
+        res = parse_xml_result_response(
+            action=self.action,
+            body=load_fixture("get_queue_url.xml"),
+        )
+        self.assertEqual(
+            res,
+            {
+                "QueueUrl": "http://sqs.mcs.mail.ru/account123/example_queue",
+            },
+        )
+
+    def test_response_with_namespace(self):
+        res = parse_xml_result_response(
+            action=self.action,
+            body=load_fixture("get_queue_url_namespace.xml"),
+        )
+        self.assertEqual(
+            res,
+            {
+                "QueueUrl": "http://sqs.mcs.mail.ru/account123/example_queue",
+            },
+        )
