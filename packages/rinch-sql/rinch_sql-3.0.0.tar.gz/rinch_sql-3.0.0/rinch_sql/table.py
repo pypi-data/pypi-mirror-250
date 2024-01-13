542308071353717366abb3d342a2a95fd125a81b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 15:05:00 +0000

    Add package rinch-sql/3.0.0/rinch_sql-3.0.0.tar.gz

---
+++ packages/rinch-sql/rinch_sql-3.0.0.tar.gz/rinch_sql-3.0.0/rinch_sql/table.py
@@ -0,0 +1,28 @@
+from functools import cache
+
+from .mysql import Mysql
+from .db_config import DbConfig
+
+
+class Table:
+    db_config: DbConfig = None
+    field_list_unique: list[str]
+
+    @classmethod
+    @cache
+    def db(cls, db_config: DbConfig = None):
+        db_config = db_config or cls.db_config
+        db = Mysql(cls, db_config)
+        return db
+
+    def __hash__(self):
+        return hash(self.__tuple__())
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__tuple__() == other.__tuple__()
+        else:
+            return False
+
+    def __tuple__(self):
+        return tuple([self.__dict__[i] for i in self.field_list_unique])
