a2ebe47643ae8f4b3387800c262dd94c9b9e13d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 15:04:47 +0000

    Add package leads/0.2.3/leads-0.2.3.tar.gz

---
+++ packages/leads/leads-0.2.3.tar.gz/leads-0.2.3/leads/controller/controller.py
@@ -0,0 +1,21 @@
+from leads.controller.device import Device
+
+
+class Controller(Device):
+    def __init__(self) -> None:
+        super().__init__()
+        self._devices: dict[str, Device] = {}
+
+    def level(self) -> int:
+        return len(self._parent_tags)
+
+    async def _attach_device(self, tag: str, device: Device) -> None:
+        self._devices[tag] = device
+        device.tag(tag)
+        await device.initialize(*tuple(self._parent_tags), self._tag)
+
+    async def device(self, tag: str, device: Device | None = None) -> Device | None:
+        if device:
+            await self._attach_device(tag, device)
+        else:
+            return self._devices[tag]
