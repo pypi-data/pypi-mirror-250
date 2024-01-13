49d5f5adfcfde79c8e5e1748635ebfd303a264d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 04:04:27 +0000

    Add package konraddatascience/0.1/konraddatascience-0.1.tar.gz

---
+++ packages/konraddatascience/konraddatascience-0.1.tar.gz/konraddatascience-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='konraddatascience',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple example package',
+    author='konrad burdach',
+    author_email='konrad.burdach@gmail.com'
+)
