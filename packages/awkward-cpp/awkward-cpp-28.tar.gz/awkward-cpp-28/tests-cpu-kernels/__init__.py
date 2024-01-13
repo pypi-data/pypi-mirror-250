ee3cd768dbaf13e3d89f0b7c57968b9a03f9f27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 05:04:55 +0000

    Add package awkward-cpp/28/awkward-cpp-28.tar.gz

---
+++ packages/awkward-cpp/awkward-cpp-28.tar.gz/awkward-cpp-28/tests-cpu-kernels/__init__.py
@@ -0,0 +1,10 @@
+# AUTO GENERATED ON 2024-01-12 AT 16:13:35
+# DO NOT EDIT BY HAND!
+#
+# To regenerate file, run
+#
+#     python dev/generate-tests.py
+#
+
+# fmt: off
+
---
+++ packages/awkward-cpp/awkward-cpp-28.tar.gz/awkward-cpp-28/tests-spec-explicit/test_awkward_IndexedArray_flatten_nextcarry.py
@@ -0,0 +1,14 @@
+import pytest
+import kernels
+
+def test_awkward_IndexedArray_flatten_nextcarry_1():
+	tocarry = [123, 123]
+	fromindex = [0, 1]
+	lencontent = 2
+	lenindex = 2
+	funcPy = getattr(kernels, 'awkward_IndexedArray_flatten_nextcarry')
+	funcPy(tocarry = tocarry,fromindex = fromindex,lencontent = lencontent,lenindex = lenindex)
+	pytest_tocarry = [0, 1]
+	assert tocarry == pytest_tocarry
+
+
