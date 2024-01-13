4a606bc6580b1a2752b1bc2e9536e9d70ef187e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 05:04:04 +0000

    Add package fiddler-client/2.5.0.dev1/fiddler_client-2.5.0.dev1-py3-none-any.whl

---
+++ packages/fiddler-client/fiddler_client-2.5.0.dev1-py3-none-any.whl/fiddler3/__init__.py
@@ -0,0 +1,4 @@
+from fiddler3.connection import Connection, ConnectionMixin, init  # noqa
+from fiddler3.entities import *  # noqa
+from fiddler3.exceptions import *  # noqa
+from fiddler3.schemas import *  # noqa
---
+++ packages/fiddler-client/fiddler_client-2.5.0.dev1-py3-none-any.whl/fiddler3/entities/__init__.py
@@ -0,0 +1,10 @@
+"""Initialize all new entities."""
+from fiddler3.entities.alert_record import AlertRecord  # noqa
+from fiddler3.entities.alert_rule import AlertRule  # noqa
+from fiddler3.entities.baseline import Baseline  # noqa
+from fiddler3.entities.dataset import Dataset  # noqa
+from fiddler3.entities.files import File  # noqa
+from fiddler3.entities.job import Job  # noqa
+from fiddler3.entities.model import Model  # noqa
+from fiddler3.entities.model_deployment import ModelDeployment  # noqa
+from fiddler3.entities.project import Project  # noqa
