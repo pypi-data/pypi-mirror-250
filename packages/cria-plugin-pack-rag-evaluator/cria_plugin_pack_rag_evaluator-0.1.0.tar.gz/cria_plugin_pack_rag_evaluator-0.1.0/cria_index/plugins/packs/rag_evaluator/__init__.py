9fbaa26698496640d3c1a6911c33af17df89d46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 05:05:11 +0000

    Add package cria-plugin-pack-rag-evaluator/0.1.0/cria_plugin_pack_rag_evaluator-0.1.0.tar.gz

---
+++ packages/cria-plugin-pack-rag-evaluator/cria_plugin_pack_rag_evaluator-0.1.0.tar.gz/cria_plugin_pack_rag_evaluator-0.1.0/cria_index/plugins/packs/rag_evaluator/__init__.py
@@ -0,0 +1,3 @@
+from cria_index.plugins.packs.rag_evaluator.base import RagEvaluatorPack
+
+__all__ = ["RagEvaluatorPack"]
---
+++ packages/cria-plugin-pack-rag-evaluator/cria_plugin_pack_rag_evaluator-0.1.0.tar.gz/cria_plugin_pack_rag_evaluator-0.1.0/cria_index/plugins/packs/rag_evaluator/base.py
@@ -0,0 +1,6 @@
+from cria_index.core.llama_packs.base import BasePack
+
+class RagEvaluatorPack(BasePack):
+    @classmethod
+    def class_name(cls):
+        return "RagEvaluatorPack"
