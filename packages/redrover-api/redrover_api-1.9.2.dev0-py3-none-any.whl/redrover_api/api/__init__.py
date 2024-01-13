011e00d15c5f4d77c4eb8aea2c8e8194d2a14fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 06:04:29 +0000

    Add package redrover-api/1.9.2.dev0/redrover_api-1.9.2.dev0-py3-none-any.whl

---
+++ packages/redrover-api/redrover_api-1.9.2.dev0-py3-none-any.whl/redrover_api/api/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from redrover_api.api.absence_api import AbsenceApi
+from redrover_api.api.absence_reason_api import AbsenceReasonApi
+from redrover_api.api.absence_reason_balance_api import AbsenceReasonBalanceApi
+from redrover_api.api.connection_api import ConnectionApi
+from redrover_api.api.organization_api import OrganizationApi
+from redrover_api.api.reference_data_api import ReferenceDataApi
+from redrover_api.api.user_api import UserApi
+from redrover_api.api.vacancy_api import VacancyApi
+from redrover_api.api.webhooks_api import WebhooksApi
