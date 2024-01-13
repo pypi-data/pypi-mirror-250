38f7aabaaac2c5a3ffb7d495539697664a24e225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 04:04:33 +0000

    Add package build-system-no-backend/0.1.0/build-system-no-backend-0.1.0.tar.gz

---
+++ packages/build-system-no-backend/build-system-no-backend-0.1.0.tar.gz/build-system-no-backend-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+# Ensure that Cython was installed
+# noinspection PyUnresolvedReferences
+import Cython
+
+setup(
+    name='build-system-no-backend',
+    version='0.1.0',
+    install_requires=[],
+)
