ce0ac00e46678cd65396ea0ff9a76143536619cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 15:04:43 +0000

    Add package proalgotrader-core/0.0.9b11/proalgotrader_core-0.0.9b11.tar.gz

---
+++ packages/proalgotrader-core/proalgotrader_core-0.0.9b11.tar.gz/proalgotrader_core-0.0.9b11/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("requirements.txt") as fp:
+    install_requires = fp.read().strip().split("\n")
+
+setup(
+    name="proalgotrader_core",
+    version="0.0.9.b11",
+    description="ProAlgoTrader core",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/krunaldodiya/proalgotrader_core",
+    author="Krunal Dodiya",
+    author_email="kunal.dodiya1@gmail.com",
+    include_package_data=True,
+    package_data={"": ["*"]},
+    packages=find_packages(exclude=["project"]),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
