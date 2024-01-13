d1b95eb9d9f156883282f4c2a20baea8f36194e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 15:05:24 +0000

    Add package verbatim/0.0.1/verbatim-0.0.1.tar.gz

---
+++ packages/verbatim/verbatim-0.0.1.tar.gz/verbatim-0.0.1/tests/test_pipeline.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import unittest
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+# pylint: disable=(wrong-import-position
+from verbatim.pipeline import Pipeline
+from verbatim.context import Context
+
+class TestStringMethods(unittest.TestCase):
+    def test_verbatim(self):
+        pass
+
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s [%(levelname)s][%(filename)s:%(lineno)d][%(funcName)s] %(message)s',
+                        datefmt='%Y-%m-%dT%H:%M:%SZ')
+
+    print(os.getcwd())
+    context: Context = Context(source_file="tests/data/test.mp3", languages=["en", "fr", "es", "it"], nb_speakers=2)
+    Pipeline(context).execute()
