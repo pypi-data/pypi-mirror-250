f040b89f2e79e8fde4291e9956cfdc1aa506119a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 15:05:19 +0000

    Add package dsnd-prob-calc/0.1/dsnd_prob_calc-0.1.tar.gz

---
+++ packages/dsnd-prob-calc/dsnd_prob_calc-0.1.tar.gz/dsnd_prob_calc-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_prob_calc',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author = "David Usoro",
+      author_email = "mulla.python@gmail.com",
+      packages=['dsnd_prob_calc'],
+      zip_safe=False)
