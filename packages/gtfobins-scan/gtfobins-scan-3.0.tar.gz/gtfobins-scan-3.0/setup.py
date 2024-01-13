687115c999126dfb32735e687060eb853a7dd29e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 14:06:00 +0000

    Add package gtfobins-scan/3.0/gtfobins-scan-3.0.tar.gz

---
+++ packages/gtfobins-scan/gtfobins-scan-3.0.tar.gz/gtfobins-scan-3.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gtfobins-scan",
+    version="3.0",
+    description="Vulnerability Scanner for GTFOBins",
+    packages=find_packages(),
+    install_requires=[
+        "gitpython",
+        "pyyaml",
+    ],
+    author="Vinal",
+    url="https://github.com/vinal-2/GTFObins",
+    license="GPL-3.0",
+    entry_points={
+        "console_scripts": [
+            "gtfobins-scan=gtfobins_scan:main",
+        ],
+    },
+    package_data={
+        "": ["gtfobins_scan.sh"],
+    },
+)
