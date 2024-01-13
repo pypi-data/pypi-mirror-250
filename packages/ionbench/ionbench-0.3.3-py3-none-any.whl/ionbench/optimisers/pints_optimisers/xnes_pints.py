2e81e3631252bfb9b3b37d13bb2a155f341884ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 03:05:29 +0000

    Add package ionbench/0.3.3/ionbench-0.3.3-py3-none-any.whl

---
+++ packages/ionbench/ionbench-0.3.3-py3-none-any.whl/ionbench/optimisers/pints_optimisers/xnes_pints.py
@@ -0,0 +1,68 @@
+import pints
+import ionbench
+import numpy as np
+from ionbench.optimisers.pints_optimisers import classes_pints
+
+
+def run(bm, x0=[], maxIter=1000, debug=False):
+    """
+    Runs XNES (Exponential Natural Evolution Strategy) from Pints using a benchmarker.
+
+    Parameters
+    ----------
+    bm : Benchmarker
+        A benchmarker to evaluate the performance of the optimisation algorithm.
+    x0 : list, optional
+        Initial parameter vector from which to start optimisation. Default is [], in which case a randomly sampled parameter vector is retrieved from bm.sample().
+    maxIter : int, optional
+        Number of iterations of XNES to run. The default is 1000.
+    debug : bool, optional
+        If True, logging messages are printed every iteration. Otherwise the default of every iteration for the first 3 and then every 20 iterations. The default is False.
+
+    Returns
+    -------
+    xbest : list
+        The best parameters identified by XNES.
+
+    """
+    if len(x0) == 0:
+        x0 = bm.sample()
+    model = classes_pints.Model(bm)
+    problem = pints.SingleOutputProblem(model, np.arange(model.bm.tmax), model.bm.data)
+    error = pints.RootMeanSquaredError(problem)
+
+    # Create an optimisation controller
+    if bm._bounded:
+        boundaries = pints.RectangularBoundaries(bm.input_parameter_space(bm.lb), bm.input_parameter_space(bm.ub))
+        opt = pints.OptimisationController(error, x0, method=pints.XNES, boundaries=boundaries)
+    else:
+        opt = pints.OptimisationController(error, x0, method=pints.XNES)
+    opt.set_max_iterations(maxIter)
+    if debug:
+        opt.set_log_interval(iters=1)
+    # Run the optimisation
+    x, f = opt.run()
+
+    model.bm.evaluate(x)
+    return x
+
+
+def get_modification(modNum=1):
+    """
+    No modification for this optimiser. Will use an empty modification.
+
+    Returns
+    -------
+    mod : modification
+        Empty modification
+
+    """
+    mod = ionbench.modification.Empty(name='xnes_pints')
+    return mod
+
+
+if __name__ == '__main__':
+    bm = ionbench.problems.staircase.HH_Benchmarker()
+    mod = get_modification()
+    mod.apply(bm)
+    run(bm, debug=True, **mod.kwargs)
