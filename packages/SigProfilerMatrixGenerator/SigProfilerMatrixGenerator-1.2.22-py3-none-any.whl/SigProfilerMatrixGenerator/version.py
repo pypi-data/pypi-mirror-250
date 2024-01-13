cc65d90c9215bcd039c376885c0e7620f4f7cde4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 03:04:45 +0000

    Add package SigProfilerMatrixGenerator/1.2.22/SigProfilerMatrixGenerator-1.2.22-py3-none-any.whl

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.22-py3-none-any.whl/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.22'
+version = '1.2.22'
+Update = 'v1.2.22: CLI calls ReferenceGenomeManager to download reference genomes and not install.py'
+
+	
