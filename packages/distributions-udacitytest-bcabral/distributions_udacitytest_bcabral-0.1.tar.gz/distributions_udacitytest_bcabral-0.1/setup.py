c8d9a3d3b08ee14179d2050706428f7bd14e2336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 04:04:53 +0000

    Add package distributions-udacitytest-bcabral/0.1/distributions_udacitytest_bcabral-0.1.tar.gz

---
+++ packages/distributions-udacitytest-bcabral/distributions_udacitytest_bcabral-0.1.tar.gz/distributions_udacitytest_bcabral-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_udacitytest_bcabral',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_udacitytest_bcabral'],
+      zip_safe=False)
