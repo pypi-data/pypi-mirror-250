c6298e5fc42e2b8ac460017b22edf1f6f551d3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 15:05:09 +0000

    Add package acgc/0.1.7/acgc-0.1.7.tar.gz

---
+++ packages/acgc/acgc-0.1.7.tar.gz/acgc-0.1.7/tests/test_imports.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+def test_acgc():
+    import acgc 
+
+def test_erroranalysis():
+    import acgc.erroranalysis
+
+def test_figstyle():
+    import acgc.figstyle
+
+def test_gc():
+    import acgc.gc
+
+def test_hysplit():
+    import acgc.hysplit
+
+def test_icartt():
+    import acgc.icartt
+
+def test_igra():
+    import acgc.igra
+
+def test_map_scalebar():
+    import acgc.map_scalebar
+
+def test_met():
+    import acgc.met
+
+def test_modetools():
+    import acgc.modetools
+
+def test_netcdf():
+    import acgc.netcdf
+
+def test_solar():
+    import acgc.solar
+
+def test_stats():
+    import acgc.stats
+
+def test_time():
+    import acgc.time
