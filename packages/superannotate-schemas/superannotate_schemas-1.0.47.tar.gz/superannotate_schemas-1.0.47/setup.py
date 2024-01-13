021ae5030fdd8dc7b8a27d9a57e7ab5b4f83741d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 03:04:51 +0000

    Add package superannotate-schemas/1.0.47/superannotate_schemas-1.0.47.tar.gz

---
+++ packages/superannotate-schemas/superannotate_schemas-1.0.47.tar.gz/superannotate_schemas-1.0.47/setup.py
@@ -0,0 +1,32 @@
+import os
+import re
+from pathlib import Path
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+def get_version():
+    init = open(os.path.join(this_directory, 'src', 'superannotate_schemas', '__init__.py')).read()
+    match = re.search(r'^__version__ = [\'"]([^\'"]+)[\'"]', init, re.M)
+    if not match:
+        raise RuntimeError('Unable to find version string.')
+    return match.group(1)
+
+
+setup(
+    name='superannotate_schemas',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version=get_version(),
+    package_dir={"": "src"},
+    data_files=[('', ['src/superannotate_schemas/schemas/draft7.json'])],
+    packages=find_packages(where="src"),
+    description='SuperAnnotate JSON Schemas',
+    author='Vaghinak Basentsyan',
+    author_email='vaghinak@superannotate.con',
+    url='https://www.superannotate.com/',
+    license='MIT',
+    description_file="README.md",
+)
