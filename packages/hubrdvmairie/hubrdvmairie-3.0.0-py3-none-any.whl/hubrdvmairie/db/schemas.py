5a6ba6eccf900e26c35d03af65ecfa4bdb216527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 06:04:42 +0000

    Add package hubrdvmairie/3.0.0/hubrdvmairie-3.0.0-py3-none-any.whl

---
+++ packages/hubrdvmairie/hubrdvmairie-3.0.0-py3-none-any.whl/hubrdvmairie/db/schemas.py
@@ -0,0 +1,159 @@
+"""
+This module contains Pydantic schemas for database models related to token and editor entities.
+It includes the following schemas:
+- TokenAntsToEditorBase
+- TokenAntsToEditorCreate
+- TokenAntsToEditor
+- TokenEditorToAntsBase
+- TokenEditorToAntsCreate
+- TokenEditorToAnts
+- EditorBase
+- EditorCreate
+- EditorUpdate
+- Editor
+"""
+import datetime
+
+from pydantic import BaseModel
+
+
+class TokenAntsToEditorBase(BaseModel):
+    """
+    Base schema for token issued by ANTS to editor
+    """
+
+    id: int
+    id_editor: int
+    token: str
+    validity_date: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class TokenAntsToEditorCreate(TokenAntsToEditorBase):
+    """
+    Schema for creating token issued by ANTS to editor
+    """
+
+    pass
+
+
+class TokenAntsToEditor(TokenAntsToEditorBase):
+    """
+    Schema for token issued by ANTS to editor
+    """
+
+    pass
+
+
+class TokenEditorToAntsBase(BaseModel):
+    """
+    Base schema for token issued by editor to ANTS
+    """
+
+    id: int
+    id_editor: int
+    token: str
+    validity_date: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class TokenEditorToAntsCreate(TokenEditorToAntsBase):
+    """
+    Schema for creating token issued by editor to ANTS
+    """
+
+    pass
+
+
+class TokenEditorToAnts(TokenEditorToAntsBase):
+    """
+    Schema for token issued by editor to ANTS
+    """
+
+    pass
+
+
+class EditorBase(BaseModel):
+    """
+    Base schema for editor
+    """
+
+    name: str
+    url: str
+    header_name: str
+
+
+class EditorCreate(EditorBase):
+    """
+    Schema for creating editor
+    """
+
+    pass
+
+
+class EditorUpdate(EditorBase):
+    """
+    Schema for updating editor
+    """
+
+    pass
+
+
+class Editor(EditorBase):
+    """
+    Schema for editor
+    """
+
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class EditorPanoramaBase(BaseModel):
+    """
+    Base schema for editor_panorama
+    """
+
+    name: str
+    logo_url: str
+    site_url: str
+    contact_url: str
+    overseas: bool
+    integration_predemande: bool
+    limit_appointment_outside: bool
+    saas: bool
+    use_queue: bool
+    is_connect_to_hub: bool
+    is_connect_to_anti_duplication: bool
+
+
+class EditorPanoramaCreate(EditorPanoramaBase):
+    """
+    Schema for creating editor
+    """
+
+    pass
+
+
+class EditorPanoramaUpdate(EditorPanoramaBase):
+    """
+    Schema for updating editor
+    """
+
+    pass
+
+
+class EditorPanorama(EditorPanoramaBase):
+    """
+    Schema for editor
+    """
+
+    id: int
+
+    class Config:
+        orm_mode = True
