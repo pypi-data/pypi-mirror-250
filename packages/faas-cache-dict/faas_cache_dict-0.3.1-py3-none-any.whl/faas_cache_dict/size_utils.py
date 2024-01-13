b78c6c1446ee6c9f5e975044ddc16be458a94b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 03:04:45 +0000

    Add package faas-cache-dict/0.3.1/faas_cache_dict-0.3.1-py3-none-any.whl

---
+++ packages/faas-cache-dict/faas_cache_dict-0.3.1-py3-none-any.whl/faas_cache_dict/size_utils.py
@@ -0,0 +1,5 @@
+import objsize
+
+
+def get_deep_byte_size(obj):
+    return objsize.get_deep_size(obj)
---
+++ packages/faas-cache-dict/faas_cache_dict-0.3.1-py3-none-any.whl/faas_cache_dict/utils.py
@@ -0,0 +1,7 @@
+def _assert(bool_, err_string=""):
+    """
+    Avoid using asserts in production code
+    https://juangarcia.co.uk/python/python-smell-assert/
+    """
+    if not bool_:
+        raise ValueError(err_string)
