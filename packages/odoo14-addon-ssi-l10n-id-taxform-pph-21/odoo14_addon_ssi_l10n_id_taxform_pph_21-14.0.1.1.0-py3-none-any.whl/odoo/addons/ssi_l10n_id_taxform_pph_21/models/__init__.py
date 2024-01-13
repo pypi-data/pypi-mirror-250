27b8f77fa908e56428d8251b68d158c3b0e99bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 05:04:09 +0000

    Add package odoo14-addon-ssi-l10n-id-taxform-pph-21/14.0.1.1.0/odoo14_addon_ssi_l10n_id_taxform_pph_21-14.0.1.1.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-l10n-id-taxform-pph-21/odoo14_addon_ssi_l10n_id_taxform_pph_21-14.0.1.1.0-py3-none-any.whl/odoo/addons/ssi_l10n_id_taxform_pph_21/models/__init__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from . import (
+    pph_21_biaya_jabatan,
+    pph_21_npwp_rate_modifier,
+    pph_21_rate,
+    pph_21_rate_line,
+    ptkp_category,
+    ptkp,
+    ptkp_line,
+    res_partner,
+)
