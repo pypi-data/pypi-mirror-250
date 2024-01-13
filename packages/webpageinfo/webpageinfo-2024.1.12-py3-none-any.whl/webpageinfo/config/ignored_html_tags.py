6832a52f7ea030d2906fa96cadac317b917a3ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 05:04:32 +0000

    Add package webpageinfo/2024.1.12/webpageinfo-2024.1.12-py3-none-any.whl

---
+++ packages/webpageinfo/webpageinfo-2024.1.12-py3-none-any.whl/webpageinfo/config/ignored_html_tags.py
@@ -0,0 +1,25 @@
+ignored_html_tags = {
+    "article",
+    "aside",
+    "blockquote",
+    "body",
+    "cite",
+    "dialog",
+    "figcaption",
+    "figure",
+    "footer",
+    "frameset",
+    "frame",  # TODO choper le contenu
+    "header",
+    "iframe",  # TODO choper le contenu
+    "label",
+    "main",
+    "noframe",
+    "noframes",
+    "section",
+    "show",
+    "tbody",
+    "transition",  # vue.js
+    "time",
+    "tt",
+}
