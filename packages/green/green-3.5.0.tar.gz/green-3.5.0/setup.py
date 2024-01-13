1bdd02620feb920c6b8298eab46f9670f371249e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 14:05:34 +0000

    Add package green/3.5.0/green-3.5.0.tar.gz

---
+++ packages/green/green-3.5.0.tar.gz/green-3.5.0/setup.py
@@ -0,0 +1,10 @@
+#
+# setup.py file for cleancut/green.
+#
+# setuptools versions older than 64.0.0 need a minimal setup.py file.
+# Versions 39.2.0 of setuptools should be compatible with python 3.5.
+#
+
+from setuptools import setup
+
+setup()
