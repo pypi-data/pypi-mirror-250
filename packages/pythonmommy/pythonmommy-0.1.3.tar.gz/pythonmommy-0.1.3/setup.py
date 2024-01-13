2646c0da439cc1fc8806467dd2243f275bb14962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 04:04:58 +0000

    Add package pythonmommy/0.1.3/pythonmommy-0.1.3.tar.gz

---
+++ packages/pythonmommy/pythonmommy-0.1.3.tar.gz/pythonmommy-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name='pythonmommy',
+    version='0.1.3',
+    author='googer',
+    author_email='googer760@gmail.com',
+    description='Mommy\'s here to support you when running python~ ❤️',
+    long_description=(Path(__file__).parent/"README.md").read_text(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
