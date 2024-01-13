bbf5a9df27645c47031b952da15837a8e52a2027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 06:04:32 +0000

    Add package sagace-sdk/0.1.0.1/sagace_sdk-0.1.0.1-py3-none-any.whl

---
+++ packages/sagace-sdk/sagace_sdk-0.1.0.1-py3-none-any.whl/sagace_sdk/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+    --------------------------------------------------------------------------------------------------------------------
+
+    Description:
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Obs.:
+
+    Author:           @diego.yosiura
+    Last Update:      12/01/2024 11:16
+    Created:          12/01/2024 11:16
+    Copyright:        (c) Ampere Consultoria Ltda
+    Original Project: sagace-v2-package
+    IDE:              PyCharm
+"""
+# Manter no topo para evitar erro de referencia curcular
+CONFIG = lambda: __CONFIG__ if __CONFIG__ is not None else Configuration()
+AUTH_PROVIDER = lambda: __AUTH_PROVIDER__ if __AUTH_PROVIDER__ is not None else Provider()
+
+
+from .utils import Configuration
+
+__CONFIG__ = None
+__AUTH_PROVIDER__ = None
+
+from .auth.provider import Provider
+
+
+def configure(config: Configuration, auth_provider: Provider):
+    global __CONFIG__, __AUTH_PROVIDER__
+    __CONFIG__ = config
+    __AUTH_PROVIDER__ = auth_provider
