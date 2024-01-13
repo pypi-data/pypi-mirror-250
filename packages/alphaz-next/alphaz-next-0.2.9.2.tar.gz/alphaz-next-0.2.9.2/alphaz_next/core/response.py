27245a750aa340ca4c35a49f7a5531ae5a85cff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-13 03:04:44 +0000

    Add package alphaz-next/0.2.9.2/alphaz-next-0.2.9.2.tar.gz

---
+++ packages/alphaz-next/alphaz-next-0.2.9.2.tar.gz/alphaz-next-0.2.9.2/alphaz_next/core/response.py
@@ -0,0 +1,71 @@
+# MODULES
+from typing import Any, Dict, Mapping
+
+# FASTAPI
+from fastapi.responses import JSONResponse as _JSONResponse, Response as _Response
+
+# STARLETTE
+from starlette.background import BackgroundTask
+
+# CORE
+from alphaz_next.core._base import extend_headers, ExtHeaders
+
+try:
+    import ujson
+except ImportError:  # pragma: nocover
+    ujson = None  # type: ignore
+
+try:
+    import orjson
+except ImportError:  # pragma: nocover
+    orjson = None  # type: ignore
+
+
+class Response(_Response):
+    def __init__(
+        self,
+        content: Any = None,
+        status_code: int = 200,
+        headers: Mapping[str, str] | None = None,
+        ext_headers: ExtHeaders | None = None,
+        media_type: str | None = None,
+        background: BackgroundTask | None = None,
+    ) -> None:
+        headers = extend_headers(
+            headers=headers,
+            ext_headers=ext_headers,
+        )
+
+        super().__init__(content, status_code, headers, media_type, background)
+
+
+class JSONResponse(_JSONResponse):
+    def __init__(
+        self,
+        content: Any,
+        status_code: int = 200,
+        headers: Dict[str, str] | None = None,
+        ext_headers: ExtHeaders | None = None,
+        media_type: str | None = None,
+        background: BackgroundTask | None = None,
+    ) -> None:
+        headers = extend_headers(
+            headers=headers,
+            ext_headers=ext_headers,
+        )
+
+        super().__init__(content, status_code, headers, media_type, background)
+
+
+class UJSONResponse(JSONResponse):
+    def render(self, content: Any) -> bytes:
+        assert ujson is not None, "ujson must be installed to use UJSONResponse"
+        return ujson.dumps(content, ensure_ascii=False).encode("utf-8")
+
+
+class ORJSONResponse(JSONResponse):
+    def render(self, content: Any) -> bytes:
+        assert orjson is not None, "orjson must be installed to use ORJSONResponse"
+        return orjson.dumps(
+            content, option=orjson.OPT_NON_STR_KEYS | orjson.OPT_SERIALIZE_NUMPY
+        )
