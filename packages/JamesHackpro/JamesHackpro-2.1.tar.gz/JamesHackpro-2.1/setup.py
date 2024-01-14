7678d972aab193098117e5bec8188e20415f2861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 03:05:36 +0000

    Add package JamesHackpro/2.1/JamesHackpro-2.1.tar.gz

---
+++ packages/JamesHackpro/JamesHackpro-2.1.tar.gz/JamesHackpro-2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='JamesHackpro',
+    version='2.1',
+    description='Facebook Random Uid Cloning. Premium Tools',
+    author='James404-Cyber',
+    url='https://github.com/James404-cyber/HACKPRO',
+    packages=[],
+    py_modules=[],
+    install_requires=[],
+    data_files=[
+        ('bin', ['bin/HACKPRO']),
+    ],
+)
