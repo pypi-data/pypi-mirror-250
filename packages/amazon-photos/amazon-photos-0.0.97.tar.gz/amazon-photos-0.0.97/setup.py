f82bf220b5a0d6231d843f13d2892a0413bb9ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 04:05:30 +0000

    Add package amazon-photos/0.0.97/amazon-photos-0.0.97.tar.gz

---
+++ packages/amazon-photos/amazon-photos-0.0.97.tar.gz/amazon-photos-0.0.97/setup.py
@@ -0,0 +1,57 @@
+from textwrap import dedent
+
+from setuptools import find_packages, setup
+from pathlib import Path
+
+install_requires = [
+    'psutil',
+    'nest-asyncio',
+    'tqdm',
+    'uvloop; platform_system != "Windows"',
+    'aiofiles',
+    'orjson',
+    'httpx[http2]',
+    'pandas',
+    'psutil',
+]
+
+extras_require = {
+    'extras': ['torch', 'torchvision', 'timm', 'Pillow'],
+}
+
+about = {}
+exec((Path().cwd() / 'amazon_photos' / '__version__.py').read_text(), about)
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    author=about['__author__'],
+    description=about['__description__'],
+    license=about['__license__'],
+    long_description=dedent((Path().cwd() / 'README.md').read_text()),
+    python_requires=">=3.10.10",
+    long_description_content_type='text/markdown',
+    author_email='trevorhobenshield@gmail.com',
+    url='https://github.com/trevorhobenshield/amazon_photos',
+    install_requires=install_requires,
+    extras_require=extras_require,
+    keywords='amazon photos api async search automation',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
