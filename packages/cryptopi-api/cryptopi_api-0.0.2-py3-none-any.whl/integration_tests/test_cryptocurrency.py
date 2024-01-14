646ee7d10c24009e2db4319ff7338c2fd6232e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 14:04:45 +0000

    Add package cryptopi-api/0.0.2/cryptopi_api-0.0.2-py3-none-any.whl

---
+++ packages/cryptopi-api/cryptopi_api-0.0.2-py3-none-any.whl/integration_tests/test_cryptocurrency.py
@@ -0,0 +1,24 @@
+"""
+test_cryptocurrency.py This file tests the /cryptocurrency routes.
+"""
+
+import requests
+from unittest import TestCase
+
+
+class TestCryptoCurrencyEndpoints(TestCase):
+    """
+    This class tests the /cryptocurrency endpoints.
+    """
+
+    def test_get_latest_quotes(self):
+        """
+        This function tests the /cryptocurrency/quotes/latest endpoint.
+        :return:
+        """
+
+        # Make the request.
+        response = requests.get("http://127.0.0.1:8000/cryptocurrency/quotes/latest?symbols=BTC&symbols=ETH")
+
+        # Assert the response.
+        self.assertEqual(response.status_code, 200)
