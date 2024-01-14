29f30c726ca5b8fc7a7e175869708d2ab4dab430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 14:04:40 +0000

    Add package map-network-viz/0.0.9/map-network-viz-0.0.9.tar.gz

---
+++ packages/map-network-viz/map-network-viz-0.0.9.tar.gz/map-network-viz-0.0.9/src/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='map_network_viz',
+    version='0.0.9',
+    packages=find_packages(),
+    install_requires=[
+        'geopandas',
+        'geopy',
+        'matplotlib',
+        'osmnx>=1.8.0',
+        'urllib3<2.0'
+    ],
+)
