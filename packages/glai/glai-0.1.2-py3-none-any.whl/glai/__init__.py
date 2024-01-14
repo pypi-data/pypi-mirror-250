50cbb8b021a2278a00358f5fbbe91a4b744fa150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 04:05:04 +0000

    Add package glai/0.1.2/glai-0.1.2-py3-none-any.whl

---
+++ packages/glai/glai-0.1.2-py3-none-any.whl/glai/__init__.py
@@ -0,0 +1,11 @@
+from .ai import AutoAI, EasyAI
+from .messages import AIMessages, AIMessage
+
+__all__ = ['AutoAI', 'EasyAI', 'AIMessages', 'AIMessage']
+# print(f"""
+# glai
+# GGUF LLAMA AI - Package for simplified text generation with Llama models quantized to GGUF format is loaded.
+# Provides high level APIs for loading models and generating text completions.
+# For more information please check README.md file or visit https://github.com/laelhalawani/glai 
+# Detailed API documentation can be found here: https://laelhalawani.github.io/glai/
+# """)
