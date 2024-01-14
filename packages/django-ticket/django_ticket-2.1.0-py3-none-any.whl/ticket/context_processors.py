4beb91f4b1721fb62b41f6406fef637b654edde8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 14:04:56 +0000

    Add package django-ticket/2.1.0/django_ticket-2.1.0-py3-none-any.whl

---
+++ packages/django-ticket/django_ticket-2.1.0-py3-none-any.whl/ticket/context_processors.py
@@ -0,0 +1,8 @@
+from .models import  Ticket
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+def get_pending_tickets(request):
+    return {
+        'pending_tickets': Ticket.objects.filter(status="pending").count(),
+    }
