2671c73cafd97c75307583131de4f3a9f0716d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 03:05:51 +0000

    Add package df-prep/0.1.3/df_prep-0.1.3.tar.gz

---
+++ packages/df-prep/df_prep-0.1.3.tar.gz/df_prep-0.1.3/README.md
@@ -0,0 +1,11 @@
+#### Build
+
+```
+python setup.py sdist bdist_wheel
+```
+
+#### Publish
+
+```
+twine upload dist/* -u __token__ -p [token]                                                                                                        
+```
---
+++ packages/df-prep/df_prep-0.1.3.tar.gz/df_prep-0.1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='df_prep',  
+    version='0.1.3',
+    author='DF',
+    author_email='your_email@example.com',
+    packages=find_packages(),
+    install_requires=[],  
+    py_modules=['df_prep'],
+)
