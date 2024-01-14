de5a46220119be41401a815ca14909b1acea8bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 03:06:43 +0000

    Add package codexregius/0.3.0/codexregius-0.3.0.tar.gz

---
+++ packages/codexregius/codexregius-0.3.0.tar.gz/codexregius-0.3.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='codexregius',
+    version='0.3.0',
+    author='StealthyPanda',
+    author_email='shaikm259@gmail.com',
+    description='A debugging library',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
