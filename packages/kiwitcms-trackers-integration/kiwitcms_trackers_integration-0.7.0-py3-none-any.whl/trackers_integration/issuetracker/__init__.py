6fc59f633048ff64378bdbb8a81cdf788c1ae83b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 04:05:29 +0000

    Add package kiwitcms-trackers-integration/0.7.0/kiwitcms_trackers_integration-0.7.0-py3-none-any.whl

---
+++ packages/kiwitcms-trackers-integration/kiwitcms_trackers_integration-0.7.0-py3-none-any.whl/trackers_integration/issuetracker/__init__.py
@@ -0,0 +1,13 @@
+# pylint: disable=unused-import
+
+# Copyright (c) 2022 Alexander Todorov <atodorov@MrSenko.com>
+#
+# Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
+"""
+    Extra Issue Tracker integration between Kiwi TCMS and various
+    Issue Trackers.
+
+    .. versionadded:: 11.6-Enterprise
+"""
+from .openproject import OpenProject  # noqa
+from .mantis import Mantis  # noqa
---
+++ packages/kiwitcms-trackers-integration/kiwitcms_trackers_integration-0.7.0-py3-none-any.whl/trackers_integration/menu.py
@@ -0,0 +1,5 @@
+from django.utils.translation import gettext_lazy as _
+
+MENU_ITEMS = [
+    (_("Personal API tokens"), "/admin/trackers_integration/apitoken/"),
+]
