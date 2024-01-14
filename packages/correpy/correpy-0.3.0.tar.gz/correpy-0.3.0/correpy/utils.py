77239fee8e18a6e8988f6155716e8dde60c7b6e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 04:05:29 +0000

    Add package correpy/0.3.0/correpy-0.3.0.tar.gz

---
+++ packages/correpy/correpy-0.3.0.tar.gz/correpy-0.3.0/correpy/utils.py
@@ -0,0 +1,18 @@
+import re
+from datetime import date, datetime
+from decimal import Decimal
+
+NUMBER_STRUCTURE_REGEX = r"(?<![\d(\.|,)])(?:0,\d{2}|[1-9]\d{0,2}(?:\.\d{3})*,\d{2}|[1-9]\d{0,2})(?![\d(\.|,)])"
+DATE_STRUCTURE_REGEX = r"[\d]{1,2}/[\d]{1,2}/[\d]{4}"
+
+
+def extract_value_from_line(*, line: str) -> Decimal:
+    if total_value := re.findall(NUMBER_STRUCTURE_REGEX, line):
+        return Decimal(total_value[-1].replace(".", "").replace(",", "."))
+
+    return Decimal(0)
+
+
+def extract_date_from_line(*, line: str) -> date:
+    reference_date_string = re.findall(DATE_STRUCTURE_REGEX, line)[0]
+    return datetime.strptime(reference_date_string, "%d/%m/%Y").date()
