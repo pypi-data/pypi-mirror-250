1e252b0224221dcfd1ee108f77bad65ec0e67f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 14:04:49 +0000

    Add package cryptopi-api/0.0.3/cryptopi-api-0.0.3.tar.gz

---
+++ packages/cryptopi-api/cryptopi-api-0.0.3.tar.gz/cryptopi-api-0.0.3/cryptopi_api/api/routes/cryptocurrencies.py
@@ -0,0 +1,40 @@
+"""
+cryptocurrencies.py This file contains the routes of the /cryptocurrency routes.
+"""
+
+from cryptopi import CoinMarketCapApi
+from cryptopi.api.urls.responses import QuoteResponse
+from cryptopi.models import Symbol
+from cryptopi.utils import find_api_key
+
+from typing import Optional
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/cryptocurrency",
+)
+
+
+@router.get("/quotes/latest")
+async def get_latest_quotes(symbol: Optional[list[str]] = None) -> QuoteResponse:
+    """
+    This function returns the latest quotes of all cryptocurrencies.
+    :return:
+    """
+
+    # Get the API key.
+    api_key = find_api_key()
+
+    # Create the API instance.
+    api = CoinMarketCapApi(api_key=api_key)
+
+    # Load the symbols.
+    symbols = [Symbol(symbol) for symbol in symbol] if symbol else None
+
+    # Define the params.
+    params = {}
+    if symbols:
+        params["symbol"] = symbols
+
+    # Get the latest quotes.
+    return api.cryptocurrency_latest_quotes()
---
+++ packages/cryptopi-api/cryptopi-api-0.0.3.tar.gz/cryptopi-api-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.3"
+DESCRIPTION = "A simple Flask API for the `cryptopi` package."
+
+setup(
+    name="cryptopi-api",
+    version=VERSION,
+    author="Jake Williamson",
+    author_email="<brianjw88@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "fastapi",
+        "cryptopi",
+        "pydantic",
+        "uvicorn",
+    ],
+    keywords=["python", "crypto", "api", "coinmarketcap", "coinmarketcap-api"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+)
