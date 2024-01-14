4f2ea478332d2780ed1a58cd516760227bc30039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 04:05:23 +0000

    Add package tve/0.0.25/tve-0.0.25.tar.gz

---
+++ packages/tve/tve-0.0.25.tar.gz/tve-0.0.25/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup,find_namespace_packages
+from os import listdir
+
+version = '0.0.25'
+
+setup(
+    name = 'tve',
+    version=version,
+    url='https://https://github.com/solar-hedgehog/tve',
+
+    author='Ryabov Pavlusha',
+    author_email='maria27perminova@gmail.com',
+
+    license='MIT',
+        
+    packages= ['tve'],
+    package_dir={"": "src"},
+
+    include_package_data=True,  # Include data files specified in MANIFEST.in
+    # package_data={
+    #     '': ['pic/*.png'],  # Include all PNG files in the 'pic' directory
+    # },
+    package_data={},
+    zip_safe=False
+)
+
+
+
+
