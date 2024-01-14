450b39ec2b4d0a273c047d82625c34548e0a9da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 04:05:26 +0000

    Add package cryptopi/0.0.3/cryptopi-0.0.3.tar.gz

---
+++ packages/cryptopi/cryptopi-0.0.3.tar.gz/cryptopi-0.0.3/cryptopi/models/__init__.py
@@ -0,0 +1,6 @@
+# noinspection PyUnresolvedReferences
+from cryptopi.api.urls.data import Symbol
+# noinspection PyUnresolvedReferences
+from cryptopi.api.urls.data import SymbolName
+# noinspection PyUnresolvedReferences
+from cryptopi.api.urls.data import SymbolSlug
