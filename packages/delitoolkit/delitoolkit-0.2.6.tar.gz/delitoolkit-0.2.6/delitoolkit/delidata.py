275320531fbd86f64385c0b7c33547508e77a8ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 03:05:25 +0000

    Add package delitoolkit/0.2.6/delitoolkit-0.2.6.tar.gz

---
+++ packages/delitoolkit/delitoolkit-0.2.6.tar.gz/delitoolkit-0.2.6/delitoolkit/delidata.py
@@ -0,0 +1,15 @@
+import csv
+import os
+
+
+class DeliData:
+    def __init__(self):
+        files = os.listdir('delidata')
+        self.corpus = {}
+        for f in files:
+            if f == '__init__.py':
+                continue
+            with open('delidata/' + f, 'r', encoding="utf8") as rf:
+                a = [{k: v for k, v in row.items()} for row in
+                     csv.DictReader(rf, delimiter='\t', skipinitialspace=True)]
+                self.corpus[f] = a
