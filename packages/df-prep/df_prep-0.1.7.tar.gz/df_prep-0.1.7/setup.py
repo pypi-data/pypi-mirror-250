7bf36eaa85202e1f9f6bf649b7c6743cbf7cfe90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 03:05:53 +0000

    Add package df-prep/0.1.7/df_prep-0.1.7.tar.gz

---
+++ packages/df-prep/df_prep-0.1.7.tar.gz/df_prep-0.1.7/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='df_prep',  
+    version='0.1.7',
+    author='DF',
+    author_email='your_email@example.com',
+    packages=find_packages(),  
+    install_requires=[],  
+)
