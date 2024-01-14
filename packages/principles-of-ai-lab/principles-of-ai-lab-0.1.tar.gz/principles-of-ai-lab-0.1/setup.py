a32cf209ed2b61943be73fcba9817579a88c2bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 03:05:54 +0000

    Add package principles-of-ai-lab/0.1/principles-of-ai-lab-0.1.tar.gz

---
+++ packages/principles-of-ai-lab/principles-of-ai-lab-0.1.tar.gz/principles-of-ai-lab-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+with open("./README.md", "r") as f:
+    info = f.read()
+
+setup(
+    name="principles-of-ai-lab",
+    version="0.1",
+    packages=find_packages(),
+    long_description=info,
+)
