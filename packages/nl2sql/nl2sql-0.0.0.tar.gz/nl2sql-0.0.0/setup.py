898d9257ce1d042acaf604cadfe0df351edfb25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 15:05:09 +0000

    Add package nl2sql/0.0.0/nl2sql-0.0.0.tar.gz

---
+++ packages/nl2sql/nl2sql-0.0.0.tar.gz/nl2sql-0.0.0/setup.py
@@ -0,0 +1,19 @@
+
+import setuptools
+
+setuptools.setup(
+    name="nl2sql",
+    version="0.0.0",
+    author="Madhup Sukoon",
+    author_email="vagrantism@outlook.in",
+    description="Naural Langucage to Structured Query Language",
+    long_description="Coming Soon",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
