718ea1596fe3b752d71e64ecdbec33fb5485b75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 14:04:46 +0000

    Add package pyappkit/0.0.21/pyappkit-0.0.21.tar.gz

---
+++ packages/pyappkit/pyappkit-0.0.21.tar.gz/pyappkit-0.0.21/README.md
@@ -0,0 +1,11 @@
+# Features
+
+| module                             | Description                 |
+| ---------------------------------- | --------------------------- |
+| [Daemon](src/pyappkit/daemon)      | Manage daemons and workers  |
+
+
+# Examples
+| location                           | Description                       |
+| ---------------------------------- | --------------------------------- |
+| [Daemon](examples/daemon)          | Examples for daemons and workers  |
---
+++ packages/pyappkit/pyappkit-0.0.21.tar.gz/pyappkit-0.0.21/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md"), "r") as f:
+    README = f.read()
+
+# This call to setup() does all the work
+setup(
+    name="pyappkit",
+    version="0.0.21",
+    description="Python Application Kit",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/stonezhong/pyappkit",
+    author="Stone Zhong",
+    author_email="stonezhong@hotmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=["jinja2", "PyYAML", "pydantic"],
+    entry_points={
+        "console_scripts": [
+            "daemon=pyappkit.daemon.mordor:cli_daemon",
+        ]
+    },
+)
