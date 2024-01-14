f5703ee1e052746aee06a0f576e2f4c2b88a054b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 03:05:44 +0000

    Add package ResPusher/0.0.10/ResPusher-0.0.10-py3-none-any.whl

---
+++ packages/ResPusher/ResPusher-0.0.10-py3-none-any.whl/ResPusher/respusher.py
@@ -0,0 +1,11 @@
+    
+
+class ResPusher():
+    """
+    channel: int [ServerChan, WXpusher, Email]
+    """
+    def __init__(self):
+        return None
+
+    def send_message(self,title,content):
+        return "success"
