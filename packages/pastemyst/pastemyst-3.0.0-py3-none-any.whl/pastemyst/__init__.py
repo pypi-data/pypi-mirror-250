1b900efda0cba96656767886dfb4b569c1bfaf3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 14:04:46 +0000

    Add package pastemyst/3.0.0/pastemyst-3.0.0-py3-none-any.whl

---
+++ packages/pastemyst/pastemyst-3.0.0-py3-none-any.whl/pastemyst/__init__.py
@@ -0,0 +1,5 @@
+from pastemyst.client import Client
+from pastemyst.constants import *
+from pastemyst.models import *
+from pastemyst.api import *
+from pastemyst.utils import *
---
+++ packages/pastemyst/pastemyst-3.0.0-py3-none-any.whl/pastemyst/models/__init__.py
@@ -0,0 +1,4 @@
+from .errors import PastemystError, HttpError, RequestError
+from .language import LanguageInfo, Language
+from .paste import ExpiresIn, EditType, Pasty, PasteEdit, Paste, PasteResult
+from .user import User
