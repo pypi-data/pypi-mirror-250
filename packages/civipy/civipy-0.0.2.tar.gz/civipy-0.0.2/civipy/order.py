3027b8dfdc36eab72c6309ab0186427c47d560a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 14:04:33 +0000

    Add package civipy/0.0.2/civipy-0.0.2.tar.gz

---
+++ packages/civipy/civipy-0.0.2.tar.gz/civipy-0.0.2/civipy/order.py
@@ -0,0 +1,11 @@
+from civipy.base import CiviCRMBase
+
+
+class CiviOrder(CiviCRMBase):
+    pass
+
+
+class CiviPayment(CiviCRMBase):
+    @classmethod
+    def cancel(cls, **kwargs):
+        return cls._post_method()("cancel", "Payment", kwargs)
