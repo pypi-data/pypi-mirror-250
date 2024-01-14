461b6ef8fd00dd883e8fa49a8aa2bdd590eed73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 15:04:59 +0000

    Add package dingraia/2.0.3/dingraia-2.0.3.tar.gz

---
+++ packages/dingraia/dingraia-2.0.3.tar.gz/dingraia-2.0.3/setup.py
@@ -0,0 +1,21 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name='dingraia',
+    version='2.0.3',
+    packages=find_packages(),
+    url='https://github.com/MeiHuaGuangShuo/Dingraia',
+    author='MeiHuaGuangShuo',
+    author_email='meihuaguangshuo@gmail.com',
+    description='A Dingtalk robot framework based on Python, supporting http and stream mode.',
+    long_description='A Dingtalk robot framework based on official, easier and advanced to use.',
+    install_requires=[
+        "loguru",
+        "requests",
+        "aiohttp",
+        "pycryptodome",
+        "websockets"
+    ],
+    python_requires='>3.7',
+    zip_safe=True,
+)
