23b5e06d84ddecb84bada98ede7b6b738006a82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 04:05:31 +0000

    Add package isolated-environment/1.0.4/isolated-environment-1.0.4.tar.gz

---
+++ packages/isolated-environment/isolated-environment-1.0.4.tar.gz/isolated-environment-1.0.4/tests/test_iso_environment.py
@@ -0,0 +1,30 @@
+"""
+Unit test file.
+"""
+
+import unittest
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from isolated_environment.api import IsolatedEnvironment
+
+HERE = Path(__file__).parent
+TEST_DIR = HERE / "test"
+
+
+class IsolatedEnvironmentTest(unittest.TestCase):
+    """Main tester class."""
+
+    def test_isolated_environment(self) -> None:
+        """Test command line interface (CLI)."""
+        with TemporaryDirectory() as tmp_dir:
+            iso_env = IsolatedEnvironment(Path(tmp_dir) / "venv")
+            with iso_env.lock():
+                iso_env.install_environment()
+                iso_env.pip_install("static-ffmpeg")
+                rtn = iso_env.run(["static_ffmpeg", "--help"])
+                self.assertEqual(0, rtn)
+
+
+if __name__ == "__main__":
+    unittest.main()
