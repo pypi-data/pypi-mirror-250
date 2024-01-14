36fa6c67fe7aeb00fc80e32dd8f2f4c979bd5cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 14:04:48 +0000

    Add package neofoodclub/0.0.6/neofoodclub-0.0.6-cp38-abi3-win_arm64.whl

---
+++ packages/neofoodclub/neofoodclub-0.0.6-cp38-abi3-win_arm64.whl/neofoodclub/__init__.py
@@ -0,0 +1,15 @@
+__title__ = "neofoodclub"
+__author__ = "diceroll123"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021-present diceroll123"
+__version__ = "0.0.1a"
+
+from .arenas import *
+from .bets import *
+from .errors import *
+from .models import *
+from .modifier import *
+from .nfc import *
+from .odds import *
+from .odds_change import *
+from .pirates import *
