3f33a4c9e6036ec2ec34849903eb428860eabe04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 04:05:42 +0000

    Add package pydsstools/2.3.2/pydsstools-2.3.2-cp310-cp310-win_amd64.whl

---
+++ packages/pydsstools/pydsstools-2.3.2-cp310-cp310-win_amd64.whl/pydsstools/src/core_heclib_imports.pyx
@@ -0,0 +1,19 @@
+import logging
+import sys
+from libc.math cimport floor
+from cpython cimport array
+from cython cimport view
+from checlib cimport *
+from datetime import datetime,timedelta
+from libc.stdlib cimport malloc, free
+from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
+from dateutil import parser
+from dateutil.relativedelta import relativedelta
+from affine import Affine
+from collections import namedtuple
+import numpy as np 
+np.seterr(over='raise')
+cimport numpy as np
+cimport libcpp 
+cimport cython
+import re
