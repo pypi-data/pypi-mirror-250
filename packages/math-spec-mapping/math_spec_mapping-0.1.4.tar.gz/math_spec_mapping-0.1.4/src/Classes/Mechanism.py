7a848637e32dddff65a7ce83032ef5e2476712b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 03:05:47 +0000

    Add package math-spec-mapping/0.1.4/math_spec_mapping-0.1.4.tar.gz

---
+++ packages/math-spec-mapping/math_spec_mapping-0.1.4.tar.gz/math_spec_mapping-0.1.4/src/Classes/Mechanism.py
@@ -0,0 +1,11 @@
+from typing import Dict
+from .Block import Block
+
+
+class Mechanism(Block):
+    def __init__(self, data: Dict):
+        data["codomain"] = tuple()
+        super().__init__(data)
+        self.logic = data["logic"]
+        self.updates = []
+        self.block_type = "Mechanism"
