8ae840266d90a0b6a6cc7f89ff607dd796a92fc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 03:06:12 +0000

    Add package cache-result/0.2.3/cache-result-0.2.3.tar.gz

---
+++ packages/cache-result/cache-result-0.2.3.tar.gz/cache-result-0.2.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# 读取README.md文件的内容
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cache-result',
+    version='0.2.3',
+    description='A python decorator for caching the results of functions',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        'Source Code': 'https://github.com/glwhappen/cache-result',
+    },
+    author='glwhappen',
+    author_email='1597721684@qq.com',
+    packages=find_packages(),
+    install_requires=[
+        'colorama==0.4.6',
+        'loguru==0.7.2',
+    ],
+)
