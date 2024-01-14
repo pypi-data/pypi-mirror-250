f6c8adf113013a86dfc4a1cb1126a5d5ad84da63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 04:05:29 +0000

    Add package kiwitcms-trackers-integration/0.7.0/kiwitcms-trackers-integration-0.7.0.tar.gz

---
+++ packages/kiwitcms-trackers-integration/kiwitcms-trackers-integration-0.7.0.tar.gz/kiwitcms-trackers-integration-0.7.0/trackers_integration/__init__.py
@@ -0,0 +1,6 @@
+"""
+    Extra Issue Tracker integration between Kiwi TCMS and various
+    Issue Trackers.
+
+    .. versionadded:: 11.6-Enterprise
+"""
---
+++ packages/kiwitcms-trackers-integration/kiwitcms-trackers-integration-0.7.0.tar.gz/kiwitcms-trackers-integration-0.7.0/trackers_integration/auth.py
@@ -0,0 +1,20 @@
+from django.core.cache import cache
+
+from trackers_integration.models import ApiToken
+
+
+def personal_api_token(issue_tracker):
+    # usually as part of automated tests
+    if not issue_tracker.request:
+        return None
+
+    token = cache.get(
+        f"api-token-of-{issue_tracker.request.user.pk}-for-{issue_tracker.bug_system.pk}",
+        ApiToken.objects.filter(
+            owner=issue_tracker.request.user, base_url=issue_tracker.bug_system.base_url
+        ).first(),
+    )
+    if token:
+        return (token.api_username, token.api_password)
+
+    return None
