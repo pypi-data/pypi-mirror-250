2283b2c62d58442ca53f4d0781893d41b07fa5bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 03:06:00 +0000

    Add package tve/0.0.19/tve-0.0.19.tar.gz

---
+++ packages/tve/tve-0.0.19.tar.gz/tve-0.0.19/setup.py
@@ -0,0 +1,18 @@
+from io import open
+from setuptools import setup
+
+version = '0.0.19'
+
+setup(
+    name = 'tve',
+    version=version,
+
+    author='Lohi',
+    author_email='usaidoleg@gmail.com',
+
+    license='Free' ,
+    
+    include_package_data=True,
+    
+    packages= ['tve']
+)
