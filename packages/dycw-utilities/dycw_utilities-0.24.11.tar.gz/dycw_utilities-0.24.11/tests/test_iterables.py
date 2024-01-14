a016575f336b96f74d8778a575b8ce2833344b99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 14:04:32 +0000

    Add package dycw-utilities/0.24.11/dycw_utilities-0.24.11.tar.gz

---
+++ packages/dycw-utilities/dycw_utilities-0.24.11.tar.gz/dycw_utilities-0.24.11/tests/test_iterables.py
@@ -0,0 +1,260 @@
+from __future__ import annotations
+
+from typing import Any
+
+from hypothesis import given
+from hypothesis.strategies import integers, sets
+from pytest import mark, param, raises
+
+from utilities.iterables import (
+    CheckDuplicatesError,
+    CheckIterablesEqualError,
+    CheckLengthError,
+    CheckLengthsEqualError,
+    CheckMappingsEqualError,
+    CheckSetsEqualError,
+    CheckSubsetError,
+    CheckSupersetError,
+    check_duplicates,
+    check_iterables_equal,
+    check_length,
+    check_lengths_equal,
+    check_mappings_equal,
+    check_sets_equal,
+    check_subset,
+    check_superset,
+    ensure_hashables,
+    is_iterable_not_str,
+)
+
+
+class TestCheckDuplicates:
+    @given(x=sets(integers()))
+    def test_main(self, *, x: set[int]) -> None:
+        check_duplicates(x)
+
+    def test_error(self) -> None:
+        with raises(
+            CheckDuplicatesError,
+            match=r"Iterable .* must not contain duplicates; got \(.*, n=2\)",
+        ):
+            check_duplicates([None, None])
+
+
+class TestCheckIterablesEqual:
+    def test_pass(self) -> None:
+        check_iterables_equal([], [])
+
+    def test_error_differing_items_and_left_longer(self) -> None:
+        with raises(
+            CheckIterablesEqualError,
+            match=r"Iterables .* and .* must be equal; items were \(.*, .*, i=.*\) and left was longer",
+        ):
+            check_iterables_equal([1, 2, 3], [9])
+
+    def test_error_differing_items_and_right_longer(self) -> None:
+        with raises(
+            CheckIterablesEqualError,
+            match=r"Iterables .* and .* must be equal; items were \(.*, .*, i=.*\) and right was longer",
+        ):
+            check_iterables_equal([9], [1, 2, 3])
+
+    def test_error_differing_items_and_same_length(self) -> None:
+        with raises(
+            CheckIterablesEqualError,
+            match=r"Iterables .* and .* must be equal; items were \(.*, .*, i=.*\)",
+        ):
+            check_iterables_equal([1, 2, 3], [1, 2, 9])
+
+    def test_error_no_differing_items_just_left_longer(self) -> None:
+        with raises(
+            CheckIterablesEqualError,
+            match=r"Iterables .* and .* must be equal; left was longer",
+        ):
+            check_iterables_equal([1, 2, 3], [1])
+
+    def test_error_no_differing_items_just_right_longer(self) -> None:
+        with raises(
+            CheckIterablesEqualError,
+            match=r"Iterables .* and .* must be equal; right was longer",
+        ):
+            check_iterables_equal([1], [1, 2, 3])
+
+
+class TestCheckLength:
+    def test_equal_pass(self) -> None:
+        check_length([], equal=0)
+
+    def test_equal_fail(self) -> None:
+        with raises(CheckLengthError, match="Object .* must have length .*; got .*"):
+            check_length([], equal=1)
+
+    @mark.parametrize("equal_or_approx", [param(10), param((11, 0.1))])
+    def test_equal_or_approx_pass(
+        self, *, equal_or_approx: int | tuple[int, float]
+    ) -> None:
+        check_length(range(10), equal_or_approx=equal_or_approx)
+
+    @mark.parametrize(
+        ("equal_or_approx", "match"),
+        [
+            param(10, "Object .* must have length .*; got .*"),
+            param(
+                (11, 0.1),
+                r"Object .* must have approximate length .* \(error .*\); got .*",
+            ),
+        ],
+    )
+    def test_equal_or_approx_fail(
+        self, *, equal_or_approx: int | tuple[int, float], match: str
+    ) -> None:
+        with raises(CheckLengthError, match=match):
+            check_length([], equal_or_approx=equal_or_approx)
+
+    def test_min_pass(self) -> None:
+        check_length([], min=0)
+
+    def test_min_error(self) -> None:
+        with raises(
+            CheckLengthError, match="Object .* must have minimum length .*; got .*"
+        ):
+            check_length([], min=1)
+
+    def test_max_pass(self) -> None:
+        check_length([], max=0)
+
+    def test_max_error(self) -> None:
+        with raises(
+            CheckLengthError, match="Object .* must have maximum length .*; got .*"
+        ):
+            check_length([1], max=0)
+
+
+class TestCheckLengthsEqual:
+    def test_pass(self) -> None:
+        check_lengths_equal([], [])
+
+    def test_error(self) -> None:
+        with raises(
+            CheckLengthsEqualError,
+            match="Sized objects .* and .* must have the same length; got .* and .*",
+        ):
+            check_lengths_equal([], [1, 2, 3])
+
+
+class TestCheckMappingsEqual:
+    def test_pass(self) -> None:
+        check_mappings_equal({}, {})
+
+    def test_error_extra_and_missing_and_differing_items(self) -> None:
+        with raises(
+            CheckMappingsEqualError,
+            match=r"Mappings .* and .* must be equal; left had extra keys .*, right had extra keys .* and items were \(.*, .*, k=.*\)",
+        ):
+            check_mappings_equal({"a": 1, "b": 2, "c": 3}, {"b": 2, "c": 9, "d": 4})
+
+    def test_error_extra_and_missing(self) -> None:
+        with raises(
+            CheckMappingsEqualError,
+            match="Mappings .* and .* must be equal; left had extra keys .* and right had extra keys .*",
+        ):
+            check_mappings_equal({"a": 1, "b": 2, "c": 3}, {"b": 2, "c": 3, "d": 4})
+
+    def test_error_extra_and_differing_items(self) -> None:
+        with raises(
+            CheckMappingsEqualError,
+            match=r"Mappings .* and .* must be equal; left had extra keys .* and items were \(.*, .*, k=.*\)",
+        ):
+            check_mappings_equal({"a": 1, "b": 2, "c": 3}, {"a": 9})
+
+    def test_error_missing_and_differing_items(self) -> None:
+        with raises(
+            CheckMappingsEqualError,
+            match=r"Mappings .* and .* must be equal; right had extra keys .* and items were \(.*, .*, k=.*\)",
+        ):
+            check_mappings_equal({"a": 1}, {"a": 9, "b": 2, "c": 3})
+
+    def test_error_extra_only(self) -> None:
+        with raises(
+            CheckMappingsEqualError,
+            match="Mappings .* and .* must be equal; left had extra keys .*",
+        ):
+            check_mappings_equal({"a": 1, "b": 2, "c": 3}, {"a": 1})
+
+    def test_error_missing_only(self) -> None:
+        with raises(
+            CheckMappingsEqualError,
+            match="Mappings .* and .* must be equal; right had extra keys .*",
+        ):
+            check_mappings_equal({"a": 1}, {"a": 1, "b": 2, "c": 3})
+
+    def test_error_differing_items_only(self) -> None:
+        with raises(
+            CheckMappingsEqualError,
+            match=r"Mappings .* and .* must be equal; items were \(.*, .*, k=.*\)",
+        ):
+            check_mappings_equal({"a": 1, "b": 2, "c": 3}, {"a": 1, "b": 2, "c": 9})
+
+
+class TestCheckSetsEqual:
+    def test_pass(self) -> None:
+        check_sets_equal(set(), set())
+
+    def test_error_extra_and_missing(self) -> None:
+        with raises(
+            CheckSetsEqualError,
+            match="Sets .* and .* must be equal; left had extra items .* and right had extra items .*",
+        ):
+            check_sets_equal({1, 2, 3}, {2, 3, 4})
+
+    def test_error_extra(self) -> None:
+        with raises(
+            CheckSetsEqualError,
+            match="Sets .* and .* must be equal; left had extra items .*",
+        ):
+            check_sets_equal({1, 2, 3}, set())
+
+    def test_error_missing(self) -> None:
+        with raises(
+            CheckSetsEqualError,
+            match="Sets .* and .* must be equal; right had extra items .*",
+        ):
+            check_sets_equal(set(), {1, 2, 3})
+
+
+class TestCheckSubset:
+    def test_pass(self) -> None:
+        check_subset(set(), set())
+
+    def test_error(self) -> None:
+        with raises(
+            CheckSubsetError,
+            match="Set .* must be a subset of .*; left had extra items .*",
+        ):
+            check_subset({1, 2, 3}, {1})
+
+
+class TestCheckSuperset:
+    def test_pass(self) -> None:
+        check_superset(set(), set())
+
+    def test_error(self) -> None:
+        with raises(
+            CheckSupersetError,
+            match="Set .* must be a superset of .*; right had extra items .*",
+        ):
+            check_superset({1}, {1, 2, 3})
+
+
+class TestEnsureHashables:
+    def test_main(self) -> None:
+        assert ensure_hashables(1, 2, a=3, b=4) == ([1, 2], {"a": 3, "b": 4})
+
+
+class TestIsIterableNotStr:
+    @mark.parametrize(
+        ("obj", "expected"),
+        [param(None, False), param([], True), param((), True), param("", False)],
+    )
+    def test_main(self, *, obj: Any, expected: bool) -> None:
+        assert is_iterable_not_str(obj) is expected
