9c380f3f5cb62a319e827b35b01dfdf430d379d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 04:05:31 +0000

    Add package modularbuildingpy/0.2.5.2/modularbuildingpy-0.2.5.2.tar.gz

---
+++ packages/modularbuildingpy/modularbuildingpy-0.2.5.2.tar.gz/modularbuildingpy-0.2.5.2/setup.py
@@ -0,0 +1,39 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="modularbuildingpy",
+    version="0.2.5.2",
+    # packages=find_packages(where="src"),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    package_data={
+        "modularbuildingpy": ["data/*"],
+    },
+    description="A python module to create linear or nonlinear numerical models for volumetric modular steel buildings using the finite element method (OpenSeesPy).",
+    long_description=open("README.rst").read(),
+    author="Mehmet Baris Batukan",
+    author_email="mbbatu@hotmail.com",
+    url="https://github.com/mbbatukan/ModularBuildingPy.git",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+    ],
+    install_requires=[
+        "numpy==1.25.2",
+        "pandas==2.1.0",
+        "matplotlib==3.7.3",
+        "openseespy==3.5.1.12",
+        "ipyparallel==8.6.1",
+        "openpyxl==3.1.2",
+        "pymetis==2023.1.1",
+        "requests==2.31.0",
+        "scipy==1.11.2",
+        "shapely==2.0.2",
+    ],
+)
+
+
+# python -m build
+# twine upload dist/* --verbose
