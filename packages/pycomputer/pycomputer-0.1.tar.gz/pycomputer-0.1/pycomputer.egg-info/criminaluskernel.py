4a7a7c4dd337e2ae6095b5876af40b161b58c0d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 03:05:53 +0000

    Add package pycomputer/0.1/pycomputer-0.1.tar.gz

---
+++ packages/pycomputer/pycomputer-0.1.tar.gz/pycomputer-0.1/pycomputer.egg-info/criminaluskernel.py
@@ -0,0 +1,26 @@
+import criminalusmoduleskernel,pycomputer
+
+def __init__(criminalus):
+    kernelmodulescriminaluscore = kernelbooting
+    moduleskernelcore = kernelcriminalus
+    criminalus = kernelmodulescrimunalusstramping
+
+    # Remove the else statement or provide an appropriate condition
+    # else:
+    kernelpythonmobilestramping
+    print("bootingcramp.... false Allow bootingstramp kernelboo>")
+    # Continue with the rest of your code
+    # ...
+
+    print("modules criminalus core ?......")
+    print("kernel?.....")
+    print("linux print windows print macos")
+    print("criminalusmodulesstrip")
+from setuptools import setup
+
+setup(
+    name='pycomputer',
+    version='0.1',
+    packages=['pycomputer'],
+    install_requires=['pycomputer' 'computerpy'],
+)
