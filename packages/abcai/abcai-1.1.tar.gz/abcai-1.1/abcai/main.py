4d4b5ab203582b343772d8146f6705c1b66c4cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 14:04:57 +0000

    Add package abcai/1.1/abcai-1.1.tar.gz

---
+++ packages/abcai/abcai-1.1.tar.gz/abcai-1.1/abcai/main.py
@@ -0,0 +1,92 @@
+import os
+
+prg = "Programs"
+
+files = os.listdir(prg)
+
+
+def list():
+    for num, i in enumerate(files):
+        print(num + 1, i)
+
+
+def run():
+    list()
+
+    ch = int(input("enter the choice"))
+
+    if ch == 1:
+        with open("./Programs/BFS(breath).py", "r") as info:
+            con = info.read()
+
+            print(con)
+            print("Done 🔥")
+
+    if ch == 2:
+        with open("./Programs/A_star.py", "r") as info:
+            con = info.read()
+            print(con)
+            print("Done 🔥")
+
+    if ch == 3:
+        with open("./Programs/DFS(depth).py", "r") as info:
+            con = info.read()
+            print(con)
+            print("Done 🔥")
+
+    if ch == 4:
+        with open("./Programs/alpha_beta_pruning.py", "r") as info:
+            con = info.read()
+            print(con)
+            print("Done 🔥")
+
+    if ch == 5:
+        with open("./Programs/cryto_arthrmatic.py", "r") as info:
+            con = info.read()
+            print(con)
+            print("Done 🔥")
+
+    if ch == 6:
+        with open("./Programs/expert_system.py", "r") as info:
+            con = info.read()
+            print(con)
+            print("Done 🔥")
+
+    if ch == 7:
+        with open("./Programs/min_max.py", "r") as info:
+            con = info.read()
+            print(con)
+            print("Done 🔥")
+
+    if ch == 8:
+        with open("./Programs/Naive_Bayes.py", "r") as info:
+            con = info.read()
+            print(con)
+            print("Done 🔥")
+
+    if ch == 9:
+        with open("./Programs/NLP_token.py", "r") as info:
+            con = info.read()
+            print(con)
+            print("Done 🔥")
+
+    if ch == 10:
+        with open("./Programs/predicate-logic.py", "r") as info:
+            con = info.read()
+            print(con)
+            print("Done 🔥")
+
+    if ch == 11:
+        with open("./Programs/sematic_net.py", "r") as info:
+            con = info.read()
+            print(con)
+            print("Done 🔥")
+
+    if ch == 12:
+        with open("./Programs/spell_check.py", "r") as info:
+            con = info.read()
+            print(con)
+            print("Done 🔥")
+
+    print("Done 🔥")
+
