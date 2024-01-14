5360921f0c19d36e409491f09f1772692eb1aa16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 03:05:43 +0000

    Add package MetroValencia/1.2.1/MetroValencia-1.2.1.tar.gz

---
+++ packages/MetroValencia/MetroValencia-1.2.1.tar.gz/MetroValencia-1.2.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='MetroValencia',
+    version='1.2.1',
+    scripts=['./metrovalencia/metrovalencia_cli'],
+    author='NikoConn',
+    description='Api for MetroValencia.',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
+    packages=['metrovalencia'],
+    install_requires=[
+        'datetime',
+        'requests',
+        'bs4',
+        'pytz'
+    ],
+    python_requires='>=3.7'
+)
