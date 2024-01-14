059a8dd3e2d8a5ddbf4a3a0da7f8106c01433dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 03:05:30 +0000

    Add package df-prep/0.1.5/df_prep-0.1.5.tar.gz

---
+++ packages/df-prep/df_prep-0.1.5.tar.gz/df_prep-0.1.5/connectors/__init__.py
@@ -0,0 +1,1 @@
+from .input_connector import InputConnector
---
+++ packages/df-prep/df_prep-0.1.5.tar.gz/df_prep-0.1.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='df_prep',  
+    version='0.1.5',
+    author='DF',
+    author_email='your_email@example.com',
+    packages=find_packages(),  
+    install_requires=[],  
+)
