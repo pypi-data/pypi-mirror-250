6112a1f9d6571095e1d7eb0bc0d2a0a0c377460c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 04:05:20 +0000

    Add package openai-utilities/0.2.1/openai-utilities-0.2.1.tar.gz

---
+++ packages/openai-utilities/openai-utilities-0.2.1.tar.gz/openai-utilities-0.2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='openai-utilities',
+    version='0.2.1',
+    description='Wrapper on top of openai python SDK primarily to interface with openai and anyscale public endpoints.',
+    url='https://github.com/soumitsalman/openai-utilities',
+    author='Soumit Salman Rahman',
+    author_email='soumitsr@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'tiktoken',
+        'icecream'
+    ],
+    zip_safe=False
+)
