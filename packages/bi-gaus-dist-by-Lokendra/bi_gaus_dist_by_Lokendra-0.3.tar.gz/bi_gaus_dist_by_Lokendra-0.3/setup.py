fa3920815bc5e71ae9a55a1153832042867297d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-14 03:05:57 +0000

    Add package bi-gaus-dist-by-Lokendra/0.3/bi_gaus_dist_by_Lokendra-0.3.tar.gz

---
+++ packages/bi-gaus-dist-by-Lokendra/bi_gaus_dist_by_Lokendra-0.3.tar.gz/bi_gaus_dist_by_Lokendra-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bi_gaus_dist_by_Lokendra',
+      version='0.3',
+      description='Gaussian distribution and Binomial distribution',
+      packages=['bi_gaus_dist_by_Lokendra'],
+      author='LokendraBhat',
+      zip_safe=False)
